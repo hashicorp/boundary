466756e646302d606cb439f07aaa2f5398b68ac4
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-03-06 18:46:55 -0500

    scanner for timestamps

---
+++ internal/oplog/store/scanners.go
@@ -0,0 +1,30 @@
+package store
+
+import (
+	"database/sql/driver"
+	fmt "fmt"
+	"time"
+
+	"github.com/gogo/protobuf/types"
+	// "github.com/golang/protobuf/ptypes"
+)
+
+func (ts *Timestamp) Scan(value interface{}) error {
+	switch t := value.(type) {
+	case time.Time:
+		var err error
+		ts.Timestamp, err = types.TimestampProto(t) // gogo version
+		// ts.Timestamp, err = ptypes.TimestampProto(t)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("Not a protobuf Timestamp")
+	}
+	return nil
+}
+
+func (ts Timestamp) Value() (driver.Value, error) {
+	return types.TimestampFromProto(ts.Timestamp) // gogo version
+	// return ptypes.Timestamp(ts.Timestamp)
+}
