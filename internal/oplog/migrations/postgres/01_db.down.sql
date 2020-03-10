aa51f524d20aa14331f405b4c0db34742839e71b
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-03-10 15:26:10 -0400

    add all the other tables/indexes that must be dropped

--- internal/oplog/migrations/postgres/01_db.down.sql
+++ internal/oplog/migrations/postgres/01_db.down.sql
@@ -1,3 +1,6 @@
 drop table if exists oplog_entries;
 drop table if exists oplog_entries;
 drop table if exists oplog_metadata;
+drop table if exists oplog_entries;
+drop table if exists oplog_tickets;
+drop index if exists idx_entry_tickets_name;
