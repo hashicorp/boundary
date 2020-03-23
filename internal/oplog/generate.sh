bd46c54bcdaf89cbd1c125a2cadd50d70d25fe82
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-03-23 17:51:29 -0400

    need the gorm tags with the test bits too

--- internal/oplog/generate.sh
+++ internal/oplog/generate.sh
@@ -8,4 +8,8 @@ protoc  --go_out=paths=source_relative:. ./store/oplog.proto
 # now, inject custom gorm tags using: 
 protoc-go-inject-tag -input=store/oplog.pb.go
 protoc  --go_out=paths=source_relative:. ./any/any.proto
+
+
 protoc  --go_out=paths=source_relative:. ./oplog_test/oplog_test.proto
+# now, inject custom gorm tags using: 
+protoc-go-inject-tag -input=store/oplog.pb.go
