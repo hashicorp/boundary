2cd8a9060692065fe7b92d0c6e6e8994824de125
Jim <jlambert@hashicorp.com>
2020-03-25 15:15:44 -0400

    fix table name.
    Co-Authored-By: Michael Gaffney <mgaffney@users.noreply.github.com>

--- internal/oplog/migrations/postgres/01_db.down.sql
+++ internal/oplog/migrations/postgres/01_db.down.sql
@@ -1,4 +1,4 @@
 drop table if exists oplog_entry;
 drop table if exists oplog_metadata;
-drop table if exists oplog_tickets;
+drop table if exists oplog_ticket;
 drop index if exists idx_entry_ticket_name;
