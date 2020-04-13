afa06528f556b0547e6c2eeca7d55d835761dd6c
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-04-13 16:25:41 -0400

    iam package docs

---
+++ internal/iam/docs.go
@@ -0,0 +1,4 @@
+// iam package is for identity access management of watchtower.  It includes typical
+// iam resources like Organizations, Projects, Users, Groups, etc.
+// It also includes iam interfaces for: Principals, Resources, Readers, Writers, etc
+package iam
