97717d6f0e227f9ef7b331b5804927498effb218
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-04-13 16:42:44 -0400

    options for iam

---
+++ internal/iam/options.go
@@ -0,0 +1,51 @@
+package iam
+
+// GetOpts - iterate the inbound Options and return a struct
+func GetOpts(opt ...Option) Options {
+	opts := getDefaultOptions()
+	for _, o := range opt {
+		o(opts)
+	}
+	return opts
+}
+
+// Option - how Options are passed as arguments
+type Option func(Options)
+
+// Options = how options are represented
+type Options map[string]interface{}
+
+func getDefaultOptions() Options {
+	return Options{
+		optionWithFriendlyName: "",
+		optionAsRootUser:       false,
+		optionWithOwnerId:      uint32(0),
+	}
+}
+
+const optionWithOwnerId = "optionWithOwnerId"
+
+// WithOwnerId provides an optional owner id
+func WithOwnerId(id uint32) Option {
+	return func(o Options) {
+		o[optionWithOwnerId] = id
+	}
+}
+
+const optionWithFriendlyName = "optionWithFriendlyName"
+
+// ByFriendlyName provides an option to search by a friendly name
+func ByFriendlyName(name string) Option {
+	return func(o Options) {
+		o[optionWithFriendlyName] = name
+	}
+}
+
+const optionAsRootUser = "optionAsRootUser"
+
+// AsRootUser provides an option to specify this is a root user
+func AsRootUser(b bool) Option {
+	return func(o Options) {
+		o[optionAsRootUser] = b
+	}
+}
