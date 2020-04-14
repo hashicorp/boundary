d8f761ff72f418a6af208e878e26cb295b306b2d
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-04-14 14:43:44 -0400

    refactor bits to internal/db

--- internal/iam/resource.go
+++ internal/iam/resource.go
@@ -3,6 +3,7 @@ package iam
 import (
 	"context"
 
+	"github.com/hashicorp/watchtower/internal/db"
 	"github.com/hashicorp/watchtower/internal/iam/store"
 )
 
@@ -16,16 +17,16 @@ type Resource interface {
 	GetFriendlyName() string
 
 	// GetPrimaryScope is the Scope that owns the Resource
-	GetPrimaryScope(ctx context.Context, r Reader) (*Scope, error)
+	GetPrimaryScope(ctx context.Context, r db.Reader) (*Scope, error)
 
 	// GetAssignableScopes specifies the scopes that this resource is available in.
 	// Public Ids are the keys to the map of Scopes returned
-	GetAssignableScopes(ctx context.Context, r Reader) (map[string]*AssignableScope, error)
+	GetAssignableScopes(ctx context.Context, r db.Reader) (map[string]*AssignableScope, error)
 
 	// GetOwner is the owner of the resource, that has
 	// full access to the resource including the right to delegate access
 	// to others
-	GetOwner(ctx context.Context, r Reader) (*User, error)
+	GetOwner(ctx context.Context, r db.Reader) (*User, error)
 
 	// Type of Resource (Target, Policy, User, Group, etc)
 	ResourceType() ResourceType
