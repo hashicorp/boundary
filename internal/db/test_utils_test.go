1a9e546c02cc18bc93e85b2c4a8b5d5a99d0e760
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-04-15 17:58:39 -0400

    fix run so it does not block forever

--- internal/db/test_utils_test.go
+++ internal/db/test_utils_test.go
@@ -3,18 +3,21 @@ package db
 import (
 	"testing"
 
+	"github.com/hashicorp/watchtower/internal/db/db_test"
 	"gotest.tools/assert"
 )
 
-func Test_All(t *testing.T) {
-	t.Run("valid", func(t *testing.T) {
-		StartTest()
-		t.Parallel()
-		cleanup, url := SetupTest(t, "migrations/postgres")
-		defer cleanup()
-		defer CompleteTest() // must come after the "defer cleanup()"
-		db, err := TestConnection(url)
-		assert.NilError(t, err)
-		defer db.Close()
+func Test_Utils(t *testing.T) {
+	StartTest()
+	t.Parallel()
+	cleanup, url := SetupTest(t, "migrations/postgres")
+	defer cleanup()
+	defer CompleteTest() // must come after the "defer cleanup()"
+	conn, err := TestConnection(url)
+	assert.NilError(t, err)
+	defer conn.Close()
+	db_test.Init(conn)
+	t.Run("nothing", func(t *testing.T) {
+
 	})
 }
