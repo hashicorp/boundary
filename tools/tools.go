7834b23f485f1a044d9cbe200bef545e075968be
Todd Knight <T.Alan.Knight@gmail.com>
2020-04-14 17:05:10 -0700

    Add comment explaining google.golang.org/protobuf vs github.com/golang/protobuf

--- tools/tools.go
+++ tools/tools.go
@@ -17,5 +17,8 @@ import (
 	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	// use this instead of google.golang.org/protobuf/cmd/protoc-gen-go since this supports grpc plugin while the other does not.
+	// see https://github.com/golang/protobuf/releases#v1.4-generated-code and
+	// https://github.com/protocolbuffers/protobuf-go/releases/tag/v1.20.0#v1.20-grpc-support
 	_ "github.com/golang/protobuf/protoc-gen-go"
 )
