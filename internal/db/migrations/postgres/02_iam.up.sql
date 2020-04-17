cae717e64494128f61cef5f084012fb08d3d5164
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-04-17 14:29:50 -0400

    define enums and auth method

--- internal/db/migrations/postgres/02_iam.up.sql
+++ internal/db/migrations/postgres/02_iam.up.sql
@@ -28,45 +28,121 @@ CREATE TABLE if not exists iam_scope (
   owner_id bigint,
   disabled BOOLEAN NOT NULL default FALSE
 );
-CREATE TABLE if not exists iam_user (
-  id bigint generated always as identity primary key,
-  create_time timestamp with time zone NOT NULL default current_timestamp,
-  update_time timestamp with time zone NOT NULL default current_timestamp,
-  public_id text not null UNIQUE,
-  friendly_name text UNIQUE,
-  name text NOT NULL,
-  primary_scope_id bigint NOT NULL REFERENCES iam_scope(id),
-  owner_id bigint REFERENCES iam_user(id),
-  disabled BOOLEAN NOT NULL default FALSE
-);
-CREATE TABLE if not exists iam_action (
+--
+-- define the iam_auth_method_type_enm lookup table
+--
+CREATE TABLE if not exists iam_scope_type_enm (
   id smallint NOT NULL primary key,
   string text NOT NULL UNIQUE
 );
-INSERT INTO iam_action (id, string)
+INSERT INTO iam_scope_type_enm (id, string)
 values
   (0, 'unknown');
-INSERT INTO iam_action (id, string)
+INSERT INTO iam_scope_type_enm (id, string)
+values
+  (1, 'organization');
+INSERT INTO iam_scope_type_enm (id, string)
+values
+  (2, 'project');
+ALTER TABLE iam_scope_type_enm
+ADD
+  CONSTRAINT iam_scope_type_enm_between_chk CHECK (
+    id BETWEEN 0
+    AND 2
+  );
+ALTER TABLE iam_scope
+ADD
+  FOREIGN KEY (type) REFERENCES iam_scope_type_enm(id);
+CREATE TABLE if not exists iam_user (
+    id bigint generated always as identity primary key,
+    create_time timestamp with time zone NOT NULL default current_timestamp,
+    update_time timestamp with time zone NOT NULL default current_timestamp,
+    public_id text not null UNIQUE,
+    friendly_name text UNIQUE,
+    name text NOT NULL,
+    primary_scope_id bigint NOT NULL REFERENCES iam_scope(id),
+    owner_id bigint REFERENCES iam_user(id),
+    disabled BOOLEAN NOT NULL default FALSE
+  );
+CREATE TABLE if not exists iam_auth_method (
+    id bigint generated always as identity primary key,
+    create_time timestamp with time zone NOT NULL default current_timestamp,
+    update_time timestamp with time zone NOT NULL default current_timestamp,
+    public_id text not null UNIQUE,
+    friendly_name text UNIQUE,
+    primary_scope_id bigint NOT NULL REFERENCES iam_scope(id),
+    owner_id bigint REFERENCES iam_user(id),
+    disabled BOOLEAN NOT NULL default FALSE,
+    type smallint NOT NULL
+  );
+CREATE TABLE if not exists iam_user_alias (
+    id bigint generated always as identity primary key,
+    create_time timestamp with time zone NOT NULL default current_timestamp,
+    update_time timestamp with time zone NOT NULL default current_timestamp,
+    public_id text not null UNIQUE,
+    friendly_name text UNIQUE,
+    name text NOT NULL,
+    primary_scope_id bigint NOT NULL REFERENCES iam_scope(id),
+    owner_id bigint NOT NULL REFERENCES iam_user(id),
+    disabled BOOLEAN NOT NULL default FALSE,
+    auth_method_id bigint NOT NULL REFERENCES iam_auth_method(id)
+  );
+--
+  -- define the iam_auth_method_type_enm lookup table
+  --
+  CREATE TABLE if not exists iam_auth_method_type_enm (
+    id smallint NOT NULL primary key,
+    string text NOT NULL UNIQUE
+  );
+INSERT INTO iam_auth_method_type_enm (id, string)
+values
+  (0, 'unknown');
+INSERT INTO iam_auth_method_type_enm (id, string)
+values
+  (1, 'userpass');
+INSERT INTO iam_auth_method_type_enm (id, string)
+values
+  (2, 'oidc');
+ALTER TABLE iam_auth_method_type_enm
+ADD
+  CONSTRAINT iam_auth_method_type_enm_between_chk CHECK (
+    id BETWEEN 0
+    AND 2
+  );
+ALTER TABLE iam_auth_method
+ADD
+  FOREIGN KEY (type) REFERENCES iam_auth_method_type_enm(id);
+--
+  -- define the iam_action_emn lookup table
+  --
+  CREATE TABLE if not exists iam_action_enm (
+    id smallint NOT NULL primary key,
+    string text NOT NULL UNIQUE
+  );
+INSERT INTO iam_action_enm (id, string)
+values
+  (0, 'unknown');
+INSERT INTO iam_action_enm (id, string)
 values
   (1, 'list');
-INSERT INTO iam_action (id, string)
+INSERT INTO iam_action_enm (id, string)
 values
   (2, 'create');
-INSERT INTO iam_action (id, string)
+INSERT INTO iam_action_enm (id, string)
 values
   (3, 'update');
-INSERT INTO iam_action (id, string)
+INSERT INTO iam_action_enm (id, string)
 values
   (4, 'edit');
-INSERT INTO iam_action (id, string)
+INSERT INTO iam_action_enm (id, string)
 values
   (5, 'delete');
-INSERT INTO iam_action (id, string)
+INSERT INTO iam_action_enm (id, string)
 values
   (6, 'authen');
-ALTER TABLE iam_action
+ALTER TABLE iam_action_enm
 ADD
-  CONSTRAINT iam_action_id_between_chk CHECK (
+  CONSTRAINT iam_action_enm_between_chk CHECK (
     id BETWEEN 0
     AND 6
   );
