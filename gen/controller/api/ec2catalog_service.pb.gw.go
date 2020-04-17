ab2dad7598fb1b88faedac4d32f9f3520945f866
Todd Knight <T.Alan.Knight@gmail.com>
2020-04-17 14:39:24 -0700

    Make all API string fields wrapped StringValues and added storage ec2catalog.

--- gen/controller/api/ec2catalog_service.pb.gw.go
+++ gen/controller/api/ec2catalog_service.pb.gw.go
@@ -126,7 +126,7 @@ func request_ControllerService_CreateEc2Catalog_0(ctx context.Context, marshaler
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Catalog); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -143,7 +143,7 @@ func local_request_ControllerService_CreateEc2Catalog_0(ctx context.Context, mar
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Catalog); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -246,6 +246,76 @@ func local_request_ControllerService_UpdateEc2Catalog_0(ctx context.Context, mar
 
 	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_ControllerService_UpdateEc2Catalog_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.UpdateEc2Catalog(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+func request_ControllerService_UpdateEc2Catalog_1(ctx context.Context, marshaler runtime.Marshaler, client ControllerServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpdateEc2CatalogRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["catalog.id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "catalog.id")
+	}
+
+	err = runtime.PopulateFieldFromPath(&protoReq, "catalog.id", val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "catalog.id", err)
+	}
+
+	msg, err := client.UpdateEc2Catalog(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_ControllerService_UpdateEc2Catalog_1(ctx context.Context, marshaler runtime.Marshaler, server ControllerServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpdateEc2CatalogRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["catalog.id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "catalog.id")
+	}
+
+	err = runtime.PopulateFieldFromPath(&protoReq, "catalog.id", val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "catalog.id", err)
 	}
 
 	msg, err := server.UpdateEc2Catalog(ctx, &protoReq)
@@ -462,6 +532,26 @@ func RegisterControllerServiceHandlerServer(ctx context.Context, mux *runtime.Se
 
 	})
 
+	mux.Handle("PATCH", pattern_ControllerService_UpdateEc2Catalog_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_ControllerService_UpdateEc2Catalog_1(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ControllerService_UpdateEc2Catalog_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("DELETE", pattern_ControllerService_DeleteEc2Catalog_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -623,6 +713,26 @@ func RegisterControllerServiceHandlerClient(ctx context.Context, mux *runtime.Se
 
 	})
 
+	mux.Handle("PATCH", pattern_ControllerService_UpdateEc2Catalog_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_ControllerService_UpdateEc2Catalog_1(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ControllerService_UpdateEc2Catalog_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("DELETE", pattern_ControllerService_DeleteEc2Catalog_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -693,6 +803,8 @@ var (
 
 	pattern_ControllerService_UpdateEc2Catalog_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "ec2_catalogs", "catalog.id"}, "", runtime.AssumeColonVerbOpt(true)))
 
+	pattern_ControllerService_UpdateEc2Catalog_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "ec2_catalogs", "catalog.id"}, "", runtime.AssumeColonVerbOpt(true)))
+
 	pattern_ControllerService_DeleteEc2Catalog_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "ec2_catalogs", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_ControllerService_RotateEc2CatalogAccessKey_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "ec2_catalogs", "id"}, "rotate", runtime.AssumeColonVerbOpt(true)))
@@ -706,6 +818,8 @@ var (
 	forward_ControllerService_CreateEc2Catalog_0 = runtime.ForwardResponseMessage
 
 	forward_ControllerService_UpdateEc2Catalog_0 = runtime.ForwardResponseMessage
+
+	forward_ControllerService_UpdateEc2Catalog_1 = runtime.ForwardResponseMessage
 
 	forward_ControllerService_DeleteEc2Catalog_0 = runtime.ForwardResponseMessage
 
