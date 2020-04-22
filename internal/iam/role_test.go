284366fdbe1095f07ecb9776f19e3153e2f63485
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-04-22 16:40:15 -0400

    better unit tests

--- internal/iam/role_test.go
+++ internal/iam/role_test.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/watchtower/internal/db"
@@ -35,5 +36,130 @@ func Test_NewRole(t *testing.T) {
 		err = w.Create(context.Background(), role)
 		assert.NilError(t, err)
 		assert.Check(t, role.Id != 0)
+	})
+	t.Run("nil-scope", func(t *testing.T) {
+		role, err := NewRole(nil)
+		assert.Check(t, err != nil)
+		assert.Check(t, role == nil)
+		assert.Equal(t, err.Error(), "error the role primary scope is nil")
+	})
+}
+
+func TestRole_Actions(t *testing.T) {
+	r := &Role{}
+	a := r.Actions()
+	assert.Equal(t, a[ActionList.String()], ActionList)
+	assert.Equal(t, a[ActionCreate.String()], ActionCreate)
+	assert.Equal(t, a[ActionUpdate.String()], ActionUpdate)
+	assert.Equal(t, a[ActionEdit.String()], ActionEdit)
+	assert.Equal(t, a[ActionDelete.String()], ActionDelete)
+}
+
+func TestRole_ResourceType(t *testing.T) {
+	r := &Role{}
+	ty := r.ResourceType()
+	assert.Equal(t, ty, ResourceTypeRole)
+}
+
+func TestRole_GetPrimaryScope(t *testing.T) {
+	db.StartTest()
+	t.Parallel()
+	cleanup, url := db.SetupTest(t, "../db/migrations/postgres")
+	defer cleanup()
+	defer db.CompleteTest() // must come after the "defer cleanup()"
+	conn, err := db.TestConnection(url)
+	assert.NilError(t, err)
+	defer conn.Close()
+
+	t.Run("valid", func(t *testing.T) {
+		w := db.GormReadWriter{Tx: conn}
+		s, err := NewScope(OrganizationScope)
+		assert.NilError(t, err)
+		assert.Check(t, s.Scope != nil)
+		err = w.Create(context.Background(), s)
+		assert.NilError(t, err)
+		assert.Check(t, s.Id != 0)
+
+		role, err := NewRole(s, WithDescription("this is a test role"))
+		assert.NilError(t, err)
+		assert.Check(t, role != nil)
+		assert.Equal(t, role.Description, "this is a test role")
+		assert.Equal(t, s.Id, role.PrimaryScopeId)
+		err = w.Create(context.Background(), role)
+		assert.NilError(t, err)
+		assert.Check(t, role.Id != 0)
+
+		primaryScope, err := role.GetPrimaryScope(context.Background(), &w)
+		assert.NilError(t, err)
+		assert.Check(t, primaryScope != nil)
+	})
+}
+
+func TestRole_AssignedRoles(t *testing.T) {
+	db.StartTest()
+	t.Parallel()
+	cleanup, url := db.SetupTest(t, "../db/migrations/postgres")
+	defer cleanup()
+	defer db.CompleteTest() // must come after the "defer cleanup()"
+	conn, err := db.TestConnection(url)
+	assert.NilError(t, err)
+	defer conn.Close()
+
+	t.Run("valid", func(t *testing.T) {
+		w := db.GormReadWriter{Tx: conn}
+		s, err := NewScope(OrganizationScope)
+		assert.NilError(t, err)
+		assert.Check(t, s.Scope != nil)
+		err = w.Create(context.Background(), s)
+		assert.NilError(t, err)
+		assert.Check(t, s.Id != 0)
+
+		user, err := NewUser(s)
+		assert.NilError(t, err)
+		err = w.Create(context.Background(), user)
+		assert.NilError(t, err)
+
+		role, err := NewRole(s, WithDescription("this is a test role"))
+		assert.NilError(t, err)
+		assert.Check(t, role != nil)
+		assert.Equal(t, role.Description, "this is a test role")
+		assert.Equal(t, s.Id, role.PrimaryScopeId)
+		err = w.Create(context.Background(), role)
+		assert.NilError(t, err)
+		assert.Check(t, role.Id != 0)
+
+		uRole, err := NewAssignedRole(s, role, user)
+		assert.NilError(t, err)
+		assert.Check(t, uRole != nil)
+		assert.Equal(t, uRole.GetRoleId(), role.Id)
+		assert.Equal(t, uRole.GetPrincipalId(), user.Id)
+		err = w.Create(context.Background(), uRole)
+		assert.NilError(t, err)
+		assert.Check(t, uRole != nil)
+		assert.Equal(t, uRole.GetPrincipalId(), user.Id)
+
+		grp, err := NewGroup(s, WithDescription("this is a test group"))
+		assert.NilError(t, err)
+		assert.Check(t, grp != nil)
+		assert.Equal(t, grp.Description, "this is a test group")
+		assert.Equal(t, s.Id, grp.PrimaryScopeId)
+		err = w.Create(context.Background(), grp)
+		assert.NilError(t, err)
+		assert.Check(t, grp.Id != 0)
+
+		gRole, err := NewAssignedRole(s, role, grp)
+		assert.NilError(t, err)
+		assert.Check(t, gRole != nil)
+		assert.Equal(t, gRole.GetRoleId(), role.Id)
+		assert.Equal(t, gRole.GetPrincipalId(), grp.Id)
+		err = w.Create(context.Background(), gRole)
+		assert.NilError(t, err)
+		assert.Check(t, gRole != nil)
+		assert.Equal(t, gRole.GetPrincipalId(), grp.Id)
+
+		allRoles, err := role.AssignedRoles(context.Background(), &w)
+		assert.NilError(t, err)
+		assert.Equal(t, len(allRoles), 2)
+		fmt.Println(allRoles)
 	})
 }
