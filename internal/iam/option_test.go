f29444ff8eae291c0316e1937a722fec1789ab5a
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-04-22 15:36:08 -0400

    unit test for WithScope

--- internal/iam/option_test.go
+++ internal/iam/option_test.go
@@ -16,4 +16,20 @@ func Test_GetOpts(t *testing.T) {
 		testOpts[optionWithFriendlyName] = "test"
 		assert.Check(t, reflect.DeepEqual(opts, testOpts))
 	})
+	t.Run("WithScope", func(t *testing.T) {
+		s, err := NewScope(OrganizationScope)
+		assert.NilError(t, err)
+		assert.Check(t, s.Scope != nil)
+
+		opts := GetOpts(WithScope(s))
+		testOpts := getDefaultOptions()
+		testOpts[optionWithScope] = s
+		assert.Check(t, reflect.DeepEqual(opts, testOpts))
+	})
+	t.Run("optionWithDescription", func(t *testing.T) {
+		opts := GetOpts(WithDescription("test desc"))
+		testOpts := getDefaultOptions()
+		testOpts[optionWithDescription] = "test desc"
+		assert.Check(t, reflect.DeepEqual(opts, testOpts))
+	})
 }
