7b8b42cd42d34e05e901d13d4e8f5bfba58d70ce
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-04-27 09:36:56 -0400

    insert tickets

--- internal/db/migrations/postgres/01_oplog.up.sql
+++ internal/db/migrations/postgres/01_oplog.up.sql
@@ -22,3 +22,33 @@ CREATE TABLE if not exists oplog_metadata (
 );
 create index if not exists idx_oplog_metatadata_key on oplog_metadata(key);
 create index if not exists idx_oplog_metatadata_value on oplog_metadata(value);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('default', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_scope', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_user', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_auth_method', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_group', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_group_member_user', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_role', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_role_grant', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_role_group', 1);
+INSERT INTO oplog_ticket (name, version)
+values
+  ('iam_role_user', 1);
