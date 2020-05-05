725a777baf856ded3ca34de04b95be68bc71fbf3
Jim <jlambert@hashicorp.com>
2020-05-05 16:35:24 -0400

    refactor friendly name to just be name (#29)

--- internal/db/migrations/postgres/03_db.up.sql
+++ internal/db/migrations/postgres/03_db.up.sql
@@ -6,8 +6,7 @@ CREATE TABLE if not exists db_test_user (
   create_time timestamp with time zone default current_timestamp,
   update_time timestamp with time zone default current_timestamp,
   public_id text NOT NULL UNIQUE,
-  friendly_name text UNIQUE,
-  name text,
+  name text UNIQUE,
   phone_number text,
   email text
 );
@@ -16,7 +15,7 @@ CREATE TABLE if not exists db_test_car (
   create_time timestamp with time zone default current_timestamp,
   update_time timestamp with time zone default current_timestamp,
   public_id text NOT NULL UNIQUE,
-  friendly_name text UNIQUE,
+  name text UNIQUE,
   model text,
   mpg smallint
 );
@@ -25,7 +24,7 @@ CREATE TABLE if not exists db_test_rental (
   create_time timestamp with time zone default current_timestamp,
   update_time timestamp with time zone default current_timestamp,
   public_id text NOT NULL UNIQUE,
-  friendly_name text UNIQUE,
+  name text UNIQUE,
   user_id bigint not null REFERENCES db_test_user(id),
   car_id bigint not null REFERENCES db_test_car(id)
 );
