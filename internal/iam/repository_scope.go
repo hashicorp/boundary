67eacdf5a2e2adc2a1d14a9c60ed1d97cfb796c2
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-07 13:47:44 -0400

    fix ctx propagation and Scope returns

--- internal/iam/repository_scope.go
+++ internal/iam/repository_scope.go
@@ -5,40 +5,48 @@ import (
 	"errors"
 )
 
+// CreateScope will create a scope in the repository and return the written scope
 func (r *Repository) CreateScope(ctx context.Context, scope *Scope, opt ...Option) (*Scope, error) {
 	if scope == nil {
 		return nil, errors.New("error scope is nil for create")
 	}
-	resource, err := r.create(context.Background(), scope)
+	resource, err := r.create(ctx, scope)
 	return resource.(*Scope), err
 }
+
+// UpdateScope will update a scope in the repository and return the written scope
 func (r *Repository) UpdateScope(ctx context.Context, scope *Scope, fieldMaskPaths []string, opt ...Option) (*Scope, error) {
 	if scope == nil {
 		return nil, errors.New("error scope is nil for update")
 	}
-	resource, err := r.update(context.Background(), scope, fieldMaskPaths)
+	resource, err := r.update(ctx, scope, fieldMaskPaths)
 	return resource.(*Scope), err
 }
-func (r *Repository) LookupScope(ctx context.Context, opt ...Option) (Scope, error) {
+
+// LookupScope will look up a scope in the respository
+func (r *Repository) LookupScope(ctx context.Context, opt ...Option) (*Scope, error) {
 	opts := getOpts(opt...)
 	withPublicId := opts.withPublicId
 	withName := opts.withName
 
-	scope := allocScope()
-
 	if withPublicId != "" {
+		scope := allocScope()
 		scope.PublicId = withPublicId
 		if err := r.reader.LookupByPublicId(ctx, &scope); err != nil {
-			return allocScope(), err
+			s := allocScope()
+			return &s, err
 		}
-		return scope, nil
+		return &scope, nil
 	}
 	if withName != "" {
+		scope := allocScope()
 		scope.Name = withName
 		if err := r.reader.LookupByName(ctx, &scope); err != nil {
-			return allocScope(), err
+			s := allocScope()
+			return &s, err
 		}
-		return scope, nil
+		return &scope, nil
 	}
-	return allocScope(), errors.New("you must look up scopes by id or name")
+	s := allocScope()
+	return &s, errors.New("you must look up scopes by id or name")
 }
