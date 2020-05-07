897777ba269f03969ed560564119cf10fa82fdab
Michael Gaffney <mike@gaffney.cc>
2020-05-07 14:10:14 -0400

    migrations: add description column and fix foreign keys

--- internal/db/migrations/postgres/10_static_host.up.sql
+++ internal/db/migrations/postgres/10_static_host.up.sql
@@ -1,9 +1,12 @@
 begin;
 
+  -- TODO: Add domain for timestamp
+
   create table static_host_catalog (
     public_id wt_public_id primary key,
     scope_id wt_public_id not null, -- TODO: add fk
     name text,
+    description text,
     create_time timestamp with time zone default current_timestamp,
     update_time timestamp with time zone default current_timestamp,
     unique(scope_id, name)
@@ -12,10 +15,11 @@ begin;
   create table static_host (
     public_id wt_public_id primary key,
     static_host_catalog_id wt_public_id not null
-      references static_host_catalog
+      references static_host_catalog (public_id)
       on delete cascade
       on update cascade,
     name text,
+    description text,
     address text not null, -- TODO: add check constraint
     create_time timestamp with time zone default current_timestamp,
     update_time timestamp with time zone default current_timestamp,
@@ -25,19 +29,27 @@ begin;
   create table static_host_set (
     public_id wt_public_id primary key,
     static_host_catalog_id wt_public_id not null
-      references static_host_catalog
+      references static_host_catalog (public_id)
       on delete cascade
       on update cascade,
     name text,
+    description text,
     create_time timestamp with time zone default current_timestamp,
     update_time timestamp with time zone default current_timestamp,
     unique(static_host_catalog_id, name)
   );
 
   create table static_host_set_member (
-    static_host_set_id wt_public_id,
-    static_host_id wt_public_id,
+    static_host_set_id wt_public_id
+      references static_host_set (public_id)
+      on delete cascade
+      on update cascade,
+    static_host_id wt_public_id
+      references static_host (public_id)
+      on delete cascade
+      on update cascade,
     primary key(static_host_set_id, static_host_id)
   );
 
 commit;
+
