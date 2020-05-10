3910eaa416ea55485960d80ff4602167dd89dd4e
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-10 17:38:06 -0400

    clean up repo user functions and start adding tests

--- internal/iam/repository_user.go
+++ internal/iam/repository_user.go
@@ -3,37 +3,50 @@ package iam
 import (
 	"context"
 	"errors"
+	"fmt"
 )
 
 func (r *Repository) CreateUser(ctx context.Context, user *User, opt ...Option) (*User, error) {
+	if user == nil {
+		return nil, errors.New("error user is nil for create")
+	}
 	resource, err := r.create(context.Background(), user)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
 	return resource.(*User), err
 }
 func (r *Repository) UpdateUser(ctx context.Context, user *User, fieldMaskPaths []string, opt ...Option) (*User, error) {
+	if user == nil {
+		return nil, errors.New("error user is nil for update")
+	}
 	resource, err := r.update(context.Background(), user, fieldMaskPaths)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update user: %w", err)
+	}
 	return resource.(*User), err
 }
 
-func (r *Repository) LookupUser(ctx context.Context, opt ...Option) (User, error) {
+func (r *Repository) LookupUser(ctx context.Context, opt ...Option) (*User, error) {
 	opts := getOpts(opt...)
 	withPublicId := opts.withPublicId
 	withName := opts.withName
 
-	user := allocUser()
-
 	if withPublicId != "" {
+		user := allocUser()
 		user.PublicId = withPublicId
 		if err := r.reader.LookupByPublicId(ctx, &user); err != nil {
-			return allocUser(), err
+			return nil, err
 		}
-		return user, nil
+		return &user, nil
 	}
 	if withName != "" {
+		user := allocUser()
 		user.Name = withName
 		if err := r.reader.LookupByName(ctx, &user); err != nil {
-			return allocUser(), err
+			return nil, err
 		}
-		return user, nil
+		return &user, nil
 	}
-	return allocUser(), errors.New("you must loop up users by id or friendly name")
+	return nil, errors.New("you must loop up users by id or friendly name")
 }
--- internal/iam/repository_user_test.go
+++ internal/iam/repository_user_test.go
@@ -1,1 +1,84 @@
 package iam
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/watchtower/internal/db"
+	"github.com/hashicorp/watchtower/internal/oplog"
+	"github.com/hashicorp/watchtower/internal/oplog/store"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Repository_CreateUser(t *testing.T) {
+	t.Parallel()
+	cleanup, conn := db.TestSetup(t, "../db/migrations/postgres")
+	defer cleanup()
+	assert := assert.New(t)
+	defer conn.Close()
+
+	t.Run("valid", func(t *testing.T) {
+		rw := &db.GormReadWriter{Tx: conn}
+		wrapper := db.TestWrapper(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+		id, err := uuid.GenerateUUID()
+		assert.Nil(err)
+
+		s, err := NewOrganization()
+		s, err = repo.CreateScope(context.Background(), s)
+		assert.Nil(err)
+		assert.True(s != nil)
+
+		u, err := NewUser(s.PublicId, WithName("fn-"+id))
+		assert.Nil(err)
+		assert.Equal(s.GetPublicId(), u.ScopeId)
+		assert.Equal(u.GetName(), "fn-"+id)
+
+		u, err = repo.CreateUser(context.Background(), u)
+		assert.Nil(err)
+		assert.True(u.CreateTime != nil)
+		assert.True(u.UpdateTime != nil)
+
+		foundUser, err := repo.LookupUser(context.Background(), WithPublicId(u.PublicId))
+		assert.Nil(err)
+		assert.Equal(foundUser.GetPublicId(), u.GetPublicId())
+		assert.Equal(foundUser.GetScopeId(), u.GetScopeId())
+		assert.Equal(foundUser.GetName(), "fn-"+id)
+
+		var metadata store.Metadata
+		err = conn.Where("key = ? and value = ?", "resource-public-id", u.PublicId).First(&metadata).Error
+		assert.Nil(err)
+
+		var foundEntry oplog.Entry
+		err = conn.Where("id = ?", metadata.EntryId).First(&foundEntry).Error
+		assert.Nil(err)
+	})
+	t.Run("bad-scope-id", func(t *testing.T) {
+		rw := &db.GormReadWriter{Tx: conn}
+		wrapper := db.TestWrapper(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+		badScopeId, err := uuid.GenerateUUID()
+		assert.Nil(err)
+		u, err := NewUser(badScopeId)
+		assert.Nil(err)
+		assert.Equal(badScopeId, u.ScopeId)
+
+		pubId := u.PublicId
+		u, err = repo.CreateUser(context.Background(), u)
+		assert.True(err != nil)
+		assert.True(u == nil)
+		// not convinced this is what we want for an error msg,
+		// but it does show the chain of errors
+		assert.Equal("failed to create user: error getting metadata for create: unable to get scope for standard metadata: error getting scope for LookupScope: record not found", err.Error())
+
+		foundUser, err := repo.LookupUser(context.Background(), WithPublicId(pubId))
+		assert.True(err != nil)
+		assert.True(foundUser == nil)
+
+		var metadata store.Metadata
+		err = conn.Where("key = ? and value = ?", "resource-public-id", pubId).First(&metadata).Error
+		assert.True(err != nil)
+
+	})
+}
