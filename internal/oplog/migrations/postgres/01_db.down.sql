0676ea51f1dfc3b68bf874f1c549e842bc45bfa6
Jim <jlambert@hashicorp.com>
2020-05-11 10:30:44 -0400

    refactor oplog to use db/internal migrations, remove InitTicket() for more reliability and simplify test step up and tear down (#37)

--- internal/oplog/migrations/postgres/01_db.down.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists oplog_entry;
-drop table if exists oplog_metadata;
-drop table if exists oplog_ticket;
-drop index if exists idx_oplog_metatadata_key;
-drop index if exists idx_oplog_metatadata_value
