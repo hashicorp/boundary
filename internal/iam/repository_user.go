ba0f0d8fff20effbe6c747b35958aa2102fa4fcd
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-12 15:57:52 -0400

    support deleting a user from the repo

--- internal/iam/repository_user.go
+++ internal/iam/repository_user.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+
+	"github.com/hashicorp/watchtower/internal/db"
 )
 
+// CreateUser will create a user in the repository and return the written user
 func (r *Repository) CreateUser(ctx context.Context, user *User, opt ...Option) (*User, error) {
 	if user == nil {
 		return nil, errors.New("error user is nil for create")
@@ -16,6 +19,8 @@ func (r *Repository) CreateUser(ctx context.Context, user *User, opt ...Option) 
 	}
 	return resource.(*User), err
 }
+
+// UpdateUser will update a user in the repository and return the written user
 func (r *Repository) UpdateUser(ctx context.Context, user *User, fieldMaskPaths []string, opt ...Option) (*User, error) {
 	if user == nil {
 		return nil, errors.New("error user is nil for update")
@@ -27,6 +32,8 @@ func (r *Repository) UpdateUser(ctx context.Context, user *User, fieldMaskPaths 
 	return resource.(*User), err
 }
 
+// LookupUser will look up a user in the repository.  If the user is not
+// found, it will return nil, nil.
 func (r *Repository) LookupUser(ctx context.Context, opt ...Option) (*User, error) {
 	opts := getOpts(opt...)
 	withPublicId := opts.withPublicId
@@ -49,4 +56,38 @@ func (r *Repository) LookupUser(ctx context.Context, opt ...Option) (*User, erro
 		return &user, nil
 	}
 	return nil, errors.New("you must loop up users by id or friendly name")
+}
+
+// DeleteUser will delete a user from the repository
+func (r *Repository) DeleteUser(ctx context.Context, opt ...Option) error {
+	opts := getOpts(opt...)
+	withPublicId := opts.withPublicId
+	withName := opts.withName
+
+	if withPublicId != "" {
+		user := allocUser()
+		user.PublicId = withPublicId
+		if err := r.writer.Delete(ctx, &user); err != nil {
+			return fmt.Errorf("unable to delete user by public id: %w", err)
+		}
+		return nil
+	}
+	if withName != "" {
+		user := allocUser()
+		user.Name = withName
+		if err := r.reader.LookupByName(ctx, &user); err != nil {
+			if err == db.ErrRecordNotFound {
+				return nil
+			}
+			return fmt.Errorf("unable to find user by name for delete: %w", err)
+		}
+		if user.PublicId == "" {
+			return fmt.Errorf("unable to delete user with unset public id")
+		}
+		if err := r.writer.Delete(ctx, &user); err != nil {
+			return fmt.Errorf("unable to delete user by name: %w", err)
+		}
+		return nil
+	}
+	return errors.New("you must delete users by id or name")
 }
