93c02edfa337158d81212de3facd776635e59746
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-13 16:50:34 -0400

    a delete to handle oplog stuff as well

--- internal/iam/repository_test.go
+++ internal/iam/repository_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"reflect"
 	"testing"
+	"time"
 
 	wrapping "github.com/hashicorp/go-kms-wrapping"
 	"github.com/hashicorp/go-uuid"
@@ -191,5 +192,42 @@ func Test_dbRepository_update(t *testing.T) {
 		assert.True(resource == nil)
 		assert.Equal(0, updatedRows)
 		assert.Equal(err.Error(), "error updating resource that is nil")
+	})
+}
+
+func Test_dbRepository_delete(t *testing.T) {
+	// t.Parallel()
+	cleanup, conn, _ := db.TestSetup(t, "postgres")
+	defer cleanup()
+	assert := assert.New(t)
+	defer conn.Close()
+
+	t.Run("valid-org", func(t *testing.T) {
+		rw := db.New(conn)
+		wrapper := db.TestWrapper(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+
+		s, err := NewOrganization()
+		retScope, err := repo.create(context.Background(), s)
+		assert.Nil(err)
+		assert.True(retScope != nil)
+		assert.True(retScope.GetPublicId() != "")
+		assert.Equal(retScope.GetName(), "")
+
+		rowsDeleted, err := repo.delete(context.Background(), s)
+		assert.Nil(err)
+		assert.Equal(1, rowsDeleted)
+
+		err = db.TestVerifyOplog(rw, s.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_DELETE), db.WithCreateNbf(5*time.Second))
+		assert.Nil(err)
+	})
+	t.Run("nil-resource", func(t *testing.T) {
+		rw := db.New(conn)
+		wrapper := db.TestWrapper(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+		deletedRows, err := repo.delete(context.Background(), nil, nil)
+		assert.True(err != nil)
+		assert.Equal(0, deletedRows)
+		assert.Equal(err.Error(), "error deleting resource that is nil")
 	})
 }
