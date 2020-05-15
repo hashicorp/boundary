b7ae0f0a96f5ee4c6a47cb73043d59e97980e81c
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-15 16:15:22 -0400

    refactor tests

--- internal/iam/repository_user_test.go
+++ internal/iam/repository_user_test.go
@@ -3,69 +3,213 @@ package iam
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/watchtower/internal/db"
+	"github.com/hashicorp/watchtower/internal/oplog"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/proto"
 )
 
-func Test_Repository_CreateUser(t *testing.T) {
+func TestRepository_CreateUser(t *testing.T) {
 	t.Parallel()
 	cleanup, conn, _ := db.TestSetup(t, "postgres")
 	defer cleanup()
-	assert := assert.New(t)
+	a := assert.New(t)
 	defer conn.Close()
+
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+	repo, err := NewRepository(rw, rw, wrapper)
+	id, err := uuid.GenerateUUID()
+	a.NoError(err)
 
 	org, _ := TestScopes(t, conn)
 
-	t.Run("valid", func(t *testing.T) {
-		rw := db.New(conn)
-		wrapper := db.TestWrapper(t)
-		repo, err := NewRepository(rw, rw, wrapper)
-		id, err := uuid.GenerateUUID()
-		assert.NoError(err)
+	type args struct {
+		orgId string
+		opt   []Option
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantDup    bool
+		wantErr    bool
+		wantErrMsg string
+	}{
+		{
+			name: "valid",
+			args: args{
+				orgId: org.PublicId,
+				opt:   []Option{WithName("valid" + id), WithDescription(id)},
+			},
+			wantErr: false,
+		},
+		{
+			name: "bad-scope-id",
+			args: args{
+				orgId: id,
+			},
+			wantErr:    false,
+			wantErrMsg: "",
+		},
+		{
+			name: "dup-name",
+			args: args{
+				orgId: id,
+				opt:   []Option{WithName("dup-name" + id)},
+			},
+			wantDup:    true,
+			wantErr:    true,
+			wantErrMsg: `failed to create user: error creating: pq: duplicate key value violates unique constraint "iam_user_name_scope_id_key"`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
 
-		u, err := NewUser(org.PublicId, WithName("fn-"+id))
-		assert.NoError(err)
-		assert.Equal(org.GetPublicId(), u.ScopeId)
-		assert.Equal(u.GetName(), "fn-"+id)
+			if tt.wantDup {
+				dup, err := NewUser(org.PublicId, tt.args.opt...)
+				assert.NoError(err)
+				dup, err = repo.CreateUser(context.Background(), dup, tt.args.opt...)
+				assert.NoError(err)
+				assert.NotNil(dup)
+			}
+			u, err := NewUser(org.PublicId, tt.args.opt...)
+			pubId := u.PublicId
+			assert.NoError(err)
+			u, err = repo.CreateUser(context.Background(), u, tt.args.opt...)
+			if tt.wantErr {
+				assert.Error(err)
+				assert.Nil(u)
+				assert.Equal(tt.wantErrMsg, err.Error())
+				err = db.TestVerifyOplog(rw, pubId, db.WithOperation(oplog.OpType_OP_TYPE_CREATE), db.WithCreateNotBefore(10*time.Second))
+				assert.Error(err)
+				assert.Equal("record not found", err.Error())
+				return
+			}
+			assert.NoError(err)
+			assert.NotNil(u.CreateTime)
+			assert.NotNil(u.UpdateTime)
 
-		u, err = repo.CreateUser(context.Background(), u)
-		assert.NoError(err)
-		assert.NotNil(u.CreateTime)
-		assert.NotNil(u.UpdateTime)
+			foundUser, err := repo.LookupUser(context.Background(), u.PublicId)
+			assert.NoError(err)
+			assert.True(proto.Equal(foundUser, u))
 
-		foundUser, err := repo.LookupUser(context.Background(), u.PublicId)
-		assert.NoError(err)
-		assert.True(proto.Equal(foundUser, u))
+			err = db.TestVerifyOplog(rw, u.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_CREATE), db.WithCreateNotBefore(10*time.Second))
+			assert.NoError(err)
+		})
+	}
+}
 
-		err = db.TestVerifyOplog(rw, u.PublicId)
-		assert.NoError(err)
-	})
-	t.Run("bad-scope-id", func(t *testing.T) {
-		rw := db.New(conn)
-		wrapper := db.TestWrapper(t)
-		repo, err := NewRepository(rw, rw, wrapper)
-		badScopeId, err := uuid.GenerateUUID()
-		assert.NoError(err)
-		u, err := NewUser(badScopeId)
-		assert.NoError(err)
-		assert.Equal(badScopeId, u.ScopeId)
+func TestRepository_UpdateUser(t *testing.T) {
+	t.Parallel()
+	cleanup, conn, _ := db.TestSetup(t, "postgres")
+	defer cleanup()
+	a := assert.New(t)
+	defer conn.Close()
 
-		pubId := u.PublicId
-		u, err = repo.CreateUser(context.Background(), u)
-		assert.Error(err)
-		assert.Nil(u)
-		// not convinced this is what we want for an error msg,
-		// but it does show the chain of errors
-		assert.Equal("failed to create user: error getting metadata for create: unable to get scope for standard metadata: error getting scope for LookupScope: record not found", err.Error())
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+	repo, err := NewRepository(rw, rw, wrapper)
+	id, err := uuid.GenerateUUID()
+	a.NoError(err)
 
-		foundUser, err := repo.LookupUser(context.Background(), pubId)
-		assert.NotNil(err)
-		assert.Nil(foundUser)
+	org, proj := TestScopes(t, conn)
 
-		err = db.TestVerifyOplog(rw, pubId)
-		assert.Error(err)
-	})
+	type args struct {
+		name           string
+		description    string
+		fieldMaskPaths []string
+		opt            []Option
+		ScopeId        string
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantRowsUpdate int
+		wantErr        bool
+		wantErrMsg     string
+		wantDup        bool
+	}{
+		{
+			name: "valid",
+			args: args{
+				name:           "valid" + id,
+				fieldMaskPaths: []string{"Name"},
+				ScopeId:        org.PublicId,
+			},
+		},
+		{
+			name: "proj-scope-id",
+			args: args{
+				name:           "proj-scope-id" + id,
+				fieldMaskPaths: []string{"Name"},
+				ScopeId:        proj.PublicId,
+			},
+			wantErr:    true,
+			wantErrMsg: "failed to update user: error on update scope is not an organization",
+		},
+		{
+			name: "empty-scope-id",
+			args: args{
+				name:           "empty-scope-id" + id,
+				fieldMaskPaths: []string{"Name"},
+				ScopeId:        "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "dup-name",
+			args: args{
+				name:           "dup-name" + id,
+				fieldMaskPaths: []string{"Name"},
+				ScopeId:        org.PublicId,
+			},
+			wantErr:    true,
+			wantDup:    true,
+			wantErrMsg: `failed to update user: error updating: pq: duplicate key value violates unique constraint "iam_user_name_scope_id_key"`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			if tt.wantDup {
+				u := TestUser(t, conn, org.PublicId)
+				u.Name = tt.args.name
+				_, _, err := repo.UpdateUser(context.Background(), u, tt.args.fieldMaskPaths, tt.args.opt...)
+				assert.NoError(err)
+			}
+
+			u := TestUser(t, conn, org.PublicId)
+
+			updateUser := allocUser()
+			updateUser.PublicId = u.PublicId
+			updateUser.ScopeId = tt.args.ScopeId
+			updateUser.Name = tt.args.name
+			updateUser.Description = tt.args.description
+
+			userAfterUpdate, updatedRows, err := repo.UpdateUser(context.Background(), &updateUser, tt.args.fieldMaskPaths, tt.args.opt...)
+			if tt.wantErr {
+				assert.Error(err)
+				assert.Nil(userAfterUpdate)
+				assert.Equal(0, updatedRows)
+				assert.Equal(tt.wantErrMsg, err.Error())
+				err = db.TestVerifyOplog(rw, u.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_UPDATE), db.WithCreateNotBefore(10*time.Second))
+				assert.Error(err)
+				assert.Equal("record not found", err.Error())
+				return
+			}
+			assert.NoError(err)
+			assert.Equal(1, updatedRows)
+			assert.NotEqual(u.UpdateTime, userAfterUpdate.UpdateTime)
+			foundUser, err := repo.LookupUser(context.Background(), u.PublicId)
+			assert.NoError(err)
+			assert.True(proto.Equal(userAfterUpdate, foundUser))
+
+			err = db.TestVerifyOplog(rw, u.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_UPDATE), db.WithCreateNotBefore(10*time.Second))
+			assert.NoError(err)
+		})
+	}
 }
