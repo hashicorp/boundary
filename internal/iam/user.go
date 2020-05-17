e99b6db56f6dfb522d919f6e88a275d517af058a
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-17 09:29:32 -0400

    refactor create vs update checks

--- internal/iam/user.go
+++ internal/iam/user.go
@@ -61,18 +61,19 @@ func (u *User) Clone() interface{} {
 // VetForWrite implements db.VetForWrite() interface
 func (u *User) VetForWrite(ctx context.Context, r db.Reader, opType db.OpType, opt ...db.Option) error {
 	opts := db.GetOpts(opt...)
-
 	if u.PublicId == "" {
 		return errors.New("error public id is empty string for user write")
 	}
-	if u.ScopeId == "" {
-		return errors.New("error scope id not set for user write")
-	}
 
-	// make sure the scope is valid for users
-	err := u.scopeIsValid(ctx, r)
-	if err != nil {
-		return err
+	if opType == db.CreateOp {
+		if u.ScopeId == "" {
+			return errors.New("error scope id not set for user write")
+		}
+		// make sure the scope is valid for users
+		err := u.scopeIsValid(ctx, r)
+		if err != nil {
+			return err
+		}
 	}
 	if opType == db.UpdateOp && u.ScopeId != "" {
 		switch len(opts.WithFieldMaskPaths) {
