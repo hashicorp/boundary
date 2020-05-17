edd9e3921c6cd195ddeedc8cf504d27c0d7bdfae
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-17 16:40:17 -0400

    carve out user schema PR changes

--- internal/db/migrations/postgres/04_iam.down.sql
+++ internal/db/migrations/postgres/04_iam.down.sql
@@ -3,19 +3,6 @@ BEGIN;
 drop table if exists iam_scope CASCADE;
 drop trigger if exists iam_scope_insert;
 drop function if exists iam_sub_scopes_func;
-drop table if exists iam_auth_method cascade;
-drop table if exists  iam_role cascade;
-drop table if exists iam_group_member_type_enm cascade;
-drop table if exists iam_group cascade cascade;
-drop table if exists iam_group_member_user cascade;
-drop view if exists iam_group_member;
-drop table if exists iam_auth_method_type_enm cascade;
-drop table if exists iam_action_enm cascade;
-drop table if exists iam_role_type_enm cascade;
-drop table if exists iam_role_user cascade;
-drop table if exists iam_role_group cascade;
-drop table if exists iam_role_grant cascade;
-drop view if exists iam_assigned_role;
-
+drop table if exists iam_user cascade;
 
 COMMIT;
