34cfb6e1e7c986413e88a1e48e23b7348e0bcae6
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-18 09:59:10 -0400

    carve out group member bits for future PR

--- internal/iam/group.go
+++ internal/iam/group.go
@@ -59,43 +59,6 @@ func allocGroup() Group {
 	}
 }
 
-// Members returns the members of the group (Users)
-func (g *Group) Members(ctx context.Context, r db.Reader) ([]GroupMember, error) {
-	const where = "group_id = ? and type = ?"
-	viewMembers := []*groupMemberView{}
-	if err := r.SearchWhere(ctx, &viewMembers, where, g.PublicId, UserMemberType.String()); err != nil {
-		return nil, err
-	}
-
-	members := []GroupMember{}
-	for _, m := range viewMembers {
-		switch m.Type {
-		case UserMemberType.String():
-			gm := &GroupMemberUser{
-				GroupMemberUser: &store.GroupMemberUser{
-					CreateTime: m.CreateTime,
-					GroupId:    m.GroupId,
-					MemberId:   m.MemberId,
-				},
-			}
-			members = append(members, gm)
-		default:
-			return nil, fmt.Errorf("error unsupported member type: %s", m.Type)
-		}
-
-	}
-	return members, nil
-}
-
-// AddMember will create a new in memory GroupMember
-func (g *Group) AddMember(ctx context.Context, r db.Reader, m Resource, opt ...db.Option) (GroupMember, error) {
-	gm, err := NewGroupMember(g, m)
-	if err != nil {
-		return nil, err
-	}
-	return gm, nil
-}
-
 // VetForWrite implements db.VetForWrite() interface and validates the group
 // before it's written
 func (g *Group) VetForWrite(ctx context.Context, r db.Reader, opType db.OpType, opt ...db.Option) error {
