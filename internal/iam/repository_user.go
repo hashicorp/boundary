429c6bd47a727024b2f9680df8b27a80381137c0
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-21 08:25:26 -0400

    update fieldMaskPaths handling and refactor err msgs

--- internal/iam/repository_user.go
+++ internal/iam/repository_user.go
@@ -2,7 +2,6 @@ package iam
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/hashicorp/watchtower/internal/db"
@@ -11,11 +10,14 @@ import (
 // CreateUser will create a user in the repository and return the written user
 func (r *Repository) CreateUser(ctx context.Context, user *User, opt ...Option) (*User, error) {
 	if user == nil {
-		return nil, errors.New("error user is nil for create")
+		return nil, fmt.Errorf("create user: missing user %w", db.ErrNilParameter)
 	}
 	resource, err := r.create(ctx, user)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create user: %w", err)
+		if uniqueError(err) {
+			return nil, fmt.Errorf("create user: user %s already exists in organization %s", user.Name, user.ScopeId)
+		}
+		return nil, fmt.Errorf("create user: %w for %s", err, user.PublicId)
 	}
 	return resource.(*User), err
 }
@@ -23,11 +25,24 @@ func (r *Repository) CreateUser(ctx context.Context, user *User, opt ...Option) 
 // UpdateUser will update a user in the repository and return the written user
 func (r *Repository) UpdateUser(ctx context.Context, user *User, fieldMaskPaths []string, opt ...Option) (*User, int, error) {
 	if user == nil {
-		return nil, db.NoRowsAffected, errors.New("error user is nil for update")
+		return nil, db.NoRowsAffected, fmt.Errorf("update user: missing user %w", db.ErrNilParameter)
+	}
+	if user.PublicId == "" {
+		return nil, db.NoRowsAffected, fmt.Errorf("update user: missing user public id %w", db.ErrNilParameter)
+	}
+	if len(fieldMaskPaths) == 0 {
+		fieldMaskPaths = []string{
+			"Name",
+			"Description",
+			"Disabled",
+		}
 	}
 	resource, rowsUpdated, err := r.update(ctx, user, fieldMaskPaths)
 	if err != nil {
-		return nil, db.NoRowsAffected, fmt.Errorf("failed to update user: %w", err)
+		if uniqueError(err) {
+			return nil, db.NoRowsAffected, fmt.Errorf("update user: user %s already exists in organization %s", user.Name, user.ScopeId)
+		}
+		return nil, db.NoRowsAffected, fmt.Errorf("update user: %w for %s", err, user.PublicId)
 	}
 	return resource.(*User), rowsUpdated, err
 }
@@ -36,13 +51,13 @@ func (r *Repository) UpdateUser(ctx context.Context, user *User, fieldMaskPaths 
 // found, it will return nil, nil.
 func (r *Repository) LookupUser(ctx context.Context, withPublicId string, opt ...Option) (*User, error) {
 	if withPublicId == "" {
-		return nil, errors.New("you cannot lookup a user with an empty public id")
+		return nil, fmt.Errorf("lookup user: missing public id %w", db.ErrNilParameter)
 	}
 
 	user := allocUser()
 	user.PublicId = withPublicId
 	if err := r.reader.LookupByPublicId(ctx, &user); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("lookup user: failed %w for %s", err, withPublicId)
 	}
 	return &user, nil
 }
@@ -50,13 +65,13 @@ func (r *Repository) LookupUser(ctx context.Context, withPublicId string, opt ..
 // DeleteUser will delete a user from the repository
 func (r *Repository) DeleteUser(ctx context.Context, withPublicId string, opt ...Option) (int, error) {
 	if withPublicId == "" {
-		return db.NoRowsAffected, errors.New("you cannot delete a user with an empty public id")
+		return db.NoRowsAffected, fmt.Errorf("delete user: missing public id %w", db.ErrNilParameter)
 	}
 	user := allocUser()
 	user.PublicId = withPublicId
 	rowsDeleted, err := r.writer.Delete(ctx, &user)
 	if err != nil {
-		return db.NoRowsAffected, fmt.Errorf("unable to delete user by public id: %w", err)
+		return db.NoRowsAffected, fmt.Errorf("delete user: failed %w for %s", err, withPublicId)
 	}
 	return rowsDeleted, nil
 }
