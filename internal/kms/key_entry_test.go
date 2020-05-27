6e53846c1429b6e7602fe636bfbb23036125344b
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-27 18:05:10 -0400

    create and delete tests

--- internal/kms/key_entry_test.go
+++ internal/kms/key_entry_test.go
@@ -2,6 +2,7 @@ package kms
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/hashicorp/go-uuid"
@@ -126,6 +127,132 @@ func TestNewKeyEntry(t *testing.T) {
 	}
 }
 
+func Test_KeyEntryCreate(t *testing.T) {
+	t.Parallel()
+	cleanup, conn, _ := db.TestSetup(t, "postgres")
+	defer func() {
+		if err := cleanup(); err != nil {
+			t.Error(err)
+		}
+		if err := conn.Close(); err != nil {
+			t.Error(err)
+		}
+	}()
+	assert := assert.New(t)
+	org := testOrg(t, conn)
+
+	id := testId(t)
+
+	t.Run("valid", func(t *testing.T) {
+		w := db.New(conn)
+		entry, err := NewKeyEntry(org.PublicId, "valid-"+id, []byte(id))
+		assert.NoError(err)
+		err = w.Create(context.Background(), entry)
+		assert.NoError(err)
+
+		found := allocKeyEntry()
+		err = w.LookupWhere(context.Background(), &found, "key_id = ?", entry.KeyId)
+		assert.NoError(err)
+		assert.Equal(entry, &found)
+
+		subKey, err := NewKeyEntry(org.PublicId, "valid-sub-"+id, []byte(id), WithParentKeyId(entry.KeyId))
+		assert.NoError(err)
+		err = w.Create(context.Background(), subKey)
+		assert.NoError(err)
+		found = allocKeyEntry()
+		err = w.LookupWhere(context.Background(), &found, "key_id = ?", subKey.KeyId)
+		assert.NoError(err)
+		assert.Equal(subKey, &found)
+	})
+	t.Run("dup", func(t *testing.T) {
+		w := db.New(conn)
+		entry, err := NewKeyEntry(org.PublicId, "dup-"+id, []byte(id))
+		assert.NoError(err)
+		err = w.Create(context.Background(), entry)
+		assert.NoError(err)
+
+		found := allocKeyEntry()
+		err = w.LookupWhere(context.Background(), &found, "key_id = ?", entry.KeyId)
+		assert.NoError(err)
+		assert.Equal(entry, &found)
+
+		entryWithParent, err := NewKeyEntry(org.PublicId, "dup-"+id, []byte(id))
+		assert.NoError(err)
+		err = w.Create(context.Background(), entryWithParent)
+		assert.Error(err)
+	})
+	t.Run("bad-org", func(t *testing.T) {
+		w := db.New(conn)
+		entry, err := NewKeyEntry(id, "bad-org-"+id, []byte(id))
+		assert.NoError(err)
+		err = w.Create(context.Background(), entry)
+		assert.Error(err)
+
+		found := allocKeyEntry()
+		err = w.LookupWhere(context.Background(), &found, "key_id = ?", entry.KeyId)
+		assert.Error(err)
+	})
+
+}
+
+func Test_KeyEntryDelete(t *testing.T) {
+	t.Parallel()
+	cleanup, conn, _ := db.TestSetup(t, "postgres")
+	defer func() {
+		if err := cleanup(); err != nil {
+			t.Error(err)
+		}
+		if err := conn.Close(); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+	repo, err := NewRepository(rw, rw, wrapper)
+	assert.NoError(t, err)
+	id := testId(t)
+	org := testOrg(t, conn)
+
+	tests := []struct {
+		name            string
+		keyEntry        *KeyEntry
+		wantRowsDeleted int
+		wantErr         bool
+		wantErrMsg      string
+	}{
+		{
+			name:            "valid",
+			keyEntry:        testKeyEntry(t, conn, org.PublicId, "valid-"+id, []byte("valid-"+id)),
+			wantErr:         false,
+			wantRowsDeleted: 1,
+		},
+		{
+			name:            "bad-id",
+			keyEntry:        func() *KeyEntry { e := allocKeyEntry(); e.KeyId = id; return &e }(),
+			wantErr:         false,
+			wantRowsDeleted: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			deleteEntry := allocKeyEntry()
+			deleteEntry.KeyId = tt.keyEntry.KeyId
+			deletedRows, err := rw.Delete(context.Background(), &deleteEntry)
+			if tt.wantRowsDeleted == 0 {
+				assert.Equal(tt.wantRowsDeleted, deletedRows)
+				return
+			}
+			assert.NoError(err)
+			assert.Equal(tt.wantRowsDeleted, deletedRows)
+			found, err := repo.LookupKeyEntry(context.Background(), tt.keyEntry.KeyId)
+			assert.True(errors.Is(err, db.ErrRecordNotFound))
+			assert.Nil(found)
+		})
+	}
+}
+
 func testOrg(t *testing.T, conn *gorm.DB) (org *iam.Scope) {
 	t.Helper()
 	assert := assert.New(t)
@@ -146,4 +273,19 @@ func testId(t *testing.T) string {
 	id, err := uuid.GenerateUUID()
 	assert.NoError(t, err)
 	return id
+}
+func testKeyEntry(t *testing.T, conn *gorm.DB, orgId, keyId string, key []byte) *KeyEntry {
+	assert := assert.New(t)
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+	repo, err := NewRepository(rw, rw, wrapper)
+	assert.NoError(err)
+
+	entry, err := NewKeyEntry(orgId, keyId, key)
+	assert.NoError(err)
+
+	createdEntry, err := repo.CreateKeyEntry(context.Background(), entry)
+	assert.NoError(err)
+	return createdEntry
+
 }
