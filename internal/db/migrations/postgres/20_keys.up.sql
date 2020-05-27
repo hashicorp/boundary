f7631785fbd5f94f1c7ae9ee438b208db44a8756
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-27 18:04:51 -0400

    remove invalid unique constraint on scope_id

--- internal/db/migrations/postgres/20_keys.up.sql
+++ internal/db/migrations/postgres/20_keys.up.sql
@@ -4,7 +4,7 @@ create table kms_key_entry (
     key_id text primary key,
     key bytea not null,
     parent_key_id text references kms_key_entry(key_id) on delete cascade on update cascade,
-    scope_id wt_public_id not null unique references iam_scope_organization(scope_id) on delete cascade on update cascade
+    scope_id wt_public_id not null references iam_scope_organization(scope_id) on delete cascade on update cascade
 );
 
 commit;
