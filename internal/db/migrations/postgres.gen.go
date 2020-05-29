1ef2182a7346051a27c7ec286e220ff34055589e
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-05-29 14:37:40 -0400

    new trigger for scope name constraints

--- internal/db/migrations/postgres.gen.go
+++ internal/db/migrations/postgres.gen.go
@@ -478,6 +478,36 @@ before
 insert on iam_scope
   for each row execute procedure default_create_time();
 
+
+-- iam_sub_names will allow us to enforce the different name constraints for
+-- organizations and projects via a before update trigger on the iam_scope
+-- table. 
+create or replace function 
+  iam_sub_names() 
+  returns trigger
+as $$ 
+begin 
+  if new.name != old.name then
+    if new.type = 'organization' then
+      update iam_scope_organization set name = new.name where scope_id = old.public_id;
+      return new;
+    end if;
+    if new.type = 'project' then
+      update iam_scope_project set name = new.name where scope_id = old.public_id;
+      return new;
+    end if;
+    raise exception 'unknown scope type';
+  end if;
+  return new;
+end;
+$$ language plpgsql;
+
+create trigger 
+  iam_sub_names 
+before 
+update on iam_scope
+  for each row execute procedure iam_sub_names();
+
 commit;
 
 `),
