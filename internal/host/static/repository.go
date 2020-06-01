4f3f9ed222dcb6d8d0effb31455af0447ab09877
Michael Gaffney <mgaffney@users.noreply.github.com>
2020-06-01 09:53:47 -0400

    Check for nil in embedded HostCatalog (#89)

--- internal/host/static/repository.go
+++ internal/host/static/repository.go
@@ -53,6 +53,9 @@ func (r *Repository) CreateCatalog(ctx context.Context, c *HostCatalog, opt ...O
 	if c == nil {
 		return nil, fmt.Errorf("create: static host catalog: %w", db.ErrNilParameter)
 	}
+	if c.HostCatalog == nil {
+		return nil, fmt.Errorf("create: static host catalog: embedded HostCatalog: %w", db.ErrNilParameter)
+	}
 	if c.HostCatalog.ScopeId == "" {
 		return nil, fmt.Errorf("create: static host catalog: no scope id: %w", db.ErrInvalidParameter)
 	}
@@ -108,6 +111,9 @@ func (r *Repository) CreateCatalog(ctx context.Context, c *HostCatalog, opt ...O
 func (r *Repository) UpdateCatalog(ctx context.Context, c *HostCatalog, fieldMask []string, opt ...Option) (*HostCatalog, int, error) {
 	if c == nil {
 		return nil, db.NoRowsAffected, fmt.Errorf("update: static host catalog: %w", db.ErrNilParameter)
+	}
+	if c.HostCatalog == nil {
+		return nil, db.NoRowsAffected, fmt.Errorf("update: static host catalog: embedded HostCatalog: %w", db.ErrNilParameter)
 	}
 	if c.PublicId == "" {
 		return nil, db.NoRowsAffected, fmt.Errorf("update: static host catalog: missing public id: %w", db.ErrInvalidParameter)
