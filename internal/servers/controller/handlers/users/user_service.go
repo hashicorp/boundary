4f173245f618682961b199e0cb5db7ed2f0148de
Todd Knight <T.Alan.Knight@gmail.com>
2020-06-05 08:37:09 -0700

    Create user handler stuff.

---
+++ internal/servers/controller/handlers/users/user_service.go
@@ -0,0 +1,325 @@
+package users
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+
+	pb "github.com/hashicorp/watchtower/internal/gen/controller/api/resources/scopes"
+	pbs "github.com/hashicorp/watchtower/internal/gen/controller/api/services"
+	"github.com/hashicorp/watchtower/internal/iam"
+	"github.com/hashicorp/watchtower/internal/servers/controller/handlers"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+var (
+	reInvalidID = regexp.MustCompile("[^A-Za-z0-9]")
+	// TODO(ICU-28): Find a way to auto update these names and enforce the mappings between wire and storage.
+	wireToStorageMask = map[string]string{
+		"name":        "Name",
+		"description": "Description",
+	}
+)
+
+// Service handles request as described by the pbs.UserServiceServer interface.
+type Service struct {
+	repo func() (*iam.Repository, error)
+}
+
+// NewService returns a user service which handles user related requests to watchtower.
+func NewService(repo func() (*iam.Repository, error)) (Service, error) {
+	if repo == nil {
+		return Service{}, fmt.Errorf("nil iam repostiroy provided")
+	}
+	return Service{repo: repo}, nil
+}
+
+var _ pbs.UserServiceServer = Service{}
+
+// CreateUser is not yet implemented but will implement the interface pbs.UserServiceServer.
+func (s Service) ListUsers(ctx context.Context, req *pbs.ListUsersRequest) (*pbs.ListUsersResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "List not enabled for this resource.")
+}
+
+// GetUsers implements the interface pbs.UserServiceServer.
+func (s Service) GetUser(ctx context.Context, req *pbs.GetUserRequest) (*pbs.GetUserResponse, error) {
+	if err := validateGetRequest(req); err != nil {
+		return nil, err
+	}
+	p, err := s.getFromRepo(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+	return &pbs.GetUserResponse{Item: p}, nil
+}
+
+// CreateUser implements the interface pbs.UserServiceServer.
+func (s Service) CreateUser(ctx context.Context, req *pbs.CreateUserRequest) (*pbs.CreateUserResponse, error) {
+	if err := validateCreateRequest(req); err != nil {
+		return nil, err
+	}
+	p, err := s.createInRepo(ctx, req.GetOrgId(), req.GetItem())
+	if err != nil {
+		return nil, err
+	}
+	return &pbs.CreateUserResponse{Item: p, Uri: fmt.Sprintf("orgs/%s/users/%s", req.GetOrgId(), p.GetId())}, nil
+}
+
+// UpdateUser implements the interface pbs.UserServiceServer.
+func (s Service) UpdateUser(ctx context.Context, req *pbs.UpdateUserRequest) (*pbs.UpdateUserResponse, error) {
+	if err := validateUpdateRequest(req); err != nil {
+		return nil, err
+	}
+	p, err := s.updateInRepo(ctx, req.GetOrgId(), req.GetId(), req.GetUpdateMask().GetPaths(), req.GetItem())
+	if err != nil {
+		return nil, err
+	}
+	return &pbs.UpdateUserResponse{Item: p}, nil
+}
+
+// DeleteUser implements the interface pbs.UserServiceServer.
+func (s Service) DeleteUser(ctx context.Context, req *pbs.DeleteUserRequest) (*pbs.DeleteUserResponse, error) {
+	if err := validateDeleteRequest(req); err != nil {
+		return nil, err
+	}
+	existed, err := s.deleteFromRepo(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+	return &pbs.DeleteUserResponse{Existed: existed}, nil
+}
+
+func (s Service) getFromRepo(ctx context.Context, id string) (*pb.User, error) {
+	repo, err := s.repo()
+	if err != nil {
+		return nil, err
+	}
+	u, err := repo.LookupUser(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	if u == nil {
+		return nil, handlers.NotFoundErrorf("User %q doesn't exist.", id)
+	}
+	return toProto(u), nil
+}
+
+func (s Service) createInRepo(ctx context.Context, orgID string, item *pb.User) (*pb.User, error) {
+	var opts []iam.Option
+	if item.GetName() != nil {
+		opts = append(opts, iam.WithName(item.GetName().GetValue()))
+	}
+	if item.GetDescription() != nil {
+		opts = append(opts, iam.WithDescription(item.GetDescription().GetValue()))
+	}
+	u, err := iam.NewUser(orgID, opts...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Unable to build user for creation: %v.", err)
+	}
+	repo, err := s.repo()
+	if err != nil {
+		return nil, err
+	}
+	out, err := repo.CreateUser(ctx, u)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Unable to create user: %v.", err)
+	}
+	if out == nil {
+		return nil, status.Error(codes.Internal, "Unable to create user but no error returned from repository.")
+	}
+	return toProto(out), nil
+}
+
+func (s Service) updateInRepo(ctx context.Context, orgID, projId string, mask []string, item *pb.User) (*pb.User, error) {
+	var opts []iam.Option
+	if desc := item.GetDescription(); desc != nil {
+		opts = append(opts, iam.WithDescription(desc.GetValue()))
+	}
+	if name := item.GetName(); name != nil {
+		opts = append(opts, iam.WithName(name.GetValue()))
+	}
+	u, err := iam.NewUser(orgID, opts...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Unable to build user for update: %v.", err)
+	}
+	u.PublicId = projId
+	dbMask, err := toDbUpdateMask(mask)
+	if err != nil {
+		return nil, err
+	}
+	if len(dbMask) == 0 {
+		return nil, handlers.InvalidArgumentErrorf("No valid fields included in the update mask.", []string{"update_mask"})
+	}
+	repo, err := s.repo()
+	if err != nil {
+		return nil, err
+	}
+	out, rowsUpdated, err := repo.UpdateUser(ctx, u, dbMask)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Unable to update user: %v.", err)
+	}
+	if rowsUpdated == 0 {
+		return nil, handlers.NotFoundErrorf("User %q doesn't exist.", projId)
+	}
+	return toProto(out), nil
+}
+
+func (s Service) deleteFromRepo(ctx context.Context, projId string) (bool, error) {
+	repo, err := s.repo()
+	if err != nil {
+		return false, err
+	}
+	rows, err := repo.DeleteScope(ctx, projId)
+	if err != nil {
+		return false, status.Errorf(codes.Internal, "Unable to delete user: %v.", err)
+	}
+	return rows > 0, nil
+}
+
+// toDbUpdateMask converts the wire format's FieldMask into a list of strings containing FieldMask paths used
+func toDbUpdateMask(paths []string) ([]string, error) {
+	var dbPaths []string
+	var invalid []string
+	for _, p := range paths {
+		for _, f := range strings.Split(p, ",") {
+			if dbField, ok := wireToStorageMask[strings.TrimSpace(f)]; ok {
+				dbPaths = append(dbPaths, dbField)
+			} else {
+				invalid = append(invalid, f)
+			}
+		}
+	}
+	if len(invalid) > 0 {
+		return nil, handlers.InvalidArgumentErrorf(fmt.Sprintf("Invalid fields passed in update_update mask: %v.", invalid), []string{"update_mask"})
+	}
+	return dbPaths, nil
+}
+
+func toProto(in *iam.User) *pb.User {
+	out := pb.User{
+		Id:          in.GetPublicId(),
+		CreatedTime: in.GetCreateTime().GetTimestamp(),
+		UpdatedTime: in.GetUpdateTime().GetTimestamp(),
+	}
+	if in.GetDescription() != "" {
+		out.Description = &wrapperspb.StringValue{Value: in.GetDescription()}
+	}
+	if in.GetName() != "" {
+		out.Name = &wrapperspb.StringValue{Value: in.GetName()}
+	}
+	return &out
+}
+
+// A validateX method should exist for each method above.  These methods do not make calls to any backing service but enforce
+// requirements on the structure of the request.  They verify that:
+//  * The path passed in is correctly formatted
+//  * All required parameters are set
+//  * There are no conflicting parameters provided
+// TODO: Populate the error in a way to allow it to be converted to the previously described error format and include all invalid fields instead of just the most recent.
+func validateGetRequest(req *pbs.GetUserRequest) error {
+	if err := validateAncestors(req); err != nil {
+		return err
+	}
+	if !validId(req.GetId(), iam.UserPrefix+"_") {
+		return handlers.InvalidArgumentErrorf("Improperly formatted identifier.", []string{"id"})
+	}
+	return nil
+}
+
+func validateCreateRequest(req *pbs.CreateUserRequest) error {
+	if err := validateAncestors(req); err != nil {
+		return err
+	}
+	item := req.GetItem()
+	if item == nil {
+		return handlers.InvalidArgumentErrorf("A user's fields must be set to something.", []string{"item"})
+	}
+	var immutableFieldsSet []string
+	if item.GetId() != "" {
+		immutableFieldsSet = append(immutableFieldsSet, "id")
+	}
+	if item.GetCreatedTime() != nil {
+		immutableFieldsSet = append(immutableFieldsSet, "created_time")
+	}
+	if item.GetUpdatedTime() != nil {
+		immutableFieldsSet = append(immutableFieldsSet, "updated_time")
+	}
+	if len(immutableFieldsSet) > 0 {
+		return handlers.InvalidArgumentErrorf("Cannot specify read only fields at creation time.", immutableFieldsSet)
+	}
+	return nil
+}
+
+func validateUpdateRequest(req *pbs.UpdateUserRequest) error {
+	if err := validateAncestors(req); err != nil {
+		return err
+	}
+	if !validId(req.GetId(), iam.UserPrefix+"_") {
+		return handlers.InvalidArgumentErrorf("Improperly formatted identifier.", []string{"id"})
+	}
+
+	if req.GetUpdateMask() == nil {
+		return handlers.InvalidArgumentErrorf("UpdateMask not provided but is required to update a user.", []string{"update_mask"})
+	}
+
+	item := req.GetItem()
+	if item == nil {
+		// It is legitimate for no item to be specified in an update request as it indicates all fields provided in
+		// the mask will be marked as unset.
+		return nil
+	}
+	if item.GetId() != "" && item.GetId() != req.GetId() {
+		return handlers.InvalidArgumentErrorf("Id in provided item and url do not match.", []string{"id"})
+	}
+	var immutableFieldsSet []string
+	if item.GetId() != "" {
+		immutableFieldsSet = append(immutableFieldsSet, "id")
+	}
+	if item.GetCreatedTime() != nil {
+		immutableFieldsSet = append(immutableFieldsSet, "created_time")
+	}
+	if item.GetUpdatedTime() != nil {
+		immutableFieldsSet = append(immutableFieldsSet, "updated_time")
+	}
+	if len(immutableFieldsSet) > 0 {
+		return handlers.InvalidArgumentErrorf("Cannot specify read only fields at update time.", immutableFieldsSet)
+	}
+
+	return nil
+}
+
+func validateDeleteRequest(req *pbs.DeleteUserRequest) error {
+	if err := validateAncestors(req); err != nil {
+		return err
+	}
+	if !validId(req.GetId(), iam.UserPrefix+"_") {
+		return handlers.InvalidArgumentErrorf("Improperly formatted id.", []string{"id"})
+	}
+	return nil
+}
+
+func validId(id, prefix string) bool {
+	if !strings.HasPrefix(id, prefix) {
+		return false
+	}
+	id = strings.TrimPrefix(id, prefix)
+	return !reInvalidID.Match([]byte(id))
+}
+
+type ancestorProvider interface {
+	GetOrgId() string
+}
+
+// validateAncestors verifies that the ancestors of this call are properly set and provided.
+func validateAncestors(r ancestorProvider) error {
+	if r.GetOrgId() == "" {
+		return handlers.InvalidArgumentErrorf("Missing organization id.", []string{"org_id"})
+	}
+	if !validId(r.GetOrgId(), "o_") {
+		return handlers.InvalidArgumentErrorf("Poorly formatted org id.", []string{"org_id"})
+	}
+	return nil
+}
---
+++ internal/servers/controller/handlers/users/user_service_test.go
@@ -0,0 +1,533 @@
+package users_test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/hashicorp/watchtower/internal/db"
+	pb "github.com/hashicorp/watchtower/internal/gen/controller/api/resources/scopes"
+	pbs "github.com/hashicorp/watchtower/internal/gen/controller/api/services"
+	"github.com/hashicorp/watchtower/internal/iam"
+	"github.com/hashicorp/watchtower/internal/servers/controller/handlers/users"
+	"google.golang.org/genproto/protobuf/field_mask"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func createDefaultUserAndRepo(t *testing.T) (*iam.User, func() (*iam.Repository, error)) {
+	t.Helper()
+	cleanup, conn, _ := db.TestSetup(t, "postgres")
+	t.Cleanup(func() {
+		if err := conn.Close(); err != nil {
+			t.Errorf("Error when closing gorm DB: %v", err)
+		}
+		if err := cleanup(); err != nil {
+			t.Errorf("Error when cleaning up TestSetup: %v", err)
+		}
+	})
+	rw := db.New(conn)
+	wrap := db.TestWrapper(t)
+	repoFn := func() (*iam.Repository, error) {
+		return iam.NewRepository(rw, rw, wrap)
+	}
+
+	o, _ := iam.TestScopes(t, conn)
+	u := iam.TestUser(t, conn, o.GetPublicId(), iam.WithDescription("default"), iam.WithName("default"))
+	return u, repoFn
+}
+
+func TestGet(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+	u, repo := createDefaultUserAndRepo(t)
+	toMerge := &pbs.GetUserRequest{
+		OrgId: u.GetScopeId(),
+		Id:    u.GetPublicId(),
+	}
+
+	wantU := &pb.User{
+		Id:          u.GetPublicId(),
+		Name:        &wrapperspb.StringValue{Value: u.GetName()},
+		Description: &wrapperspb.StringValue{Value: u.GetDescription()},
+		CreatedTime: u.CreateTime.GetTimestamp(),
+		UpdatedTime: u.UpdateTime.GetTimestamp(),
+	}
+
+	cases := []struct {
+		name    string
+		req     *pbs.GetUserRequest
+		res     *pbs.GetUserResponse
+		errCode codes.Code
+	}{
+		{
+			name:    "Get an Existing User",
+			req:     &pbs.GetUserRequest{Id: u.GetPublicId()},
+			res:     &pbs.GetUserResponse{Item: wantU},
+			errCode: codes.OK,
+		},
+		{
+			name:    "Get a non existant User",
+			req:     &pbs.GetUserRequest{Id: iam.UserPrefix + "_DoesntExis"},
+			res:     nil,
+			errCode: codes.NotFound,
+		},
+		{
+			name:    "Wrong id prefix",
+			req:     &pbs.GetUserRequest{Id: "j_1234567890"},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name:    "space in id",
+			req:     &pbs.GetUserRequest{Id: iam.UserPrefix + "_1 23456789"},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			req := proto.Clone(toMerge).(*pbs.GetUserRequest)
+			proto.Merge(req, tc.req)
+
+			s, err := users.NewService(repo)
+			require.NoError(err, "Couldn't create new user service.")
+
+			got, gErr := s.GetUser(context.Background(), req)
+			assert.Equal(tc.errCode, status.Code(gErr), "GetUser(%+v) got error %v, wanted %v", req, gErr, tc.errCode)
+			assert.True(proto.Equal(got, tc.res), "GetUser(%q) got response %q, wanted %q", req, got, tc.res)
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	require := require.New(t)
+	u, repo := createDefaultUserAndRepo(t)
+
+	s, err := users.NewService(repo)
+	require.NoError(err, "Error when getting new user service.")
+
+	cases := []struct {
+		name    string
+		req     *pbs.DeleteUserRequest
+		res     *pbs.DeleteUserResponse
+		errCode codes.Code
+	}{
+		{
+			name: "Delete an Existing User",
+			req: &pbs.DeleteUserRequest{
+				OrgId: u.GetScopeId(),
+				Id:    u.GetPublicId(),
+			},
+			res: &pbs.DeleteUserResponse{
+				Existed: true,
+			},
+			errCode: codes.OK,
+		},
+		{
+			name: "Delete bad user id User",
+			req: &pbs.DeleteUserRequest{
+				OrgId: u.GetScopeId(),
+				Id:    iam.UserPrefix + "_doesntexis",
+			},
+			res: &pbs.DeleteUserResponse{
+				Existed: false,
+			},
+			errCode: codes.OK,
+		},
+		{
+			name: "Delete bad org id User",
+			req: &pbs.DeleteUserRequest{
+				OrgId: "o_doesntexis",
+				Id:    u.GetPublicId(),
+			},
+			res: &pbs.DeleteUserResponse{
+				Existed: false,
+			},
+			errCode: codes.OK,
+		},
+		{
+			name: "Bad org formatting",
+			req: &pbs.DeleteUserRequest{
+				OrgId: "bad_format",
+				Id:    u.GetPublicId(),
+			},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name: "Bad User Id formatting",
+			req: &pbs.DeleteUserRequest{
+				OrgId: u.GetScopeId(),
+				Id:    "bad_format",
+			},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+			got, gErr := s.DeleteUser(context.Background(), tc.req)
+			assert.Equal(tc.errCode, status.Code(gErr), "DeleteUser(%+v) got error %v, wanted %v", tc.req, gErr, tc.errCode)
+			assert.EqualValuesf(tc.res, got, "DeleteUser(%q) got response %q, wanted %q", tc.req, got, tc.res)
+		})
+	}
+}
+
+func TestDelete_twice(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+	u, repo := createDefaultUserAndRepo(t)
+
+	s, err := users.NewService(repo)
+	require.NoError(err, "Error when getting new user service")
+	req := &pbs.DeleteUserRequest{
+		OrgId: u.GetScopeId(),
+		Id:    u.GetPublicId(),
+	}
+	got, gErr := s.DeleteUser(context.Background(), req)
+	assert.NoError(gErr, "First attempt")
+	assert.True(got.GetExisted(), "Expected existed to be true for the first delete.")
+	got, gErr = s.DeleteUser(context.Background(), req)
+	assert.NoError(gErr, "Second attempt")
+	assert.False(got.GetExisted(), "Expected existed to be false for the second delete.")
+}
+
+func TestCreate(t *testing.T) {
+	require := require.New(t)
+	defaultUser, repo := createDefaultUserAndRepo(t)
+	defaultProjCreated, err := ptypes.Timestamp(defaultUser.GetCreateTime().GetTimestamp())
+	require.NoError(err, "Error converting proto to timestamp.")
+	toMerge := &pbs.CreateUserRequest{
+		OrgId: defaultUser.GetScopeId(),
+	}
+
+	cases := []struct {
+		name    string
+		req     *pbs.CreateUserRequest
+		res     *pbs.CreateUserResponse
+		errCode codes.Code
+	}{
+		{
+			name: "Create a valid User",
+			req: &pbs.CreateUserRequest{Item: &pb.User{
+				Name:        &wrapperspb.StringValue{Value: "name"},
+				Description: &wrapperspb.StringValue{Value: "desc"},
+			}},
+			res: &pbs.CreateUserResponse{
+				Uri: fmt.Sprintf("orgs/%s/users/u_", defaultUser.GetScopeId()),
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "name"},
+					Description: &wrapperspb.StringValue{Value: "desc"},
+				},
+			},
+			errCode: codes.OK,
+		},
+		{
+			name: "Can't specify Id",
+			req: &pbs.CreateUserRequest{Item: &pb.User{
+				Id: "not allowed to be set",
+			}},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name: "Can't specify Created Time",
+			req: &pbs.CreateUserRequest{Item: &pb.User{
+				CreatedTime: ptypes.TimestampNow(),
+			}},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name: "Can't specify Update Time",
+			req: &pbs.CreateUserRequest{Item: &pb.User{
+				UpdatedTime: ptypes.TimestampNow(),
+			}},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+			req := proto.Clone(toMerge).(*pbs.CreateUserRequest)
+			proto.Merge(req, tc.req)
+
+			s, err := users.NewService(repo)
+			require.NoError(err, "Error when getting new user service.")
+
+			got, gErr := s.CreateUser(context.Background(), req)
+			assert.Equal(tc.errCode, status.Code(gErr), "CreateUser(%+v) got error %v, wanted %v", req, gErr, tc.errCode)
+			if got != nil {
+				strings.HasPrefix(got.GetUri(), tc.res.Uri)
+				strings.HasPrefix(got.GetItem().GetId(), iam.UserPrefix+"_")
+				gotCreateTime, err := ptypes.Timestamp(got.GetItem().GetCreatedTime())
+				require.NoError(err, "Error converting proto to timestamp.")
+				gotUpdateTime, err := ptypes.Timestamp(got.GetItem().GetUpdatedTime())
+				require.NoError(err, "Error converting proto to timestamp.")
+				// Verify it is a user created after the test setup's default user
+				assert.True(gotCreateTime.After(defaultProjCreated), "New user should have been created after default user. Was created %v, which is after %v", gotCreateTime, defaultProjCreated)
+				assert.True(gotUpdateTime.After(defaultProjCreated), "New user should have been updated after default user. Was updated %v, which is after %v", gotUpdateTime, defaultProjCreated)
+
+				// Clear all values which are hard to compare against.
+				got.Uri, tc.res.Uri = "", ""
+				got.Item.Id, tc.res.Item.Id = "", ""
+				got.Item.CreatedTime, got.Item.UpdatedTime, tc.res.Item.CreatedTime, tc.res.Item.UpdatedTime = nil, nil, nil, nil
+			}
+			assert.True(proto.Equal(got, tc.res), "CreateUser(%q) got response %q, wanted %q", req, got, tc.res)
+		})
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	require := require.New(t)
+	u, repoFn := createDefaultUserAndRepo(t)
+	tested, err := users.NewService(repoFn)
+	require.NoError(err, "Error when getting new user service.")
+
+	resetUser := func() {
+		repo, err := repoFn()
+		require.NoError(err, "Couldn't get a new repo")
+		u, _, err = repo.UpdateUser(context.Background(), u, []string{"Name", "Description"})
+		require.NoError(err, "Failed to reset the user")
+	}
+
+	projCreated, err := ptypes.Timestamp(u.GetCreateTime().GetTimestamp())
+	require.NoError(err, "Error converting proto to timestamp")
+	toMerge := &pbs.UpdateUserRequest{
+		OrgId: u.GetScopeId(),
+		Id:    u.GetPublicId(),
+	}
+
+	cases := []struct {
+		name    string
+		req     *pbs.UpdateUserRequest
+		res     *pbs.UpdateUserResponse
+		errCode codes.Code
+	}{
+		{
+			name: "Update an Existing User",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"name", "description"},
+				},
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "new"},
+					Description: &wrapperspb.StringValue{Value: "desc"},
+				},
+			},
+			res: &pbs.UpdateUserResponse{
+				Item: &pb.User{
+					Id:          u.GetPublicId(),
+					Name:        &wrapperspb.StringValue{Value: "new"},
+					Description: &wrapperspb.StringValue{Value: "desc"},
+					CreatedTime: u.GetCreateTime().GetTimestamp(),
+				},
+			},
+			errCode: codes.OK,
+		},
+		{
+			name: "Multiple Paths in single string",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"name,description"},
+				},
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "new"},
+					Description: &wrapperspb.StringValue{Value: "desc"},
+				},
+			},
+			res: &pbs.UpdateUserResponse{
+				Item: &pb.User{
+					Id:          u.GetPublicId(),
+					Name:        &wrapperspb.StringValue{Value: "new"},
+					Description: &wrapperspb.StringValue{Value: "desc"},
+					CreatedTime: u.GetCreateTime().GetTimestamp(),
+				},
+			},
+			errCode: codes.OK,
+		},
+		{
+			name: "No Update Mask Is Invalid Argument",
+			req: &pbs.UpdateUserRequest{
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "updated name"},
+					Description: &wrapperspb.StringValue{Value: "updated desc"},
+				},
+			},
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name: "No Paths in Mask Is Invalid Argument",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{Paths: []string{}},
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "updated name"},
+					Description: &wrapperspb.StringValue{Value: "updated desc"},
+				},
+			},
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name: "Only non-existant paths in Mask Is Invalid Argument",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{Paths: []string{"nonexistant_field"}},
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "updated name"},
+					Description: &wrapperspb.StringValue{Value: "updated desc"},
+				},
+			},
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name: "Unset Name",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"name"},
+				},
+				Item: &pb.User{
+					Description: &wrapperspb.StringValue{Value: "ignored"},
+				},
+			},
+			res: &pbs.UpdateUserResponse{
+				Item: &pb.User{
+					Id:          u.GetPublicId(),
+					Description: &wrapperspb.StringValue{Value: "default"},
+					CreatedTime: u.GetCreateTime().GetTimestamp(),
+				},
+			},
+			errCode: codes.OK,
+		},
+		{
+			name: "Update Only Name",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"name"},
+				},
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "updated"},
+					Description: &wrapperspb.StringValue{Value: "ignored"},
+				},
+			},
+			res: &pbs.UpdateUserResponse{
+				Item: &pb.User{
+					Id:          u.GetPublicId(),
+					Name:        &wrapperspb.StringValue{Value: "updated"},
+					Description: &wrapperspb.StringValue{Value: "default"},
+					CreatedTime: u.GetCreateTime().GetTimestamp(),
+				},
+			},
+			errCode: codes.OK,
+		},
+		{
+			name: "Update Only Description",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"description"},
+				},
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "ignored"},
+					Description: &wrapperspb.StringValue{Value: "notignored"},
+				},
+			},
+			res: &pbs.UpdateUserResponse{
+				Item: &pb.User{
+					Id:          u.GetPublicId(),
+					Name:        &wrapperspb.StringValue{Value: "default"},
+					Description: &wrapperspb.StringValue{Value: "notignored"},
+					CreatedTime: u.GetCreateTime().GetTimestamp(),
+				},
+			},
+			errCode: codes.OK,
+		},
+		// TODO: Updating a non existant user should result in a NotFound exception but currently results in
+		// the repoFn returning an internal error.
+		{
+			name: "Update a Non Existing User",
+			req: &pbs.UpdateUserRequest{
+				Id: iam.UserPrefix + "_DoesntExis",
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"description"},
+				},
+				Item: &pb.User{
+					Name:        &wrapperspb.StringValue{Value: "new"},
+					Description: &wrapperspb.StringValue{Value: "desc"},
+				},
+			},
+			errCode: codes.Internal,
+		},
+		{
+			name: "Cant change Id",
+			req: &pbs.UpdateUserRequest{
+				Id: u.GetPublicId(),
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"id"},
+				},
+				Item: &pb.User{
+					Id:          iam.UserPrefix + "_somethinge",
+					Name:        &wrapperspb.StringValue{Value: "new"},
+					Description: &wrapperspb.StringValue{Value: "new desc"},
+				}},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name: "Cant specify Created Time",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"created_time"},
+				},
+				Item: &pb.User{
+					CreatedTime: ptypes.TimestampNow(),
+				},
+			},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name: "Cant specify Updated Time",
+			req: &pbs.UpdateUserRequest{
+				UpdateMask: &field_mask.FieldMask{
+					Paths: []string{"updated_time"},
+				},
+				Item: &pb.User{
+					UpdatedTime: ptypes.TimestampNow(),
+				},
+			},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			defer resetUser()
+			assert := assert.New(t)
+			req := proto.Clone(toMerge).(*pbs.UpdateUserRequest)
+			proto.Merge(req, tc.req)
+
+			got, gErr := tested.UpdateUser(context.Background(), req)
+			assert.Equal(tc.errCode, status.Code(gErr), "UpdateUser(%+v) got error %v, wanted %v", req, gErr, tc.errCode)
+
+			if got != nil {
+				assert.NotNilf(tc.res, "Expected UpdateUser response to be nil, but was %v", got)
+				gotUpdateTime, err := ptypes.Timestamp(got.GetItem().GetUpdatedTime())
+				require.NoError(err, "Error converting proto to timestamp")
+				// Verify it is a user updated after it was created
+				assert.True(gotUpdateTime.After(projCreated), "Updated user should have been updated after it's creation. Was updated %v, which is after %v", gotUpdateTime, projCreated)
+
+				// Clear all values which are hard to compare against.
+				got.Item.UpdatedTime, tc.res.Item.UpdatedTime = nil, nil
+			}
+			assert.True(proto.Equal(got, tc.res), "UpdateUser(%q) got response %q, wanted %q", req, got, tc.res)
+		})
+	}
+}
