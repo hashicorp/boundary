ebd32d23a9d5b3bf400e4c700a69552e888722d6
Todd Knight <T.Alan.Knight@gmail.com>
2020-06-08 11:59:05 -0700

    Inlining the error checks into the table driven tests.  Use protocmp for assertions to make printing out diffs easier to parse.

--- internal/servers/controller/handlers/errors_test.go
+++ internal/servers/controller/handlers/errors_test.go
@@ -2,20 +2,19 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/hashicorp/go-hclog"
 	pb "github.com/hashicorp/watchtower/internal/gen/controller/api"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	sdpb "google.golang.org/genproto/googleapis/rpc/errdetails"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/testing/protocmp"
 )
 
 func TestApiErrorHandler(t *testing.T) {
@@ -24,145 +23,79 @@ func TestApiErrorHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "madeup/for/the/test", nil)
 	require.NoError(err)
 	mux := runtime.NewServeMux()
-	_, outMarsh := runtime.MarshalerForRequest(mux, req)
+	inMarsh, outMarsh := runtime.MarshalerForRequest(mux, req)
 
 	tested := ErrorHandler(hclog.L())
 
 	testCases := []struct {
-		name          string
-		err           error
-		statusDetails []proto.Message
-		expected      *pb.Error
+		name     string
+		err      error
+		expected *pb.Error
 	}{
 		{
 			name: "Not Found",
-			err:  status.Error(codes.NotFound, "test"),
+			err:  NotFoundErrorf("Test"),
 			expected: &pb.Error{
-				Status:  404,
-				Code:    codes.NotFound.String(),
-				Message: "test",
+				Status:  http.StatusNotFound,
+				Code:    "NotFound",
+				Message: "Test",
+			},
+		},
+		{
+			name: "Invalid Fields",
+			err: InvalidArgumentErrorf("Test", map[string]string{
+				"k1": "v1",
+				"k2": "v2",
+			}),
+			expected: &pb.Error{
+				Status:  http.StatusBadRequest,
+				Code:    "InvalidArgument",
+				Message: "Test",
+				Details: &pb.ErrorDetails{
+					RequestFields: []*pb.FieldError{{
+						Name:        "k1",
+						Description: "v1",
+					},
+						{
+							Name:        "k2",
+							Description: "v2",
+						}},
+				},
 			},
 		},
 		{
 			name: "GrpcGateway Routing Error",
 			err:  runtime.ErrUnknownURI,
 			expected: &pb.Error{
-				Status:  404,
-				Code:    codes.NotFound.String(),
+				Status:  http.StatusNotFound,
+				Code:    "NotFound",
 				Message: http.StatusText(http.StatusNotFound),
 			},
 		},
 		{
-			name: "Invalid Fields",
-			err:  status.Error(codes.InvalidArgument, "test"),
-			statusDetails: []proto.Message{
-				&sdpb.BadRequest{
-					FieldViolations: []*sdpb.BadRequest_FieldViolation{
-						{Field: "first", Description: "first desc"},
-						{Field: "second", Description: "second desc"},
-					},
-				},
-			},
+			name: "Unknown error",
+			err:  errors.New("Some random error"),
 			expected: &pb.Error{
-				Status:  400,
-				Code:    codes.InvalidArgument.String(),
-				Message: "test",
-				Details: &pb.ErrorDetails{
-					RequestFields: []*pb.FieldError{{
-						Name:        "first",
-						Description: "first desc",
-					},
-						{
-							Name:        "second",
-							Description: "second desc",
-						}},
-				},
+				Status:  http.StatusInternalServerError,
+				Code:    "Unknown",
+				Message: "Some random error",
 			},
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			if tc.statusDetails != nil {
-				s, ok := status.FromError(tc.err)
-				assert.True(ok)
-				s, err := s.WithDetails(tc.statusDetails...)
-				assert.NoError(err)
-				tc.err = s.Err()
-			}
-
 			w := httptest.NewRecorder()
 			tested(ctx, mux, outMarsh, w, req, tc.err)
 			resp := w.Result()
 			assert.EqualValues(tc.expected.Status, resp.StatusCode)
 
 			got, err := ioutil.ReadAll(resp.Body)
-			assert.NoError(err)
-			want, err := outMarsh.Marshal(tc.expected)
-			t.Logf("Got marshaled error: %q", want)
-			assert.NoError(err)
-			assert.JSONEq(string(want), string(got))
+			require.NoError(err)
+
+			gotErr := &pb.Error{}
+			err = inMarsh.Unmarshal(got, gotErr)
+			require.NoError(err)
+			assert.Empty(cmp.Diff(tc.expected, gotErr, protocmp.Transform()))
 		})
 	}
-}
-
-func TestNotFoundErrorf(t *testing.T) {
-	assert, require := assert.New(t), require.New(t)
-	ctx := context.Background()
-	req, err := http.NewRequest("GET", "madeup/for/the/test", nil)
-	require.NoError(err)
-	mux := runtime.NewServeMux()
-	inMarsh, outMarsh := runtime.MarshalerForRequest(mux, req)
-	tested := ErrorHandler(hclog.L())
-	w := httptest.NewRecorder()
-
-	in := NotFoundErrorf("Not found error message %s %s", "with", "params")
-	tested(ctx, mux, outMarsh, w, req, in)
-	resp := w.Result()
-	gotBody, err := ioutil.ReadAll(resp.Body)
-	require.NoError(err)
-
-	wantErr := &pb.Error{
-		Status:  http.StatusNotFound,
-		Code:    "Not found error message with params",
-		Message: "NotFound",
-	}
-
-	gotErr := &pb.Error{}
-	err = inMarsh.Unmarshal(gotBody, gotErr)
-	require.NoError(err)
-	_ = wantErr
-	//assert.Equal(wantErr, gotErr)
-	assert.EqualValues(http.StatusNotFound, resp.StatusCode)
-	assert.Equal("Not found error message with params", gotErr.GetMessage())
-	assert.EqualValues(http.StatusNotFound, gotErr.GetStatus())
-	assert.Equal("NotFound", gotErr.GetCode())
-	assert.Nil(gotErr.Details)
-}
-
-func TestInvalidArgumentErrorf(t *testing.T) {
-	assert, require := assert.New(t), require.New(t)
-	ctx := context.Background()
-	req, err := http.NewRequest("GET", "madeup/for/the/test", nil)
-	if err != nil {
-		t.Fatalf("Couldn't create test request")
-	}
-	mux := runtime.NewServeMux()
-	inMarsh, outMarsh := runtime.MarshalerForRequest(mux, req)
-	tested := ErrorHandler(hclog.L())
-	w := httptest.NewRecorder()
-
-	in := InvalidArgumentErrorf("Test", map[string]string{"k": "v", "k2": "v2"})
-	tested(ctx, mux, outMarsh, w, req, in)
-	resp := w.Result()
-	gotBody, err := ioutil.ReadAll(resp.Body)
-	require.NoError(err)
-
-	gotErr := &pb.Error{}
-	err = inMarsh.Unmarshal(gotBody, gotErr)
-	require.NoError(err)
-	assert.EqualValues(http.StatusBadRequest, resp.StatusCode)
-	assert.Equal("Test", gotErr.GetMessage())
-	assert.EqualValues(http.StatusBadRequest, gotErr.GetStatus())
-	assert.Equal("BadRequest", gotErr.GetCode())
-	assert.NotNil(gotErr.Details)
 }
