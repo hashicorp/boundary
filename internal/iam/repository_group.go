1bc40494767da53d8e533317bfe87a175ace2f3a
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-06-09 20:05:59 -0400

    align with other repos

--- internal/iam/repository_group.go
+++ internal/iam/repository_group.go
@@ -2,8 +2,8 @@ package iam
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/watchtower/internal/db"
 )
@@ -11,28 +11,62 @@ import (
 // CreateGroup will create a group in the repository and return the written group
 func (r *Repository) CreateGroup(ctx context.Context, group *Group, opt ...Option) (*Group, error) {
 	if group == nil {
-		return nil, errors.New("error group is nil for create")
+		return nil, fmt.Errorf("create group: missing group %w", db.ErrNilParameter)
 	}
 	id, err := newGroupId()
 	if err != nil {
 		return nil, fmt.Errorf("create group: %w", err)
 	}
-	group.PublicId = id
-	resource, err := r.create(ctx, group)
+	g := group.Clone().(*Group)
+	g.PublicId = id
+	resource, err := r.create(ctx, g)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create group: %w", err)
+		if db.IsUniqueError(err) {
+			return nil, fmt.Errorf("create group: group %s already exists in scope %s", group.Name, group.ScopeId)
+		}
+		return nil, fmt.Errorf("create group: %w for %s", err, g.PublicId)
 	}
 	return resource.(*Group), err
 }
 
-// UpdateGroup will update a group in the repository and return the written group
+// UpdateGroup will update a group in the repository and return the written
+// group. fieldMaskPaths provides field_mask.proto paths for fields that should
+// be updated.  Fields will be set to NULL if the field is a zero value and
+// included in fieldMask. Name and Description are the only updatable fields,
+// If no updatable fields are included in the fieldMaskPaths, then an error is returned.
 func (r *Repository) UpdateGroup(ctx context.Context, group *Group, fieldMaskPaths []string, opt ...Option) (*Group, int, error) {
 	if group == nil {
-		return nil, db.NoRowsAffected, errors.New("error group is nil for update")
+		return nil, db.NoRowsAffected, fmt.Errorf("update group: missing group %w", db.ErrNilParameter)
 	}
-	resource, rowsUpdated, err := r.update(ctx, group, fieldMaskPaths, nil)
+	if group.PublicId == "" {
+		return nil, db.NoRowsAffected, fmt.Errorf("update group: missing group public id %w", db.ErrInvalidParameter)
+	}
+	for _, f := range fieldMaskPaths {
+		switch {
+		case strings.EqualFold("name", f):
+		case strings.EqualFold("description", f):
+		default:
+			return nil, db.NoRowsAffected, fmt.Errorf("update group: field: %s: %w", f, db.ErrInvalidFieldMask)
+		}
+	}
+	var dbMask, nullFields []string
+	dbMask, nullFields = buildUpdatePaths(
+		map[string]interface{}{
+			"name":        group.Name,
+			"description": group.Description,
+		},
+		fieldMaskPaths,
+	)
+	if len(dbMask) == 0 && len(nullFields) == 0 {
+		return nil, db.NoRowsAffected, fmt.Errorf("update user: %w", db.ErrEmptyFieldMask)
+	}
+	g := group.Clone().(*Group)
+	resource, rowsUpdated, err := r.update(ctx, g, fieldMaskPaths, nil)
 	if err != nil {
-		return nil, db.NoRowsAffected, fmt.Errorf("failed to update group: %w", err)
+		if db.IsUniqueError(err) {
+			return nil, db.NoRowsAffected, fmt.Errorf("update group: group %s already exists in organization %s", group.Name, group.ScopeId)
+		}
+		return nil, db.NoRowsAffected, fmt.Errorf("update group: %w for %s", err, group.PublicId)
 	}
 	return resource.(*Group), rowsUpdated, err
 }
@@ -41,27 +75,29 @@ func (r *Repository) UpdateGroup(ctx context.Context, group *Group, fieldMaskPat
 // found, it will return nil, nil.
 func (r *Repository) LookupGroup(ctx context.Context, withPublicId string, opt ...Option) (*Group, error) {
 	if withPublicId == "" {
-		return nil, errors.New("you cannot lookup a group with an empty public id")
+		return nil, fmt.Errorf("lookup group: missing public id %w", db.ErrNilParameter)
 	}
-
 	g := allocGroup()
 	g.PublicId = withPublicId
 	if err := r.reader.LookupByPublicId(ctx, &g); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("lookup group: failed %w for %s", err, withPublicId)
 	}
 	return &g, nil
 }
 
-// DeleteGroup will delete a group from the repository
+// DeleteGroup will delete a group from the repository.
 func (r *Repository) DeleteGroup(ctx context.Context, withPublicId string, opt ...Option) (int, error) {
 	if withPublicId == "" {
-		return db.NoRowsAffected, errors.New("you cannot delete a group with an empty public id")
+		return db.NoRowsAffected, fmt.Errorf("delete group: missing public id %w", db.ErrNilParameter)
 	}
 	g := allocGroup()
 	g.PublicId = withPublicId
+	if err := r.reader.LookupByPublicId(ctx, &g); err != nil {
+		return db.NoRowsAffected, fmt.Errorf("delete group: failed %w for %s", err, withPublicId)
+	}
 	rowsDeleted, err := r.writer.Delete(ctx, &g)
 	if err != nil {
-		return db.NoRowsAffected, fmt.Errorf("unable to delete group by public id: %w", err)
+		return db.NoRowsAffected, fmt.Errorf("delete group: failed %w for %s", err, withPublicId)
 	}
 	return rowsDeleted, nil
 }
