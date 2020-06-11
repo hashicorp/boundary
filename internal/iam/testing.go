99a1d5d5e3421d89e69e0e0725884a81a495b36f
Jim Lambert <jimlambrt@Jims-MBP-3.home>
2020-06-11 11:17:32 -0400

    add TestRole()

--- internal/iam/testing.go
+++ internal/iam/testing.go
@@ -101,3 +101,24 @@ func TestGroup(t *testing.T, conn *gorm.DB, orgId string, opt ...Option) *Group 
 	require.NotEmpty(grp.PublicId)
 	return grp
 }
+
+// TestRole creates a role suitable for testing.
+func TestRole(t *testing.T, conn *gorm.DB, scopeId string, opt ...Option) *Role {
+	t.Helper()
+	require := require.New(t)
+	rw := db.New(conn)
+
+	role, err := NewRole(scopeId, opt...)
+	require.NoError(err)
+	id, err := newRoleId()
+	require.NoError(err)
+	role.PublicId = id
+	err = rw.Create(context.Background(), role)
+	require.NoError(err)
+	require.NotEmpty(role.PublicId)
+
+	opts := getOpts(opt...)
+	require.Equal(opts.withDescription, role.Description)
+	require.Equal(opts.withName, role.Name)
+	return role
+}
