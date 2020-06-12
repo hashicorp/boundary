4e73cbc8eddf098993370804bb447714e63c3434
Michael Gaffney <mike@gaffney.cc>
2020-06-12 16:51:01 -0400

    Add schema for auth methods and userpass

---
+++ internal/db/migrations/postgres/11_auth.up.sql
@@ -0,0 +1,91 @@
+begin;
+
+  create table auth_usrpass_argon2_conf (
+    id bigint generated always as identity primary key,
+    iam_scope_id wt_public_id not null
+      references iam_scope(public_id)
+      on delete cascade
+      on update cascade,
+    create_time wt_timestamp,
+    iterations int not null
+      check(iterations > 0),
+    memory int not null
+      check(memory > 0),
+    threads int not null
+      check(threads > 0),
+    -- key_length unit is bytes
+    key_length int not null
+    -- minimum of 16 bytes (128 bits)
+      check(key_length >= 16),
+    unique(iam_scope_id, iterations, memory, threads, key_length)
+  );
+
+  -- TODO(mgaffney) 06/2020: create, no updates
+
+  -- an iam_user can have 0 to 1 auth_method
+  -- an auth_method belongs to 1 and only 1 iam_user
+  create table auth_method (
+    auth_method_id wt_private_id primary key,
+    iam_user_id wt_public_id not null unique
+      references iam_user (public_id)
+      on delete cascade
+      on update cascade,
+    create_time wt_timestamp
+  );
+
+  -- auth_usrpass is a auth_method
+  -- a user_name is unique within a scope
+  create table auth_usrpass (
+    auth_method_id wt_private_id primary key
+      references auth_method (auth_method_id)
+      on delete cascade
+      on update cascade,
+    -- NOTE: there is no constraint to enforce the user is in this scope
+    -- TODO(mgaffney) 06/2020: add insert trigger to check user is in scope
+    iam_scope_id wt_public_id not null
+      references iam_scope(public_id)
+      on delete cascade
+      on update cascade,
+    user_name text not null
+      -- TODO(mgaffney) 06/2020:
+      -- check all lowercase
+      -- check no spaces
+      check(
+        length(trim(user_name)) > 5
+      ),
+    create_time wt_timestamp,
+    update_time wt_timestamp,
+    unique(iam_scope_id, user_name)
+  );
+
+
+  -- auth_usrpass_argon2 is a auth_usrpass
+  create table auth_usrpass_argon2 (
+    auth_method_id wt_private_id primary key
+      references auth_usrpass (auth_method_id)
+      on delete cascade
+      on update cascade,
+    argon2_conf_id bigint not null
+      references auth_usrpass_argon2_conf (id)
+      on delete restrict
+      on update restrict,
+    create_time wt_timestamp,
+    update_time wt_timestamp,
+    salt bytea not null,
+    hashed_password bytea not null
+
+    -- TODO(mgaffney) 06/2020: look at protobuf for oplog entry for encryption tags
+    -- TODO(mgaffney) 06/2020: trigger - prevent changes to scope id
+
+    -- TODO(mgaffney) 06/2020: Salt and hashed_password will be encrypted with
+    -- the database DEK. Add foreign key to database DEK.
+
+  );
+
+  /*
+  TODO(mgaffney) 06/2020: auth_usrpass_argon2_conf needs a trigger to prevent
+  updates. The output from the argon2 hash function is different if any of the
+  input parameters, including the config parameters, have changed.
+  */
+
+commit;
