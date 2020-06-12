6ea74b5880f67bc615f6f632a409cc3ce53463b4
Michael Gaffney <mike@gaffney.cc>
2020-06-12 16:49:52 -0400

    Add wt_private_id domain type

--- internal/db/migrations/postgres/01_domain_types.down.sql
+++ internal/db/migrations/postgres/01_domain_types.down.sql
@@ -2,6 +2,7 @@ begin;
 
 drop domain wt_timestamp;
 drop domain wt_public_id;
+drop domain wt_private_id;
 drop function update_time_column() cascade;
 drop function immutable_create_time_func() cascade;
 commit;
