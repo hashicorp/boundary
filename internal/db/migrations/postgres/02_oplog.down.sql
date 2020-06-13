2d650b241d4a77ac2d32c5936a3406b68aef4df5
Michael Gaffney <mgaffney@users.noreply.github.com>
2020-06-13 07:20:40 -0400

    Fix down migrations (#120)
    * Fix iam down migrations
    
    * Fix '03_db' down migrations
    
    * Fix oplog down migrations
    
    * Fix domain down migrations
    
    * Run make gen

--- internal/db/migrations/postgres/02_oplog.down.sql
+++ internal/db/migrations/postgres/02_oplog.down.sql
@@ -1,18 +1,7 @@
 begin;
 
-drop table if exists oplog_entry cascade;
-
-drop trigger if exists update_oplog_entry_update_time on oplog_entry;
-drop trigger if exists update_oplog_entry_create_time on oplog_entry;
-
-drop table if exists oplog_ticket cascade;
-
-drop trigger if exists update_oplog_ticket_update_time on oplog_ticket;
-drop trigger if exists update_oplog_ticket_create_time on oplog_ticket;
-
-drop table if exists oplog_metadata cascade;
-
-drop trigger if exists update_oplog_metadata_update_time on oplog_metadata;
-drop trigger if exists update_oplog_metadata_create_time on oplog_metadata;
+drop table oplog_metadata cascade;
+drop table oplog_ticket cascade;
+drop table oplog_entry cascade;
 
 commit;
