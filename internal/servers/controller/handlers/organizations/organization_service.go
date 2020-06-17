cb7823dae35792c1eecc9097cf3193a1b6eb36ae
Todd Knight <T.Alan.Knight@gmail.com>
2020-06-17 16:07:26 -0700

    Create the organization Get handler.

---
+++ internal/servers/controller/handlers/organizations/organization_service.go
@@ -0,0 +1,109 @@
+package organizations
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+
+	pb "github.com/hashicorp/watchtower/internal/gen/controller/api/resources/scopes"
+	pbs "github.com/hashicorp/watchtower/internal/gen/controller/api/services"
+	"github.com/hashicorp/watchtower/internal/iam"
+	"github.com/hashicorp/watchtower/internal/servers/controller/common"
+	"github.com/hashicorp/watchtower/internal/servers/controller/handlers"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+const orgIdFieldName = "org_id"
+
+var (
+	reInvalidID = regexp.MustCompile("[^A-Za-z0-9]")
+)
+
+// Service handles request as described by the pbs.OrganizationServiceServer interface.
+type Service struct {
+	repo common.IamRepoFactory
+}
+
+// NewService returns a project service which handles project related requests to watchtower.
+func NewService(repo common.IamRepoFactory) (Service, error) {
+	if repo == nil {
+		return Service{}, fmt.Errorf("nil iam repository provided")
+	}
+	return Service{repo: repo}, nil
+}
+
+var _ pbs.OrganizationServiceServer = Service{}
+
+// ListOrganizations is not yet implemented but will implement the interface pbs.OrganizationServiceServer.
+func (s Service) ListOrganizations(ctx context.Context, req *pbs.ListOrganizationsRequest) (*pbs.ListOrganizationsResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "List not enabled for this resource.")
+}
+
+// GetOrganizations implements the interface pbs.OrganizationServiceServer.
+func (s Service) GetOrganization(ctx context.Context, req *pbs.GetOrganizationRequest) (*pbs.GetOrganizationResponse, error) {
+	if err := validateGetRequest(req); err != nil {
+		return nil, err
+	}
+	p, err := s.getFromRepo(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+	return &pbs.GetOrganizationResponse{Item: p}, nil
+}
+
+func (s Service) getFromRepo(ctx context.Context, id string) (*pb.Organization, error) {
+	repo, err := s.repo()
+	if err != nil {
+		return nil, err
+	}
+	p, err := repo.LookupScope(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	if p == nil {
+		return nil, handlers.NotFoundErrorf("Organization %q doesn't exist.", id)
+	}
+	return toProto(p), nil
+}
+
+func toProto(in *iam.Scope) *pb.Organization {
+	out := pb.Organization{
+		Id:          in.GetPublicId(),
+		CreatedTime: in.GetCreateTime().GetTimestamp(),
+		UpdatedTime: in.GetUpdateTime().GetTimestamp(),
+	}
+	if in.GetDescription() != "" {
+		out.Description = &wrapperspb.StringValue{Value: in.GetDescription()}
+	}
+	if in.GetName() != "" {
+		out.Name = &wrapperspb.StringValue{Value: in.GetName()}
+	}
+	return &out
+}
+
+// A validateX method should exist for each method above.  These methods do not make calls to any backing service but enforce
+// requirements on the structure of the request.  They verify that:
+//  * The path passed in is correctly formatted
+//  * All required parameters are set
+//  * There are no conflicting parameters provided
+func validateGetRequest(req *pbs.GetOrganizationRequest) error {
+	badFields := make(map[string]string)
+	if !validId(req.GetId(), iam.OrganizationScope.Prefix()+"_") {
+		badFields["id"] = "Invalid formatted project id."
+	}
+	if len(badFields) > 0 {
+		return handlers.InvalidArgumentErrorf("Improperly formatted identifier.", badFields)
+	}
+	return nil
+}
+
+func validId(id, prefix string) bool {
+	if !strings.HasPrefix(id, prefix) {
+		return false
+	}
+	id = strings.TrimPrefix(id, prefix)
+	return !reInvalidID.Match([]byte(id))
+}
