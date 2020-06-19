1c10c87d1d2b0a9a2fb8baf6c24d137e2038fa12
Michael Gaffney <mike@gaffney.cc>
2020-06-19 10:21:44 -0400

    Add triggers for auth_method and auth_account subtypes

--- internal/db/migrations/postgres.gen.go
+++ internal/db/migrations/postgres.gen.go
@@ -337,6 +337,9 @@ commit;
 		bytes: []byte(`
 begin;
 
+  drop function insert_auth_account_subtype;
+  drop function insert_auth_method_subtype;
+
   drop table auth_account cascade;
   drop table auth_method cascade;
 
@@ -351,14 +354,39 @@ begin;
 
   -- base table for auth methods
   create table auth_method (
-    public_id wt_public_id primary key
+    auth_method_id wt_public_id primary key
   );
 
 
   -- base table for auth accounts
   create table auth_account (
-    public_id wt_public_id primary key
+    auth_account_id wt_public_id primary key
   );
+
+
+  create or replace function
+    insert_auth_method_subtype()
+    returns trigger
+  as $$
+  begin
+    insert into auth_method (auth_method_id)
+    values
+      (new.auth_method_id);
+    return new;
+  end;
+  $$ language plpgsql;
+
+  create or replace function
+    insert_auth_account_subtype()
+    returns trigger
+  as $$
+  begin
+    insert into auth_account (auth_account_id)
+    values
+      (new.auth_account_id);
+    return new;
+  end;
+  $$ language plpgsql;
 
 commit;
 
