1023c907294b947e6d45987b2593597c74adaf61
Michael Gaffney <mgaffney@users.noreply.github.com>
2020-06-21 13:16:03 -0400

    Enforce auth_account to iam_user plus iam_scope constraints (#137)

--- internal/db/migrations/postgres.gen.go
+++ internal/db/migrations/postgres.gen.go
@@ -332,66 +332,6 @@ commit;
 
 `),
 	},
-	"migrations/04_auth.down.sql": {
-		name: "04_auth.down.sql",
-		bytes: []byte(`
-begin;
-
-  drop function insert_auth_account_subtype;
-  drop function insert_auth_method_subtype;
-
-  drop table auth_account cascade;
-  drop table auth_method cascade;
-
-commit;
-
-`),
-	},
-	"migrations/04_auth.up.sql": {
-		name: "04_auth.up.sql",
-		bytes: []byte(`
-begin;
-
-  -- base table for auth methods
-  create table auth_method (
-    auth_method_id wt_public_id primary key
-  );
-
-
-  -- base table for auth accounts
-  create table auth_account (
-    auth_account_id wt_public_id primary key
-  );
-
-
-  create or replace function
-    insert_auth_method_subtype()
-    returns trigger
-  as $$
-  begin
-    insert into auth_method (auth_method_id)
-    values
-      (new.auth_method_id);
-    return new;
-  end;
-  $$ language plpgsql;
-
-  create or replace function
-    insert_auth_account_subtype()
-    returns trigger
-  as $$
-  begin
-    insert into auth_account (auth_account_id)
-    values
-      (new.auth_account_id);
-    return new;
-  end;
-  $$ language plpgsql;
-
-commit;
-
-`),
-	},
 	"migrations/06_iam.down.sql": {
 		name: "06_iam.down.sql",
 		bytes: []byte(`
@@ -568,7 +508,12 @@ create table iam_user (
     description text,
     scope_id wt_public_id not null references iam_scope_organization(scope_id) on delete cascade on update cascade,
     unique(name, scope_id),
-    disabled boolean not null default false
+    disabled boolean not null default false,
+
+    -- The order of columns is important for performance. See:
+    -- https://dba.stackexchange.com/questions/58970/enforcing-constraints-two-tables-away/58972#58972
+    -- https://dba.stackexchange.com/questions/27481/is-a-composite-index-also-good-for-queries-on-the-first-field
+    unique(scope_id, public_id)
   );
 
 create trigger 
@@ -644,6 +589,95 @@ before
 insert on iam_group
   for each row execute procedure default_create_time();
   
+commit;
+
+`),
+	},
+	"migrations/07_auth.down.sql": {
+		name: "07_auth.down.sql",
+		bytes: []byte(`
+begin;
+
+  drop function insert_auth_account_subtype;
+  drop function insert_auth_method_subtype;
+
+  drop table auth_account cascade;
+  drop table auth_method cascade;
+
+commit;
+
+`),
+	},
+	"migrations/07_auth.up.sql": {
+		name: "07_auth.up.sql",
+		bytes: []byte(`
+begin;
+
+  -- Design influenced by:
+  -- https://dba.stackexchange.com/questions/58970/enforcing-constraints-two-tables-away/58972#58972
+  --
+  -- iam_scope ←─────  auth_method
+  --    ↑                  ↑
+  -- iam_user  ←─────  auth_account
+
+  -- base table for auth methods
+  create table auth_method (
+    auth_method_id wt_public_id primary key,
+    iam_scope_id wt_public_id not null
+      references iam_scope(public_id)
+      on delete cascade
+      on update cascade,
+
+    -- The order of columns is important for performance. See:
+    -- https://dba.stackexchange.com/questions/58970/enforcing-constraints-two-tables-away/58972#58972
+    -- https://dba.stackexchange.com/questions/27481/is-a-composite-index-also-good-for-queries-on-the-first-field
+    unique(iam_scope_id, auth_method_id)
+  );
+
+
+  -- base table for auth accounts
+  create table auth_account (
+    auth_account_id wt_public_id primary key,
+    auth_method_id wt_public_id not null,
+    iam_scope_id wt_public_id not null,
+    iam_user_id wt_public_id,
+    foreign key (iam_scope_id, auth_method_id)
+      references auth_method (iam_scope_id, auth_method_id)
+      on delete cascade
+      on update cascade,
+    foreign key (iam_scope_id, iam_user_id)
+      references iam_user (scope_id, public_id)
+      on delete set null
+      on update cascade
+  );
+
+
+  create or replace function
+    insert_auth_method_subtype()
+    returns trigger
+  as $$
+  begin
+    insert into auth_method
+      (auth_method_id, iam_scope_id)
+    values
+      (new.auth_method_id, new.iam_scope_id);
+    return new;
+  end;
+  $$ language plpgsql;
+
+  create or replace function
+    insert_auth_account_subtype()
+    returns trigger
+  as $$
+  begin
+    insert into auth_account
+      (auth_account_id, auth_method_id, iam_scope_id)
+    values
+      (new.auth_account_id, new.auth_method_id, new.iam_scope_id);
+    return new;
+  end;
+  $$ language plpgsql;
+
 commit;
 
 `),
