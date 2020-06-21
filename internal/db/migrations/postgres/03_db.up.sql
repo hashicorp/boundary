40b242eec9e1e8f955ba868c3be00ca9d4d1c579
Jim Lambert <jlambert@hashicorp.com>
2020-06-21 10:11:55 -0400

    add test scooter

--- internal/db/migrations/postgres/03_db.up.sql
+++ internal/db/migrations/postgres/03_db.up.sql
@@ -1,8 +1,9 @@
 begin;
 
--- create test tables used in the unit tests for the internal/db package
--- these tables (db_test_user, db_test_car, db_test_rental) are not part
--- of the watchtower domain model... they are simply used for testing the internal/db package
+-- create test tables used in the unit tests for the internal/db package 
+-- these tables (db_test_user, db_test_car, db_test_rental, db_test_scooter) are
+-- not part of the watchtower domain model... they are simply used for testing
+-- the internal/db package 
 create table if not exists db_test_user (
   id bigint generated always as identity primary key,
   create_time wt_timestamp,
@@ -87,5 +88,33 @@ before
 insert on db_test_rental
   for each row execute procedure default_create_time();
 
+
+create table if not exists db_test_scooter (
+  id bigint generated always as identity primary key,
+  create_time wt_timestamp,
+  update_time wt_timestamp,
+  private_id text not null unique,
+  name text unique,
+  model text,
+  mpg smallint
+);
+
+create trigger 
+  update_time_column 
+before 
+update on db_test_scooter 
+  for each row execute procedure update_time_column();
+
+create trigger 
+  create_time_column
+before
+update on db_test_scooter 
+  for each row execute procedure immutable_create_time_func();
+
+create trigger 
+  default_create_time_column
+before
+insert on db_test_scooter
+  for each row execute procedure default_create_time();
 
 commit;
