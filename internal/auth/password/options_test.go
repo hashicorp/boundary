6080c7e9d12c30f559a02b9c6493aac59cb8deee
Michael Gaffney <mike@gaffney.cc>
2020-06-23 10:30:27 -0400

    Add options

---
+++ internal/auth/password/options_test.go
@@ -0,0 +1,24 @@
+package password
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetOpts(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	t.Run("WithName", func(t *testing.T) {
+		opts := getOpts(WithName("test"))
+		testOpts := getDefaultOptions()
+		testOpts.withName = "test"
+		assert.Equal(opts, testOpts)
+	})
+	t.Run("WithDescription", func(t *testing.T) {
+		opts := getOpts(WithDescription("test desc"))
+		testOpts := getDefaultOptions()
+		testOpts.withDescription = "test desc"
+		assert.Equal(opts, testOpts)
+	})
+}
