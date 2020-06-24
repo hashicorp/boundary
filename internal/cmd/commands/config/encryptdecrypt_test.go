8afcbd4c8ea85a61e6027d75aac0aa7fc221b1f1
Jeff Malnick <malnick@gmail.com>
2020-06-24 13:08:13 -0700

    Add config CLI command and encrypt/decrypt sub-commands (#77)

---
+++ internal/cmd/commands/config/encryptdecrypt_test.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/watchtower/internal/cmd/base"
+	"github.com/mitchellh/cli"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	configEncryptPath = "./fixtures/configEncrypt.hcl"
+	configDecryptPath = "./fixtures/configDecrypt.hcl"
+)
+
+func TestEncryptDecrypt(t *testing.T) {
+
+	cases := []struct {
+		encrypt bool
+		config  string
+		exp     string
+	}{
+		{
+			encrypt: true,
+			config:  configEncryptPath,
+			exp:     "",
+		},
+		{
+			encrypt: false,
+			config:  configDecryptPath,
+			exp:     configEncryptPath,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("Test encrypt %v", c.encrypt), func(t *testing.T) {
+
+			var b bytes.Buffer
+
+			ui := &cli.BasicUi{
+				Reader:      bufio.NewReader(os.Stdin),
+				Writer:      &b,
+				ErrorWriter: &b,
+			}
+
+			cmd := &EncryptDecryptCommand{
+				Command: base.New(ui),
+				Encrypt: c.encrypt}
+
+			if err := cmd.Run([]string{c.config}); err != 0 {
+				assert.Equal(t, err, 0)
+			}
+
+			got := b.String()
+			assert.Greater(t, len(got), 0)
+			fmt.Printf("%s\n", got)
+
+			// If it's encrypting, then we can assume that it did the right thing since
+			// there are many tests on the underlying codebase for that. If it's not
+			// encrypting, compare it to the cleartext to verify because we can.
+			if !c.encrypt {
+				expected, err := ioutil.ReadFile(c.exp)
+				assert.NoError(t, err)
+
+				assert.Equal(t, strings.TrimSpace(string(expected)), strings.TrimSpace(string(got)))
+			}
+		})
+	}
+}
---
+++ internal/cmd/commands/config/fixtures/configDecrypt.hcl
@@ -0,0 +1,17 @@
+kms "aead" {
+  purpose = "config"
+  aead_type = "aes-gcm"
+  key = "c964AJj8VW8w4hKz/Jd8MvuLt0kkcjVuFqMiMvTvvN8="
+}
+
+kms "aead" {
+  purpose = "controller"
+  aead_type = "aes-gcm"
+  key ="{{decrypt(CkgsDUoQ7B5JhE1ye2yVAm3Ss_KNbLQgBhKtibtGSvpGOrwQ3zRTF_qoHPHX-xuqLyZ9zsb3cMo6OpphKOj7AeTn1_iiqZ_VaCsqAA)}}"
+}
+
+kms "aead" {
+  purpose = "worker"
+  aead_type = "aes-gcm"
+  key ="{{decrypt(CkiRTINwX19TnC3AB-zx5E133TXI9KzBWb8TxfVDrPb9m3Yfm9K99OkuJgRTj1rjmeMF-Kpl-0oouEc8_mNk6oPIqD8nUNvH3FYqAA)}}"
+}
