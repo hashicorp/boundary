6281a0ea4c93f0df8f8dcf99c7c4560820bc86f5
Todd Knight <T.Alan.Knight@gmail.com>
2020-06-24 08:30:15 -0700

    fix: Move helper test methods to testing.go and inlined helper methods that weren't needed.

--- internal/authtoken/authtoken_test.go
+++ internal/authtoken/authtoken_test.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"testing"
 
-	"github.com/hashicorp/vault/sdk/helper/base62"
 	"github.com/hashicorp/watchtower/internal/authtoken/store"
 	"github.com/hashicorp/watchtower/internal/db"
 	"github.com/hashicorp/watchtower/internal/iam"
-	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
@@ -211,54 +209,4 @@ func TestAuthToken_Update(t *testing.T) {
 			assert.Equal(tt.cnt, cnt)
 		})
 	}
-}
-
-func testAuthToken(t *testing.T, conn *gorm.DB) *AuthToken {
-	t.Helper()
-	assert := assert.New(t)
-	org, _ := iam.TestScopes(t, conn)
-	u := iam.TestUser(t, conn, org.GetPublicId())
-	amId := setupAuthMethod(t, conn, org.GetPublicId())
-	sess, err := NewAuthToken(org.GetPublicId(), u.GetPublicId(), amId)
-	assert.NoError(err)
-	assert.NotNil(sess)
-	id, err := newAuthTokenId()
-	assert.NoError(err)
-	assert.NotEmpty(id)
-	sess.PublicId = id
-
-	token, err := newAuthToken()
-	assert.NoError(err)
-	assert.NotEmpty(token)
-	sess.Token = token
-
-	w := db.New(conn)
-	err2 := w.Create(context.Background(), sess)
-	assert.NoError(err2)
-	return sess
-}
-
-// Returns auth method id
-// TODO: Remove this when the auth method repos are created with the relevant test methods.
-func setupAuthMethod(t *testing.T, conn *gorm.DB, scope string) string {
-	t.Helper()
-	require := require.New(t)
-	insert := `insert into auth_method
-	(public_id, scope_id)
-	values
-	($1, $2);`
-	amId, err := authMethodId()
-	require.NoError(err)
-	db := conn.DB()
-	_, err = db.Query(insert, amId, scope)
-	require.NoError(err)
-	return amId
-}
-
-func authMethodId() (string, error) {
-	id, err := base62.Random(10)
-	if err != nil {
-		return "", err
-	}
-	return "am_" + id, nil
 }
---
+++ internal/authtoken/testing.go
@@ -0,0 +1,53 @@
+package authtoken
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/watchtower/internal/db"
+	"github.com/hashicorp/watchtower/internal/iam"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testAuthToken(t *testing.T, conn *gorm.DB) *AuthToken {
+	t.Helper()
+	assert := assert.New(t)
+	org, _ := iam.TestScopes(t, conn)
+	u := iam.TestUser(t, conn, org.GetPublicId())
+	amId := setupAuthMethod(t, conn, org.GetPublicId())
+	sess, err := NewAuthToken(org.GetPublicId(), u.GetPublicId(), amId)
+	assert.NoError(err)
+	assert.NotNil(sess)
+	id, err := newAuthTokenId()
+	assert.NoError(err)
+	assert.NotEmpty(id)
+	sess.PublicId = id
+
+	token, err := newAuthToken()
+	assert.NoError(err)
+	assert.NotEmpty(token)
+	sess.Token = token
+
+	w := db.New(conn)
+	err2 := w.Create(context.Background(), sess)
+	assert.NoError(err2)
+	return sess
+}
+
+// Returns auth method id
+// TODO: Remove this when the auth method repos are created with the relevant test methods.
+func setupAuthMethod(t *testing.T, conn *gorm.DB, scope string) string {
+	t.Helper()
+	require := require.New(t)
+	insert := `insert into auth_method
+	(public_id, scope_id)
+	values
+	($1, $2);`
+	amId, err := db.NewPublicId("am")
+	require.NoError(err)
+	_, err = conn.DB().Exec(insert, amId, scope)
+	require.NoError(err)
+	return amId
+}
