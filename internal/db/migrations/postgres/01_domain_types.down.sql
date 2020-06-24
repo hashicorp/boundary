66412447348b965fc567edb4ec5ccbdb91bcc00f
Jim <jlambert@hashicorp.com>
2020-06-24 12:33:49 -0400

    add support for WithVersion option to updates (#126)

--- internal/db/migrations/postgres/01_domain_types.down.sql
+++ internal/db/migrations/postgres/01_domain_types.down.sql
@@ -2,9 +2,11 @@ begin;
 
 drop domain wt_timestamp;
 drop domain wt_public_id;
+drop domain wt_version;
 
 drop function default_create_time;
 drop function immutable_create_time_func;
 drop function update_time_column;
+drop function update_version_column;
 
 commit;
--- internal/db/option_test.go
+++ internal/db/option_test.go
@@ -94,4 +94,16 @@ func Test_getOpts(t *testing.T) {
 		testOpts.WithLimit = 1
 		assert.Equal(opts, testOpts)
 	})
+	t.Run("WithVersion", func(t *testing.T) {
+		assert := assert.New(t)
+		// test default of 0
+		opts := GetOpts()
+		testOpts := getDefaultOptions()
+		testOpts.WithVersion = 0
+		assert.Equal(opts, testOpts)
+		opts = GetOpts(WithVersion(2))
+		testOpts = getDefaultOptions()
+		testOpts.WithVersion = 2
+		assert.Equal(opts, testOpts)
+	})
 }
