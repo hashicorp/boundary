b5645ef3bae5d50eedb0b1a704edbfa3a8a54e3f
Randall Morey <randall@randallmorey.com>
2020-06-29 18:45:15 -0400

    chore: update the default build command and add UI_COMMITISH support for controlling the UI build

--- scripts/uigen.sh
+++ scripts/uigen.sh
@@ -4,12 +4,15 @@ set -e
 
 origdir=$(pwd)
 tempdir=$(mktemp -d update-ui-assets.XXXXXX)
+ui_commitish=${UI_COMMITISH:develop}
 
 cd $tempdir
 git clone https://github.com/hashicorp/watchtower-ui
 cd watchtower-ui
+git fetch origin ${ui_commitish}
+git checkout ${ui_commitish}
 
-docker-compose -f docker-compose-embedding.yml run build_production
+docker-compose -f docker-compose-embedding.yml run build
 
 cd $origdir
 
