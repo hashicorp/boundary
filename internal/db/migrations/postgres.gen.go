e0b194f5ed2ceadf102ca702918eba1b2a87902a
Todd Knight <T.Alan.Knight@gmail.com>
2020-07-02 09:01:06 -0700

    Fixing down sql file.

--- internal/db/migrations/postgres.gen.go
+++ internal/db/migrations/postgres.gen.go
@@ -901,8 +901,8 @@ commit;
 		bytes: []byte(`
 begin;
 
-  drop table auth_token cascade;
   drop view auth_token_view cascade;
+  drop table auth_token cascade;
 
   drop function update_last_access_time_column cascade;
   drop function immutable_auth_token_columns cascade;
