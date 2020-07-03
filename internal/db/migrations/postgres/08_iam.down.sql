e6eb054cd456edaeed285cc4fce62cd08fb9ad75
Jim Lambert <jlambert@hashicorp.com>
2020-07-03 05:16:49 -0400

    more bits migrated to 06 iam schema files

--- internal/db/migrations/postgres/08_iam.down.sql
+++ internal/db/migrations/postgres/08_iam.down.sql
@@ -1,14 +1,8 @@
 BEGIN;
 
 drop table if exists iam_auth_method cascade;
-drop table if exists iam_group_member_type_enm cascade;
-drop table if exists iam_group cascade cascade;
-drop table if exists iam_group_member cascade;
-drop view if exists iam_group_member;
 drop table if exists iam_auth_method_type_enm cascade;
 drop table if exists iam_action_enm cascade;
-drop table if exists iam_role_grant cascade;
-drop view if exists iam_assigned_role;
 
 
 COMMIT;
