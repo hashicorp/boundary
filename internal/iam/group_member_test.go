a157b0479eeaec9beec4aa228762c260b07a5c80
Jim Lambert <jlambert@hashicorp.com>
2020-07-03 05:54:25 -0400

    basic group members

--- internal/iam/group_member_test.go
+++ internal/iam/group_member_test.go
@@ -8,31 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGroup_AddUser(t *testing.T) {
-	t.Parallel()
-	cleanup, conn, _ := db.TestSetup(t, "postgres")
-	defer func() {
-		err := cleanup()
-		assert.NoError(t, err)
-		err = conn.Close()
-		assert.NoError(t, err)
-	}()
-	t.Run("valid", func(t *testing.T) {
-		assert := assert.New(t)
-		w := db.New(conn)
-		org, _ := TestScopes(t, conn)
-		user := TestUser(t, conn, org.PublicId)
-		grp := TestGroup(t, conn, org.PublicId)
-		gm, err := grp.AddUser(user.PublicId)
-		assert.NoError(err)
-		assert.NotNil(gm)
-		assert.Equal(gm.(*GroupMemberUser).GroupId, grp.PublicId)
-		err = w.Create(context.Background(), gm)
-		assert.NoError(err)
-		assert.Equal("user", gm.GetType())
-	})
-}
-
 func Test_NewGroupMember(t *testing.T) {
 	t.Parallel()
 	cleanup, conn, _ := db.TestSetup(t, "postgres")
@@ -49,25 +24,25 @@ func Test_NewGroupMember(t *testing.T) {
 		user := TestUser(t, conn, s.PublicId)
 		grp := TestGroup(t, conn, s.PublicId)
 
-		gm, err := grp.AddUser(user.PublicId)
+		gm, err := NewGroupMember(grp.PublicId, user.PublicId)
 		assert.NoError(err)
 		assert.NotNil(gm)
 		err = w.Create(context.Background(), gm)
 		assert.NoError(err)
 
-		members, err := grp.Members(context.Background(), w)
-		assert.NoError(err)
-		assert.Equal(1, len(members))
-		assert.Equal(members[0].GetMemberId(), user.PublicId)
-		assert.Equal(members[0].GetGroupId(), grp.PublicId)
+		// members, err := grp.Members(context.Background(), w)
+		// assert.NoError(err)
+		// assert.Equal(1, len(members))
+		// assert.Equal(members[0].GetMemberId(), user.PublicId)
+		// assert.Equal(members[0].GetGroupId(), grp.PublicId)
 
-		rowsDeleted, err := w.Delete(context.Background(), gm)
-		assert.NoError(err)
-		assert.Equal(1, rowsDeleted)
+		// rowsDeleted, err := w.Delete(context.Background(), gm)
+		// assert.NoError(err)
+		// assert.Equal(1, rowsDeleted)
 
-		members, err = grp.Members(context.Background(), w)
-		assert.NoError(err)
-		assert.Equal(0, len(members))
+		// members, err = grp.Members(context.Background(), w)
+		// assert.NoError(err)
+		// assert.Equal(0, len(members))
 	})
 	t.Run("bad-type", func(t *testing.T) {
 		assert := assert.New(t)
@@ -76,12 +51,12 @@ func Test_NewGroupMember(t *testing.T) {
 		role := TestRole(t, conn, s.PublicId)
 
 		grp := TestGroup(t, conn, s.PublicId)
-		gm, err := grp.AddUser(role.PublicId)
+		gm, err := NewGroupMember(grp.PublicId, role.PublicId)
 		assert.NoError(err)
 		assert.NotNil(gm)
 		err = w.Create(context.Background(), gm)
 		assert.Error(err)
-		assert.Equal(err.Error(), `create: failed pq: insert or update on table "iam_group_member_user" violates foreign key constraint "iam_group_member_user_member_id_fkey"`)
+		assert.Equal(err.Error(), `create: failed pq: insert or update on table "iam_group_member" violates foreign key constraint "iam_group_member_member_id_fkey"`)
 
 	})
 	t.Run("nil-user", func(t *testing.T) {
@@ -89,9 +64,9 @@ func Test_NewGroupMember(t *testing.T) {
 		s := testOrg(t, conn, "", "")
 		grp := TestGroup(t, conn, s.PublicId)
 
-		gm, err := grp.AddUser("")
+		gm, err := NewGroupMember(grp.PublicId, "")
 		assert.Error(err)
 		assert.Nil(gm)
-		assert.Equal(err.Error(), "error the user public id is unset")
+		assert.Equal(err.Error(), "new group member: missing user id: invalid parameter")
 	})
 }
