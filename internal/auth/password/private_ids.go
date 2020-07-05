fb7731ef7783ca1d0b61f5220e2ab0da3bae8d8b
Michael Gaffney <mike@gaffney.cc>
2020-07-05 12:33:27 -0400

    Add ID prefix for Argon2 Credentials

--- internal/auth/password/private_ids.go
+++ internal/auth/password/private_ids.go
@@ -9,10 +9,19 @@ import (
 // Prefixes for private ids for types in the password package.
 const (
 	argon2ConfigurationPrefix = "arg2conf"
+	argon2CredentialPrefix    = "arg2cred"
 )
 
 func newArgon2ConfigurationId() (string, error) {
 	id, err := db.NewPublicId(argon2ConfigurationPrefix)
+	if err != nil {
+		return "", fmt.Errorf("new password argon2 configuration id: %w", err)
+	}
+	return id, err
+}
+
+func newArgon2CredentialId() (string, error) {
+	id, err := db.NewPublicId(argon2CredentialPrefix)
 	if err != nil {
 		return "", fmt.Errorf("new password argon2 configuration id: %w", err)
 	}
