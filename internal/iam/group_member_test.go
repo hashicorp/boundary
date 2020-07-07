0a70d85fe70cbb407f276cd1f5d6041eb75db58a
Jim Lambert <jlambert@hashicorp.com>
2020-07-07 12:36:38 -0400

    New and Create member tests

--- internal/iam/group_member_test.go
+++ internal/iam/group_member_test.go
@@ -2,10 +2,13 @@ package iam
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/hashicorp/watchtower/internal/db"
+	"github.com/hashicorp/watchtower/internal/iam/store"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_NewGroupMember(t *testing.T) {
@@ -17,56 +20,233 @@ func Test_NewGroupMember(t *testing.T) {
 		err = conn.Close()
 		assert.NoError(t, err)
 	}()
-	t.Run("valid", func(t *testing.T) {
-		assert := assert.New(t)
-		w := db.New(conn)
-		s := testOrg(t, conn, "", "")
-		user := TestUser(t, conn, s.PublicId)
-		grp := TestGroup(t, conn, s.PublicId)
+	org, proj := TestScopes(t, conn)
+	orgGroup := TestGroup(t, conn, org.PublicId)
+	projGroup := TestGroup(t, conn, proj.PublicId)
+	user := TestUser(t, conn, org.PublicId)
 
-		gm, err := NewGroupMember(grp.PublicId, user.PublicId)
-		assert.NoError(err)
-		assert.NotNil(gm)
-		err = w.Create(context.Background(), gm)
-		assert.NoError(err)
+	type args struct {
+		groupId string
+		userId  string
+		opt     []Option
+	}
+	tests := []struct {
+		name      string
+		args      args
+		want      *GroupMember
+		wantErr   bool
+		wantIsErr error
+	}{
+		{
+			name: "valid-org",
+			args: args{
+				groupId: orgGroup.PublicId,
+				userId:  user.PublicId,
+			},
+			want: func() *GroupMember {
+				gm := allocGroupMember()
+				gm.GroupId = orgGroup.PublicId
+				gm.MemberId = user.PublicId
+				return &gm
+			}(),
+		},
+		{
+			name: "valid-proj",
+			args: args{
+				groupId: projGroup.PublicId,
+				userId:  user.PublicId,
+			},
+			want: func() *GroupMember {
+				gm := allocGroupMember()
+				gm.GroupId = projGroup.PublicId
+				gm.MemberId = user.PublicId
+				return &gm
+			}(),
+		},
+		{
+			name: "missing-group",
+			args: args{
+				userId: user.PublicId,
+			},
+			want:      nil,
+			wantErr:   true,
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "missing-user",
+			args: args{
+				groupId: projGroup.PublicId,
+			},
+			want:      nil,
+			wantErr:   true,
+			wantIsErr: db.ErrInvalidParameter,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
 
-		// members, err := grp.Members(context.Background(), w)
-		// assert.NoError(err)
-		// assert.Equal(1, len(members))
-		// assert.Equal(members[0].GetMemberId(), user.PublicId)
-		// assert.Equal(members[0].GetGroupId(), grp.PublicId)
+			got, err := NewGroupMember(tt.args.groupId, tt.args.userId, tt.args.opt...)
+			if tt.wantErr {
+				require.Error(err)
+				assert.True(errors.Is(err, tt.wantIsErr))
+				return
+			}
+			require.NoError(err)
+			assert.Equal(tt.want, got)
+		})
+	}
+}
 
-		// rowsDeleted, err := w.Delete(context.Background(), gm)
-		// assert.NoError(err)
-		// assert.Equal(1, rowsDeleted)
+func Test_GroupMemberCreate(t *testing.T) {
+	t.Parallel()
+	cleanup, conn, _ := db.TestSetup(t, "postgres")
+	defer func() {
+		err := cleanup()
+		assert.NoError(t, err)
+		err = conn.Close()
+		assert.NoError(t, err)
+	}()
+	org, proj := TestScopes(t, conn)
+	type args struct {
+		gm *GroupMember
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantDup    bool
+		wantErr    bool
+		wantErrMsg string
+		wantIsErr  error
+	}{
+		{
+			name: "valid-with-org",
+			args: args{
+				gm: func() *GroupMember {
+					g := TestGroup(t, conn, org.PublicId)
+					u := TestUser(t, conn, org.PublicId)
+					gm, err := NewGroupMember(g.PublicId, u.PublicId)
+					require.NoError(t, err)
+					return gm
+				}(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid-with-proj",
+			args: args{
+				gm: func() *GroupMember {
+					g := TestGroup(t, conn, proj.PublicId)
+					u := TestUser(t, conn, org.PublicId)
+					gm, err := NewGroupMember(g.PublicId, u.PublicId)
+					require.NoError(t, err)
+					return gm
+				}(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "bad-group-id",
+			args: args{
+				gm: func() *GroupMember {
+					id := testId(t)
+					u := TestUser(t, conn, org.PublicId)
+					gm, err := NewGroupMember(id, u.PublicId)
+					require.NoError(t, err)
+					return gm
+				}(),
+			},
+			wantErr:    true,
+			wantErrMsg: `create: failed pq: insert or update on table "iam_group_member" violates foreign key constraint`,
+		},
+		{
+			name: "bad-user-id",
+			args: args{
+				gm: func() *GroupMember {
+					id := testId(t)
+					g := TestGroup(t, conn, proj.PublicId)
+					gm, err := NewGroupMember(g.PublicId, id)
+					require.NoError(t, err)
+					return gm
+				}(),
+			},
+			wantErr:    true,
+			wantErrMsg: `create: failed pq: insert or update on table "iam_group_member" violates foreign key constraint`,
+		},
+		{
+			name: "missing-group-id",
+			args: args{
+				gm: func() *GroupMember {
+					u := TestUser(t, conn, org.PublicId)
+					return &GroupMember{
+						GroupMember: &store.GroupMember{
+							GroupId:  "",
+							MemberId: u.PublicId,
+						},
+					}
+				}(),
+			},
+			wantErr:   true,
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "missing-user-id",
+			args: args{
+				gm: func() *GroupMember {
+					g := TestGroup(t, conn, org.PublicId)
+					return &GroupMember{
+						GroupMember: &store.GroupMember{
+							GroupId:  g.PublicId,
+							MemberId: "",
+						},
+					}
+				}(),
+			},
+			wantErr:   true,
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "dup-at-org",
+			args: args{
+				gm: func() *GroupMember {
+					g := TestGroup(t, conn, org.PublicId)
+					u := TestUser(t, conn, org.PublicId)
+					gm, err := NewGroupMember(g.PublicId, u.PublicId)
+					require.NoError(t, err)
+					return gm
+				}(),
+			},
+			wantDup:    true,
+			wantErr:    true,
+			wantErrMsg: `create: failed pq: duplicate key value violates unique constraint "iam_group_member_pkey"`,
+		},
+	}
 
-		// members, err = grp.Members(context.Background(), w)
-		// assert.NoError(err)
-		// assert.Equal(0, len(members))
-	})
-	t.Run("bad-type", func(t *testing.T) {
-		assert := assert.New(t)
-		w := db.New(conn)
-		s := testOrg(t, conn, "", "")
-		role := TestRole(t, conn, s.PublicId)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			w := db.New(conn)
+			if tt.wantDup {
+				gm := tt.args.gm.Clone().(*GroupMember)
+				err := w.Create(context.Background(), gm)
+				require.NoError(err)
+			}
+			gm := tt.args.gm.Clone().(*GroupMember)
+			err := w.Create(context.Background(), gm)
+			if tt.wantErr {
+				require.Error(err)
+				assert.Contains(err.Error(), tt.wantErrMsg)
+				if tt.wantIsErr != nil {
+					assert.True(errors.Is(err, tt.wantIsErr))
+				}
+				return
+			}
+			assert.NoError(err)
 
-		grp := TestGroup(t, conn, s.PublicId)
-		gm, err := NewGroupMember(grp.PublicId, role.PublicId)
-		assert.NoError(err)
-		assert.NotNil(gm)
-		err = w.Create(context.Background(), gm)
-		assert.Error(err)
-		assert.Equal(err.Error(), `create: failed pq: insert or update on table "iam_group_member" violates foreign key constraint "iam_group_member_member_id_fkey"`)
-
-	})
-	t.Run("nil-user", func(t *testing.T) {
-		assert := assert.New(t)
-		s := testOrg(t, conn, "", "")
-		grp := TestGroup(t, conn, s.PublicId)
-
-		gm, err := NewGroupMember(grp.PublicId, "")
-		assert.Error(err)
-		assert.Nil(gm)
-		assert.Equal(err.Error(), "new group member: missing user id: invalid parameter")
-	})
+			found := allocGroupMember()
+			err = w.LookupWhere(context.Background(), &found, "group_id = ? and member_id = ?", gm.GroupId, gm.MemberId)
+			require.NoError(err)
+			assert.Equal(gm, &found)
+		})
+	}
 }
