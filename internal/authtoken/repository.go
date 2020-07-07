92cf445489e4179b1ad926ef9139fa9da5307fba
Michael Gaffney <mike@gaffney.cc>
2020-07-07 08:31:38 -0400

    SQL changes

--- internal/authtoken/repository.go
+++ internal/authtoken/repository.go
@@ -135,7 +135,7 @@ func (r *Repository) LookupAuthToken(ctx context.Context, id string, opt ...Opti
 	at := allocAuthToken()
 	at.PublicId = id
 	// Aggregate the fields across auth token and auth accounts by using this view instead of issuing 2 db lookups.
-	at.SetTableName("auth_token_view")
+	at.SetTableName("auth_token_account")
 	if err := r.reader.LookupByPublicId(ctx, at); err != nil {
 		if errors.Is(err, db.ErrRecordNotFound) {
 			return nil, nil
--- internal/db/migrations/postgres/11_auth_token.up.sql
+++ internal/db/migrations/postgres/11_auth_token.up.sql
@@ -6,33 +6,41 @@ begin;
     public_id wt_public_id primary key,
     token bytea not null unique,
     auth_account_id wt_public_id not null
-        references auth_account(public_id)
-        on delete cascade
-        on update cascade,
+      references auth_account(public_id)
+      on delete cascade
+      on update cascade,
     create_time wt_timestamp,
     update_time wt_timestamp,
     -- This column is not updated every time this auth token is accessed.
     -- It is updated after X minutes from the last time it was updated on
     -- a per row basis.
-    approximate_last_access_time wt_timestamp check(
+    approximate_last_access_time wt_timestamp
+      check(
         approximate_last_access_time <= expiration_time
-    ),
-    expiration_time wt_timestamp check(
-            create_time <= expiration_time
-    )
+      ),
+    expiration_time wt_timestamp
+      check(
+        create_time <= expiration_time
+      )
   );
 
-  create view auth_token_view as
-  select at.*,
-         aa.scope_id,
-         aa.iam_user_id,
-         aa.auth_method_id
-  from auth_token as at
-      inner join
-      auth_account as aa ON at.auth_account_id = aa.public_id;
+  create view auth_token_account as
+        select at.public_id,
+               at.token,
+               at.auth_account_id,
+               at.create_time,
+               at.update_time,
+               at.approximate_last_access_time,
+               at.expiration_time,
+               aa.scope_id,
+               aa.iam_user_id,
+               aa.auth_method_id
+          from auth_token as at
+    inner join auth_account as aa
+            on at.auth_account_id = aa.public_id;
 
   create or replace function
-    update_last_access_time_column()
+    update_last_access_time()
     returns trigger
   as $$
   begin
@@ -46,7 +54,7 @@ begin;
   $$ language plpgsql;
 
   comment on function
-    update_last_access_time_column()
+    update_last_access_time()
   is
     'function used in before update triggers to properly set last_access_time columns';
 
@@ -59,14 +67,14 @@ begin;
       raise exception 'auth_account_id cannot be set to %', new.auth_account_id;
     end if;
     if new.token is distinct from old.token then
-        raise exception 'token cannot be set to %', new.token;
+      raise exception 'token cannot be set to %', new.token;
     end if;
     return new;
   end;
   $$ language plpgsql;
 
   comment on function
-      immutable_auth_token_columns()
+    immutable_auth_token_columns()
   is
     'function used in before update triggers to make specific columns immutable';
 
@@ -78,7 +86,7 @@ begin;
   as $$
   begin
     if new.expiration_time < new.create_time then
-        new.expiration_time = new.create_time;
+      new.expiration_time = new.create_time;
     end if;
     return new;
   end;
@@ -105,9 +113,9 @@ begin;
     for each row execute procedure update_time_column();
 
   create trigger
-    update_last_access_time_column
+    update_last_access_time
   before update on auth_token
-    for each row execute procedure update_last_access_time_column();
+    for each row execute procedure update_last_access_time();
 
   create trigger
     immutable_create_time
