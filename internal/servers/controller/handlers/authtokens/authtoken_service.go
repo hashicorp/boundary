ef6c4f098577350189fc098263403f9728f4f922
Todd Knight <T.Alan.Knight@gmail.com>
2020-07-07 17:21:06 -0700

    Creating Auth Token Get List and Delete API.

---
+++ internal/servers/controller/handlers/authtokens/authtoken_service.go
@@ -0,0 +1,200 @@
+package authtokens
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/watchtower/internal/authtoken"
+	"github.com/hashicorp/watchtower/internal/db"
+	pb "github.com/hashicorp/watchtower/internal/gen/controller/api/resources/authtokens"
+	pbs "github.com/hashicorp/watchtower/internal/gen/controller/api/services"
+	"github.com/hashicorp/watchtower/internal/servers/controller/handlers"
+	"github.com/hashicorp/watchtower/internal/types/scope"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const orgIdFieldName = "org_id"
+
+var (
+	reInvalidID = regexp.MustCompile("[^A-Za-z0-9]")
+)
+
+// Service handles request as described by the pbs.AuthTokenServiceServer interface.
+type Service struct {
+	repoFn func() (*authtoken.Repository, error)
+}
+
+// NewService returns a user service which handles user related requests to watchtower.
+func NewService(repo func() (*authtoken.Repository, error)) (Service, error) {
+	if repo == nil {
+		return Service{}, fmt.Errorf("nil iam repository provided")
+	}
+	return Service{repoFn: repo}, nil
+}
+
+var _ pbs.AuthTokenServiceServer = Service{}
+
+// ListAuthTokens implements the interface pbs.AuthTokenServiceServer.
+func (s Service) ListAuthTokens(ctx context.Context, req *pbs.ListAuthTokensRequest) (*pbs.ListAuthTokensResponse, error) {
+	if err := validateListRequest(req); err != nil {
+		return nil, err
+	}
+	ul, err := s.listFromRepo(ctx, req.GetOrgId())
+	if err != nil {
+		return nil, err
+	}
+	return &pbs.ListAuthTokensResponse{Items: ul}, nil
+}
+
+// GetAuthTokens implements the interface pbs.AuthTokenServiceServer.
+func (s Service) GetAuthToken(ctx context.Context, req *pbs.GetAuthTokenRequest) (*pbs.GetAuthTokenResponse, error) {
+	auth := handlers.ToTokenMetadata(ctx)
+	_ = auth
+	if err := validateGetRequest(req); err != nil {
+		return nil, err
+	}
+	u, err := s.getFromRepo(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+	return &pbs.GetAuthTokenResponse{Item: u}, nil
+}
+
+// DeleteAuthToken implements the interface pbs.AuthTokenServiceServer.
+func (s Service) DeleteAuthToken(ctx context.Context, req *pbs.DeleteAuthTokenRequest) (*pbs.DeleteAuthTokenResponse, error) {
+	auth := handlers.ToTokenMetadata(ctx)
+	_ = auth
+	if err := validateDeleteRequest(req); err != nil {
+		return nil, err
+	}
+	existed, err := s.deleteFromRepo(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+	return &pbs.DeleteAuthTokenResponse{Existed: existed}, nil
+}
+
+func (s Service) getFromRepo(ctx context.Context, id string) (*pb.AuthToken, error) {
+	repo, err := s.repoFn()
+	if err != nil {
+		return nil, err
+	}
+	u, err := repo.LookupAuthToken(ctx, id)
+	if err != nil {
+		if errors.Is(err, db.ErrRecordNotFound) {
+			return nil, handlers.NotFoundErrorf("AuthToken %q doesn't exist.", id)
+		}
+		return nil, err
+	}
+	if u == nil {
+		return nil, handlers.NotFoundErrorf("AuthToken %q doesn't exist.", id)
+	}
+	return toProto(u), nil
+}
+
+func (s Service) deleteFromRepo(ctx context.Context, id string) (bool, error) {
+	repo, err := s.repoFn()
+	if err != nil {
+		return false, err
+	}
+	rows, err := repo.DeleteAuthToken(ctx, id)
+	if err != nil {
+		if errors.Is(err, db.ErrRecordNotFound) {
+			return false, nil
+		}
+		return false, status.Errorf(codes.Internal, "Unable to delete user: %v.", err)
+	}
+	return rows > 0, nil
+}
+
+func (s Service) listFromRepo(ctx context.Context, orgId string) ([]*pb.AuthToken, error) {
+	repo, err := s.repoFn()
+	_ = repo
+	if err != nil {
+		return nil, err
+	}
+	ul, err := []*authtoken.AuthToken{}, nil //repo.ListAuthTokens(ctx, orgId)
+	if err != nil {
+		return nil, err
+	}
+	var outUl []*pb.AuthToken
+	for _, u := range ul {
+		outUl = append(outUl, toProto(u))
+	}
+	return outUl, nil
+}
+
+func toProto(in *authtoken.AuthToken) *pb.AuthToken {
+	out := pb.AuthToken{
+		Id:                      in.GetPublicId(),
+		CreatedTime:             in.GetCreateTime().GetTimestamp(),
+		UpdatedTime:             in.GetUpdateTime().GetTimestamp(),
+		ApproximateLastUsedTime: in.GetApproximateLastAccessTime().GetTimestamp(),
+		ExpirationTime:          in.GetExpirationTime().GetTimestamp(),
+		UserId:                  in.GetIamUserId(),
+		AuthMethodId:            in.GetAuthMethodId(),
+	}
+	return &out
+}
+
+// A validateX method should exist for each method above.  These methods do not make calls to any backing service but enforce
+// requirements on the structure of the request.  They verify that:
+//  * The path passed in is correctly formatted
+//  * All required parameters are set
+//  * There are no conflicting parameters provided
+func validateGetRequest(req *pbs.GetAuthTokenRequest) error {
+	badFields := validateAncestors(req)
+	if !validId(req.GetId(), authtoken.AuthTokenPrefix+"_") {
+		badFields["id"] = "Invalid formatted user id."
+	}
+	if len(badFields) > 0 {
+		return handlers.InvalidArgumentErrorf("Improperly formatted identifier.", badFields)
+	}
+	return nil
+}
+
+func validateDeleteRequest(req *pbs.DeleteAuthTokenRequest) error {
+	badFields := validateAncestors(req)
+	if !validId(req.GetId(), authtoken.AuthTokenPrefix+"_") {
+		badFields["id"] = "Incorrectly formatted identifier."
+	}
+	if len(badFields) > 0 {
+		return handlers.InvalidArgumentErrorf("Errors in provided fields.", badFields)
+	}
+	return nil
+}
+
+func validateListRequest(req *pbs.ListAuthTokensRequest) error {
+	badFields := validateAncestors(req)
+	if len(badFields) > 0 {
+		return handlers.InvalidArgumentErrorf("Improperly formatted identifier.", badFields)
+	}
+	return nil
+}
+
+func validId(id, prefix string) bool {
+	if !strings.HasPrefix(id, prefix) {
+		return false
+	}
+	id = strings.TrimPrefix(id, prefix)
+	return !reInvalidID.Match([]byte(id))
+}
+
+type ancestorProvider interface {
+	GetOrgId() string
+}
+
+// validateAncestors verifies that the ancestors of this call are properly set and provided.
+func validateAncestors(r ancestorProvider) map[string]string {
+	if r.GetOrgId() == "" {
+		return map[string]string{orgIdFieldName: "Missing organization id."}
+	}
+	if !validId(r.GetOrgId(), scope.Organization.Prefix()+"_") {
+		return map[string]string{orgIdFieldName: "Improperly formatted identifier."}
+	}
+	return map[string]string{}
+}
