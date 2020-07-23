c19a59a36ac68d9f44c1d1ddae8df2032c9c51f5
Michael Gaffney <mike@gaffney.cc>
2020-07-23 09:33:46 -0400

    Add AuthMethod and Account resources

---
+++ internal/auth/password/account.go
@@ -0,0 +1,79 @@
+package password
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/watchtower/internal/auth/password/store"
+	"github.com/hashicorp/watchtower/internal/db"
+	"github.com/hashicorp/watchtower/internal/oplog"
+	"google.golang.org/protobuf/proto"
+)
+
+// An Account contains a user name. It is owned by an auth method.
+type Account struct {
+	*store.Account
+	tableName string
+
+	// CredentialID is included when Authenticate or ChangePassword is
+	// called. A new CredentialID is generated when a password is changed.
+	CredentialID string `gorm:"-"`
+}
+
+// NewAccount creates a new in memory Account with userName assigned to
+// authMethodId. Name and description are the only valid options. All other
+// options are ignored.
+func NewAccount(authMethodId string, userName string, opt ...Option) (*Account, error) {
+	// NOTE(mgaffney): The scopeId in the embedded *store.Account is
+	// populated by a trigger in the database.
+	if authMethodId == "" {
+		return nil, fmt.Errorf("new: password account: no auth method id: %w", db.ErrInvalidParameter)
+	}
+	if userName == "" {
+		return nil, fmt.Errorf("new: password account: no user name: %w", db.ErrInvalidParameter)
+	}
+
+	opts := getOpts(opt...)
+	a := &Account{
+		Account: &store.Account{
+			AuthMethodId: authMethodId,
+			UserName:     userName,
+			Name:         opts.withName,
+			Description:  opts.withDescription,
+		},
+	}
+	return a, nil
+}
+
+func (a *Account) clone() *Account {
+	cp := proto.Clone(a.Account)
+	return &Account{
+		Account: cp.(*store.Account),
+	}
+}
+
+// TableName returns the table name.
+func (a *Account) TableName() string {
+	if a.tableName != "" {
+		return a.tableName
+	}
+	return "auth_password_account"
+}
+
+// SetTableName sets the table name.
+func (a *Account) SetTableName(n string) {
+	if n != "" {
+		a.tableName = n
+	}
+}
+
+func (a *Account) oplog(op oplog.OpType) oplog.Metadata {
+	metadata := oplog.Metadata{
+		"resource-public-id": []string{a.GetPublicId()},
+		"resource-type":      []string{"password account"},
+		"op-type":            []string{op.String()},
+	}
+	if a.AuthMethodId != "" {
+		metadata["auth-method-id"] = []string{a.AuthMethodId}
+	}
+	return metadata
+}
---
+++ internal/auth/password/authmethod.go
@@ -0,0 +1,72 @@
+package password
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/watchtower/internal/auth/password/store"
+	"github.com/hashicorp/watchtower/internal/db"
+	"github.com/hashicorp/watchtower/internal/oplog"
+	"google.golang.org/protobuf/proto"
+)
+
+// A AuthMethod contains accounts and password configurations. It is owned
+// by a scope.
+type AuthMethod struct {
+	*store.AuthMethod
+	tableName string
+}
+
+// NewAuthMethod creates a new in memory AuthMethod assigned to scopeId.
+// Name and description are the only valid options. All other options are
+// ignored.
+func NewAuthMethod(scopeId string, opt ...Option) (*AuthMethod, error) {
+	if scopeId == "" {
+		return nil, fmt.Errorf("new: password auth method: no scope id: %w", db.ErrInvalidParameter)
+	}
+
+	opts := getOpts(opt...)
+	a := &AuthMethod{
+		AuthMethod: &store.AuthMethod{
+			ScopeId:           scopeId,
+			Name:              opts.withName,
+			Description:       opts.withDescription,
+			MinUserNameLength: 5,
+			MinPasswordLength: 8,
+		},
+	}
+	return a, nil
+}
+
+func (a *AuthMethod) clone() *AuthMethod {
+	cp := proto.Clone(a.AuthMethod)
+	return &AuthMethod{
+		AuthMethod: cp.(*store.AuthMethod),
+	}
+}
+
+// TableName returns the table name.
+func (a *AuthMethod) TableName() string {
+	if a.tableName != "" {
+		return a.tableName
+	}
+	return "auth_password_method"
+}
+
+// SetTableName sets the table name.
+func (a *AuthMethod) SetTableName(n string) {
+	if n != "" {
+		a.tableName = n
+	}
+}
+
+func (a *AuthMethod) oplog(op oplog.OpType) oplog.Metadata {
+	metadata := oplog.Metadata{
+		"resource-public-id": []string{a.GetPublicId()},
+		"resource-type":      []string{"password auth method"},
+		"op-type":            []string{op.String()},
+	}
+	if a.ScopeId != "" {
+		metadata["scope-id"] = []string{a.ScopeId}
+	}
+	return metadata
+}
