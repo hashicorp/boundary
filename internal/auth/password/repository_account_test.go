7897c1cf524abab6f3b6a18be7c72b6cf37f0b4a
Michael Gaffney <mgaffney@users.noreply.github.com>
2020-07-24 10:23:35 -0400

    Add base resources for password authentication (#213)
    * Add base SQL and protobuf files
    
    * Add options and public Ids
    
    * Add AuthMethod and Account resources
    
    * Add repository
    
    * Add CreateAuthMethod to repository
    
    * Add CreateAccount method to repository
    
    * Remove redundant foreign keys
    
    * Document relationships between iam and auth tables
    
    * Document relationships between auth and auth_password tables
    
    * Add documentation for subtype triggers
    
    * Use wt_scope_id domain type for scope_id
    
    * Refactor: move test helper function
    
    * Remove check from SetTableName
    
    * Remove check from another SetTableName
    
    * Use org scope in tests
    
    * Remove handling of CreateTime and UpdateTime from docs

---
+++ internal/auth/password/repository_account_test.go
@@ -0,0 +1,257 @@
+package password
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/hashicorp/watchtower/internal/auth/password/store"
+	"github.com/hashicorp/watchtower/internal/db"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCheckUserName(t *testing.T) {
+	var tests = []struct {
+		in   string
+		want bool
+	}{
+		{"", false},
+		{" leading-spaces", false},
+		{"trailing-spaces ", false},
+		{"contains spaces", false},
+		{"NotLowerCase", false},
+		{"valid.username", true},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.in, func(t *testing.T) {
+			assert.Equal(t, tt.want, validUserName(tt.in))
+		})
+	}
+}
+
+func TestRepository_CreateAccount(t *testing.T) {
+	conn, _ := db.TestSetup(t, "postgres")
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+
+	authMethods := testAuthMethods(t, conn, 1)
+	authMethod := authMethods[0]
+
+	var tests = []struct {
+		name      string
+		in        *Account
+		opts      []Option
+		want      *Account
+		wantIsErr error
+	}{
+		{
+			name:      "nil-Account",
+			wantIsErr: db.ErrNilParameter,
+		},
+		{
+			name:      "nil-embedded-Account",
+			in:        &Account{},
+			wantIsErr: db.ErrNilParameter,
+		},
+		{
+			name: "invalid-no-scope-id",
+			in: &Account{
+				Account: &store.Account{},
+			},
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "invalid-public-id-set",
+			in: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					PublicId:     "sthc_OOOOOOOOOO",
+				},
+			},
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "invalid-username-uppercase",
+			in: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					UserName:     "KaZmiErcZak11",
+				},
+			},
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "invalid-username-leading-space",
+			in: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					UserName:     " kazmierczak12",
+				},
+			},
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "invalid-username-trailing-space",
+			in: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					UserName:     "kazmierczak13 ",
+				},
+			},
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "invalid-username-space-in-name",
+			in: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					UserName:     "kazmier czak14",
+				},
+			},
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "valid-no-options",
+			in: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					UserName:     "kazmierczak",
+				},
+			},
+			want: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					UserName:     "kazmierczak",
+				},
+			},
+		},
+		{
+			name: "valid-with-name",
+			in: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					Name:         "test-name-repo",
+					UserName:     "kazmierczak1",
+				},
+			},
+			want: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					Name:         "test-name-repo",
+					UserName:     "kazmierczak1",
+				},
+			},
+		},
+		{
+			name: "valid-with-description",
+			in: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					Description:  ("test-description-repo"),
+					UserName:     "kazmierczak2",
+				},
+			},
+			want: &Account{
+				Account: &store.Account{
+					AuthMethodId: authMethod.PublicId,
+					Description:  ("test-description-repo"),
+					UserName:     "kazmierczak2",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			repo, err := NewRepository(rw, rw, wrapper)
+			assert.NoError(err)
+			require.NotNil(repo)
+			got, err := repo.CreateAccount(context.Background(), tt.in, tt.opts...)
+			if tt.wantIsErr != nil {
+				assert.Truef(errors.Is(err, tt.wantIsErr), "want err: %q got: %q", tt.wantIsErr, err)
+				assert.Nil(got)
+				return
+			}
+			require.NoError(err)
+			assert.Empty(tt.in.PublicId)
+			require.NotNil(got)
+			assertPublicId(t, AccountPrefix, got.PublicId)
+			assert.NotSame(tt.in, got)
+			assert.Equal(tt.want.Name, got.Name)
+			assert.Equal(tt.want.Description, got.Description)
+			assert.Equal(got.CreateTime, got.UpdateTime)
+		})
+	}
+
+	t.Run("invalid-duplicate-names", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+		assert.NoError(err)
+		require.NotNil(repo)
+
+		authMethods := testAuthMethods(t, conn, 1)
+		authMethod := authMethods[0]
+
+		in := &Account{
+			Account: &store.Account{
+				AuthMethodId: authMethod.GetPublicId(),
+				Name:         "test-name-repo",
+				UserName:     "kazmierczak3",
+			},
+		}
+
+		got, err := repo.CreateAccount(context.Background(), in)
+		require.NoError(err)
+		require.NotNil(got)
+		assertPublicId(t, AccountPrefix, got.PublicId)
+		assert.NotSame(in, got)
+		assert.Equal(in.Name, got.Name)
+		assert.Equal(in.Description, got.Description)
+		assert.Equal(got.CreateTime, got.UpdateTime)
+
+		got2, err := repo.CreateAccount(context.Background(), in)
+		assert.Truef(errors.Is(err, db.ErrNotUnique), "want err: %v got: %v", db.ErrNotUnique, err)
+		assert.Nil(got2)
+	})
+
+	t.Run("valid-duplicate-names-diff-parents", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+		assert.NoError(err)
+		require.NotNil(repo)
+
+		authMethods := testAuthMethods(t, conn, 2)
+		authMethoda, authMethodb := authMethods[0], authMethods[1]
+		in := &Account{
+			Account: &store.Account{
+				Name:     "test-name-repo",
+				UserName: "kazmierczak4",
+			},
+		}
+		in2 := in.clone()
+
+		in.AuthMethodId = authMethoda.GetPublicId()
+		got, err := repo.CreateAccount(context.Background(), in)
+		require.NoError(err)
+		require.NotNil(got)
+		assertPublicId(t, AccountPrefix, got.PublicId)
+		assert.NotSame(in, got)
+		assert.Equal(in.Name, got.Name)
+		assert.Equal(in.Description, got.Description)
+		assert.Equal(got.CreateTime, got.UpdateTime)
+
+		in2.AuthMethodId = authMethodb.GetPublicId()
+		got2, err := repo.CreateAccount(context.Background(), in2)
+		assert.NoError(err)
+		require.NotNil(got2)
+		assertPublicId(t, AccountPrefix, got2.PublicId)
+		assert.NotSame(in2, got2)
+		assert.Equal(in2.Name, got2.Name)
+		assert.Equal(in2.Description, got2.Description)
+		assert.Equal(got2.CreateTime, got2.UpdateTime)
+	})
+}
---
+++ internal/db/migrations/postgres/12_auth_password.up.sql
@@ -0,0 +1,223 @@
+begin;
+
+/*
+
+       ┌────────────────┐                 ┌──────────────────────┐             ┌────────────────────────────┐
+       │  auth_method   │                 │ auth_password_method │             │     auth_password_conf     │
+       ├────────────────┤                 ├──────────────────────┤             ├────────────────────────────┤
+       │ public_id (pk) │                 │ public_id (pk,fk)    │            ╱│ public_id          (pk,fk) │
+       │ scope_id  (fk) │┼┼─────────────○┼│ scope_id  (fk)       │┼┼─────────○─│ password_method_id (fk)    │
+       │                │                 │ ...                  │            ╲│                            │
+       └────────────────┘                 └──────────────────────┘             └────────────────────────────┘
+                ┼                                     ┼                                       ┼
+                ┼                                     ┼                                       ┼
+                │                                     │                                       │
+                │ ▲fk1                                │ ▲fk1                                  │ ▲fk1
+                │                                     │                                       │
+                ○                                     ○                                       ○
+               ╱│╲                                   ╱│╲                                     ╱│╲
+  ┌──────────────────────────┐          ┌──────────────────────────┐          ┌───────────────────────────────┐
+  │       auth_account       │          │  auth_password_account   │          │   auth_password_credential    │
+  ├──────────────────────────┤          ├──────────────────────────┤          ├───────────────────────────────┤
+  │ public_id      (pk)      │          │ public_id      (pk,fk2)  │          │ public_id           (pk)      │
+  │ scope_id       (fk1,fk2) │   ◀fk2   │ scope_id       (fk1,fk2) │   ◀fk2   │ password_method_id  (fk1,fk2) │
+  │ auth_method_id (fk1)     │┼┼──────○┼│ auth_method_id (fk1,fk2) │┼┼──────○┼│ password_conf_id    (fk1)     │
+  │ iam_user_id    (fk2)     │          │ ...                      │          │ password_account_id (fk2)     │
+  └──────────────────────────┘          └──────────────────────────┘          └───────────────────────────────┘
+
+  An auth_method is "abstract". An auth_password_method is a concrete
+  auth_method. For every row in auth_password_method there is one row in
+  auth_method with the same public_id and scope_id.
+
+  Similarly, an auth_account is "abstract". An auth_password_account is a concrete
+  auth_account. For every row in auth_password_account there is one row in
+  auth_account with the same public_id, scope_id, and auth_method_id.
+
+  Both auth_password_conf and auth_password_credential are abstract. Each
+  password key derivation function will require a concrete auth_password_conf
+  and auth_password_credential table.
+
+  An auth_method can have 0 or 1 auth_password_method.
+  An auth_account can have 0 or 1 auth_password_account.
+
+  An auth_password_method belongs to 1 auth_method.
+  An auth_password_method can have 0 to many auth_password_accounts.
+  An auth_password_method can have 0 to many auth_password_confs.
+
+  An auth_password_account belongs to 1 auth_account.
+  An auth_password_account belongs to 1 auth_password_method.
+  An auth_password_account can have 0 or 1 auth_password_credential.
+
+  An auth_password_conf belongs to 1 auth_password_method.
+  An auth_password_conf can have 0 to many auth_password_credentials.
+
+  An auth_password_credential belongs to 1 auth_password_account.
+  An auth_password_credential belongs to 1 auth_password_conf.
+
+*/
+
+  create table auth_password_method (
+    public_id wt_public_id primary key,
+    scope_id wt_scope_id not null,
+    name text,
+    description text,
+    create_time wt_timestamp,
+    update_time wt_timestamp,
+    min_user_name_length int not null default 5,
+    min_password_length int not null default 8,
+    foreign key (scope_id, public_id)
+      references auth_method (scope_id, public_id)
+      on delete cascade
+      on update cascade,
+    unique(scope_id, name),
+    unique(scope_id, public_id)
+  );
+
+  create trigger
+    insert_auth_method_subtype
+  before insert on auth_password_method
+    for each row execute procedure insert_auth_method_subtype();
+
+  create table auth_password_account (
+    public_id wt_public_id primary key,
+    auth_method_id wt_public_id not null,
+    -- NOTE(mgaffney): The scope_id type is not wt_scope_id because the domain
+    -- check is executed before the insert trigger which retrieves the scope_id
+    -- causing an insert to fail.
+    scope_id text not null,
+    name text,
+    description text,
+    create_time wt_timestamp,
+    update_time wt_timestamp,
+    user_name text not null
+      check(
+        lower(trim(user_name)) = user_name
+        and
+        length(user_name) > 0
+      ),
+    foreign key (scope_id, auth_method_id)
+      references auth_password_method (scope_id, public_id)
+      on delete cascade
+      on update cascade,
+    foreign key (scope_id, auth_method_id, public_id)
+      references auth_account (scope_id, auth_method_id, public_id)
+      on delete cascade
+      on update cascade,
+    unique(auth_method_id, name),
+    unique(auth_method_id, user_name),
+    unique(auth_method_id, public_id)
+  );
+
+  create trigger
+    insert_auth_account_subtype
+  before insert on auth_password_account
+    for each row execute procedure insert_auth_account_subtype();
+
+  create table auth_password_conf (
+    public_id wt_public_id primary key,
+    password_method_id wt_public_id not null
+      references auth_password_method (public_id)
+      on delete cascade
+      on update cascade
+      deferrable initially deferred,
+    unique(password_method_id, public_id)
+  );
+
+  -- insert_auth_password_conf_subtype() is a trigger function for concrete
+  -- implementations of auth_password_conf
+  create or replace function
+    insert_auth_password_conf_subtype()
+    returns trigger
+  as $$
+  begin
+    insert into auth_password_conf
+      (public_id, password_method_id)
+    values
+      (new.public_id, new.password_method_id);
+    return new;
+  end;
+  $$ language plpgsql;
+
+  create table auth_password_credential (
+    public_id wt_public_id primary key,
+    password_account_id wt_public_id not null unique,
+    password_conf_id wt_public_id not null,
+    password_method_id wt_public_id not null,
+    foreign key (password_method_id, password_conf_id)
+      references auth_password_conf (password_method_id, public_id)
+      on delete cascade
+      on update cascade,
+    foreign key (password_method_id, password_account_id)
+      references auth_password_account (auth_method_id, public_id)
+      on delete cascade
+      on update cascade,
+    unique(password_method_id, password_conf_id, password_account_id)
+  );
+
+  -- insert_auth_password_credential_subtype() is a trigger function for concrete
+  -- implementations of auth_password_credential
+  create or replace function
+    insert_auth_password_credential_subtype()
+    returns trigger
+  as $$
+  begin
+
+    select auth_password_account.auth_method_id
+      into new.password_method_id
+    from auth_password_account
+    where auth_password_account.public_id = new.password_account_id;
+
+    insert into auth_password_credential
+      (public_id, password_account_id, password_conf_id, password_method_id)
+    values
+      (new.public_id, new.password_account_id, new.password_conf_id, new.password_method_id);
+    return new;
+  end;
+  $$ language plpgsql;
+
+  --
+  -- triggers for time columns
+  ---
+
+  create trigger
+    update_time_column
+  before
+  update on auth_password_method
+    for each row execute procedure update_time_column();
+
+  create trigger
+    immutable_create_time
+  before
+  update on auth_password_method
+    for each row execute procedure immutable_create_time_func();
+
+  create trigger
+    default_create_time_column
+  before
+  insert on auth_password_method
+    for each row execute procedure default_create_time();
+
+  create trigger
+    update_time_column
+  before
+  update on auth_password_account
+    for each row execute procedure update_time_column();
+
+  create trigger
+    immutable_create_time
+  before
+  update on auth_password_account
+    for each row execute procedure immutable_create_time_func();
+
+  create trigger
+    default_create_time_column
+  before
+  insert on auth_password_account
+    for each row execute procedure default_create_time();
+
+  insert into oplog_ticket (name, version)
+  values
+    ('auth_password_method', 1),
+    ('auth_password_account', 1);
+
+commit;
