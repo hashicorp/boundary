bef6c7ffddfe12dc275f0daf6ca677af3635e8f4
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2020-07-28 12:11:42 -0400

    Migrate jobs stuff to servers

--- internal/db/migrations/postgres/08_servers.sql
+++ internal/db/migrations/postgres/08_servers.sql
@@ -1,7 +1,11 @@
 begin;
 
+-- For now at least the IDs will be the same as the name, because this allows us
+-- to not have to persist some generated ID to worker and controller nodes.
+-- Eventually we may want them to diverge, so we have both here for now.
+
 create table workers (
-    id text primary key,
+    private_id text primary key,
     name text unique,
     description text,
     first_seen_time wt_timestamp,
@@ -9,7 +13,7 @@ create table workers (
   );
 
 create table controllers (
-    id text primary key,
+    private_id text primary key,
     name text unique,
     description text,
     first_seen_time wt_timestamp,
