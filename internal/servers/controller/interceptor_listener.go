9dc1c8f6a35758706eaeabc8abf39656e2abd063
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2020-07-30 17:57:02 -0400

    Add some docs to the intercepting listener

--- internal/servers/controller/interceptor_listener.go
+++ internal/servers/controller/interceptor_listener.go
@@ -6,6 +6,26 @@ import (
 	"net"
 )
 
+// interceptingListener allows us to validate the nonce from a connection before
+// handing it off to the gRPC server. It is expected that the first thing a
+// connection sends after successful TLS validation is the nonce that was
+// contained in the KMS-encrypted TLS info. The reason is for replay attacks --
+// since the ALPN NextProtos are sent in the clear, we don't want anyone
+// sniffing the connection to simply replay the hello message and gain access.
+// By requiring the information encrypted within that message to match the first
+// bytes sent in the connection itself, we require that the service making the
+// incoming connection had to either be the service that did the initial
+// encryption, or had to be able to decrypt that against the same KMS key. This
+// means that KMS access is a requirement, and simple replay itself is not
+// sufficient.
+//
+// Note that this is semi-weak against a scenario where the value is decrypted
+// later since a controller restart would clear the cache. We could store a list
+// of seen nonces in the database, but since the original certificate was only
+// good for 3 minutes and 30 seconds anyways, the decryption would need to
+// happen within a short time window instead of much later. We can adjust this
+// window if we want (or even make it tunable), or store values in the DB as
+// well until the certificate expiration.
 type interceptingListener struct {
 	baseLn net.Listener
 	c      *Controller
