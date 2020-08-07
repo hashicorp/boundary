f84991c0f14b6fefd9dd32132bfc9940e399b0f7
Todd Knight <T.Alan.Knight@gmail.com>
2020-08-07 10:18:40 -0700

    Use authenticate and Auth Method repo functions (#242)

--- internal/servers/controller/handler_test.go
+++ internal/servers/controller/handler_test.go
@@ -13,11 +13,16 @@ import (
 )
 
 func TestAuthenticationHandler(t *testing.T) {
-	c := NewTestController(t, &TestControllerOpts{DefaultOrgId: "o_1234567890"})
+	c := NewTestController(t, &TestControllerOpts{
+		DefaultOrgId:                 "o_1234567890",
+		DisableAuthorizationFailures: true,
+		DefaultUsername:              "admin",
+		DefaultPassword:              "password123",
+	})
 	defer c.Shutdown()
 
-	resp, err := http.Post(fmt.Sprintf("%s/v1/scopes/o_1234567890/auth-methods/am_1234567890:authenticate", c.ApiAddrs()[0]), "application/json",
-		strings.NewReader(`{"token_type": null, "credentials": {"name":"test", "password": "test"}}`))
+	resp, err := http.Post(fmt.Sprintf("%s/v1/scopes/o_1234567890/auth-methods/paum_1234567890:authenticate", c.ApiAddrs()[0]), "application/json",
+		strings.NewReader(`{"token_type": null, "credentials": {"name":"admin", "password": "password123"}}`))
 	require.NoError(t, err)
 	assert.Equal(t, http.StatusOK, resp.StatusCode, "Got response: %v", resp)
 
