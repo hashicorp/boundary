8130f311ba1071cb9b5923b98fa8f3c7f1b54347
Jim Lambert <jlambert@hashicorp.com>
2020-08-10 16:47:38 -0400

    get all the tables dropped

--- internal/db/migrations/postgres/20_keys.down.sql
+++ internal/db/migrations/postgres/20_keys.down.sql
@@ -2,9 +2,13 @@ begin;
 
 drop table kms_key_entry_purpose_enm cascade;
 drop table kms_external_config cascade;
-drop table kms_root cascade;
 drop table kms_root_key cascade;
-drop table kms_database cascade;
+drop table kms_root_key_version cascade;
 drop table kms_database_key cascade;
+drop table kms_database_key_version cascade;
+drop table kms_oplog_key cascade;
+drop table kms_oplog_key_version cascade;
+drop table kms_session_key cascade;
+drop table kms_session_key_version cascade;
 
 commit;
