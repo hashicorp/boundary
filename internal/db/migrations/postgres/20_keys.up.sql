a40edb6a75a1a88b86d17f4e3eb03e0f20ec9150
Jim Lambert <jlambert@hashicorp.com>
2020-08-10 16:45:46 -0400

    looks like a decent initial version with all tables defined.

--- internal/db/migrations/postgres/20_keys.up.sql
+++ internal/db/migrations/postgres/20_keys.up.sql
@@ -88,14 +88,14 @@ insert on kms_root_key
 
 create table kms_root_key_version (
   private_id wt_private_id primary key,
-  root_id  wt_private_id not null 
+  root_key_id  wt_private_id not null 
     references kms_root_key(private_id) 
     on delete cascade 
     on update cascade,
   version wt_version not null default 1,
   key bytea not null,
   create_time wt_timestamp,
-  unique(root_id, version)
+  unique(root_key_id, version)
 );
 
  -- define the immutable fields for kms_root_key_version (all of them)
@@ -103,7 +103,7 @@ create trigger
   immutable_columns
 before
 update on kms_root_key_version
-  for each row execute procedure immutable_columns('private_id', 'root_id', 'version', 'key', 'create_time');
+  for each row execute procedure immutable_columns('private_id', 'root_key_id', 'version', 'key', 'create_time');
 
 create trigger 
   default_create_time_column
@@ -113,7 +113,7 @@ insert on kms_root_key_version
 
 create table kms_database_key (
   private_id wt_private_id primary key,
-  root_id wt_private_id
+  root_key_id wt_private_id
     references kms_root_key(private_id)
     on delete cascade
     on update cascade,
@@ -125,7 +125,7 @@ create trigger
   immutable_columns
 before
 update on kms_database_key
-  for each row execute procedure immutable_columns('private_id', 'root_id', 'create_time');
+  for each row execute procedure immutable_columns('private_id', 'root_key_id', 'create_time');
 
 create trigger 
   default_create_time_column
@@ -139,8 +139,8 @@ create table kms_database_key_version (
     references kms_database_key(private_id) 
     on delete cascade 
     on update cascade, 
-  root_key_id wt_private_id 
-    references kms_root_key(private_id) 
+  root_key_version_id wt_private_id 
+    references kms_root_key_version(private_id) 
     on delete cascade 
     on update cascade,
   version wt_version not null default 1,
@@ -149,13 +149,12 @@ create table kms_database_key_version (
   unique(database_id, version)
 );
 
-
  -- define the immutable fields for kms_database_key_version (all of them)
 create trigger 
   immutable_columns
 before
 update on kms_database_key_version
-  for each row execute procedure immutable_columns('private_id', 'database_id', 'root_key_id', 'version', 'key', 'create_time');
+  for each row execute procedure immutable_columns('private_id', 'database_id', 'root_key_version_id', 'version', 'key', 'create_time');
   
 create trigger 
   default_create_time_column
@@ -166,7 +165,7 @@ insert on kms_database_key_version
 
 create table kms_oplog_key (
   private_id wt_private_id primary key,
-  root_id wt_private_id
+  root_key_id wt_private_id
     references kms_root_key(private_id)
     on delete cascade
     on update cascade,
@@ -178,7 +177,7 @@ create trigger
   immutable_columns
 before
 update on kms_oplog_key
-  for each row execute procedure immutable_columns('private_id', 'root_id', 'create_time');
+  for each row execute procedure immutable_columns('private_id', 'root_key_id', 'create_time');
 
 create trigger 
   default_create_time_column
@@ -188,32 +187,83 @@ insert on kms_oplog_key
 
 create table kms_oplog_key_version (
   private_id wt_private_id primary key,
-  oplog_id wt_private_id 
+  oplog_key_id wt_private_id 
     references kms_oplog_key(private_id) 
     on delete cascade 
     on update cascade, 
-  root_key_id wt_private_id 
-    references kms_root_key(private_id) 
+  root_key_version_id wt_private_id 
+    references kms_root_key_version(private_id) 
     on delete cascade 
     on update cascade,
   version wt_version not null default 1,
   key bytea not null,
   create_time wt_timestamp,
-  unique(oplog_id, version)
+  unique(oplog_key_id, version)
 );
-
 
  -- define the immutable fields for kms_oplog_key_version (all of them)
 create trigger 
   immutable_columns
 before
 update on kms_oplog_key_version
-  for each row execute procedure immutable_columns('private_id', 'oplog_id', 'root_key_id', 'version', 'key', 'create_time');
+  for each row execute procedure immutable_columns('private_id', 'oplog_key_id', 'root_key_version_id', 'version', 'key', 'create_time');
   
 create trigger 
   default_create_time_column
 before
 insert on kms_oplog_key_version
+  for each row execute procedure default_create_time();
+
+create table kms_session_key (
+  private_id wt_private_id primary key,
+  root_key_id wt_private_id
+    references kms_root_key(private_id)
+    on delete cascade
+    on update cascade,
+  create_time wt_timestamp
+);
+
+ -- define the immutable fields for kms_oplog_key (all of them)
+create trigger 
+  immutable_columns
+before
+update on kms_session_key
+  for each row execute procedure immutable_columns('private_id', 'root_key_id', 'create_time');
+
+create trigger 
+  default_create_time_column
+before
+insert on kms_session_key
+  for each row execute procedure default_create_time();
+
+create table kms_session_key_version (
+  private_id wt_private_id primary key,
+  session_key_id wt_private_id 
+    references kms_session_key(private_id) 
+    on delete cascade 
+    on update cascade, 
+  root_key_version_id wt_private_id 
+    references kms_root_key_version(private_id) 
+    on delete cascade 
+    on update cascade,
+  version wt_version not null default 1,
+  key bytea not null,
+  create_time wt_timestamp,
+  unique(session_key_id, version)
+);
+
+
+ -- define the immutable fields for kms_session_key_version (all of them)
+create trigger 
+  immutable_columns
+before
+update on kms_session_key_version
+  for each row execute procedure immutable_columns('private_id', 'session_key_id', 'root_key_version_id', 'version', 'key', 'create_time');
+  
+create trigger 
+  default_create_time_column
+before
+insert on kms_session_key_version
   for each row execute procedure default_create_time();
 
 commit;
