9e331ba25c7f7f6d4e4da1c6123856d2f50b781b
Jim Lambert <jlambert@hashicorp.com>
2020-08-12 10:10:46 -0400

    add WithLimit option

--- internal/kms/options.go
+++ internal/kms/options.go
@@ -14,8 +14,20 @@ type Option func(*options)
 
 // options = how options are represented
 type options struct {
+	withLimit int
 }
 
 func getDefaultOptions() options {
-	return options{}
+	return options{
+		withLimit: 0,
+	}
+}
+
+// WithLimit provides an option to provide a limit.  Intentionally allowing
+// negative integers.   If WithLimit < 0, then unlimited results are returned.
+// If WithLimit == 0, then default limits are used for results.
+func WithLimit(limit int) Option {
+	return func(o *options) {
+		o.withLimit = limit
+	}
 }
