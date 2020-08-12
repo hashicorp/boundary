923b7a8c4172f430e31ea17c50415827b49ed2ee
Jim Lambert <jlambert@hashicorp.com>
2020-08-12 07:21:33 -0400

    define a KmsType

---
+++ internal/kms/external_kms_type_test.go
@@ -0,0 +1,58 @@
+package kms
+
+import (
+	"testing"
+
+	"github.com/bmizerany/assert"
+)
+
+func Test_ExternalKmsType(t *testing.T) {
+
+	tests := []struct {
+		typeString string
+		want       KmsType
+	}{
+		{
+			typeString: "unknownkms",
+			want:       UnknownKms,
+		},
+		{
+			typeString: "devkms",
+			want:       DevKms,
+		},
+		{
+			typeString: "awskms",
+			want:       AwsKms,
+		},
+		{
+			typeString: "gcpkms",
+			want:       GcpKms,
+		},
+		{
+			typeString: "alicloudkms",
+			want:       AliCloudKms,
+		},
+		{
+			typeString: "azurekms",
+			want:       AzureKms,
+		},
+		{
+			typeString: "ocikms",
+			want:       OciKms,
+		},
+		{
+			typeString: "vaulttransitkms",
+			want:       VaultTransitKms,
+		},
+		{
+			typeString: "HsmPkcs11Kms",
+			want:       HsmPkcs11Kms,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.typeString, func(t *testing.T) {
+			assert.Equalf(t, tt.want, Map[tt.typeString], "unexpected type for %s", tt.typeString)
+			assert.Equalf(t, tt.typeString, tt.want.String(), "unexpected string for %s", tt.typeString)
+		})
+	}
+}
