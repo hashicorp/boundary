5ee53688918f610fb7920b9adb71e83d47aa0373
Jim Lambert <jlambert@hashicorp.com>
2020-08-12 07:26:44 -0400

    basic options

---
+++ internal/kms/options.go
@@ -0,0 +1,21 @@
+package kms
+
+// getOpts - iterate the inbound Options and return a struct
+func getOpts(opt ...Option) options {
+	opts := getDefaultOptions()
+	for _, o := range opt {
+		o(&opts)
+	}
+	return opts
+}
+
+// Option - how Options are passed as arguments
+type Option func(*options)
+
+// options = how options are represented
+type options struct {
+}
+
+func getDefaultOptions() options {
+	return options{}
+}
