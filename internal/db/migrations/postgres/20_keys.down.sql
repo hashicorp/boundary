dd7ff4873fb0aa603b2114d33fbf3ce087c9eb96
Jim Lambert <jlambert@hashicorp.com>
2020-08-12 07:21:09 -0400

    clean up

--- internal/db/migrations/postgres/20_keys.down.sql
+++ internal/db/migrations/postgres/20_keys.down.sql
@@ -10,5 +10,6 @@ drop table kms_oplog_key cascade;
 drop table kms_oplog_key_version cascade;
 drop table kms_session_key cascade;
 drop table kms_session_key_version cascade;
+drop function kms_scope_valid() cascade;
 
 commit;
