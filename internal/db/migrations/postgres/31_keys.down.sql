3ca50fce86327af80f49ef8d45064d8555c0dbcd
Jim Lambert <jlambert@hashicorp.com>
2020-08-17 12:59:31 -0400

    remove dep functions

--- internal/db/migrations/postgres/31_keys.down.sql
+++ internal/db/migrations/postgres/31_keys.down.sql
@@ -8,7 +8,5 @@ drop table kms_oplog_key cascade;
 drop table kms_oplog_key_version cascade;
 drop table kms_session_key cascade;
 drop table kms_session_key_version cascade;
-drop function kms_scope_valid() cascade;
-drop function kms_version_column cascade;
 
 commit;
