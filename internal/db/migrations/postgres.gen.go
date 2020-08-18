029f1a723e3fa5ad2acae861fe321345d1633ec4
Jim Lambert <jlambert@hashicorp.com>
2020-08-18 17:05:07 -0400

    fix triggers

--- internal/db/migrations/postgres.gen.go
+++ internal/db/migrations/postgres.gen.go
@@ -2459,8 +2459,7 @@ begin
   insert into target
     (public_id, scope_id)
   values
-    (new.public_id, new.scope_id)
-  on conflict (public_id) do nothing;
+    (new.public_id, new.scope_id);
   return new;
 end;
 $$ language plpgsql;
@@ -2473,11 +2472,7 @@ as $$
 begin
   delete from target
   where 
-    public_id = old.public_id and
-    not exists(
-        select count(*) from target_all_subtypes 
-        where public_id = old.public_id and scope_id = old.scope_id
-    );
+    public_id = old.public_id;
   return null; -- result is ignored since this is an after trigger
 end;
 $$ language plpgsql;
@@ -2503,7 +2498,7 @@ $$ language plpgsql;
 
 -- target_host_set_scope_valid() is a before insert trigger function for target_host_set 
 create or replace function
-  target_host_set_scope_valid()
+  target_host_set_scope_valid_orig()
   returns trigger
 as $$
 declare hc_scope_id text;
@@ -2539,6 +2534,78 @@ begin
 end;
 $$ language plpgsql;
 
+
+-- target_host_set_scope_valid() is a before insert trigger function for target_host_set 
+create or replace function
+  target_host_set_scope_valid()
+  returns trigger
+as $$
+declare hs_scope_id text;
+declare hs_scope_type text;
+declare hs_scope_parent_id text;
+declare t_scope_id text;
+declare t_scope_type text;
+declare t_scope_parent_id text;
+begin
+  select 
+    hc.scope_id,
+    s.parent_id,
+    s.type
+  from 
+    scope s,
+    host_set hs,
+    host_catalog hc
+  where 
+    hs.catalog_id = hc.public_id and
+    hc.scope_id = s.scope_id and 
+    hs.public_id = new.host_set_id
+  into hs_scope_id, hs_scope_type, hs_scope_parent_id;
+
+  select 
+    s.public_id,
+    s.type,
+    s.parent_id
+  from 
+    scope s,
+    target t
+  where 
+    s.public_id = new.target_id 
+  into t_scope_id, t_scope_type, t_scope_parent_id;
+  
+  if hs_scope_id == t_scope_id then
+    if hs_scope_type = 'org' then
+      return new;
+    end if;
+    if hs_scope_type = 'project' then
+      return new;
+    end if;
+    raise exception 'scopes (% == %) match but invalid scope type % (must be org or project)', hs_scope_id, t_scope_id, scope_type;
+  end if;
+
+  if t_scope_type = 'org' then
+    -- Allow if target scope is the parent of the host set; this is, if the host
+    -- set belongs to a direct child scope of the target's org 
+    if t_scope_id = hs_scope_parent_id then
+      return new;
+    end if;
+    raise exception 'host set scope % is not a child project of the target scope %', hs_scope_id, t_scope_id;
+  end if;
+
+  if hs_scope_type = 'org' then
+    -- Allow if host set scope is the parent of the target; this is, if the
+    -- target belongs to a direct child scope of the host set's org  
+    if hs_scope_id = t_scope_parent_id then
+      return new;
+    end if;
+    raise exception 'target scope % is not a child project of the host set scope %', t_scope_id, hs_scope_id;
+  end if;
+
+  -- well, it's not a valid scope relationship
+  raise exception 'target scope % and host set scope % are not equal and not related via an org', t_scope_id, hs_scope_id;
+end;
+$$ language plpgsql;
+
+
 commit;
 `),
 	},
@@ -2557,6 +2624,41 @@ commit;
 	"migrations/31_targets.up.sql": {
 		name: "31_targets.up.sql",
 		bytes: []byte(`
+/*
+                                   ┌─────────────────┐
+┌─────────────────┐                │   target_tcp    │
+│     target      │                ├─────────────────┤
+├─────────────────┤                │public_id        │
+│public_id        │┼─────────────○┼│scope_id         │
+│scope_id         │                │default_port     │
+│                 │                │name (not null)  │
+└─────────────────┘                │description      │
+         ┼                         └─────────────────┘
+         │                                            
+         ○                                            
+        ╱│╲                                           
+┌─────────────────┐                                   
+│ target_host_set │                                   
+├─────────────────┤                                   
+│target_id        │                                   
+│host_set_id      │                                   
+│                 │                                   
+└─────────────────┘                                   
+        ╲│╱                                           
+         ○                                            
+         │                                            
+         │                                            
+         ┼                                            
+┌─────────────────┐                                   
+│    host_set     │                                   
+├─────────────────┤                                   
+│public_id        │                                   
+│catalog_id       │                                   
+│                 │                                   
+└─────────────────┘                                                                            
+
+*/
+
 begin;
 
 create table target (
