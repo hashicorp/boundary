5a521c7d61f0bff8270489f2741ee6ca85d4997a
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2020-08-18 15:46:06 -0400

    Fix some tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -20,11 +20,8 @@ import (
 
 func TestHandler_AuthDecoration(t *testing.T) {
 	conn, _ := db.TestSetup(t, "postgres")
-	rw := db.New(conn)
 	wrapper := db.TestWrapper(t)
-	kms := kms.TestKms(t, conn, wrapper)
-	iamRepo, err := iam.NewRepository(rw, rw, kms)
-	require.NoError(t, err)
+	iamRepo := iam.TestRepo(t, conn, wrapper)
 	iamRepoFn := func() (*iam.Repository, error) {
 		return iamRepo, nil
 	}
@@ -277,8 +274,7 @@ func TestAuthTokenAuthenticator(t *testing.T) {
 	kms := kms.TestKms(t, conn, wrapper)
 	tokenRepo, err := authtoken.NewRepository(rw, rw, kms)
 	require.NoError(t, err)
-	iamRepo, err := iam.NewRepository(rw, rw, kms)
-	require.NoError(t, err)
+	iamRepo := iam.TestRepo(t, conn, wrapper)
 	tokenRepoFn := func() (*authtoken.Repository, error) {
 		return tokenRepo, nil
 	}
