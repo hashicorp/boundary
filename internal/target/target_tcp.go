3700d93c14a23b727dd5d78186ee4191bf8f095e
Jim Lambert <jlambert@hashicorp.com>
2020-08-19 16:59:13 -0400

    basic storage for tcp targets

---
+++ internal/target/target_tcp.go
@@ -0,0 +1,81 @@
+package target
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/target/store"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	DefaultTcpTableName = "target_tcp"
+)
+
+type TcpTarget struct {
+	*store.TcpTarget
+	tableName string `gorm:"-"`
+}
+
+// NewRootKey creates a new in memory tcp target.  WithName, WithDescription and
+// WithDefaultPort options are supported
+func NewTcpTarget(scopeId string, opt ...Option) (*TcpTarget, error) {
+	opts := getOpts(opt...)
+	if scopeId == "" {
+		return nil, fmt.Errorf("new tcp target: missing scope id: %w", db.ErrInvalidParameter)
+	}
+	t := &TcpTarget{
+		TcpTarget: &store.TcpTarget{
+			ScopeId:     scopeId,
+			Name:        opts.withName,
+			Description: opts.withDescription,
+			DefaultPort: opts.withDefaultPort,
+		},
+	}
+	return t, nil
+}
+
+// allocTcpTarget will allocate a tcp target
+func allocTcpTarget() TcpTarget {
+	return TcpTarget{
+		TcpTarget: &store.TcpTarget{},
+	}
+}
+
+// Clone creates a clone of the TcpTarget
+func (t *TcpTarget) Clone() interface{} {
+	cp := proto.Clone(t.TcpTarget)
+	return &TcpTarget{
+		TcpTarget: cp.(*store.TcpTarget),
+	}
+}
+
+// VetForWrite implements db.VetForWrite() interface and validates the tcp target
+// before it's written.
+func (t *Target) VetForWrite(ctx context.Context, r db.Reader, opType db.OpType, opt ...db.Option) error {
+	if t.PrivateId == "" {
+		return fmt.Errorf("tcp target vet for write: missing private id: %w", db.ErrInvalidParameter)
+	}
+	if opType == db.CreateOp {
+		if t.ScopeId == "" {
+			return fmt.Errorf("tcp target vet for write: missing scope id: %w", db.ErrInvalidParameter)
+		}
+	}
+	return nil
+}
+
+// TableName returns the tablename to override the default gorm table name
+func (t *Target) TableName() string {
+	if t.tableName != "" {
+		return t.tableName
+	}
+	return DefaultRootKeyTableName
+}
+
+// SetTableName sets the tablename and satisfies the ReplayableMessage
+// interface. If the caller attempts to set the name to "" the name will be
+// reset to the default name.
+func (t *Target) SetTableName(n string) {
+	t.tableName = n
+}
