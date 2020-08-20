6e8e6d99416a72dcc0460622b23da1e53bb7755e
Jim Lambert <jlambert@hashicorp.com>
2020-08-20 12:51:04 -0400

    lookup and list targets

--- internal/target/repository.go
+++ internal/target/repository.go
@@ -1,10 +1,14 @@
 package target
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/hashicorp/boundary/internal/db"
 	"github.com/hashicorp/boundary/internal/kms"
+	wrapping "github.com/hashicorp/go-kms-wrapping"
 )
 
 var (
@@ -44,4 +48,72 @@ func NewRepository(r db.Reader, w db.Writer, kms *kms.Kms, opt ...Option) (*Repo
 		kms:          kms,
 		defaultLimit: opts.withLimit,
 	}, nil
+}
+
+// LookupTarget will look up a target in the repository.  If the target is not
+// found, it will return nil, nil.  No options are currently supported.
+func (r *Repository) LookupTarget(ctx context.Context, keyWrapper wrapping.Wrapper, publicId string, opt ...Option) (Target, error) {
+	if publicId == "" {
+		return nil, fmt.Errorf("lookup target: missing private id: %w", db.ErrNilParameter)
+	}
+	if keyWrapper == nil {
+		return nil, fmt.Errorf("lookup target: missing key wrapper: %w", db.ErrNilParameter)
+	}
+	target := allocTargetView()
+	target.PublicId = publicId
+	if err := r.reader.LookupById(ctx, &target); err != nil {
+		return nil, fmt.Errorf("lookup target: failed %w for %s", err, publicId)
+	}
+	subType, err := target.TargetSubType()
+	if err != nil {
+		return nil, fmt.Errorf("lookup target: %w", err)
+	}
+	return subType, nil
+}
+
+// ListTargets in targets in a scope.  Supports the WithScopeId, WithUserId, WithLimit, WithTargetType options.
+func (r *Repository) ListTargets(ctx context.Context, opt ...Option) ([]Target, error) {
+	opts := getOpts(opt...)
+	if opts.withScopeId == "" && opts.withUserId == "" {
+		return nil, fmt.Errorf("list targets: must specify either a scope id or user id: %w", db.ErrInvalidParameter)
+	}
+	var where []string
+	var args []interface{}
+	if opts.withScopeId != "" {
+		where, args = append(where, "scope_id = ?"), append(args, opts.withScopeId)
+	}
+	if opts.withTargetType != nil {
+		where, args = append(where, "type = ?"), append(args, opts.withTargetType)
+	}
+
+	var foundTargets []*targetView
+	err := r.list(ctx, &foundTargets, strings.Join(where, " and "), args, opt...)
+	if err != nil {
+		return nil, fmt.Errorf("list targets: %w", err)
+	}
+
+	targets := make([]Target, 0, len(foundTargets))
+
+	for _, t := range foundTargets {
+		subType, err := t.TargetSubType()
+		if err != nil {
+			return nil, fmt.Errorf("list targets: %w", err)
+		}
+		targets = append(targets, subType)
+	}
+	return targets, nil
+}
+
+// list will return a listing of resources and honor the WithLimit option or the
+// repo defaultLimit
+func (r *Repository) list(ctx context.Context, resources interface{}, where string, args []interface{}, opt ...Option) error {
+	opts := getOpts(opt...)
+	limit := r.defaultLimit
+	var dbOpts []db.Option
+	if opts.withLimit != 0 {
+		// non-zero signals an override of the default limit for the repo.
+		limit = opts.withLimit
+	}
+	dbOpts = append(dbOpts, db.WithLimit(limit))
+	return r.reader.SearchWhere(ctx, resources, where, args, dbOpts...)
 }
