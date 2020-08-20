e823b2e58469f7b3f51749aca37c8bb780da9728
Jim Lambert <jlambert@hashicorp.com>
2020-08-20 16:50:24 -0400

    test creating host sets with test target

--- internal/target/testing_test.go
+++ internal/target/testing_test.go
@@ -1,23 +1,35 @@
 package target
 
 import (
+	"context"
 	"testing"
 
 	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/host/static"
 	"github.com/hashicorp/boundary/internal/iam"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_TestTcpTarget(t *testing.T) {
 	t.Helper()
-	assert, require := assert.New(t), require.New(t)
+	require := require.New(t)
 	conn, _ := db.TestSetup(t, "postgres")
 	wrapper := db.TestWrapper(t)
 	org, _ := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
+	cats := static.TestCatalogs(t, conn, org.PublicId, 1)
+	hsets := static.TestSets(t, conn, cats[0].GetPublicId(), 2)
+	var sets []string
+	for _, s := range hsets {
+		sets = append(sets, s.PublicId)
+	}
 	name := testTargetName(t, org.PublicId)
-	target := TestTcpTarget(t, conn, org.PublicId, name)
+	target := TestTcpTarget(t, conn, org.PublicId, name, WithHostSets(sets))
 	require.NotNil(t)
-	assert.NotEmpty(target.PublicId)
-	assert.Equal(name, target.Name)
+	require.NotEmpty(target.PublicId)
+	require.Equal(name, target.Name)
+
+	rw := db.New(conn)
+	foundSets, err := fetchHostSets(context.Background(), rw, target.PublicId)
+	require.NoError(err)
+	require.Equal(sets, foundSets)
 }
