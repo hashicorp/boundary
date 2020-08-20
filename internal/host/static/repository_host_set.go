77e0139185db33c421f2ceed9ecd12df2b8392bf
Michael Gaffney <mike@gaffney.cc>
2020-08-20 07:44:02 -0400

    Implement UpdateSet

--- internal/host/static/repository_host_set.go
+++ internal/host/static/repository_host_set.go
@@ -3,8 +3,10 @@ package static
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/boundary/internal/db"
+	dbcommon "github.com/hashicorp/boundary/internal/db/common"
 	"github.com/hashicorp/boundary/internal/oplog"
 )
 
@@ -52,4 +54,78 @@ func (r *Repository) CreateSet(ctx context.Context, s *HostSet, opt ...Option) (
 		return nil, fmt.Errorf("create: static host set: in catalog: %s: %w", s.CatalogId, err)
 	}
 	return newSet, nil
+}
+
+// UpdateSet updates the repository entry for s.PublicId with the values in
+// s for the fields listed in fieldMaskPaths. It returns a new HostSet
+// containing the updated values and a count of the number of records
+// updated. s is not changed.
+//
+// s must contain a valid PublicId. Only s.Name and s.Description can be
+// updated. If s.Name is set to a non-empty string, it must be unique
+// within s.CatalogId.
+//
+// An attribute of s will be set to NULL in the database if the attribute
+// in s is the zero value and it is included in fieldMaskPaths.
+func (r *Repository) UpdateSet(ctx context.Context, s *HostSet, version uint32, fieldMaskPaths []string, opt ...Option) (*HostSet, int, error) {
+	if s == nil {
+		return nil, db.NoRowsAffected, fmt.Errorf("update: static host set: %w", db.ErrNilParameter)
+	}
+	if s.HostSet == nil {
+		return nil, db.NoRowsAffected, fmt.Errorf("update: static host set: embedded HostSet: %w", db.ErrNilParameter)
+	}
+	if s.PublicId == "" {
+		return nil, db.NoRowsAffected, fmt.Errorf("update: static host set: missing public id: %w", db.ErrInvalidParameter)
+	}
+	if version == 0 {
+		return nil, db.NoRowsAffected, fmt.Errorf("update: static host set: no version supplied: %w", db.ErrInvalidParameter)
+	}
+
+	for _, f := range fieldMaskPaths {
+		switch {
+		case strings.EqualFold("Name", f):
+		case strings.EqualFold("Description", f):
+		default:
+			return nil, db.NoRowsAffected, fmt.Errorf("update: static host set: field: %s: %w", f, db.ErrInvalidFieldMask)
+		}
+	}
+	var dbMask, nullFields []string
+	dbMask, nullFields = dbcommon.BuildUpdatePaths(
+		map[string]interface{}{
+			"Name":        s.Name,
+			"Description": s.Description,
+		},
+		fieldMaskPaths,
+	)
+	if len(dbMask) == 0 && len(nullFields) == 0 {
+		return nil, db.NoRowsAffected, fmt.Errorf("update: static host set: %w", db.ErrEmptyFieldMask)
+	}
+
+	s = s.clone()
+
+	var rowsUpdated int
+	var returnedHostSet *HostSet
+	_, err := r.writer.DoTx(ctx, db.StdRetryCnt, db.ExpBackoff{},
+		func(_ db.Reader, w db.Writer) error {
+			returnedHostSet = s.clone()
+			var err error
+			rowsUpdated, err = w.Update(ctx, returnedHostSet, dbMask, nullFields,
+				db.WithOplog(r.wrapper, s.oplog(oplog.OpType_OP_TYPE_UPDATE)),
+				db.WithVersion(&version))
+			if err == nil && rowsUpdated > 1 {
+				return db.ErrMultipleRecords
+			}
+			return err
+		},
+	)
+
+	if err != nil {
+		if db.IsUniqueError(err) {
+			return nil, db.NoRowsAffected, fmt.Errorf("update: static host set: %s: name %s already exists: %w",
+				s.PublicId, s.Name, db.ErrNotUnique)
+		}
+		return nil, db.NoRowsAffected, fmt.Errorf("update: static host set: %s: %w", s.PublicId, err)
+	}
+
+	return returnedHostSet, rowsUpdated, nil
 }
--- internal/host/static/repository_host_set_test.go
+++ internal/host/static/repository_host_set_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/boundary/internal/db"
+	dbassert "github.com/hashicorp/boundary/internal/db/assert"
 	"github.com/hashicorp/boundary/internal/host/static/store"
 	"github.com/hashicorp/boundary/internal/iam"
 	"github.com/hashicorp/boundary/internal/oplog"
@@ -192,5 +193,414 @@ func TestRepository_CreateSet(t *testing.T) {
 		assert.Equal(in2.Name, got2.Name)
 		assert.Equal(in2.Description, got2.Description)
 		assert.Equal(got2.CreateTime, got2.UpdateTime)
+	})
+}
+
+func TestRepository_UpdateHostSet(t *testing.T) {
+	conn, _ := db.TestSetup(t, "postgres")
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+
+	changeName := func(n string) func(*HostSet) *HostSet {
+		return func(s *HostSet) *HostSet {
+			s.Name = n
+			return s
+		}
+	}
+
+	changeDescription := func(d string) func(*HostSet) *HostSet {
+		return func(s *HostSet) *HostSet {
+			s.Description = d
+			return s
+		}
+	}
+
+	makeNil := func() func(*HostSet) *HostSet {
+		return func(s *HostSet) *HostSet {
+			return nil
+		}
+	}
+
+	makeEmbeddedNil := func() func(*HostSet) *HostSet {
+		return func(s *HostSet) *HostSet {
+			return &HostSet{}
+		}
+	}
+
+	deletePublicId := func() func(*HostSet) *HostSet {
+		return func(s *HostSet) *HostSet {
+			s.PublicId = ""
+			return s
+		}
+	}
+
+	nonExistentPublicId := func() func(*HostSet) *HostSet {
+		return func(s *HostSet) *HostSet {
+			s.PublicId = "abcd_OOOOOOOOOO"
+			return s
+		}
+	}
+
+	combine := func(fns ...func(s *HostSet) *HostSet) func(*HostSet) *HostSet {
+		return func(s *HostSet) *HostSet {
+			for _, fn := range fns {
+				s = fn(s)
+			}
+			return s
+		}
+	}
+
+	var tests = []struct {
+		name      string
+		orig      *HostSet
+		chgFn     func(*HostSet) *HostSet
+		masks     []string
+		want      *HostSet
+		wantCount int
+		wantIsErr error
+	}{
+		{
+			name: "nil-host-set",
+			orig: &HostSet{
+				HostSet: &store.HostSet{},
+			},
+			chgFn:     makeNil(),
+			masks:     []string{"Name", "Description"},
+			wantIsErr: db.ErrNilParameter,
+		},
+		{
+			name: "nil-embedded-host-set",
+			orig: &HostSet{
+				HostSet: &store.HostSet{},
+			},
+			chgFn:     makeEmbeddedNil(),
+			masks:     []string{"Name", "Description"},
+			wantIsErr: db.ErrNilParameter,
+		},
+		{
+			name: "no-public-id",
+			orig: &HostSet{
+				HostSet: &store.HostSet{},
+			},
+			chgFn:     deletePublicId(),
+			masks:     []string{"Name", "Description"},
+			wantIsErr: db.ErrInvalidParameter,
+		},
+		{
+			name: "updating-non-existent-host-set",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name: "test-name-repo",
+				},
+			},
+			chgFn:     combine(nonExistentPublicId(), changeName("test-update-name-repo")),
+			masks:     []string{"Name"},
+			wantIsErr: db.ErrRecordNotFound,
+		},
+		{
+			name: "empty-field-mask",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name: "test-name-repo",
+				},
+			},
+			chgFn:     changeName("test-update-name-repo"),
+			wantIsErr: db.ErrEmptyFieldMask,
+		},
+		{
+			name: "read-only-fields-in-field-mask",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name: "test-name-repo",
+				},
+			},
+			chgFn:     changeName("test-update-name-repo"),
+			masks:     []string{"PublicId", "CreateTime", "UpdateTime", "CatalogId"},
+			wantIsErr: db.ErrInvalidFieldMask,
+		},
+		{
+			name: "unknown-field-in-field-mask",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name: "test-name-repo",
+				},
+			},
+			chgFn:     changeName("test-update-name-repo"),
+			masks:     []string{"Bilbo"},
+			wantIsErr: db.ErrInvalidFieldMask,
+		},
+		{
+			name: "change-name",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name: "test-name-repo",
+				},
+			},
+			chgFn: changeName("test-update-name-repo"),
+			masks: []string{"Name"},
+			want: &HostSet{
+				HostSet: &store.HostSet{
+					Name: "test-update-name-repo",
+				},
+			},
+			wantCount: 1,
+		},
+		{
+			name: "change-description",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Description: "test-description-repo",
+				},
+			},
+			chgFn: changeDescription("test-update-description-repo"),
+			masks: []string{"Description"},
+			want: &HostSet{
+				HostSet: &store.HostSet{
+					Description: "test-update-description-repo",
+				},
+			},
+			wantCount: 1,
+		},
+		{
+			name: "change-name-and-description",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name:        "test-name-repo",
+					Description: "test-description-repo",
+				},
+			},
+			chgFn: combine(changeDescription("test-update-description-repo"), changeName("test-update-name-repo")),
+			masks: []string{"Name", "Description"},
+			want: &HostSet{
+				HostSet: &store.HostSet{
+					Name:        "test-update-name-repo",
+					Description: "test-update-description-repo",
+				},
+			},
+			wantCount: 1,
+		},
+		{
+			name: "delete-name",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name:        "test-name-repo",
+					Description: "test-description-repo",
+				},
+			},
+			masks: []string{"Name"},
+			chgFn: combine(changeDescription("test-update-description-repo"), changeName("")),
+			want: &HostSet{
+				HostSet: &store.HostSet{
+					Description: "test-description-repo",
+				},
+			},
+			wantCount: 1,
+		},
+		{
+			name: "delete-description",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name:        "test-name-repo",
+					Description: "test-description-repo",
+				},
+			},
+			masks: []string{"Description"},
+			chgFn: combine(changeDescription(""), changeName("test-update-name-repo")),
+			want: &HostSet{
+				HostSet: &store.HostSet{
+					Name: "test-name-repo",
+				},
+			},
+			wantCount: 1,
+		},
+		{
+			name: "do-not-delete-name",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name:        "test-name-repo",
+					Description: "test-description-repo",
+				},
+			},
+			masks: []string{"Description"},
+			chgFn: combine(changeDescription("test-update-description-repo"), changeName("")),
+			want: &HostSet{
+				HostSet: &store.HostSet{
+					Name:        "test-name-repo",
+					Description: "test-update-description-repo",
+				},
+			},
+			wantCount: 1,
+		},
+		{
+			name: "do-not-delete-description",
+			orig: &HostSet{
+				HostSet: &store.HostSet{
+					Name:        "test-name-repo",
+					Description: "test-description-repo",
+				},
+			},
+			masks: []string{"Name"},
+			chgFn: combine(changeDescription(""), changeName("test-update-name-repo")),
+			want: &HostSet{
+				HostSet: &store.HostSet{
+					Name:        "test-update-name-repo",
+					Description: "test-description-repo",
+				},
+			},
+			wantCount: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			repo, err := NewRepository(rw, rw, wrapper)
+			assert.NoError(err)
+			require.NotNil(repo)
+
+			_, prj := iam.TestScopes(t, conn)
+			catalog := testCatalogs(t, conn, prj.PublicId, 1)[0]
+
+			tt.orig.CatalogId = catalog.PublicId
+			orig, err := repo.CreateSet(context.Background(), tt.orig)
+			assert.NoError(err)
+			require.NotNil(orig)
+
+			if tt.chgFn != nil {
+				orig = tt.chgFn(orig)
+			}
+			got, gotCount, err := repo.UpdateSet(context.Background(), orig, 1, tt.masks)
+			if tt.wantIsErr != nil {
+				assert.Truef(errors.Is(err, tt.wantIsErr), "want err: %q got: %q", tt.wantIsErr, err)
+				assert.Equal(tt.wantCount, gotCount, "row count")
+				assert.Nil(got)
+				return
+			}
+			assert.NoError(err)
+			assert.Empty(tt.orig.PublicId)
+			require.NotNil(got)
+			assertPublicId(t, HostSetPrefix, got.PublicId)
+			assert.Equal(tt.wantCount, gotCount, "row count")
+			assert.NotSame(tt.orig, got)
+			assert.Equal(tt.orig.CatalogId, got.CatalogId)
+			dbassert := dbassert.New(t, rw)
+			if tt.want.Name == "" {
+				dbassert.IsNull(got, "name")
+				return
+			}
+			assert.Equal(tt.want.Name, got.Name)
+			if tt.want.Description == "" {
+				dbassert.IsNull(got, "description")
+				return
+			}
+			assert.Equal(tt.want.Description, got.Description)
+			if tt.wantCount > 0 {
+				assert.NoError(db.TestVerifyOplog(t, rw, got.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_UPDATE), db.WithCreateNotBefore(10*time.Second)))
+			}
+		})
+	}
+
+	t.Run("invalid-duplicate-names", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+		assert.NoError(err)
+		require.NotNil(repo)
+
+		name := "test-dup-name"
+		_, prj := iam.TestScopes(t, conn)
+		catalog := testCatalogs(t, conn, prj.PublicId, 1)[0]
+		ss := testSets(t, conn, catalog.PublicId, 2)
+
+		sA, sB := ss[0], ss[1]
+
+		sA.Name = name
+		got1, gotCount1, err := repo.UpdateSet(context.Background(), sA, 1, []string{"name"})
+		assert.NoError(err)
+		require.NotNil(got1)
+		assert.Equal(name, got1.Name)
+		assert.Equal(1, gotCount1, "row count")
+		assert.NoError(db.TestVerifyOplog(t, rw, sA.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_UPDATE), db.WithCreateNotBefore(10*time.Second)))
+
+		sB.Name = name
+		got2, gotCount2, err := repo.UpdateSet(context.Background(), sB, 1, []string{"name"})
+		assert.Truef(errors.Is(err, db.ErrNotUnique), "want err: %v got: %v", db.ErrNotUnique, err)
+		assert.Nil(got2)
+		assert.Equal(db.NoRowsAffected, gotCount2, "row count")
+		err = db.TestVerifyOplog(t, rw, sB.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_UPDATE), db.WithCreateNotBefore(10*time.Second))
+		assert.Error(err)
+		assert.True(errors.Is(db.ErrRecordNotFound, err))
+	})
+
+	t.Run("valid-duplicate-names-diff-Catalogs", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+		assert.NoError(err)
+		require.NotNil(repo)
+
+		_, prj := iam.TestScopes(t, conn)
+		catalogs := testCatalogs(t, conn, prj.PublicId, 2)
+
+		catalogA, catalogB := catalogs[0], catalogs[1]
+
+		in := &HostSet{
+			HostSet: &store.HostSet{
+				Name: "test-name-repo",
+			},
+		}
+		in2 := in.clone()
+
+		in.CatalogId = catalogA.PublicId
+		got, err := repo.CreateSet(context.Background(), in)
+		assert.NoError(err)
+		require.NotNil(got)
+		assertPublicId(t, HostSetPrefix, got.PublicId)
+		assert.NotSame(in, got)
+		assert.Equal(in.Name, got.Name)
+		assert.Equal(in.Description, got.Description)
+
+		in2.CatalogId = catalogB.PublicId
+		in2.Name = "first-name"
+		got2, err := repo.CreateSet(context.Background(), in2)
+		assert.NoError(err)
+		require.NotNil(got2)
+		got2.Name = got.Name
+		got3, gotCount3, err := repo.UpdateSet(context.Background(), got2, 1, []string{"name"})
+		assert.NoError(err)
+		require.NotNil(got3)
+		assert.NotSame(got2, got3)
+		assert.Equal(got.Name, got3.Name)
+		assert.Equal(got2.Description, got3.Description)
+		assert.Equal(1, gotCount3, "row count")
+		assert.NoError(db.TestVerifyOplog(t, rw, got2.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_UPDATE), db.WithCreateNotBefore(10*time.Second)))
+	})
+
+	t.Run("change-scope-id", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		repo, err := NewRepository(rw, rw, wrapper)
+		assert.NoError(err)
+		require.NotNil(repo)
+
+		_, prj := iam.TestScopes(t, conn)
+		catalogs := testCatalogs(t, conn, prj.PublicId, 2)
+
+		catalogA, catalogB := catalogs[0], catalogs[1]
+
+		sA := testSets(t, conn, catalogA.PublicId, 1)[0]
+		sB := testSets(t, conn, catalogB.PublicId, 1)[0]
+
+		assert.NotEqual(sA.CatalogId, sB.CatalogId)
+		orig := sA.clone()
+
+		sA.CatalogId = sB.CatalogId
+		assert.Equal(sA.CatalogId, sB.CatalogId)
+
+		got1, gotCount1, err := repo.UpdateSet(context.Background(), sA, 1, []string{"name"})
+
+		assert.NoError(err)
+		require.NotNil(got1)
+		assert.Equal(orig.CatalogId, got1.CatalogId)
+		assert.Equal(1, gotCount1, "row count")
+		assert.NoError(db.TestVerifyOplog(t, rw, sA.PublicId, db.WithOperation(oplog.OpType_OP_TYPE_UPDATE), db.WithCreateNotBefore(10*time.Second)))
 	})
 }
