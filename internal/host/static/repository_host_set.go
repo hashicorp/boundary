0775383781316c8b7328b4f797d301b4e59ed616
Michael Gaffney <mike@gaffney.cc>
2020-08-20 14:42:22 -0400

    Add method signatures for host set operations

--- internal/host/static/repository_host_set.go
+++ internal/host/static/repository_host_set.go
@@ -220,3 +220,26 @@ func (r *Repository) DeleteSet(ctx context.Context, scopeId string, publicId str
 
 	return rowsDeleted, nil
 }
+
+// AddSetMembers adds hostIds to setId in the repository. It returns a
+// slice of all host set members in setId. A host must belong to the same
+// catalog as the set to be added. The version must match the current
+// version of the setId in the repository.
+func (r *Repository) AddSetMembers(ctx context.Context, scopeId string, setId string, version uint32, hostIds []string, opt ...Option) ([]*HostSetMember, error) {
+	panic("not implemented")
+}
+
+// DeleteSetMembers deletes hostIds from setId in the repository. It
+// returns the number of hosts deleted from the set. The version must match
+// the current version of the setId in the repository.
+func (r *Repository) DeleteSetMembers(ctx context.Context, scopeId string, setId string, version uint32, hostIds []string, opt ...Option) (int, error) {
+	panic("not implemented")
+}
+
+// ReplaceSetMembers replaces the hosts in setId with hostIds in the
+// repository. It returns a slice of all host set members in setId. A host
+// must belong to the same catalog as the set to be added. The version must
+// match the current version of the setId in the repository.
+func (r *Repository) ReplaceSetMembers(ctx context.Context, scopeId string, setId string, version uint32, hostIds []string, opt ...Option) ([]*HostSetMember, error) {
+	panic("not implemented")
+}
