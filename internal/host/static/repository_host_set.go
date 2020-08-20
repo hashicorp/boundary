a1d7652203fcdd96a453dbe9cb6c61a059ba4762
Michael Gaffney <mike@gaffney.cc>
2020-08-20 15:11:45 -0400

    Rename ReplaceSetMembers to SetSetMembers

--- internal/host/static/repository_host_set.go
+++ internal/host/static/repository_host_set.go
@@ -236,10 +236,10 @@ func (r *Repository) DeleteSetMembers(ctx context.Context, scopeId string, setId
 	panic("not implemented")
 }
 
-// ReplaceSetMembers replaces the hosts in setId with hostIds in the
+// SetSetMembers replaces the hosts in setId with hostIds in the
 // repository. It returns a slice of all host set members in setId. A host
 // must belong to the same catalog as the set to be added. The version must
 // match the current version of the setId in the repository.
-func (r *Repository) ReplaceSetMembers(ctx context.Context, scopeId string, setId string, version uint32, hostIds []string, opt ...Option) ([]*HostSetMember, error) {
+func (r *Repository) SetSetMembers(ctx context.Context, scopeId string, setId string, version uint32, hostIds []string, opt ...Option) ([]*HostSetMember, error) {
 	panic("not implemented")
 }
