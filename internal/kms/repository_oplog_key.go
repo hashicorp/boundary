a13f0cb26922ddae5a4da4af468a1b2cdfed6a3e
Jim Lambert <jlambert@hashicorp.com>
2020-08-31 16:49:27 -0400

    OplogKey repo functions with unit tests

---
+++ internal/kms/repository_oplog_key.go
@@ -0,0 +1,147 @@
+package kms
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/boundary/internal/db"
+	wrapping "github.com/hashicorp/go-kms-wrapping"
+)
+
+// CreateOplogKey inserts into the repository and returns the new key and the
+// key version. There are no valid options at this time.
+func (r *Repository) CreateOplogKey(ctx context.Context, rkvWrapper wrapping.Wrapper, key []byte, opt ...Option) (*OplogKey, *OplogKeyVersion, error) {
+	var returnedDk, returnedDv interface{}
+	_, err := r.writer.DoTx(
+		ctx,
+		db.StdRetryCnt,
+		db.ExpBackoff{},
+		func(reader db.Reader, w db.Writer) error {
+			var err error
+			if returnedDk, returnedDv, err = CreateOplogKeyTx(ctx, reader, w, rkvWrapper, key); err != nil {
+				return err
+			}
+			return nil
+		},
+	)
+	if err != nil {
+		return nil, nil, fmt.Errorf("create oplog key: %w", err)
+	}
+	return returnedDk.(*OplogKey), returnedDv.(*OplogKeyVersion), err
+}
+
+// CreateOplogKeyTx inserts into the db (via db.Writer) and returns the new key
+// and the key version. This function encapsulates all the work required within
+// a db.TxHandler and allows this capability to be shared with the iam repo.
+func CreateOplogKeyTx(ctx context.Context, r db.Reader, w db.Writer, rkvWrapper wrapping.Wrapper, key []byte) (*OplogKey, *OplogKeyVersion, error) {
+	if rkvWrapper == nil {
+		return nil, nil, fmt.Errorf("create oplog key: missing key wrapper: %w", db.ErrInvalidParameter)
+	}
+	if len(key) == 0 {
+		return nil, nil, fmt.Errorf("create oplog key: missing key: %w", db.ErrInvalidParameter)
+	}
+	rootKeyVersionId := rkvWrapper.KeyID()
+	switch {
+	case !strings.HasPrefix(rootKeyVersionId, RootKeyVersionPrefix):
+		return nil, nil, fmt.Errorf("create oplog key: root key version id %s doesn't start with prefix %s: %w", rootKeyVersionId, RootKeyVersionPrefix, db.ErrInvalidParameter)
+	case rootKeyVersionId == "":
+		return nil, nil, fmt.Errorf("create oplog key: missing root key version id: %w", db.ErrInvalidParameter)
+	}
+	rv := AllocRootKeyVersion()
+	rv.PrivateId = rootKeyVersionId
+	err := r.LookupById(ctx, &rv)
+	if err != nil {
+		return nil, nil, fmt.Errorf("create oplog key: unable to lookup root key version %s: %w", rootKeyVersionId, err)
+	}
+
+	dk := AllocOplogKey()
+	dv := AllocOplogKeyVersion()
+	id, err := newOplogKeyId()
+	if err != nil {
+		return nil, nil, fmt.Errorf("create oplog key: %w", err)
+	}
+	dk.PrivateId = id
+	dk.RootKeyId = rv.RootKeyId
+
+	id, err = newOplogKeyVersionId()
+	if err != nil {
+		return nil, nil, fmt.Errorf("create oplog key: %w", err)
+	}
+	dv.PrivateId = id
+	dv.OplogKeyId = dk.PrivateId
+	dv.RootKeyVersionId = rootKeyVersionId
+	dv.Key = key
+	if err := dv.Encrypt(ctx, rkvWrapper); err != nil {
+		return nil, nil, fmt.Errorf("create oplog key: %w", err)
+	}
+
+	// no oplog entries for keys
+	if err := w.Create(ctx, &dk); err != nil {
+		return nil, nil, fmt.Errorf("create oplog key: key create: %w", err)
+	}
+	// no oplog entries for key versions
+	if err := w.Create(ctx, &dv); err != nil {
+		return nil, nil, fmt.Errorf("create oplog key: version create: %w", err)
+	}
+
+	return &dk, &dv, err
+}
+
+// LookupOplogKey will look up a key in the repository.  If the key is not
+// found, it will return nil, nil.
+func (r *Repository) LookupOplogKey(ctx context.Context, privateId string, opt ...Option) (*OplogKey, error) {
+	if privateId == "" {
+		return nil, fmt.Errorf("lookup oplog key: missing private id: %w", db.ErrInvalidParameter)
+	}
+	k := AllocOplogKey()
+	k.PrivateId = privateId
+	if err := r.reader.LookupById(ctx, &k); err != nil {
+		return nil, fmt.Errorf("lookup oplog key: failed %w for %s", err, privateId)
+	}
+	return &k, nil
+}
+
+// DeleteOplogKey deletes the key for the provided id from the
+// repository returning a count of the number of records deleted.  All options
+// are ignored.
+func (r *Repository) DeleteOplogKey(ctx context.Context, privateId string, opt ...Option) (int, error) {
+	if privateId == "" {
+		return db.NoRowsAffected, fmt.Errorf("delete oplog key: missing private id: %w", db.ErrInvalidParameter)
+	}
+	k := AllocOplogKey()
+	k.PrivateId = privateId
+	if err := r.reader.LookupById(ctx, &k); err != nil {
+		return db.NoRowsAffected, fmt.Errorf("delete oplog key: failed %w for %s", err, privateId)
+	}
+
+	var rowsDeleted int
+	_, err := r.writer.DoTx(
+		ctx,
+		db.StdRetryCnt,
+		db.ExpBackoff{},
+		func(_ db.Reader, w db.Writer) (err error) {
+			dk := k.Clone()
+			// no oplog entries for root keys
+			rowsDeleted, err = w.Delete(ctx, dk)
+			if err == nil && rowsDeleted > 1 {
+				return db.ErrMultipleRecords
+			}
+			return err
+		},
+	)
+	if err != nil {
+		return db.NoRowsAffected, fmt.Errorf("delete oplog key: %s: %w", privateId, err)
+	}
+	return rowsDeleted, nil
+}
+
+// ListOplogKeys will list the keys.  Supports the WithLimit option.
+func (r *Repository) ListOplogKeys(ctx context.Context, opt ...Option) ([]*OplogKey, error) {
+	var keys []*OplogKey
+	err := r.list(ctx, &keys, "1=1", nil, opt...)
+	if err != nil {
+		return nil, fmt.Errorf("list oplog keys: %w", err)
+	}
+	return keys, nil
+}
---
+++ internal/kms/repository_oplog_key_test.go
@@ -0,0 +1,301 @@
+package kms_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/iam"
+	"github.com/hashicorp/boundary/internal/kms"
+	"github.com/hashicorp/boundary/internal/oplog"
+	wrapping "github.com/hashicorp/go-kms-wrapping"
+	"github.com/hashicorp/go-kms-wrapping/wrappers/aead"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestRepository_CreateOplogKey(t *testing.T) {
+	t.Parallel()
+	conn, _ := db.TestSetup(t, "postgres")
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+	repo, err := kms.NewRepository(rw, rw)
+	require.NoError(t, err)
+	org, _ := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
+	require.NoError(t, conn.Where("1=1").Delete(kms.AllocRootKey()).Error)
+	rk := kms.TestRootKey(t, conn, org.PublicId)
+	_, rkvWrapper := kms.TestRootKeyVersion(t, conn, wrapper, rk.PrivateId)
+
+	type args struct {
+		scopeId    string
+		key        []byte
+		keyWrapper wrapping.Wrapper
+		opt        []kms.Option
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantErr     bool
+		wantIsError error
+	}{
+		{
+			name: "valid-org",
+			args: args{
+				scopeId:    org.PublicId,
+				key:        []byte("test key"),
+				keyWrapper: rkvWrapper,
+			},
+			wantErr: false,
+		},
+		{
+			name: "nil-key",
+			args: args{
+				scopeId:    org.PublicId,
+				keyWrapper: rkvWrapper,
+			},
+			wantErr:     true,
+			wantIsError: db.ErrInvalidParameter,
+		},
+		{
+			name: "empty-key",
+			args: args{
+				scopeId:    org.PublicId,
+				keyWrapper: rkvWrapper,
+				key:        []byte(""),
+			},
+			wantErr:     true,
+			wantIsError: db.ErrInvalidParameter,
+		},
+		{
+			name: "nil-wrapper",
+			args: args{
+				scopeId:    org.PublicId,
+				key:        []byte("test key"),
+				keyWrapper: nil,
+			},
+			wantErr:     true,
+			wantIsError: db.ErrInvalidParameter,
+		},
+		{
+			name: "not-rkv-wrapper",
+			args: args{
+				scopeId:    org.PublicId,
+				key:        []byte("test key"),
+				keyWrapper: wrapper,
+			},
+			wantErr:     true,
+			wantIsError: db.ErrInvalidParameter,
+		},
+		{
+			name: "wrapper-missing-id",
+			args: args{
+				scopeId: org.PublicId,
+				key:     []byte("test key"),
+				keyWrapper: func() wrapping.Wrapper {
+					w := db.TestWrapper(t)
+					_, err = w.(*aead.Wrapper).SetConfig(map[string]string{
+						"key_id": "",
+					})
+					require.NoError(t, err)
+					return w
+				}(),
+			},
+			wantErr:     true,
+			wantIsError: db.ErrInvalidParameter,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			dk, dv, err := repo.CreateOplogKey(context.Background(), tt.args.keyWrapper, tt.args.key, tt.args.opt...)
+			if tt.wantErr {
+				assert.Error(err)
+				assert.Nil(dk)
+				if tt.wantIsError != nil {
+					assert.True(errors.Is(err, tt.wantIsError))
+				}
+				return
+			}
+			require.NoError(err)
+			assert.NotNil(dk.CreateTime)
+			foundKey, err := repo.LookupOplogKey(context.Background(), dk.PrivateId)
+			assert.NoError(err)
+			assert.True(proto.Equal(foundKey, dk))
+
+			// make sure there was no oplog written
+			err = db.TestVerifyOplog(t, rw, dk.PrivateId, db.WithOperation(oplog.OpType_OP_TYPE_CREATE), db.WithCreateNotBefore(10*time.Second))
+			assert.Error(err)
+			assert.True(errors.Is(err, db.ErrRecordNotFound))
+
+			assert.NotNil(dv.CreateTime)
+			foundKeyVersion, err := repo.LookupOplogKeyVersion(context.Background(), tt.args.keyWrapper, dv.PrivateId)
+			assert.NoError(err)
+			assert.True(proto.Equal(foundKeyVersion, dv))
+
+			// make sure there was no oplog written
+			err = db.TestVerifyOplog(t, rw, dv.PrivateId, db.WithOperation(oplog.OpType_OP_TYPE_CREATE), db.WithCreateNotBefore(10*time.Second))
+			assert.Error(err)
+			assert.True(errors.Is(err, db.ErrRecordNotFound))
+		})
+	}
+}
+
+func TestRepository_DeleteOplogKey(t *testing.T) {
+	t.Parallel()
+	conn, _ := db.TestSetup(t, "postgres")
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+	repo, err := kms.NewRepository(rw, rw)
+	require.NoError(t, err)
+	org, _ := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
+	require.NoError(t, conn.Where("1=1").Delete(kms.AllocRootKey()).Error)
+	rk := kms.TestRootKey(t, conn, org.PublicId)
+
+	type args struct {
+		key *kms.OplogKey
+		opt []kms.Option
+	}
+	tests := []struct {
+		name            string
+		args            args
+		wantRowsDeleted int
+		wantErr         bool
+		wantIsError     error
+	}{
+		{
+			name: "valid",
+			args: args{
+				key: kms.TestOplogKey(t, conn, rk.PrivateId),
+			},
+			wantRowsDeleted: 1,
+			wantErr:         false,
+		},
+		{
+			name: "no-private-id",
+			args: args{
+				key: func() *kms.OplogKey {
+					k := kms.AllocOplogKey()
+					return &k
+				}(),
+			},
+			wantRowsDeleted: 0,
+			wantErr:         true,
+			wantIsError:     db.ErrInvalidParameter,
+		},
+		{
+			name: "not-found",
+			args: args{
+				key: func() *kms.OplogKey {
+					id, err := db.NewPublicId(kms.RootKeyPrefix)
+					require.NoError(t, err)
+					k := kms.AllocOplogKey()
+					k.PrivateId = id
+					require.NoError(t, err)
+					return &k
+				}(),
+			},
+			wantRowsDeleted: 0,
+			wantErr:         true,
+			wantIsError:     db.ErrRecordNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			deletedRows, err := repo.DeleteOplogKey(context.Background(), tt.args.key.PrivateId, tt.args.opt...)
+			if tt.wantErr {
+				require.Error(err)
+				assert.Equal(0, deletedRows)
+				if tt.wantIsError != nil {
+					assert.True(errors.Is(err, tt.wantIsError))
+				}
+				// make sure there was no oplog written
+				err = db.TestVerifyOplog(t, rw, tt.args.key.PrivateId, db.WithOperation(oplog.OpType_OP_TYPE_DELETE), db.WithCreateNotBefore(10*time.Second))
+				assert.Error(err)
+				assert.True(errors.Is(db.ErrRecordNotFound, err))
+				return
+			}
+			require.NoError(err)
+			assert.Equal(tt.wantRowsDeleted, deletedRows)
+			foundKey, err := repo.LookupOplogKey(context.Background(), tt.args.key.PrivateId)
+			assert.Error(err)
+			assert.Nil(foundKey)
+			assert.True(errors.Is(err, db.ErrRecordNotFound))
+
+			// make sure there was no oplog written
+			err = db.TestVerifyOplog(t, rw, tt.args.key.PrivateId, db.WithOperation(oplog.OpType_OP_TYPE_DELETE), db.WithCreateNotBefore(10*time.Second))
+			assert.Error(err)
+			assert.True(errors.Is(db.ErrRecordNotFound, err))
+		})
+	}
+}
+
+func TestRepository_ListOplogKeys(t *testing.T) {
+	t.Parallel()
+	conn, _ := db.TestSetup(t, "postgres")
+	const testLimit = 10
+	rw := db.New(conn)
+	repo, err := kms.NewRepository(rw, rw, kms.WithLimit(testLimit))
+	require.NoError(t, err)
+	wrapper := db.TestWrapper(t)
+
+	type args struct {
+		opt []kms.Option
+	}
+	tests := []struct {
+		name      string
+		createCnt int
+		args      args
+		wantCnt   int
+		wantErr   bool
+	}{
+		{
+			name:      "no-limit",
+			createCnt: repo.DefaultLimit() + 1,
+			args: args{
+				opt: []kms.Option{kms.WithLimit(-1)},
+			},
+			wantCnt: repo.DefaultLimit() + 1, // org and project both have keys, plus global scope
+			wantErr: false,
+		},
+		{
+			name:      "default-limit",
+			createCnt: repo.DefaultLimit() + 1,
+			args:      args{},
+			wantCnt:   repo.DefaultLimit(),
+			wantErr:   false,
+		},
+		{
+			name:      "custom-limit",
+			createCnt: repo.DefaultLimit() + 1,
+			args: args{
+				opt: []kms.Option{kms.WithLimit(3)},
+			},
+			wantCnt: 3,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		require.NoError(t, conn.Where("1=1").Delete(kms.AllocRootKey()).Error)
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			for i := 0; i < tt.createCnt; i++ {
+				_, proj := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
+				require.NoError(conn.Where("scope_id=?", []interface{}{proj.PublicId}).Delete(kms.AllocRootKey()).Error)
+				rk := kms.TestRootKey(t, conn, proj.PublicId)
+				kms.TestOplogKey(t, conn, rk.PrivateId)
+				require.NoError(err)
+			}
+			got, err := repo.ListOplogKeys(context.Background(), tt.args.opt...)
+			if tt.wantErr {
+				require.Error(err)
+				return
+			}
+			require.NoError(err)
+			assert.Equal(tt.wantCnt, len(got))
+		})
+	}
+}
