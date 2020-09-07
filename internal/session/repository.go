09837f476c3a9521e56d2c0d8452187b05622e6a
Jim Lambert <jlambert@hashicorp.com>
2020-09-07 11:07:54 -0400

    added ListSessions with unit tests

--- internal/session/repository.go
+++ internal/session/repository.go
@@ -3,6 +3,8 @@ package session
 import (
 	"context"
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/hashicorp/boundary/internal/db"
 	"github.com/hashicorp/boundary/internal/kms"
@@ -56,8 +58,24 @@ func (r *Repository) LookupSession(ctx context.Context, publicId string, opt ...
 	panic("not implemented")
 }
 
+// ListSessions will sessions.  Supports the WithLimit, WithScopeId and WithOrder options.
 func (r *Repository) ListSessions(ctx context.Context, opt ...Option) ([]*Session, error) {
-	panic("not implemented")
+	opts := getOpts(opt...)
+	var where []string
+	var args []interface{}
+	switch {
+	case opts.withScopeId != "":
+		where, args = append(where, "scope_id = ?"), append(args, opts.withScopeId)
+	case opts.withUserId != "":
+		where, args = append(where, "user_id = ?"), append(args, opts.withUserId)
+	}
+
+	var sessions []*Session
+	err := r.list(ctx, &sessions, strings.Join(where, " and"), args, opt...)
+	if err != nil {
+		return nil, fmt.Errorf("list sessions: %w", err)
+	}
+	return sessions, nil
 }
 
 func (r *Repository) DeleteSession(ctx context.Context, publicId string, opt ...Option) (int, error) {
@@ -70,4 +88,32 @@ func (r *Repository) UpdateSession(ctx context.Context, s *Session, version uint
 
 func (r *Repository) UpdateState(ctx context.Context, sessionId string, sessionVersion uint32, s Status, opt ...Option) (*Session, []*State, int, error) {
 	panic("not implemented")
+}
+
+// list will return a listing of resources and honor the WithLimit option or the
+// repo defaultLimit.  Supports WithOrder option.
+func (r *Repository) list(ctx context.Context, resources interface{}, where string, args []interface{}, opt ...Option) error {
+	opts := getOpts(opt...)
+	limit := r.defaultLimit
+	var dbOpts []db.Option
+	if opts.withLimit != 0 {
+		// non-zero signals an override of the default limit for the repo.
+		limit = opts.withLimit
+	}
+	dbOpts = append(dbOpts, db.WithLimit(limit))
+	if opts.withOrder != "" {
+		dbOpts = append(dbOpts, db.WithOrder(opts.withOrder))
+	}
+	return r.reader.SearchWhere(ctx, resources, where, args, dbOpts...)
+}
+
+func fetchStates(ctx context.Context, r db.Reader, sessionId string, opt ...db.Option) ([]*State, error) {
+	var states []*State
+	if err := r.SearchWhere(ctx, &states, "session_id = ?", []interface{}{sessionId}, opt...); err != nil {
+		return nil, fmt.Errorf("fetch session states: %w", err)
+	}
+	if len(states) == 0 {
+		return nil, nil
+	}
+	return states, nil
 }
--- internal/session/repository_test.go
+++ internal/session/repository_test.go
@@ -1,9 +1,12 @@
 package session
 
 import (
+	"context"
 	"testing"
 
+	"github.com/golang/protobuf/ptypes"
 	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/iam"
 	"github.com/hashicorp/boundary/internal/kms"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -71,4 +74,123 @@ func TestNewRepository(t *testing.T) {
 			assert.Equal(tt.want, got)
 		})
 	}
+}
+
+func TestRepository_ListSession(t *testing.T) {
+	t.Parallel()
+	conn, _ := db.TestSetup(t, "postgres")
+	const testLimit = 10
+	wrapper := db.TestWrapper(t)
+	iamRepo := iam.TestRepo(t, conn, wrapper)
+	rw := db.New(conn)
+	kms := kms.TestKms(t, conn, wrapper)
+	repo, err := NewRepository(rw, rw, kms, WithLimit(testLimit))
+	require.NoError(t, err)
+	composedOf := TestSessionParams(t, conn, wrapper, iamRepo)
+
+	type args struct {
+		opt []Option
+	}
+	tests := []struct {
+		name      string
+		createCnt int
+		args      args
+		wantCnt   int
+		wantErr   bool
+	}{
+		{
+			name:      "no-limit",
+			createCnt: repo.defaultLimit + 1,
+			args: args{
+				opt: []Option{WithLimit(-1)},
+			},
+			wantCnt: repo.defaultLimit + 1,
+			wantErr: false,
+		},
+		{
+			name:      "default-limit",
+			createCnt: repo.defaultLimit + 1,
+			args:      args{},
+			wantCnt:   repo.defaultLimit,
+			wantErr:   false,
+		},
+		{
+			name:      "custom-limit",
+			createCnt: repo.defaultLimit + 1,
+			args: args{
+				opt: []Option{WithLimit(3)},
+			},
+			wantCnt: 3,
+			wantErr: false,
+		},
+		{
+			name:      "withScopeId",
+			createCnt: repo.defaultLimit + 1,
+			args: args{
+				opt: []Option{WithScopeId(composedOf.ScopeId)},
+			},
+			wantCnt: repo.defaultLimit,
+			wantErr: false,
+		},
+		{
+			name:      "bad-withScopeId",
+			createCnt: repo.defaultLimit + 1,
+			args: args{
+				opt: []Option{WithScopeId("o_thisIsNotValid")},
+			},
+			wantCnt: 0,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			require.NoError(conn.Where("1=1").Delete(allocSession()).Error)
+			testSessions := []*Session{}
+			for i := 0; i < tt.createCnt; i++ {
+				s := TestSession(t, conn, composedOf)
+				testSessions = append(testSessions, s)
+			}
+			assert.Equal(tt.createCnt, len(testSessions))
+			got, err := repo.ListSessions(context.Background(), tt.args.opt...)
+			if tt.wantErr {
+				require.Error(err)
+				return
+			}
+			require.NoError(err)
+			assert.Equal(tt.wantCnt, len(got))
+		})
+	}
+	t.Run("withOrder", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		require.NoError(conn.Where("1=1").Delete(allocSession()).Error)
+		wantCnt := 5
+		for i := 0; i < wantCnt; i++ {
+			_ = TestSession(t, conn, composedOf)
+		}
+		got, err := repo.ListSessions(context.Background(), WithOrder("create_time asc"))
+		require.NoError(err)
+		assert.Equal(wantCnt, len(got))
+
+		for i := 0; i < len(got)-1; i++ {
+			first, err := ptypes.Timestamp(got[i].GetCreateTime().Timestamp)
+			require.NoError(err)
+			second, err := ptypes.Timestamp(got[i+1].GetCreateTime().Timestamp)
+			require.NoError(err)
+			assert.True(first.Before(second))
+		}
+	})
+	t.Run("withUserId", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		require.NoError(conn.Where("1=1").Delete(allocSession()).Error)
+		wantCnt := 5
+		for i := 0; i < wantCnt; i++ {
+			_ = TestSession(t, conn, composedOf)
+		}
+		s := TestDefaultSession(t, conn, wrapper, iamRepo)
+		got, err := repo.ListSessions(context.Background(), WithUserId(s.UserId))
+		require.NoError(err)
+		assert.Equal(1, len(got))
+		assert.Equal(got[0].UserId, s.UserId)
+	})
 }
