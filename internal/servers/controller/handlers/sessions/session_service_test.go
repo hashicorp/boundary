feac996fea3580bbfc9349576647dcd75a2b615b
Todd Knight <T.Alan.Knight@gmail.com>
2020-09-16 13:45:01 -0700

    Adding the base for session service testing.  Tests fail.

--- internal/servers/controller/handlers/sessions/session_service_test.go
+++ internal/servers/controller/handlers/sessions/session_service_test.go
@@ -1,1 +1,101 @@
-package sessions
+package sessions_test
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/boundary/internal/auth"
+	"github.com/hashicorp/boundary/internal/db"
+	pb "github.com/hashicorp/boundary/internal/gen/controller/api/resources/sessions"
+	pbs "github.com/hashicorp/boundary/internal/gen/controller/api/services"
+	"github.com/hashicorp/boundary/internal/host/static"
+	"github.com/hashicorp/boundary/internal/iam"
+	"github.com/hashicorp/boundary/internal/servers/controller/handlers/sessions"
+	"github.com/hashicorp/boundary/internal/session"
+	"github.com/hashicorp/boundary/internal/target"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestGetSession(t *testing.T) {
+	conn, _ := db.TestSetup(t, "postgres")
+	wrap := db.TestWrapper(t)
+	iamRepo := iam.TestRepo(t, conn, wrap)
+
+	iamRepoFn := func() (*iam.Repository, error) {
+		return iamRepo, nil
+	}
+	sessRepoFn := func() (*session.Repository, error) {
+		return nil, nil
+	}
+
+	o, p := iam.TestScopes(t, iamRepo)
+	u := iam.TestUser(t, iamRepo, o.GetPublicId())
+	hc := static.TestCatalogs(t, conn, p.GetPublicId(), 1)[0]
+	hs := static.TestSets(t, conn, hc.GetPublicId(), 1)[0]
+	h := static.TestHosts(t, conn, hc.GetPublicId(), 1)[0]
+	static.TestSetMembers(t, conn, hs.GetPublicId(), []*static.Host{h})
+	tar := target.TestTcpTarget(t, conn, p.GetPublicId(), "test", target.WithHostSets([]string{hs.GetPublicId()}))
+
+	sess := session.TestSession(t, conn, wrap, session.ComposedOf{
+		UserId:      u.GetPublicId(),
+		HostId:      h.GetPublicId(),
+		TargetId:    tar.GetPublicId(),
+		HostSetId:   hs.GetPublicId(),
+		AuthTokenId: "",
+		ScopeId:     p.GetPublicId(),
+	})
+
+	wireSess := &pb.Session{
+		Id: sess.GetPublicId(),
+	}
+
+	cases := []struct {
+		name    string
+		scopeId string
+		req     *pbs.GetSessionRequest
+		res     *pbs.GetSessionResponse
+		errCode codes.Code
+	}{
+		{
+			name:    "Get a session",
+			scopeId: sess.ScopeId,
+			req:     &pbs.GetSessionRequest{Id: p.GetPublicId()},
+			res:     &pbs.GetSessionResponse{Item: wireSess},
+			errCode: codes.OK,
+		},
+		{
+			name:    "Get a non existant Session",
+			req:     &pbs.GetSessionRequest{Id: session.SessionPrefix + "_DoesntExis"},
+			res:     nil,
+			errCode: codes.NotFound,
+		},
+		{
+			name:    "Wrong id prefix",
+			req:     &pbs.GetSessionRequest{Id: "j_1234567890"},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+		{
+			name:    "space in id",
+			req:     &pbs.GetSessionRequest{Id: session.SessionPrefix + "_1 23456789"},
+			res:     nil,
+			errCode: codes.InvalidArgument,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+
+			s, err := sessions.NewService(sessRepoFn, iamRepoFn)
+			require.NoError(err, "Couldn't create new group service.")
+
+			got, gErr := s.GetSession(auth.DisabledAuthTestContext(auth.WithScopeId(tc.scopeId)), tc.req)
+			assert.Equal(tc.errCode, status.Code(gErr), "GetSession(%+v) got error %v, wanted %v", tc.req, gErr, tc.errCode)
+			assert.Empty(cmp.Diff(got, tc.res, protocmp.Transform()), "GetSession(%q) got response\n%q, wanted\n%q", tc.req, got, tc.res)
+		})
+	}
+}
