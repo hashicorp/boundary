57fb229ab20dc9b2fb1c43481efb5ad453f1e177
Michael Gaffney <mike@gaffney.cc>
2020-09-16 12:25:47 -0400

    Add views for host dimension ingress

--- internal/db/migrations/postgres/65_wh_session_dimensions.down.sql
+++ internal/db/migrations/postgres/65_wh_session_dimensions.down.sql
@@ -1,5 +1,7 @@
 begin;
 
+  drop view whx_host_dimension_current;
+  drop view whx_host_dimension;
   drop table wh_user_dimension;
   drop table wh_host_dimension;
 
--- internal/db/migrations/postgres/65_wh_session_dimensions.up.sql
+++ internal/db/migrations/postgres/65_wh_session_dimensions.up.sql
@@ -80,4 +80,76 @@ begin;
     row_expiration_time           wt_timestamp
   );
 
+create view whx_host_dimension as
+select
+       h.public_id                     as host_id,
+       'static host'                   as host_type,
+       coalesce(h.name, 'None')        as host_name,
+       coalesce(h.description, 'None') as host_description,
+       coalesce(h.address, 'Unknown')  as host_address,
+       s.public_id                     as host_set_id,
+       'static host set'               as host_set_type,
+       coalesce(s.name, 'None')        as host_set_name,
+       coalesce(s.description, 'None') as host_set_description,
+       c.public_id                     as host_catalog_id,
+       'static host catalog'           as host_catalog_type,
+       coalesce(c.name, 'None')        as host_catalog_name,
+       coalesce(c.description, 'None') as host_catalog_description,
+       t.public_id                     as target_id,
+       'tcp target'                    as target_type,
+       coalesce(t.name, 'None')        as target_name,
+       coalesce(t.description, 'None') as target_description,
+       p.public_id                     as project_id,
+       coalesce(p.name, 'None')        as project_name,
+       coalesce(p.description, 'None') as project_description,
+       o.public_id                     as host_organization_id,
+       coalesce(o.name, 'None')        as host_organization_name,
+       coalesce(o.description, 'None') as host_organization_description
+  from static_host as h,
+       static_host_catalog as c,
+       static_host_set_member as m,
+       static_host_set as s,
+       target_host_set as ts,
+       target_tcp as t,
+       iam_scope as p,
+       iam_scope as o
+ where h.catalog_id = c.public_id
+   and h.public_id = m.host_id
+   and s.public_id = m.set_id
+   and t.public_id = ts.target_id
+   and s.public_id = ts.host_set_id
+   and p.public_id = t.scope_id
+   and p.type = 'project'
+   and o.public_id = p.parent_id
+   and o.type = 'org'
+;
+
+create view whx_host_dimension_current as
+select host_id,
+       host_type,
+       host_name,
+       host_description,
+       host_address,
+       host_set_id,
+       host_set_type,
+       host_set_name,
+       host_set_description,
+       host_catalog_id,
+       host_catalog_type,
+       host_catalog_name,
+       host_catalog_description,
+       target_id,
+       target_type,
+       target_name,
+       target_description,
+       project_id,
+       project_name,
+       project_description,
+       host_organization_id,
+       host_organization_name,
+       host_organization_description
+  from wh_host_dimension
+ where current_row_indicator = 'Current'
+;
+
 commit;
