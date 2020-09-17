3cf9ab7ce42c95b51807ef1fad5a0aeb349b3058
Jim Lambert <jlambert@hashicorp.com>
2020-09-17 15:49:46 -0400

    added terminateSessionCte

--- internal/session/query.go
+++ internal/session/query.go
@@ -17,4 +17,38 @@ with not_active as (
 )
 select * from not_active;
 `
+
+	terminateSessionCte = `
+	insert into session_state
+	with terminated as (
+		select s.public_id, 'terminated' as state
+		from 
+			session s
+		where 
+			s.version = 1  and
+			s.public_id in (
+				-- sesions without any connections
+				select s.public_id 
+				from 
+					session s
+				left join session_connection sc ON sc.session_id = s.public_id 
+				where 
+					sc.session_id is null
+					and s.public_id = $1
+				union
+				-- sessions where all connections are closed
+				select s.public_id 
+				from
+					session s,
+					session_connection c,
+					session_connection_state cs
+				where
+					s.public_id = c.session_id and
+					c.public_id = cs.connection_id and
+					cs.state = 'closed' and 
+					s.public_id = $1
+			) 
+	)
+	select * from terminated;
+`
 )
