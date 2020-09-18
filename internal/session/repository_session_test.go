4a1dadaaa82fac44aa78e8e75910d9172240b919
Jim Lambert <jlambert@hashicorp.com>
2020-09-18 13:51:48 -0400

    unit test for CloseConnections

--- internal/session/repository_session_test.go
+++ internal/session/repository_session_test.go
@@ -583,7 +583,99 @@ func TestRepository_TerminateSession(t *testing.T) {
 }
 
 func TestRepository_CloseConnections(t *testing.T) {
-	panic("test not implemented")
+	t.Parallel()
+	conn, _ := db.TestSetup(t, "postgres")
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+	iamRepo := iam.TestRepo(t, conn, wrapper)
+	kms := kms.TestKms(t, conn, wrapper)
+	repo, err := NewRepository(rw, rw, kms)
+	require.NoError(t, err)
+
+	setupFn := func(cnt int) []CloseWith {
+		s := TestDefaultSession(t, conn, wrapper, iamRepo)
+		srv := TestWorker(t, conn, wrapper)
+		tofu := TestTofu(t)
+		s, _, err := repo.ActivateSession(context.Background(), s.PublicId, s.Version, srv.PrivateId, srv.Type, tofu)
+		require.NoError(t, err)
+		cw := make([]CloseWith, 0, cnt)
+		for i := 0; i < cnt; i++ {
+			c := TestConnection(t, conn, s.PublicId, "127.0.0.1", 22, "127.0.0.1", 2222)
+			require.NoError(t, err)
+			cw = append(cw, CloseWith{
+				ConnectionId:      c.PublicId,
+				ConnectionVersion: c.Version,
+				BytesUp:           1,
+				BytesDown:         2,
+				ClosedReason:      ConnectionClosedByUser,
+			})
+		}
+		return cw
+	}
+	tests := []struct {
+		name        string
+		closeWith   []CloseWith
+		reason      TerminationReason
+		wantErr     bool
+		wantIsError error
+	}{
+		{
+			name:      "valid",
+			closeWith: setupFn(2),
+			reason:    ClosedByUser,
+		},
+		{
+			name:        "empty-closed-with",
+			closeWith:   []CloseWith{},
+			reason:      ClosedByUser,
+			wantErr:     true,
+			wantIsError: db.ErrInvalidParameter,
+		},
+		{
+			name: "missing-ConnectionId",
+			closeWith: func() []CloseWith {
+				cw := setupFn(2)
+				cw[1].ConnectionId = ""
+				return cw
+
+			}(),
+			reason:      ClosedByUser,
+			wantErr:     true,
+			wantIsError: db.ErrInvalidParameter,
+		},
+		{
+			name: "ConnectionVersion",
+			closeWith: func() []CloseWith {
+				cw := setupFn(2)
+				cw[1].ConnectionVersion = 0
+				return cw
+
+			}(),
+			reason:      ClosedByUser,
+			wantErr:     true,
+			wantIsError: db.ErrInvalidParameter,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			resp, err := repo.CloseConnections(context.Background(), tt.closeWith)
+			if tt.wantErr {
+				require.Error(err)
+				if tt.wantIsError != nil {
+					assert.Truef(errors.Is(err, tt.wantIsError), "unexpected error %s", err.Error())
+				}
+				return
+			}
+			require.NoError(err)
+			assert.Equal(len(tt.closeWith), len(resp))
+			for _, r := range resp {
+				require.NotNil(r.Connection)
+				require.NotNil(r.ConnectionStates)
+				assert.Equal(StatusClosed.String(), r.ConnectionStates[0].Status)
+			}
+		})
+	}
 }
 
 func TestRepository_CancelSession(t *testing.T) {
