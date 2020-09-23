ba9f6b2cd6362a594f84868eb40803ea20ed2787
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2020-09-23 15:47:30 -0500

    [Website] Add Auth  (#417)
    * Initial auth commit
    
    * Add Auth0 to preview env config
    
    * Proper website package name

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,13 @@
 const withHashicorp = require('@hashicorp/nextjs-scripts')
 const path = require('path')
 
+// log out our primary environment variables for clarity in build logs
+console.log(`HASHI_ENV: ${process.env.HASHI_ENV}`)
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+console.log(`NEXTAUTH_URL: ${process.env.NEXTAUTH_URL}`)
+console.log(`OKTA_DOMAIN: ${process.env.OKTA_DOMAIN}`)
+console.log(`AUTH0_DOMAIN: ${process.env.AUTH0_DOMAIN}`)
+
 module.exports = withHashicorp({
   defaultLayout: true,
   transpileModules: ['is-absolute-url', '@hashicorp/react-mega-nav'],
@@ -12,5 +19,7 @@ module.exports = withHashicorp({
     SEGMENT_WRITE_KEY: 'xxx',
     BUGSNAG_CLIENT_KEY: 'xxx',
     BUGSNAG_SERVER_KEY: 'xxx',
+    NEXT_PUBLIC_OKTA_DOMAIN: process.env.OKTA_DOMAIN,
+    NEXT_PUBLIC_AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   },
 })
