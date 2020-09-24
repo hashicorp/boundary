7fcbffbe88d7855841a8705f5acabb921f8c35a5
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2020-09-24 12:19:00 -0400

    Remove not-set value. (#427)
    I don't think the conditions exist here that necessitated it being
    introduced into Vault, but I do sometimes see a baffling condition where
    the address coming from the flag is empty. I don't think we need this so
    take it out.

--- internal/cmd/commands/server/server.go
+++ internal/cmd/commands/server/server.go
@@ -89,7 +89,6 @@ func (c *Command) Flags() *base.FlagSets {
 	f.StringVar(&base.StringVar{
 		Name:       "log-level",
 		Target:     &c.flagLogLevel,
-		Default:    base.NotSetValue,
 		EnvVar:     "BOUNDARY_LOG_LEVEL",
 		Completion: complete.PredictSet("trace", "debug", "info", "warn", "err"),
 		Usage: "Log verbosity level. Supported values (in order of more detail to less) are " +
@@ -99,7 +98,6 @@ func (c *Command) Flags() *base.FlagSets {
 	f.StringVar(&base.StringVar{
 		Name:       "log-format",
 		Target:     &c.flagLogFormat,
-		Default:    base.NotSetValue,
 		Completion: complete.PredictSet("standard", "json"),
 		Usage:      `Log format. Supported values are "standard" and "json".`,
 	})
