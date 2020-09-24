8251786be6d4ffdd1578035e045380f1d85523d0
Sam Salisbury <samsalisbury@gmail.com>
2020-09-24 13:49:20 +0100

    add packagespec

--- scripts/uigen.sh
+++ scripts/uigen.sh
@@ -2,21 +2,41 @@
 
 set -e
 
-origdir=$(pwd)
-tempdir=$(mktemp -d update-ui-assets.XXXXXX)
-ui_commitish=${UI_COMMITISH:-develop}
+ui_commitish="${UI_COMMITISH:-develop}"
 
-cd $tempdir
-git clone https://github.com/hashicorp/boundary-ui
-cd boundary-ui
-git fetch origin ${ui_commitish}
-git checkout ${ui_commitish}
+targetdir="internal/ui"
+shafile="${targetdir}/VERSION"
+shafileabs="$(pwd)/${shafile}"
+tempdir="${targetdir}/source"
+uirepodir="${tempdir}/boundary-ui"
+mkdir -p "${tempdir}"
+echo "*" > "${tempdir}/.gitignore"
 
-docker-compose -f docker-compose-embedding.yml run build
+if ! [ -d "${uirepodir}/.git" ]; then
+	git clone https://github.com/hashicorp/boundary-ui "${uirepodir}"
+fi
 
-cd $origdir
+(
+	cd "${uirepodir}"
+	git reset --hard
+	git fetch origin "${ui_commitish}"
+	git checkout "${ui_commitish}"
+	if ! docker-compose -f docker-compose-embedding.yml run build; then
+		echo "==> UI build failed."
+		exit 1
+	fi
+	git log -n1 --pretty=oneline > "${shafileabs}"
+	echo "# Above commit is used for production builds." >> "${shafileabs}"
+)
 
-go-bindata -fs -o internal/ui/assets.go -pkg ui -prefix "${origdir}/${tempdir}/boundary-ui/ui/core/dist" "${origdir}/${tempdir}/boundary-ui/ui/core/dist" "${origdir}/${tempdir}/boundary-ui/ui/core/dist/assets"
+uidir="${uirepodir}/ui/core/dist"
 
+target="${targetdir}/assets.go"
 
-rm -rf $tempdir
+go-bindata -fs -o "${target}.tmp" -pkg ui -prefix "${uidir}" "${uidir}" "${uidir}/assets"
+
+printf "// +build ui\n" > "${target}"
+cat "${target}.tmp" >> "${target}"
+rm "${target}.tmp"
+
+rm -rf "$tempdir"
