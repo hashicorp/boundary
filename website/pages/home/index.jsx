8e0a5e8bd11e08b34691a2b82c166ed79ab80b1a
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2020-09-25 13:21:54 -0400

    [Website] Add Boundary Features section to homepage (#436)
    * Install and implement ProductFeaturesList
    
    * Add images and content
    
    * Inline the product features

--- website/pages/home/index.jsx
+++ website/pages/home/index.jsx
@@ -1,4 +1,5 @@
 import HomepageHero from 'components/homepage-hero'
+import ProductFeaturesList from '@hashicorp/react-product-features-list'
 
 export default function HomePage() {
   return (
@@ -16,6 +17,47 @@ export default function HomePage() {
             text: 'Get Started',
             url: '/docs/getting-started',
             type: 'inbound',
+          },
+        ]}
+      />
+      <ProductFeaturesList
+        heading="Boundary Features"
+        features={[
+          {
+            title: 'Identity-based access',
+            content:
+              'Enables privileged sessions for users and  applications based on user identity and role.',
+            icon: require('./img/boundary-identitybasedaccess.jpg?url'),
+          },
+          {
+            title: 'Session management',
+            content:
+              'Ensures access control regardless of user or operators’ infrastructure.',
+            icon: require('./img/boundary-sessionmanagement.jpg?url'),
+          },
+          {
+            title: 'Platform agnostic',
+            content:
+              'One workflow for identity-based access across clouds, kubernetes clusters, and on-prem infrastructure.',
+            icon: require('./img/boundary-platformagnosticity.jpg?url'),
+          },
+          {
+            title: 'Session visibility',
+            content:
+              'Visibility into session metrics, events, logs, and traces with the ability to export data to business intelligence and event monitoring tools.',
+            icon: require('./img/boundary-sessionvisibility.jpg?url'),
+          },
+          {
+            title: 'Configuration as code',
+            content:
+              'Ability to establish secure access controls and route resources dynamically.',
+            icon: require('./img/boundary-configascode.jpg?url'),
+          },
+          {
+            title: 'Managing dynamic environments',
+            content:
+              'Secure access to dynamic systems and applications with automated controls.',
+            icon: require('./img/boundary-managingdynamicenvironments.jpg?url'),
           },
         ]}
       />
