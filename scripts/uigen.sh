2521bfc7037fb08063a9ba2dca59b1222cefda05
Sam Salisbury <samsalisbury@gmail.com>
2020-09-26 08:38:15 +0100

    scripts/uigen: obey shellcheck

--- scripts/uigen.sh
+++ scripts/uigen.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-origdir=$(pwd)
-tempdir=$(mktemp -d update-ui-assets.XXXXXX)
-ui_commitish=${UI_COMMITISH:-develop}
+origdir="$(pwd)"
+tempdir="$(mktemp -d update-ui-assets.XXXXXX)"
+ui_commitish="${UI_COMMITISH:-develop}"
 
-cd $tempdir
+cd "$tempdir"
 git clone https://github.com/hashicorp/boundary-ui
 cd boundary-ui
-git fetch origin ${ui_commitish}
-git checkout ${ui_commitish}
+git fetch origin "${ui_commitish}"
+git checkout "${ui_commitish}"
 
 docker-compose -f docker-compose-embedding.yml run build
 
-cd $origdir
+cd "$origdir"
 
 go-bindata -fs -o internal/ui/assets_dev.go -pkg ui -prefix "${origdir}/${tempdir}/boundary-ui/ui/core/dist" "${origdir}/${tempdir}/boundary-ui/ui/core/dist" "${origdir}/${tempdir}/boundary-ui/ui/core/dist/assets"
 
@@ -23,4 +23,4 @@ echo '// +build dev' > internal/ui/assets_dev.go
 cat internal/ui/.assets_dev.go >> internal/ui/assets_dev.go
 rm internal/ui/.assets_dev.go
 
-rm -rf $tempdir
+rm -rf "$tempdir"
