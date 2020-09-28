afe89415832b4078b00633f01d624ddda7d64273
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2020-09-28 17:23:27 -0400

    Add CSS and adjust structure

--- website/components/how-it-works/index.jsx
+++ website/components/how-it-works/index.jsx
@@ -2,25 +2,32 @@ import s from './how-it-works.module.css'
 
 export default function HowItWorks({ sections }) {
   return (
-    <div className={s.howItWorks}>
-      <div className={s.image}>
-        <img src={require('./img/how-it-works.jpg?url')} />
+    <div className={s.root}>
+      <h2 className="g-type-display-2">How it works</h2>
+      <div className={`g-grid-container ${s.contentContainer}`}>
+        <div className={s.image}>
+          <img alt="" src={require('./img/how-it-works.jpg?url')} />
+        </div>
+        <ul className={s.sections}>
+          {/* TODO Remove sections ternary when sections is no longer temp */}
+          {sections
+            ? sections.map(({ title, description, logos, footerText }) => (
+                <li key="title">
+                  <h4 className="g-type-display-4">{title}</h4>
+                  <p className="g-type-body">{description}</p>
+                  {logos ? (
+                    <div>{`<LogoList logos={logos} /> here`}</div>
+                  ) : null}
+                  {footerText ? (
+                    <p className={`g-type-tag-label ${s.footerText}`}>
+                      {footerText}
+                    </p>
+                  ) : null}
+                </li>
+              ))
+            : null}
+        </ul>
       </div>
-      <ul className={s.sections}>
-        {/* TODO Remove sections ternary when sections is no longer temp */}
-        {sections
-          ? sections.map(({ title, description, logos, footerText }) => (
-              <li key="title">
-                <h4 className="g-type-display-4">{title}</h4>
-                <p className="g-type-body">{description}</p>
-                {logos ? <div>{`<LogoList logos={logos} /> here`}</div> : null}
-                {footerText ? (
-                  <p className="g-type-tag-label">{footerText}</p>
-                ) : null}
-              </li>
-            ))
-          : null}
-      </ul>
     </div>
   )
 }
