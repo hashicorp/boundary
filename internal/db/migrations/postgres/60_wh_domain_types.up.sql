a9e806b6736b2d944b6732c7c9394d36ae95e059
Michael Gaffney <mgaffney@users.noreply.github.com>
2020-09-30 14:09:40 -0400

    Data warehouse (#475)
    * Define initial warehouse schema
    
    * Add session id as the primary key to the session accumulating fact table
    
    * Insert a row to use instead of nulls in the time and date dimensions
    
    * Add missing attributes and a partial index
    
    * Rename files
    
    * Refactor: move fact table to a separate migration
    
    * Add domain types for the warehouse
    
    * Add session connection transaction fact table
    
    * Change columns to match ingress data types
    
    * Add views for host dimension ingress
    
    * Create source and target views for the host and user dimensions
    
    * Generate random ID for dimension table primary key
    
    * Formatting changes
    
    * Add current dimension id to target views
    
    * Implement upsert function for host dimension
    
    * Use domain types for upsert function parameters
    
    * Fix indentation
    
    * Add wh_public_id domain type
    
    * Fix value of type columns when the other columns are null
    
    * Implement upsert function for user dimension
    
    * Add warehouse domain types for timestamps and text fields
    
    * Move fact table migrations to a higher number
    
    * Add functions for getting the current date and current time IDs
    
    * Rename backend to endpoint in column names
    
    * Rename fact tables
    
    The fact tables are actually accumulating fact tables not transactional
    fact tables.
    
    * Add default value to connection_authorized_time column
    
    * Update session fact table
    
    * Add session_max_seconds, session_connection_limit, and default_port to wh_host_dimension
    
    * Move session table above connection table
    
    * Add after insert trigger to session table for warehouse ingress
    
    * Add fk from connection warehouse table to session warehouse table
    
    * Implement insert trigger for session connections
    
    * Update warehouse for a user having multiple auth accounts
    
    * Rename warehouse columns to be consistent with operational tables
    
    * Add connection specific columns to the connection fact table
    
    * Update session calculations in warehouse
    
    * Rename functions and formatting changes
    
    * Rename session columns to match names of session states
    
    * Implement warehouse triggers for session and connection state changes
    
    * Run 'make migrations'
    
    * Use a static start and end date for the warehouse date dimension
    
    * Document warehouse functions
    
    * Remove outdated TODO comments
    
    * Add TODO comments for additional dimensions and fact table columns

---
+++ internal/db/migrations/postgres/60_wh_domain_types.up.sql
@@ -0,0 +1,84 @@
+begin;
+
+  create extension if not exists "pgcrypto";
+
+  create domain wh_inet_port as integer
+  check(
+    value > 0
+    and
+    value <= 65535
+  );
+  comment on domain wh_inet_port is
+  'An ordinal number between 1 and 65535 representing a network port';
+
+  create domain wh_bytes_transmitted as bigint
+  check(
+    value >= 0
+  );
+  comment on domain wh_bytes_transmitted is
+  'A non-negative integer representing the number of bytes transmitted';
+
+  -- wh_dim_id generates and returns a random ID which should be considered as
+  -- universally unique.
+  create or replace function wh_dim_id()
+    returns text
+  as $$
+    select encode(digest(gen_random_bytes(16), 'sha256'), 'base64');
+  $$ language sql;
+
+  create domain wh_dim_id as text
+  check(
+    length(trim(value)) > 0
+  );
+  comment on domain wh_dim_id is
+  'Random ID generated with pgcrypto';
+
+  create domain wh_public_id as text
+  check(
+    value = 'None'
+    or
+    length(trim(value)) > 10
+  );
+  comment on domain wh_public_id is
+  'Equivalent to wt_public_id but also allows the value to be ''None''';
+
+  create domain wh_timestamp as timestamp with time zone not null;
+  comment on domain wh_timestamp is
+  'Timestamp used in warehouse tables';
+
+  create domain wh_dim_text as text not null
+  check(
+    length(trim(value)) > 0
+  );
+  comment on domain wh_dim_text is
+  'Text fields in dimension tables are always not null and always not empty strings';
+
+  -- wh_date_id returns the wh_date_dimension id for ts.
+  create or replace function wh_date_id(ts wh_timestamp)
+    returns integer
+  as $$
+    select to_char(ts, 'YYYYMMDD')::integer;
+  $$ language sql;
+
+  -- wh_time_id returns the wh_time_of_day_dimension id for ts.
+  create or replace function wh_time_id(ts wh_timestamp)
+    returns integer
+  as $$
+    select to_char(ts, 'SSSS')::integer;
+  $$ language sql;
+
+  -- wh_date_id returns the wh_date_dimension id for current_timestamp.
+  create or replace function wh_current_date_id()
+    returns integer
+  as $$
+    select wh_date_id(current_timestamp);
+  $$ language sql;
+
+  -- wh_time_id returns the wh_time_of_day_dimension id for current_timestamp.
+  create or replace function wh_current_time_id()
+    returns integer
+  as $$
+    select wh_time_id(current_timestamp);
+  $$ language sql;
+
+commit;
---
+++ internal/db/migrations/postgres/62_wh_datetime.down.sql
@@ -0,0 +1,6 @@
+begin;
+
+  drop table wh_time_of_day_dimension;
+  drop table wh_date_dimension;
+
+commit;
