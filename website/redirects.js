e61f5ecc227919c02d8f5a7a162574cc3d5094cb
Kyle MacDonald <kmacdonald@hashicorp.com>
2020-10-05 13:00:50 -0400

    website: add basic support for redirects.js

---
+++ website/redirects.js
@@ -0,0 +1,9 @@
+module.exports = [
+  // define your custom redirects within this file.
+  // vercel's redirect documentation: https://vercel.com/docs/configuration#project/redirects
+  //   {
+  //     source: '/foo',
+  //     destination: '/bar',
+  //     permanent: false,
+  //   },
+]
