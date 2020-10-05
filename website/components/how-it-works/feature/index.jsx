b3f35e42a7b62dc5f07f5503fadc7a4c1f0cceee
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2020-10-05 14:54:08 -0400

    [Website] Add "How it works" section (#483)
    * Create skeleton and implement component
    
    * Add sections
    
    * Add CSS and adjust structure
    
    * Remove sections ternary
    
    * Change nomenclature for clarity
    
    * Mobile tweaks
    
    * Tweaks inspired by HCP
    
    * Create LogoList component skeleton
    
    * Implement LogoList on main component
    
    - Shift footertext responsibility to it
    - Assume logos will be passed in, because it doesn't render otherwise
      anyway with the right checks
    
    * Implement some logos/restructure LogoList
    
    * CSS adjustments to logolist/section layout
    
    * Show example for too many integrations
    
    * Remove cutoff logic
    
    * Add SVGs for diagram stack
    
    * Combine SVGs into a component
    
    This is necessary to act on various parts of the image independently.
    
    * Fully combine SVG and remove unneeded CSS
    
    * Further CSS cleanup
    
    * Implement stickyness
    
    * Set up active example checks for graphic
    
    * Implement reactivity
    
    - Apply styles based on inactivity or activity, whichever fits best
      per-element
    - Add expected state to the general HowItWorks component, where it will
      be managed and passed into the diagram
    - Make icon and arrow spacers use the same spacer class
    
    * CSS tweaks for design/organization
    
    * Implement scroll-to-animate functionality!
    
    * Fill out icons for integrations
    
    * Feature updates
    
    - Move LogoList into folder
    - Give descriptions some margin
    
    * Mobile treatment tweaks
    
    * Import logic from the cloud-platform page on .com
    
    This fixes a bug in which the in-view example would not update in an
    expected manner.
    
    * Remove max-width adjustment on UL
    
    * Fix need for height on the graphic
    
    * Padding/margin tweaks
    
    * Minor CSS tweaks
    
    * Final padding adjustments

---
+++ website/components/how-it-works/feature/index.jsx
@@ -0,0 +1,26 @@
+import LogoList from './logo-list'
+import s from './feature.module.css'
+import { useInView } from 'react-intersection-observer'
+import { useState } from 'react'
+
+export default function Feature({
+  title,
+  description,
+  logos,
+  onInViewStatusChanged,
+}) {
+  const [ref, inView] = useInView({ threshold: 0.8 })
+  const [inViewStatus, setInViewStatus] = useState(false)
+  if (inView != inViewStatus) {
+    setInViewStatus(inView)
+    onInViewStatusChanged(inView)
+  }
+
+  return (
+    <div className={s.root} ref={ref}>
+      <h4 className="g-type-display-4">{title}</h4>
+      <p className="g-type-body">{description}</p>
+      {logos ? <LogoList logos={logos} /> : null}
+    </div>
+  )
+}
