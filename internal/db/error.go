b1eda904518f9378cf38df1e55c79b1e0fb9876f
Jim Lambert <jlambert@hashicorp.com>
2020-10-05 16:19:24 -0400

    draft

--- internal/db/error.go
+++ internal/db/error.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/lib/pq"
 )
@@ -14,7 +15,7 @@ var (
 
 	// ErrInvalidParameter is returned by create and update methods if
 	// an attribute on a struct contains illegal or invalid values.
-	ErrInvalidParameter = errors.New("invalid parameter")
+	ErrInvalidParameter = NewError(WithErrorMsg("invalid parameter"))
 
 	// ErrInvalidFieldMask is returned by update methods if the field mask
 	// contains unknown fields or fields that cannot be updated.
@@ -38,49 +39,6 @@ var (
 	// changed resulting in the transaction being rolled back.
 	ErrMultipleRecords = errors.New("multiple records")
 )
-
-type Dialect string
-
-const (
-	UnknownDialect  Dialect = "unknown"
-	PostgresDialect Dialect = "postgres"
-)
-
-// String representation of the dialect
-func (d Dialect) String() string {
-	return string(d)
-}
-
-type dialectError struct {
-	Dialect Dialect
-	Message string
-	Code    string
-}
-
-type Error struct {
-	Msg string
-
-	Class ErrClass
-	Code  ErrCode
-
-	dialectError *dialectError
-}
-
-type ErrClass string
-type ErrCode string
-
-func NewError(Msg string, t ErrClass, code ErrCode) error {
-	panic("")
-}
-func NewFromDialect(dbError error) error {
-	panic("")
-}
-func ParseError(errStr string) error {
-	panic("")
-}
-func (e *Error) Error() string {
-	panic("")
-}
 
 // IsUniqueError returns a boolean indicating whether the error is known to
 // report a unique constraint violation.
@@ -131,4 +89,111 @@ func IsNotNullError(err error) bool {
 	}
 
 	return false
+}
+
+// Error provides the ability to specify a msg, code and wrapped error.
+type Error struct {
+	// Msg for the error
+	Msg string
+	// Code is the error's code
+	Code    *ErrCode
+	Wrapped error
+}
+
+// NewError creates a new Error and supports the options of:
+// 	WithErrorCode() - allows you to specify an error code
+// 	WithWrap() - allows you to specify an error to wrap
+// 	WithErrorMsg() - allows you to specify an error msg
+func NewError(opt ...Option) error {
+	opts := GetOpts(opt...)
+	if opts.withErrMsg == "" && opts.withErrCode == nil {
+		opts.withErrMsg = "unknown error"
+
+	}
+	return &Error{
+		Wrapped: opts.withErr,
+		Msg:     opts.withErrMsg,
+		Code:    opts.withErrCode,
+	}
+}
+
+// Error satisfies the error interface and returns a string representation of
+// the error.
+func (e *Error) Error() string {
+	var msgs []string
+	if e.Msg != "" {
+		msgs = append(msgs, e.Msg)
+	}
+	if e.Code != nil {
+		if info, ok := errorCodeInfo[*e.Code]; ok {
+			msgs = append(msgs, info.Message, info.Class.String())
+		}
+	}
+	if len(msgs) == 0 {
+		msgs = append(msgs, "unknown")
+	}
+	return strings.Join(msgs, ":")
+}
+
+// Unwrap implements the errors.Unwrap interface and allows callers to use the
+// errors.Is() and errors.As() functions effectively for any wrapped errors.
+func (e *Error) Unwrap(err error) error {
+	return e.Wrapped
+}
+
+// ErrClass specifies the class of error (unknown, parameter, integrity, etc).
+type ErrClass uint32
+
+// ErrCode specifies a code for the error.
+type ErrCode uint32
+type ErrInfo struct {
+	Class   ErrClass
+	Message string
+}
+
+const (
+	UnknownErrClass ErrClass = 0
+	ParameterError  ErrClass = 1
+	IntegrityError  ErrClass = 2
+)
+
+func (e ErrClass) String() string {
+	return [...]string{
+		"unknown",
+		"parameter violation",
+		"integrity violation",
+	}[e]
+}
+
+const (
+	ErrCodeInvalidParameter ErrCode = 100
+	ErrCodeCheckConstraint  ErrCode = 1000
+	ErrCodeNotNull          ErrCode = 1100
+	ErrCodeUnique           ErrCode = 1200
+)
+
+func (e ErrCode) String() string {
+	if i, ok := errorCodeInfo[e]; ok {
+		return i.Message
+	}
+	return "unknown"
+}
+
+var errorCodeInfo = map[ErrCode]ErrInfo{
+	ErrCodeInvalidParameter: {
+		Message: "invalid parameter",
+		Class:   ParameterError,
+	},
+	ErrCodeCheckConstraint: {
+		Message: "constraint check failed",
+		Class:   IntegrityError,
+	},
+	ErrCodeNotNull: {
+		Message: "must not be empty (null) violation",
+		Class:   IntegrityError,
+	},
+	ErrCodeUnique: {
+		Message: "must be unique violation",
+		Class:   IntegrityError,
+	},
 }
