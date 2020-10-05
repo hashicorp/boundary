547c97b7e4bdc5afe0ebb013f144b2618f31f603
Jim Lambert <jlambert@hashicorp.com>
2020-10-05 19:31:23 -0400

    prevent session state changes after session has terminated.

--- internal/session/repository_session_test.go
+++ internal/session/repository_session_test.go
@@ -1037,10 +1037,32 @@ func TestRepository_CancelSession(t *testing.T) {
 		overrideSessionVersion *uint32
 		wantErr                bool
 		wantIsError            error
+		wantStatus             Status
 	}{
 		{
-			name:    "valid",
-			session: setupFn(),
+			name:       "valid",
+			session:    setupFn(),
+			wantStatus: StatusCanceling,
+		},
+		{
+			name: "already-terminated",
+			session: func() *Session {
+				session := TestDefaultSession(t, conn, wrapper, iamRepo)
+				c := TestConnection(t, conn, session.PublicId, "127.0.0.1", 22, "127.0.0.1", 2222)
+				cw := CloseWith{
+					ConnectionId: c.PublicId,
+					BytesUp:      1,
+					BytesDown:    1,
+					ClosedReason: ConnectionClosedByUser,
+				}
+				_, err = repo.CloseConnections(context.Background(), []CloseWith{cw})
+				require.NoError(t, err)
+				s, _, err := repo.LookupSession(context.Background(), session.PublicId)
+				require.NoError(t, err)
+				assert.Equal(t, StatusTerminated, s.States[0].Status)
+				return session
+			}(),
+			wantStatus: StatusTerminated,
 		},
 		{
 			name:    "bad-session-id",
@@ -1050,7 +1072,8 @@ func TestRepository_CancelSession(t *testing.T) {
 				require.NoError(t, err)
 				return &id
 			}(),
-			wantErr: true,
+			wantErr:    true,
+			wantStatus: StatusCanceling,
 		},
 		{
 			name:    "missing-session-id",
@@ -1059,6 +1082,7 @@ func TestRepository_CancelSession(t *testing.T) {
 				id := ""
 				return &id
 			}(),
+			wantStatus:  StatusCanceling,
 			wantErr:     true,
 			wantIsError: db.ErrInvalidParameter,
 		},
@@ -1069,7 +1093,8 @@ func TestRepository_CancelSession(t *testing.T) {
 				v := uint32(101)
 				return &v
 			}(),
-			wantErr: true,
+			wantStatus: StatusCanceling,
+			wantErr:    true,
 		},
 		{
 			name:    "missing-version-id",
@@ -1078,6 +1103,7 @@ func TestRepository_CancelSession(t *testing.T) {
 				v := uint32(0)
 				return &v
 			}(),
+			wantStatus:  StatusCanceling,
 			wantErr:     true,
 			wantIsError: db.ErrInvalidParameter,
 		},
@@ -1110,7 +1136,7 @@ func TestRepository_CancelSession(t *testing.T) {
 			require.NoError(err)
 			require.NotNil(s)
 			require.NotNil(s.States)
-			assert.Equal(StatusCanceling, s.States[0].Status)
+			assert.Equal(tt.wantStatus, s.States[0].Status)
 
 			stateCnt := len(s.States)
 			origStartTime := s.States[0].StartTime
@@ -1120,7 +1146,7 @@ func TestRepository_CancelSession(t *testing.T) {
 			require.NotNil(s2)
 			require.NotNil(s2.States)
 			assert.Equal(stateCnt, len(s2.States))
-			assert.Equal(StatusCanceling, s.States[0].Status)
+			assert.Equal(tt.wantStatus, s.States[0].Status)
 			assert.Equal(origStartTime, s2.States[0].StartTime)
 		})
 	}
