776558f2e2ec2c13d2365f254b7f1c1f95e24bb8
Jim Lambert <jlambert@hashicorp.com>
2020-10-06 10:59:35 -0400

    ErrClass.String() unit tests

--- internal/db/error_test.go
+++ internal/db/error_test.go
@@ -291,3 +291,39 @@ func TestError_Unwrap(t *testing.T) {
 		})
 	}
 }
+
+func TestErrClass_String(t *testing.T) {
+	tests := []struct {
+		name string
+		e    ErrClass
+		want string
+	}{
+		{
+			name: "UnknownErrClass",
+			e:    UnknownErrClass,
+			want: "unknown",
+		},
+		{
+			name: "ParameterError",
+			e:    ParameterError,
+			want: "parameter violation",
+		},
+		{
+			name: "IntegrityError",
+			e:    IntegrityError,
+			want: "integrity violation",
+		},
+		{
+			name: "SearchError",
+			e:    SearchError,
+			want: "search issue",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			got := tt.e.String()
+			assert.Equal(tt.want, got)
+		})
+	}
+}
