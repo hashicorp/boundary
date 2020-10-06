74881bd13a566d2e5959cdde75c3a7d46fffab44
Brandon Romano <BrandonRRomano@gmail.com>
2020-10-06 00:44:42 -0700

    Plugging in proper content into the landing page (#545)
    * Misc content updates to align with copy doc
    
    * Plug in feature SVGs
    
    * Move use cases up
    
    * Remove the MegaNav - to be replaced soon
    
    * Updates the nav
    
    * Add footer content, security page
    
    * Remove react-mega-nav from styles

--- website/components/footer/index.jsx
+++ website/components/footer/index.jsx
@@ -8,6 +8,11 @@ export default function Footer({ openConsentManager }) {
           <Link href="/docs">
             <a>Docs</a>
           </Link>
+          <a href="https://learn.hashicorp.com/boundary">Learn</a>
+          <a href="https://hashicorp.com/privacy">Privacy</a>
+          <Link href="/security">
+            <a>Security</a>
+          </Link>
           <a onClick={openConsentManager}>Consent Manager</a>
         </div>
       </div>
