25c3b7c3b2147c6aa084dadf04ac7d28a815f369
Brandon Romano <BrandonRRomano@gmail.com>
2020-10-06 00:01:00 -0700

    Plug in feature SVGs

--- website/pages/home/index.jsx
+++ website/pages/home/index.jsx
@@ -121,37 +121,37 @@ export default function HomePage() {
             title: 'Identity-based access',
             content:
               'Enables privileged sessions for users and applications based on user identity and role.',
-            icon: require('./img/boundary-identitybasedaccess.jpg?url'),
+            icon: require('./img/features/identity-based-access.svg?url'),
           },
           {
             title: 'Session management',
             content:
               'Ensures access control regardless of user or operators’ infrastructure.',
-            icon: require('./img/boundary-sessionmanagement.jpg?url'),
+            icon: require('./img/features/session-management.svg?url'),
           },
           {
             title: 'Platform-agnosticity',
             content:
               'One workflow for identity-based access across clouds, kubernetes clusters, and on-prem infrastructure.',
-            icon: require('./img/boundary-platformagnosticity.jpg?url'),
+            icon: require('./img/features/platform-agnosticity.svg?url'),
           },
           {
             title: 'Session visibility',
             content:
               'Visibility into session metrics, events, logs, and traces with the ability to export data to business intelligence and event monitoring tools.',
-            icon: require('./img/boundary-sessionvisibility.jpg?url'),
+            icon: require('./img/features/session-visibility.svg?url'),
           },
           {
             title: 'Configuration as code',
             content:
               'Ability to establish secure access controls and route resources dynamically.',
-            icon: require('./img/boundary-configascode.jpg?url'),
+            icon: require('./img/features/config-as-code.svg?url'),
           },
           {
             title: 'Managing dynamic environments',
             content:
               'Secure access to dynamic systems and applications with automated controls.',
-            icon: require('./img/boundary-managingdynamicenvironments.jpg?url'),
+            icon: require('./img/features/managing-dynamic-environments.svg?url'),
           },
         ]}
       />
