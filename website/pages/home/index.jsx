4f1fe3ae4ebd344105fb9d99c4aa2645011de012
Brandon Romano <BrandonRRomano@gmail.com>
2020-10-06 21:39:09 -0700

    Fix features padding

--- website/pages/home/index.jsx
+++ website/pages/home/index.jsx
@@ -151,47 +151,49 @@ export default function HomePage() {
         />
       </div>
 
-      <ProductFeaturesList
-        heading="Boundary Features"
-        features={[
-          {
-            title: 'Identity-based access',
-            content:
-              'Enables privileged sessions for users and applications based on user identity and role.',
-            icon: require('./img/features/identity-based-access.svg?url'),
-          },
-          {
-            title: 'Session management',
-            content:
-              'Ensures access control regardless of user or operators’ infrastructure.',
-            icon: require('./img/features/session-management.svg?url'),
-          },
-          {
-            title: 'Platform agnostic',
-            content:
-              'One workflow for identity-based access across clouds, kubernetes clusters, and on-prem infrastructure.',
-            icon: require('./img/features/platform-agnosticity.svg?url'),
-          },
-          {
-            title: 'Session visibility',
-            content:
-              'Visibility into session metrics, events, logs, and traces with the ability to export data to business intelligence and event monitoring tools.',
-            icon: require('./img/features/session-visibility.svg?url'),
-          },
-          {
-            title: 'Configuration as code',
-            content:
-              'Ability to establish secure access controls and route resources dynamically.',
-            icon: require('./img/features/config-as-code.svg?url'),
-          },
-          {
-            title: 'Managing dynamic environments',
-            content:
-              'Secure access to dynamic systems and applications with automated controls.',
-            icon: require('./img/features/managing-dynamic-environments.svg?url'),
-          },
-        ]}
-      />
+      <section className="features-section">
+        <ProductFeaturesList
+          heading="Boundary Features"
+          features={[
+            {
+              title: 'Identity-based access',
+              content:
+                'Enables privileged sessions for users and applications based on user identity and role.',
+              icon: require('./img/features/identity-based-access.svg?url'),
+            },
+            {
+              title: 'Session management',
+              content:
+                'Ensures access control regardless of user or operators’ infrastructure.',
+              icon: require('./img/features/session-management.svg?url'),
+            },
+            {
+              title: 'Platform agnostic',
+              content:
+                'One workflow for identity-based access across clouds, kubernetes clusters, and on-prem infrastructure.',
+              icon: require('./img/features/platform-agnosticity.svg?url'),
+            },
+            {
+              title: 'Session visibility',
+              content:
+                'Visibility into session metrics, events, logs, and traces with the ability to export data to business intelligence and event monitoring tools.',
+              icon: require('./img/features/session-visibility.svg?url'),
+            },
+            {
+              title: 'Configuration as code',
+              content:
+                'Ability to establish secure access controls and route resources dynamically.',
+              icon: require('./img/features/config-as-code.svg?url'),
+            },
+            {
+              title: 'Managing dynamic environments',
+              content:
+                'Secure access to dynamic systems and applications with automated controls.',
+              icon: require('./img/features/managing-dynamic-environments.svg?url'),
+            },
+          ]}
+        />
+      </section>
 
       <BrandedCta
         heading="Ready to get started?"
