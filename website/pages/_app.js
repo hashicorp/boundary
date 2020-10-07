90b67aec376815d340d8b121c1dd155b573fffd7
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2020-10-06 23:36:26 -0400

    [Website] Add alert banner (#557)
    * Bring over waypoint's implementation
    
    * Prove that it works/theme to boundary
    
    * revert to hidden for now

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -12,6 +12,8 @@ import ProductSubnav from '../components/subnav'
 import Footer from 'components/footer'
 import Error from './_error'
 import { productName } from '../data/metadata'
+import AlertBanner from '@hashicorp/react-alert-banner'
+import alertBannerData, { ALERT_BANNER_ACTIVE } from 'data/alert-banner'
 
 NProgress({ Router })
 const { ConsentManager, openConsentManager } = createConsentManager({
@@ -32,6 +34,9 @@ function App({ Component, pageProps }) {
           image="https://www.boundaryproject.io/img/og-image.png"
           icon={[{ href: '/favicon.ico' }]}
         />
+        {ALERT_BANNER_ACTIVE && (
+          <AlertBanner {...alertBannerData} theme="red" />
+        )}
         <ProductSubnav />
         <div className="content">
           <Component {...pageProps} />
