bc56783dd6fce967b34b823d96a6304df254eb50
Jeff Malnick <malnick@gmail.com>
2020-10-07 18:04:15 -0700

    all the data

--- testing/cli/create-data.sh
+++ testing/cli/create-data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+terraform apply
+
 export BOUNDARY_ADDR=http://127.0.0.1:9200
 
-PG_IMAGE=$(docker run -d -p 5439:5439 -e POSTGRES_PASSWORD=dbpass -e POSTGRES_USER=user -e POSTGRES_DB=demo --rm --name postgres postgres)
+PG_IMAGE=$(docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=dbpass -e POSTGRES_USER=user -e POSTGRES_DB=demo --rm --name postgres postgres)
 function cleanup() {
   echo 'cleaning up docker images..'
   docker kill $PG_IMAGE
@@ -9,11 +11,27 @@ function cleanup() {
 trap cleanup SIGKILL SIGINT EXIT
 
 function login() {
-  boundary authenticate password -auth-method-id ampw_1234567890 -login-name admin -password password
+  boundary authenticate password -auth-method-id ampw_1234567890 -login-name $1 -password foofoofoo
 }
 
 function list_targets() {
   boundary targets list -scope-id p_1234567890 -format json | jq '.[]["id"]'
 }
 
-login
+function strip() {
+  echo "$1" | tr -d '"'
+}
+
+function connect_nc() {
+  id=$(strip $1)
+  echo "foo" | boundary connect -exec nc -target-id $id -- {{boundary.ip}} {{boundary.port}}
+}
+
+for USR in  "jim" "mike" "todd" "randy" "susmitha" "jeff" "pete" "harold" 
+do
+  login $USR
+  for TGT in $(list_targets); do
+    echo "Connecting user $USR to target $TGT and sending foo data..."
+    connect_nc $TGT
+  done
+done
--- testing/cli/main.tf
+++ testing/cli/main.tf
@@ -87,6 +87,7 @@ resource "boundary_target" "postgres" {
   description              = "Postgres server"
   scope_id                 = var.default_project_id
   session_connection_limit = -1
+  session_max_seconds      = 2
   default_port             = 5432
   host_set_ids = [
     var.default_host_set
