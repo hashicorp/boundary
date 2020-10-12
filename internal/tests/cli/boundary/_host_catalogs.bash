1e34848653e5edd7433a1619e7402bef5822c014
Jeff Malnick <malnick@gmail.com>
2020-10-12 14:55:33 -0700

    tests: add host catalog CLI tests

--- internal/tests/cli/boundary/_host_catalogs.bash
+++ internal/tests/cli/boundary/_host_catalogs.bash
@@ -1,15 +1,19 @@
 function create_host_catalog() {
-  local sid=$1
-  local name=$2
-  boundary host-catalogs create static -scope-id $sid -name $name -description 'test host catalog'
+  local name=$1
+  local sid=$2
+  echo "sid $sid"
+  boundary host-catalogs create static \
+    -scope-id $sid \
+    -name $name \
+    -description 'test host catalog'
 }
 
 function read_host_catalog() {
-  boundary hosts read -id $1
+  boundary host-catalogs read -id $1
 }
 
 function delete_host_catalog() {
-  boundary hosts delete -id $1
+  boundary host-catalogs delete -id $1
 }
 
 function list_host_catalogs() {
