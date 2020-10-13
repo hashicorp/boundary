a132688ef284d20a2d1fdb09734e1594df1da492
Jim Lambert <jlambert@hashicorp.com>
2020-10-13 15:18:00 -0400

    refactor into separate files

--- internal/errors/code.go
+++ internal/errors/code.go
@@ -1,30 +1,7 @@
 package errors
 
-// Kind specifies the kind of error (unknown, parameter, integrity, etc).
-type Kind uint32
-
 // Code specifies a code for the error.
 type Code uint32
-type Info struct {
-	Kind    Kind
-	Message string
-}
-
-const (
-	Other Kind = iota
-	Parameter
-	Integrity
-	Search
-)
-
-func (e Kind) String() string {
-	return map[Kind]string{
-		Other:     "unknown",
-		Parameter: "parameter violation",
-		Integrity: "integrity violation",
-		Search:    "search issue",
-	}[e]
-}
 
 const (
 	InvalidParameter Code = 100
@@ -40,31 +17,4 @@ func (e Code) String() string {
 		return i.Message
 	}
 	return "unknown"
-}
-
-var errorCodeInfo = map[Code]Info{
-	InvalidParameter: {
-		Message: "invalid parameter",
-		Kind:    Parameter,
-	},
-	CheckConstraint: {
-		Message: "constraint check failed",
-		Kind:    Integrity,
-	},
-	NotNull: {
-		Message: "must not be empty (null) violation",
-		Kind:    Integrity,
-	},
-	NotUnique: {
-		Message: "must be unique violation",
-		Kind:    Integrity,
-	},
-	RecordNotFound: {
-		Message: "record not fouind",
-		Kind:    Search,
-	},
-	MultipleRecords: {
-		Message: "multiple records",
-		Kind:    Search,
-	},
 }
---
+++ internal/errors/info.go
@@ -0,0 +1,33 @@
+package errors
+
+type Info struct {
+	Kind    Kind
+	Message string
+}
+
+var errorCodeInfo = map[Code]Info{
+	InvalidParameter: {
+		Message: "invalid parameter",
+		Kind:    Parameter,
+	},
+	CheckConstraint: {
+		Message: "constraint check failed",
+		Kind:    Integrity,
+	},
+	NotNull: {
+		Message: "must not be empty (null) violation",
+		Kind:    Integrity,
+	},
+	NotUnique: {
+		Message: "must be unique violation",
+		Kind:    Integrity,
+	},
+	RecordNotFound: {
+		Message: "record not fouind",
+		Kind:    Search,
+	},
+	MultipleRecords: {
+		Message: "multiple records",
+		Kind:    Search,
+	},
+}
