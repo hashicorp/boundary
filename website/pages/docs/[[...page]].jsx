72b66f0a627c08470b95655f00b06d79184dc2a1
Radek Simko <radek.simko@gmail.com>
2020-10-14 21:07:10 +0100

    Update website/pages/docs/[[...page]].jsx
    Co-authored-by: Brandon Romano <BrandonRRomano@gmail.com>

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -12,7 +12,7 @@ function DocsLayout(props) {
   return (
     <DocsPage
       productName={productName}
-      productSlug="boundary"
+      productSlug="red"
       subpath={subpath}
       order={order}
       staticProps={props}
