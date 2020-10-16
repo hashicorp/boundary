f7d7d59fcec9b4111defc0af8602e29eb20f8ca1
Jim Lambert <jlambert@hashicorp.com>
2020-10-16 10:46:21 -0400

    docs and unit tests

--- internal/errors/code.go
+++ internal/errors/code.go
@@ -3,19 +3,30 @@ package errors
 // Code specifies a code for the error.
 type Code uint32
 
-const (
-	Unknown          Code = 0
-	InvalidParameter Code = 100
-	CheckConstraint  Code = 1000
-	NotNull          Code = 1001
-	NotUnique        Code = 1002
-	RecordNotFound   Code = 1100
-	MultipleRecords  Code = 1101
-)
-
-func (e Code) String() string {
-	if i, ok := errorCodeInfo[e]; ok {
-		return i.Message
-	}
-	return "unknown"
+// String will return the Code's Info.Message
+func (c Code) String() string {
+	return c.Info().Message
 }
+
+// Info will look up the Code's Info.  If the Info is not found, it will return
+// Info for an Unknown Code.
+func (c Code) Info() Info {
+	if info, ok := errorCodeInfo[c]; ok {
+		return info
+	}
+	return errorCodeInfo[Unknown]
+}
+
+const (
+	Unknown Code = 0 // Unknown will be equal to a zero value for Codes
+
+	// General function errors are reserved Codes 100-999
+	InvalidParameter Code = 100 // InvalidParameter represents and invalid parameter for an operation.
+
+	// DB errors are resevered Codes from 1000-1999
+	CheckConstraint Code = 1000 // CheckConstraint represents a check constraint error
+	NotNull         Code = 1001 // NotNull represents a value must not be null error
+	NotUnique       Code = 1002 // NotUnique represents a value must be unique error
+	RecordNotFound  Code = 1100 // RecordNotFound represents that a record/row was not found matching the criteria
+	MultipleRecords Code = 1101 // MultipleRecords represents that multiple records/rows were found matching the criteria
+)
---
+++ internal/errors/code_test.go
@@ -0,0 +1,68 @@
+package errors
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCode_Both_String_Info(t *testing.T) {
+	tests := []struct {
+		name string
+		c    Code
+		want Code
+	}{
+		{
+			name: "undefined-code",
+			c:    Code(4294967295),
+			want: Unknown,
+		},
+		{
+			name: "default-value",
+			want: Unknown,
+		},
+		{
+			name: "Unknown",
+			c:    Unknown,
+			want: Unknown,
+		},
+		{
+			name: "InvalidParameter",
+			c:    InvalidParameter,
+			want: InvalidParameter,
+		},
+		{
+			name: "CheckConstraint",
+			c:    CheckConstraint,
+			want: CheckConstraint,
+		},
+		{
+			name: "NotNull",
+			c:    NotNull,
+			want: NotNull,
+		},
+		{
+			name: "NotUnique",
+			c:    NotUnique,
+			want: NotUnique,
+		},
+		{
+			name: "RecordNotFound",
+			c:    RecordNotFound,
+			want: RecordNotFound,
+		},
+		{
+			name: "MultipleRecords",
+			c:    MultipleRecords,
+			want: MultipleRecords,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			assert.Equal(errorCodeInfo[tt.want], tt.c.Info())
+			assert.Equal(errorCodeInfo[tt.want].Message, tt.c.String())
+
+		})
+	}
+}
