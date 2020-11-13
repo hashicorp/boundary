af8fe560a2dc550f4dc4550b7a0df5a42c4fc843
Jeff Malnick <malnick@gmail.com>
2020-11-13 09:49:04 -0800

    remove tests for zeroed values

--- internal/authtoken/options_test.go
+++ internal/authtoken/options_test.go
@@ -27,25 +27,11 @@ func Test_GetOpts(t *testing.T) {
 		assert.Equal(opts, testOpts)
 	})
 
-	t.Run("WithTokenTimeToLiveDurationZeroed", func(t *testing.T) {
-		assert := assert.New(t)
-		opts := getOpts(WithTokenTimeToLiveDuration(0))
-		testOpts := getDefaultOptions()
-		assert.Equal(opts, testOpts)
-	})
-
 	t.Run("WithTokenTimeToLiveStale", func(t *testing.T) {
 		assert := assert.New(t)
 		opts := getOpts(WithTokenTimeToStaleDuration(1 * time.Hour))
 		testOpts := getDefaultOptions()
 		testOpts.withTokenTimeToStaleDuration = 1 * time.Hour
-		assert.Equal(opts, testOpts)
-	})
-
-	t.Run("WithTokenTimeToLiveStaleZeroed", func(t *testing.T) {
-		assert := assert.New(t)
-		opts := getOpts(WithTokenTimeToStaleDuration(0))
-		testOpts := getDefaultOptions()
 		assert.Equal(opts, testOpts)
 	})
 }
