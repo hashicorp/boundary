b42ab354c4595484fac2dfad15d98c68a3a2d9e8
Jim Lambert <jlambert@hashicorp.com>
2021-01-23 11:51:08 -0500

    define iam_acct_info and iam_user_acct_info

--- internal/db/schema/migrations/postgres/dev/86_iam.up.sql
+++ internal/db/schema/migrations/postgres/dev/86_iam.up.sql
@@ -1,30 +1,48 @@
 begin;
 
--- iam_user_with_acct_info provides a consolidated view of users that includes
--- account info from the user scope's account info auth method
-create view iam_user_with_acct_info as
+-- add the account_info_auth_method_id which determines which auth_method is
+-- designated as for "account info" in the user's scope.  
+alter table iam_scope
+add column account_info_auth_method_id wt_public_id -- allowed to be null and is mutable of course.
+references auth_method(public_id)
+on update cascade
+on delete cascade;
+
+-- iam_user_acct_info provides account info for users by determining which
+-- auth_method is designated as for "account info" in the user's scope via the
+-- scope's account_info_auth_method_id.  Every sub-type of auth_account must be
+-- added to this view's union.
+create view iam_acct_info as
 select 
-    u.public_id,
-    u.scope_id,
-    u.name,
-    u.description, 
-    u.create_time,
-    u.update_time,
-    u.version,
+    aa.iam_user_id,
     oa.subject_id as login_name,
     oa.full_name as full_name,
     oa.email as email
 from 	
-    scope s
-    auth_account aa
+    iam_scope s,
+    auth_account aa,
 	auth_oidc_account oa
-	iam_user u
 where
-	u.public_id = aa.iam_user_id and
     aa.public_id = oa.public_id and 
     aa.auth_method_id = s.account_info_auth_method_id 
 union 
 select 
+    iam_user_id,
+    pa.login_name,
+    '' as full_name,
+    '' as email
+from 	
+    iam_scope s,
+    auth_account aa,
+	auth_password_account pa
+where
+    aa.public_id = pa.public_id and 
+    aa.auth_method_id = s.account_info_auth_method_id;
+
+-- iam_user_acct_info provides a simple way to retrieve entries that include
+-- both the iam_user fields with an outer join to the user's account info.
+create view iam_user_acct_info as
+select 
     u.public_id,
     u.scope_id,
     u.name,
@@ -32,18 +50,11 @@ select
     u.create_time,
     u.update_time,
     u.version,
-    oa.subject_id as login_name,
-    oa.full_name as full_name,
-    oa.email as email
+    i.login_name,
+    i.full_name,
+    i.email
 from 	
-    scope s
-    auth_account aa
-	auth_password_account pa
 	iam_user u
-where
-	u.public_id = aa.iam_user_id and
-    aa.public_id = pa.public_id and 
-    aa.auth_method_id = s.account_info_auth_method_id 
-
+left outer join iam_acct_info i on u.public_id = i.iam_user_id;
 
 commit;
