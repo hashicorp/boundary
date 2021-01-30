30da4ad8bc659baff982706751d52f683b17a8e7
Jim Lambert <jlambert@hashicorp.com>
2021-01-30 17:29:55 -0500

    add docs and fix-up validate

--- internal/auth/oidc/certificate.go
+++ internal/auth/oidc/certificate.go
@@ -1,17 +1,28 @@
 package oidc
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+
 	"github.com/hashicorp/boundary/internal/auth/oidc/store"
 	"github.com/hashicorp/boundary/internal/errors"
 	"github.com/hashicorp/boundary/internal/oplog"
 	"google.golang.org/protobuf/proto"
 )
 
+// DefaultCertificateTableName defines the default table name for a certificate
+const DefaultCertificateTableName = "auth_oidc_certificate"
+
+// Certificate defines a certificate to use as part of a trust root when
+// connecting to the auth method's OIDC Provider.
 type Certificate struct {
 	*store.Certificate
 	tableName string
 }
 
+// NewCertificate creates a new in memory certificate for an OIDC auth method.
+// Certificates are "owned" by their coresponding OIDC auth method.
 func NewCertificate(authMethodId string, cert string) (*Certificate, error) {
 	const op = "oidc.NewCallbackUrl"
 
@@ -27,20 +38,34 @@ func NewCertificate(authMethodId string, cert string) (*Certificate, error) {
 	return c, nil
 }
 
-func (a *Certificate) validate(caller errors.Op) error {
-	if a.Cert == "" {
+// validate the Certifcate and on success return nil
+func (c *Certificate) validate(caller errors.Op) error {
+	if c.OidcMethodId == "" {
+		return errors.New(errors.InvalidParameter, caller, "missing oidc auth method id")
+	}
+	if c.Cert == "" {
 		return errors.New(errors.InvalidParameter, caller, "empty cert")
+	}
+	block, _ := pem.Decode([]byte(c.Cert))
+	if block == nil {
+		return errors.New(errors.InvalidParameter, caller, "failed to parse certificate PEM")
+	}
+	_, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return errors.New(errors.InvalidParameter, caller, fmt.Sprintf("failed to parse certificate: %s"+err.Error()), errors.WithWrap(err))
 	}
 	return nil
 }
 
-func allocCertificate() Certificate {
+// AllocCertificate makes an empty one in memory
+func AllocCertificate() Certificate {
 	return Certificate{
 		Certificate: &store.Certificate{},
 	}
 }
 
-func (c *Certificate) clone() *Certificate {
+// Clone a Certificate
+func (c *Certificate) Clone() *Certificate {
 	cp := proto.Clone(c.Certificate)
 	return &Certificate{
 		Certificate: cp.(*store.Certificate),
@@ -52,7 +77,7 @@ func (c *Certificate) TableName() string {
 	if c.tableName != "" {
 		return c.tableName
 	}
-	return "auth_oidc_certificate"
+	return DefaultCertificateTableName
 }
 
 // SetTableName sets the table name.
@@ -60,6 +85,7 @@ func (c *Certificate) SetTableName(n string) {
 	c.tableName = n
 }
 
+// oplog will create oplog metadata for the Certificate.
 func (c *Certificate) oplog(op oplog.OpType, authMethodScopeId string) oplog.Metadata {
 	metadata := oplog.Metadata{
 		"resource-public-id": []string{c.OidcMethodId}, // the auth method is the root aggregate
