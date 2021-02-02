b2d06f59a6abc99932e03614c8bd38e225e85c90
Todd Knight <T.Alan.Knight@gmail.com>
2021-02-02 12:20:33 -0700

    Update internal/iam/repository_scope.go
    Co-authored-by: Jeff Mitchell <jeffrey.mitchell@gmail.com>

--- internal/iam/repository_scope.go
+++ internal/iam/repository_scope.go
@@ -286,7 +286,7 @@ func (r *Repository) CreateScope(ctx context.Context, s *Scope, userId string, o
 					}
 					grants = append(grants, roleGrant)
 
-					roleGrant, err = NewRoleGrant(defaultRolePublicId, "type=scope;actions=list:self")
+					roleGrant, err = NewRoleGrant(defaultRolePublicId, "id=*;type=scope;actions=list:self")
 					if err != nil {
 						return errors.Wrap(err, op, errors.WithMsg("unable to create in memory role grant"))
 					}
