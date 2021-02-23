6fae78959edbbbdf789c2f13f0eb8a727957e332
Jim Lambert <jlambert@hashicorp.com>
2021-02-23 15:48:09 -0500

    unit tests for: applyUpdate(...) and validateFieldMask(..)

--- internal/auth/oidc/repository_auth_method_update_test.go
+++ internal/auth/oidc/repository_auth_method_update_test.go
@@ -6,8 +6,184 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/hashicorp/boundary/internal/auth/oidc/store"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func Test_validateFieldMask(t *testing.T) {
+	tests := []struct {
+		name      string
+		fieldMask []string
+		wantErr   bool
+	}{
+		{
+			name: "all-valid-fields",
+			fieldMask: []string{
+				"Name",
+				"Description",
+				"OperationalState",
+				"DiscoveryUrl",
+				"ClientId",
+				"ClientSecret",
+				"MaxAge",
+				"SigningAlgs",
+				"CallbackUrls",
+				"AudClaims",
+				"Certificates",
+			},
+		},
+		{
+			name:      "invalid",
+			fieldMask: []string{"Invalid", "Name"},
+			wantErr:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+			err := validateFieldMask(tt.fieldMask)
+			if tt.wantErr {
+				require.Error(err)
+				return
+			}
+			require.NoError(err)
+		})
+	}
+}
+
+func Test_applyUpdate(t *testing.T) {
+	tests := []struct {
+		name      string
+		new       *AuthMethod
+		orig      *AuthMethod
+		fieldMask []string
+		want      *AuthMethod
+	}{
+		{
+			name: "valid-all-fields",
+			new: &AuthMethod{
+				AuthMethod: &store.AuthMethod{
+					Name:             "new-name",
+					Description:      "new-description",
+					OperationalState: string(ActivePublicState),
+					DiscoveryUrl:     "new-discovery-url",
+					ClientId:         "new-client-id",
+					ClientSecret:     "new-client-secret",
+					MaxAge:           100,
+					SigningAlgs:      []string{"new-alg1", "new-alg2"},
+					CallbackUrls:     []string{"new-callback-1", "new-callback-2"},
+					AudClaims:        []string{"new-aud-1", "new-aud-2"},
+					Certificates:     []string{"new-pem1", "new-pem-2"},
+				},
+			},
+			orig: &AuthMethod{
+				AuthMethod: &store.AuthMethod{
+					Name:             "orig-name",
+					Description:      "orig-description",
+					OperationalState: string(InactiveState),
+					DiscoveryUrl:     "orig-discovery-url",
+					ClientId:         "orig-client-id",
+					ClientSecret:     "orig-client-secret",
+					MaxAge:           100,
+					SigningAlgs:      []string{"orig-alg1", "orig-alg2"},
+					CallbackUrls:     []string{"orig-callback-1", "orig-callback-2"},
+					AudClaims:        []string{"orig-aud-1", "orig-aud-2"},
+					Certificates:     []string{"orig-pem1", "orig-pem-2"},
+				},
+			},
+			want: &AuthMethod{
+				AuthMethod: &store.AuthMethod{
+					Name:             "new-name",
+					Description:      "new-description",
+					OperationalState: string(ActivePublicState),
+					DiscoveryUrl:     "new-discovery-url",
+					ClientId:         "new-client-id",
+					ClientSecret:     "new-client-secret",
+					MaxAge:           100,
+					SigningAlgs:      []string{"new-alg1", "new-alg2"},
+					CallbackUrls:     []string{"new-callback-1", "new-callback-2"},
+					AudClaims:        []string{"new-aud-1", "new-aud-2"},
+					Certificates:     []string{"new-pem1", "new-pem-2"},
+				},
+			},
+			fieldMask: []string{
+				"Name",
+				"Description",
+				"OperationalState",
+				"DiscoveryUrl",
+				"ClientId",
+				"ClientSecret",
+				"MaxAge",
+				"SigningAlgs",
+				"CallbackUrls",
+				"AudClaims",
+				"Certificates",
+			},
+		},
+		{
+			name: "nil-value-objects",
+			new: &AuthMethod{
+				AuthMethod: &store.AuthMethod{
+					Name:             "new-name",
+					Description:      "new-description",
+					OperationalState: string(ActivePublicState),
+					DiscoveryUrl:     "new-discovery-url",
+					ClientId:         "new-client-id",
+					ClientSecret:     "new-client-secret",
+					MaxAge:           100,
+				},
+			},
+			orig: &AuthMethod{
+				AuthMethod: &store.AuthMethod{
+					Name:             "orig-name",
+					Description:      "orig-description",
+					OperationalState: string(InactiveState),
+					DiscoveryUrl:     "orig-discovery-url",
+					ClientId:         "orig-client-id",
+					ClientSecret:     "orig-client-secret",
+					MaxAge:           100,
+					SigningAlgs:      []string{"orig-alg1", "orig-alg2"},
+					CallbackUrls:     []string{"orig-callback-1", "orig-callback-2"},
+					AudClaims:        []string{"orig-aud-1", "orig-aud-2"},
+					Certificates:     []string{"orig-pem1", "orig-pem-2"},
+				},
+			},
+			want: &AuthMethod{
+				AuthMethod: &store.AuthMethod{
+					Name:             "new-name",
+					Description:      "new-description",
+					OperationalState: string(ActivePublicState),
+					DiscoveryUrl:     "new-discovery-url",
+					ClientId:         "new-client-id",
+					ClientSecret:     "new-client-secret",
+					MaxAge:           100,
+				},
+			},
+			fieldMask: []string{
+				"Name",
+				"Description",
+				"OperationalState",
+				"DiscoveryUrl",
+				"ClientId",
+				"ClientSecret",
+				"MaxAge",
+				"SigningAlgs",
+				"CallbackUrls",
+				"AudClaims",
+				"Certificates",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			got := applyUpdate(tt.new, tt.orig, tt.fieldMask)
+			assert.Equal(got, tt.want)
+		})
+	}
+}
 
 type mockClient struct {
 	mockDo func(req *http.Request) (*http.Response, error)
