91fcc10ad53d42708ab0d9a65127f1f0030a0bf1
Jim <jlambert@hashicorp.com>
2021-02-23 16:53:28 -0500

    Update internal/auth/oidc/repository_auth_method_create.go
    Co-authored-by: Michael Gaffney <mgaffney@users.noreply.github.com>

--- internal/auth/oidc/repository_auth_method_create.go
+++ internal/auth/oidc/repository_auth_method_create.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/boundary/internal/oplog"
 )
 
-// CreateAuthMethod creates m (*AuthMethod) in the repo along with its
+// CreateAuthMethod creates am (*AuthMethod) in the repo along with its
 // associated embedded optional value objects of SigningAlgs, CallbackUrls,
 // AudClaims and Certificates. and returns the newly created AuthMethod
 // (with its PublicId set)
