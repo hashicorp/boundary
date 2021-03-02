befdc566c4bcad4799f612e043db86c347d20a2e
Jim Lambert <jlambert@hashicorp.com>
2021-03-02 16:43:05 -0500

    start of implementation

--- internal/auth/oidc/service_callback.go
+++ internal/auth/oidc/service_callback.go
@@ -2,8 +2,13 @@ package oidc
 
 import (
 	"context"
+	"fmt"
+	"time"
 
-	"github.com/hashicorp/boundary/internal/kms"
+	"github.com/golang/protobuf/proto"
+	"github.com/hashicorp/boundary/internal/auth/oidc/request"
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/cap/oidc"
 )
 
 // Callback is an oidc domain service function for processing a successful OIDC
@@ -33,10 +38,90 @@ import (
 // auth token for the authenticated user.
 func Callback(
 	ctx context.Context,
-	kms *kms.Kms,
 	oidcRepoFn OidcRepoFactory,
 	iamRepoFn IamRepoFactory,
 	atRepoFn AuthTokenRepFactory,
-	authMethodId, state, code string) (finalRedirect string, e error) {
+	apiAddr,
+	authMethodId,
+	state, code string) (finalRedirect string, e error) {
+	const op = "oidc.Callback"
+	if oidcRepoFn == nil {
+		return "", errors.New(errors.InvalidParameter, op, "missing oidc repository function")
+	}
+	if iamRepoFn == nil {
+		return "", errors.New(errors.InvalidParameter, op, "missing iam repository function")
+	}
+	if atRepoFn == nil {
+		return "", errors.New(errors.InvalidParameter, op, "missing auth token repository function")
+	}
+	if authMethodId == "" {
+		return "", errors.New(errors.InvalidParameter, op, "missing auth method id")
+	}
+	if state == "" {
+		return "", errors.New(errors.InvalidParameter, op, "missing state")
+	}
+	if code == "" {
+		return "", errors.New(errors.InvalidParameter, op, "missing code")
+	}
+	r, err := oidcRepoFn()
+	if err != nil {
+		return "", errors.Wrap(err, op)
+	}
+	am, err := r.lookupAuthMethod(ctx, authMethodId)
+	if err != nil {
+		return "", errors.Wrap(err, op)
+	}
+	if am == nil {
+		return "", errors.New(errors.RecordNotFound, op, fmt.Sprintf("auth method %s not found", authMethodId))
+	}
+	requestWrapper, err := requestWrappingWrapper(ctx, r.kms, am.ScopeId, authMethodId)
+	if err != nil {
+		return "", errors.Wrap(err, op)
+	}
+	stateWrapper, err := unwrapMessage(ctx, state)
+	if err != nil {
+		return "", errors.Wrap(err, op)
+	}
+	stateBytes, err := decryptMessage(ctx, requestWrapper, stateWrapper)
+	if err != nil {
+		return "", errors.Wrap(err, op)
+	}
+	var reqState request.State
+	if err := proto.Unmarshal(stateBytes, &reqState); err != nil {
+		return "", errors.New(errors.Unknown, op, "unable to unmarshal request state", errors.WithWrap(err))
+	}
+
+	// get the provider from the cache (if possible)
+	provider, err := providerCache().get(ctx, am)
+	if err != nil {
+		return "", errors.Wrap(err, op)
+	}
+	hash, err := provider.ConfigHash()
+	if err != nil {
+		return "", errors.New(errors.Unknown, op, "unable to get provider config hash", errors.WithWrap(err))
+	}
+	if reqState.ProviderConfigHash != hash && am.OperationalState == string(InactiveState) {
+		return fmt.Sprintf(FinalRedirectEndpoint, apiAddr), nil
+	}
+
+	if time.Now().After(reqState.CreateTime.Timestamp.AsTime()) {
+		return "", errors.New(errors.AuthAttemptExpired, op, "request state has expired")
+	}
+
+	opts := []oidc.Option{
+		oidc.WithState(state),
+		oidc.WithNonce(reqState.Nonce),
+	}
+	switch {
+	case am.MaxAge == -1:
+		opts = append(opts, oidc.WithMaxAge(0))
+	case am.MaxAge > 0:
+		opts = append(opts, oidc.WithMaxAge(uint(am.MaxAge)))
+	default:
+	}
+	if len(am.AudClaims) > 0 {
+		opts = append(opts, oidc.WithAudiences(am.AudClaims...))
+	}
+	// oidcRequest, err := oidc.NewRequest(AttemptExpiration, fmt.Sprintf(CallbackEndpoint, apiAddr, am.PublicId), opts...)
 	panic("to-do")
 }
