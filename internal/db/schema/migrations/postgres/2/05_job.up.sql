5ed844c942f83a97697e69bb20de3db787c14970
Louis Ruch <louisruch@gmail.com>
2021-03-19 12:43:49 -0700

    Feedback

--- internal/db/schema/migrations/postgres/2/05_job.up.sql
+++ internal/db/schema/migrations/postgres/2/05_job.up.sql
@@ -1,7 +1,14 @@
 begin;
 
+create table job_name_enm (
+    name wt_name not null primary key
+);
+
+comment on table job_name_enm is
+    'job_name_enm is an enumeration table where each row contains the name of a job.';
+
 create table job (
-    id text primary key,
+    private_id wt_private_id primary key,
     name wt_name not null
         constraint job_name_enm_fkey
             references job_name_enm (name)
@@ -11,22 +18,31 @@ create table job (
     code text not null
         constraint job_code_must_be_not_empty
             check(length(trim(code)) > 0),
-    next_scheduled_run timestamp with time zone not null
-        default current_timestamp,
+    next_scheduled_run wt_timestamp,
 
     constraint job_name_code_uq
         unique(name, code)
 );
 
 comment on table job is
-    'job is a base table where each row represents a unique job that can only have one running instance at any specific time.';
+    'job is a table where each row represents a unique job that can only have one running instance at any specific time.';
 
-create table job_name_enm (
+create table job_run_status_enm (
     name text not null primary key
+        constraint only_predefined_job_status_allowed
+            check(name in ('running', 'complete', 'failed', 'interrupted'))
 );
 
-comment on table job_name_enm is
-    'job_name_enm is an enumeration table where each row contains the name of a valid job.';
+comment on table job_run_status_enm is
+    'job_run_status_enm is an enumeration table where each row contains a valid job run state.';
+
+insert into job_run_status_enm (name)
+    values
+    ('running'),
+    ('complete'),
+    ('failed'),
+    ('interrupted');
+
 
 create table job_run (
      id serial primary key,
@@ -40,12 +56,12 @@ create table job_run (
              references server(private_id)
              on delete set null
              on update cascade,
-     start_time timestamp with time zone not null
-         default current_timestamp,
+     start_time wt_timestamp,
      end_time timestamp with time zone,
-     last_heartbeat timestamp with time zone not null
-         default current_timestamp,
-     completed_count int not null,
+     last_heartbeat wt_timestamp,
+     completed_count int not null
+        constraint job_run_completed_count_can_not_be_negative
+            check(completed_count >= 0),
      total_count int not null
          constraint job_run_total_count_greater_than_zero
             check(total_count > 0),
@@ -65,19 +81,3 @@ comment on table job_run is
 create unique index job_run_status_constraint
     on job_run (job_id)
     where status = 'running';
-
-create table job_run_status_enm (
-    name text not null primary key
-        constraint only_predefined_job_status_allowed
-            check(name in ('running', 'complete', 'failed', 'interrupted'))
-);
-
-comment on table job_run_status_enm is
-    'job_run_status_enm is an enumeration table where each row contains a valid job run state.';
-
-insert into job_run_status_enm (name)
-    values
-    ('running'),
-    ('complete'),
-    ('failed'),
-    ('interrupted');
