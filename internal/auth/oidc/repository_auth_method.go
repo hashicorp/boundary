425e1f3a51a832cab2c53ea650ab13ec4c9e46f9
Jim Lambert <jlambert@hashicorp.com>
2021-03-19 11:03:46 -0400

    fix error msg

--- internal/auth/oidc/repository_auth_method.go
+++ internal/auth/oidc/repository_auth_method.go
@@ -42,7 +42,7 @@ func (r *Repository) upsertAccount(ctx context.Context, am *AuthMethod, IdTokenC
 		return nil, errors.New(errors.Unknown, op, "issuer is not present in ID Token, which should not be possible")
 	}
 	if sub, ok = IdTokenClaims["sub"].(string); !ok {
-		return nil, errors.New(errors.Unknown, op, "subject is not present in Access Token, which should not be possible")
+		return nil, errors.New(errors.Unknown, op, "subject is not present in ID Token, which should not be possible")
 	}
 
 	columns := []string{"public_id", "auth_method_id", "issuer_id", "subject_id"}
