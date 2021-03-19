4b82bcaba20c67240557836ecc1d7871eccc8844
Louis Ruch <louisruch@gmail.com>
2021-03-19 15:11:59 -0700

    Add general system errors to API interceptor

--- internal/servers/controller/handlers/errors.go
+++ internal/servers/controller/handlers/errors.go
@@ -161,11 +161,17 @@ func backendErrorToApiError(inErr error) error {
 		return InvalidArgumentErrorf(genericUniquenessMsg, nil)
 	}
 
-	// We haven't been able to identify what this backend error is, return it as an internal error
+	var statusCode int32 = http.StatusInternalServerError
+	var domainErr *errors.Err
+	if errors.As(inErr, &domainErr) && domainErr.Code >= 400 && domainErr.Code <= 599 {
+		// Domain error codes 400-599 align with http client and server error codes, use the domain error code instead of 500
+		statusCode = int32(domainErr.Code)
+	}
+
 	// TODO: Don't return potentially sensitive information (like which user id an account
 	//  is already associated with when attempting to re-associate it).
 	return &apiError{
-		status: http.StatusInternalServerError,
+		status: statusCode,
 		inner:  &pb.Error{Kind: codes.Internal.String(), Message: inErr.Error()},
 	}
 }
