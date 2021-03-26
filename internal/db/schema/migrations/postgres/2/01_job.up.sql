96180786c9581c20c58bed5446c9134cd16715f7
Louis Ruch <louisruch@gmail.com>
2021-03-26 09:53:26 -0700

    Add job tables (#1034)
    * Add job tables

---
+++ internal/db/schema/migrations/postgres/2/01_job.up.sql
@@ -0,0 +1,87 @@
+begin;
+
+create table job (
+     private_id wt_private_id primary key,
+     name wt_name not null,
+     description wt_description not null,
+     code text not null
+         constraint code_too_short
+             check (length(trim(code)) > 0)
+         constraint code_too_long
+             check (length(trim(code)) < 128),
+     next_scheduled_run timestamp with time zone not null,
+
+     constraint job_name_code_uq
+         unique(name, code)
+);
+
+comment on table job is
+    'job is a table where each row represents a unique job that can only have one running instance at any specific time.';
+
+create trigger immutable_columns before update on job
+    for each row execute procedure immutable_columns('private_id', 'name', 'code');
+
+create table job_run_status_enm (
+    name text not null primary key
+        constraint only_predefined_job_status_allowed
+            check(name in ('running', 'completed', 'failed', 'interrupted'))
+);
+
+comment on table job_run_status_enm is
+    'job_run_status_enm is an enumeration table where each row contains a valid job run state.';
+
+insert into job_run_status_enm (name)
+    values
+    ('running'),
+    ('completed'),
+    ('failed'),
+    ('interrupted');
+
+create table job_run (
+     id bigint generated always as identity primary key,
+     job_id wt_private_id not null
+         constraint job_fkey
+             references job(private_id)
+             on delete cascade
+             on update cascade,
+     server_id wt_private_id
+         constraint server_fkey
+             references server(private_id)
+             on delete set null
+             on update cascade,
+     create_time wt_timestamp,
+     update_time wt_timestamp,
+     end_time timestamp with time zone,
+     completed_count int not null
+         default 0
+         constraint completed_count_can_not_be_negative
+             check(completed_count >= 0),
+     total_count int not null
+         default 0
+         constraint total_count_can_not_be_negative
+             check(total_count >= 0),
+     status text not null
+         constraint job_run_status_enm_fkey
+             references job_run_status_enm (name)
+             on delete restrict
+             on update cascade,
+
+     constraint job_run_completed_count_less_than_equal_to_total_count
+         check(completed_count <= total_count)
+);
+
+comment on table job_run is
+    'job_run is a table where each row represents an instance of a job run that is either actively running or has already completed.';
+
+create unique index job_run_status_constraint
+    on job_run (job_id)
+    where status = 'running';
+
+create trigger update_time_column before update on job_run
+    for each row execute procedure update_time_column();
+
+create trigger default_create_time_column before insert on job_run
+    for each row execute procedure default_create_time();
+
+create trigger immutable_columns before update on job_run
+    for each row execute procedure immutable_columns('id', 'job_id', 'create_time');
