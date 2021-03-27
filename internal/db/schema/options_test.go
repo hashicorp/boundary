53dd78fbd5994564920090ca06e7b87fc7d63ff2
Jim Lambert <jlambert@hashicorp.com>
2021-03-27 10:44:29 -0400

    add WithMigrationStates(...) option

---
+++ internal/db/schema/options_test.go
@@ -0,0 +1,22 @@
+package schema
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test_GetOpts provides unit tests for GetOpts and all the options
+func Test_GetOpts(t *testing.T) {
+	t.Parallel()
+	t.Run("WithMigrationStates", func(t *testing.T) {
+		assert := assert.New(t)
+		oState := TestCloneMigrationStates(t)
+		nState := TestCreatePartialMigrationState(oState["postgres"], 8)
+		oState["postgres"] = nState
+		opts := getOpts(WithMigrationStates(oState))
+		testOpts := getDefaultOptions()
+		testOpts.withMigrationStates = oState
+		assert.Equal(opts, testOpts)
+	})
+}
