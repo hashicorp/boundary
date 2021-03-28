78d9f33bb6a6e03a6a1dbc1f795cc6481c81d2c2
Jim Lambert <jlambert@hashicorp.com>
2021-03-28 17:36:30 -0400

    schema changes needed to support unique auth method names across subtypes within a scope

--- internal/db/schema/postgres_migration.gen.go
+++ internal/db/schema/postgres_migration.gen.go
@@ -4,7 +4,7 @@ package schema
 
 func init() {
 	migrationStates["postgres"] = migrationState{
-		binarySchemaVersion: 2087,
+		binarySchemaVersion: 2090,
 		upMigrations: map[int][]byte{
 			1: []byte(`
 create domain wt_public_id as text
@@ -5773,6 +5773,93 @@ from
 where 
   s.primary_auth_method_id is null and 
   s.public_id = m.scope_id;
+`),
+			2090: []byte(`
+-- By adding the name column to the base auth method type, the database can
+-- ensure that auth method names are unique across all sub types.
+alter table auth_method
+  add column name wt_name;
+
+alter table auth_method
+  add constraint auth_method_scope_id_name_uq
+    unique (scope_id, name);
+
+
+-- insert_auth_method_subtype() is a replacement of the function definition in
+-- migraiton 07_auth.up.sql  This new defintion also inserts the sub type's name
+-- into the base type. The name column must be on the base type, so the database
+-- can ensure that auth method names are unique across all sub types.
+create or replace function
+  insert_auth_method_subtype()
+  returns trigger
+as $$
+begin
+  insert into auth_method
+    (public_id, scope_id, name)
+  values
+    (new.public_id, new.scope_id, new.name);
+  return new;
+end;
+$$ language plpgsql;
+comment on function insert_auth_method_subtype() is
+'insert_auth_method_subtype() inserts sub type date into the base type auth method table';
+
+-- update_auth_method_subtype() is a new function intended to be used in "before
+-- update" triggers for all auth method sub types.  It's purpose is to ensure
+-- that the name column is syncronized between the sub and base auth method
+-- types.  The name column must be on the base type, so the database can ensure
+-- that auth method names are unique across all sub types.
+create or replace function
+  update_auth_method_subtype()
+  returns trigger
+as $$
+begin
+  if new.name != old.name then 
+    update auth_method set name = new.name;
+  end if;
+  return new;
+end;
+$$ language plpgsql;
+comment on function update_auth_method_subtype() is
+'update_auth_method_subtype() will update base auth method type name column with new values from sub type';
+
+create trigger
+  update_auth_method_subtype
+before insert on auth_oidc_method
+  for each row execute procedure update_auth_method_subtype();
+
+
+create trigger
+  update_auth_method_subtype
+before insert on auth_password_method
+  for each row execute procedure update_auth_method_subtype();
+
+
+-- delete_auth_method_subtype() is an after trigger function for subytypes of
+-- auth_method
+create or replace function
+  delete_auth_method_subtype()
+  returns trigger
+as $$
+begin
+  delete from auth_method
+  where public_id = old.public_id;
+  return null; -- results are ignore since this is an after trigger.
+end;
+$$ language plpgsql;
+comment on function delete_auth_method_subtype is
+'delete_auth_method_subtype() is an after trigger function for subytypes of auth_method';
+
+create trigger
+  delete_auth_method_subtype
+after delete on auth_oidc_method
+  for each row execute procedure delete_auth_method_subtype();
+
+
+create trigger
+  delete_auth_method_subtype
+after delete on auth_password_method 
+  for each row execute procedure delete_auth_method_subtype();
 `),
 		},
 	}
