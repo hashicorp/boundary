793a56f0c0bce7347f6ab94ee6a74af44d382fe8
Jim Lambert <jlambert@hashicorp.com>
2021-04-20 08:09:10 -0400

    add a query to help operators deal with migration issues.

--- internal/db/schema/postgres_migration.gen.go
+++ internal/db/schema/postgres_migration.gen.go
@@ -5870,7 +5870,27 @@ comment on view auth_password_method_with_is_primary is
 `),
 			3001: []byte(`
 -- this constraint is intended to ensure that a user cannot have more than one
--- account per auth_method
+-- account per auth_method. 
+--
+-- If this constraint causes the migrations to fail the operartor can run the
+-- following query to get a list of user ids which have more that one account
+-- within the same auth method.  At that point, the operator will need to pick
+-- which account to keep.    
+----------------------------------------------------------------------------
+-- with too_many_accounts(iam_user_id, acct_count) as (
+--   select 
+--     iam_user_id, 
+--     count(*) as acct_count
+--   from 	
+--       auth_account 
+--   group by auth_method_id, iam_user_id 
+-- )
+-- select 
+--   *
+-- from
+--   too_many_accounts
+-- where 
+--   acct_count > 1;
 alter table auth_account
   add constraint auth_account_auth_method_id_public_id_uq
     unique(auth_method_id, iam_user_id);
