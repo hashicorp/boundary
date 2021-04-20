fd20b7da16c48290c87af3ba1957c4c31a983a81
Jim Lambert <jlambert@hashicorp.com>
2021-04-20 19:01:46 -0400

    add test for Delete

--- internal/auth/oidc/claims_scope_test.go
+++ internal/auth/oidc/claims_scope_test.go
@@ -57,6 +57,83 @@ func TestClaimsScope_Create(t *testing.T) {
 	}
 }
 
+func TestClaimsScope_Delete(t *testing.T) {
+	t.Parallel()
+	conn, _ := db.TestSetup(t, "postgres")
+	wrapper := db.TestWrapper(t)
+	kmsCache := kms.TestKms(t, conn, wrapper)
+	org, _ := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
+	rw := db.New(conn)
+
+	databaseWrapper, err := kmsCache.GetWrapper(context.Background(), org.PublicId, kms.KeyPurposeDatabase)
+	require.NoError(t, err)
+
+	testAuthMethod :=
+		TestAuthMethod(t, conn, databaseWrapper, org.PublicId, InactiveState, "alice_rp", "my-dogs-name",
+			WithApiUrl(TestConvertToUrls(t, "https://api.com")[0]), WithAudClaims("alice.com")) // seed an extra callback url to just make sure the delete only gets the right num of rows
+
+	testResource := func(authMethodId string, claimsScope string) *ClaimsScope {
+		c, err := NewClaimsScope(authMethodId, claimsScope)
+		require.NoError(t, err)
+		return c
+	}
+	tests := []struct {
+		name            string
+		claimsScope     *ClaimsScope
+		wantRowsDeleted int
+		overrides       func(*ClaimsScope)
+		wantErr         bool
+		wantErrMsg      string
+	}{
+		{
+			name:            "valid",
+			claimsScope:     testResource(testAuthMethod.PublicId, "email"),
+			wantErr:         false,
+			wantRowsDeleted: 1,
+		},
+		{
+			name:            "bad-OidcMethodId",
+			claimsScope:     testResource(testAuthMethod.PublicId, "email"),
+			overrides:       func(c *ClaimsScope) { c.OidcMethodId = "bad-id" },
+			wantErr:         false,
+			wantRowsDeleted: 0,
+		},
+		{
+			name:            "bad-scope",
+			claimsScope:     testResource(testAuthMethod.PublicId, "valid-3"),
+			overrides:       func(c *ClaimsScope) { c.Scope = "bad-scope" },
+			wantErr:         false,
+			wantRowsDeleted: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			ctx := context.Background()
+			cp := tt.claimsScope.Clone()
+			require.NoError(rw.Create(ctx, &cp))
+
+			if tt.overrides != nil {
+				tt.overrides(cp)
+			}
+			deletedRows, err := rw.Delete(ctx, &cp)
+			if tt.wantErr {
+				require.Error(err)
+				return
+			}
+			require.NoError(err)
+			if tt.wantRowsDeleted == 0 {
+				assert.Equal(tt.wantRowsDeleted, deletedRows)
+				return
+			}
+			assert.Equal(tt.wantRowsDeleted, deletedRows)
+			found := AllocClaimsScope()
+			err = rw.LookupWhere(ctx, &found, "oidc_method_id = ? and scope = ?", tt.claimsScope.OidcMethodId, tt.claimsScope.Scope)
+			assert.True(errors.IsNotFoundError(err))
+		})
+	}
+}
+
 func TestClaimsScope_Clone(t *testing.T) {
 	t.Parallel()
 	conn, _ := db.TestSetup(t, "postgres")
