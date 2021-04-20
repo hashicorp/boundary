41ec5e3dd69434fb03d0c63fd7e96c665c989299
Jim Lambert <jlambert@hashicorp.com>
2021-04-20 16:14:28 -0400

    unit tests for Clone(...) and SetTablename(...)

--- internal/auth/oidc/claims_scope_test.go
+++ internal/auth/oidc/claims_scope_test.go
@@ -6,8 +6,11 @@ import (
 
 	"github.com/hashicorp/boundary/internal/db"
 	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/iam"
+	"github.com/hashicorp/boundary/internal/kms"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestClaimsScope_Create(t *testing.T) {
@@ -50,6 +53,72 @@ func TestClaimsScope_Create(t *testing.T) {
 				found := AllocClaimsScope()
 				require.NoError(rw.LookupWhere(ctx, &found, "oidc_method_id = ? and scope = ?", tt.args.authMethodId, tt.args.claimsScope))
 			}
+		})
+	}
+}
+
+func TestClaimsScope_Clone(t *testing.T) {
+	t.Parallel()
+	conn, _ := db.TestSetup(t, "postgres")
+	wrapper := db.TestWrapper(t)
+	kmsCache := kms.TestKms(t, conn, wrapper)
+
+	t.Run("valid", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		org, _ := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
+		databaseWrapper, err := kmsCache.GetWrapper(context.Background(), org.PublicId, kms.KeyPurposeDatabase)
+		require.NoError(err)
+		m := TestAuthMethod(t, conn, databaseWrapper, org.PublicId, InactiveState, "alice_rp", "my-dogs-name",
+			WithIssuer(TestConvertToUrls(t, "https://alice.com")[0]), WithApiUrl(TestConvertToUrls(t, "https://api.com")[0]))
+		orig, err := NewClaimsScope(m.PublicId, "profile")
+		require.NoError(err)
+		cp := orig.Clone()
+		assert.True(proto.Equal(cp.ClaimsScope, orig.ClaimsScope))
+	})
+	t.Run("not-equal", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		org, _ := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
+		databaseWrapper, err := kmsCache.GetWrapper(context.Background(), org.PublicId, kms.KeyPurposeDatabase)
+		require.NoError(err)
+		m := TestAuthMethod(t, conn, databaseWrapper, org.PublicId, InactiveState, "alice_rp", "my-dogs-name",
+			WithIssuer(TestConvertToUrls(t, "https://alice.com")[0]), WithApiUrl(TestConvertToUrls(t, "https://api.com")[0]))
+		orig, err := NewClaimsScope(m.PublicId, "email")
+		require.NoError(err)
+		orig2, err := NewClaimsScope(m.PublicId, "profile")
+		require.NoError(err)
+
+		cp := orig.Clone()
+		assert.True(!proto.Equal(cp.ClaimsScope, orig2.ClaimsScope))
+	})
+}
+
+func TestClaimsScope_SetTableName(t *testing.T) {
+	t.Parallel()
+	defaultTableName := defaultClaimsScopeTableName
+	tests := []struct {
+		name      string
+		setNameTo string
+		want      string
+	}{
+		{
+			name:      "new-name",
+			setNameTo: "new-name",
+			want:      "new-name",
+		},
+		{
+			name:      "reset to default",
+			setNameTo: "",
+			want:      defaultTableName,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			def := AllocClaimsScope()
+			require.Equal(defaultTableName, def.TableName())
+			m := AllocClaimsScope()
+			m.SetTableName(tt.setNameTo)
+			assert.Equal(tt.want, m.TableName())
 		})
 	}
 }
