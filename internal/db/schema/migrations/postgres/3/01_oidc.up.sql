0ca54d5e426773e495a6d8bc14768f7c462c7e37
Jim <jlambert@hashicorp.com>
2021-04-20 07:04:35 -0400

    schema changes. (#1148)

---
+++ internal/db/schema/migrations/postgres/3/01_oidc.up.sql
@@ -0,0 +1,36 @@
+begin;
+
+
+-- auth_oidc_scope entries are the optional scopes for a specific oidc auth
+-- method.  There can be 0 or more for each parent oidc auth method.  If an auth
+-- method has any scopes, they will be added to provider requests along with the
+-- default of "openid". 
+create table auth_oidc_scope (
+  create_time wt_timestamp,
+  oidc_method_id wt_public_id 
+    constraint auth_oidc_method_fkey
+    references auth_oidc_method(public_id)
+    on delete cascade
+    on update cascade,
+  scope text not null
+    constraint scope_must_not_be_empty
+       check(length(trim(scope)) > 0) 
+    constraint scope_must_be_less_than_1024_chars
+      check(length(trim(scope)) < 1024)
+    constraint scope_must_not_be_openid -- the default scope is not allowed, since it's redundant
+      check(lower(trim(scope)) != 'openid'),
+  primary key(oidc_method_id, scope)
+);
+comment on table auth_oidc_scope is
+'auth_oidc_scope entries are the optional scopes for a specific oidc auth method.  There can be 0 or more for each parent oidc auth method.  If an auth method has any scopes, they will be added to provider requests along with the openid default.';
+
+
+
+
+create trigger
+  default_create_time_column
+before
+insert on auth_oidc_scope
+  for each row execute procedure default_create_time();
+
+commit;
