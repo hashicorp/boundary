45e23ddfe4d1b3231ff330fd61923762402a0627
Jim Lambert <jlambert@hashicorp.com>
2021-04-22 11:14:39 -0400

    add ClaimsScopes support

--- internal/auth/oidc/repository_auth_method_read.go
+++ internal/auth/oidc/repository_auth_method_read.go
@@ -158,6 +158,9 @@ func (r *Repository) getAuthMethods(ctx context.Context, authMethodId string, sc
 		if agg.Certs != "" {
 			am.Certificates = strings.Split(agg.Certs, aggregateDelimiter)
 		}
+		if agg.Scopes != "" {
+			am.ClaimsScopes = strings.Split(agg.Scopes, aggregateDelimiter)
+		}
 		authMethods = append(authMethods, &am)
 	}
 	return authMethods, nil
@@ -187,6 +190,7 @@ type authMethodAgg struct {
 	ApiUrl                            string
 	Auds                              string
 	Certs                             string
+	Scopes                            string
 }
 
 // TableName returns the table name for gorm
