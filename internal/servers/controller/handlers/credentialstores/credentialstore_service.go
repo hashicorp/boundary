6934dee73acca451bdd987f5f2a593af60115cb2
Todd Knight <T.Alan.Knight@gmail.com>
2021-05-04 11:52:23 -0700

    Creating the credential store resource in the API along with mask mapping.

---
+++ internal/servers/controller/handlers/credentialstores/credentialstore_service.go
@@ -0,0 +1,35 @@
+package credentialstores
+
+import (
+	"github.com/hashicorp/boundary/internal/credential/vault/store"
+	pb "github.com/hashicorp/boundary/internal/gen/controller/api/resources/credentialstores"
+	"github.com/hashicorp/boundary/internal/servers/controller/handlers"
+	"github.com/hashicorp/boundary/internal/types/action"
+)
+
+var (
+	maskManager handlers.MaskManager
+
+	// IdActions contains the set of actions that can be performed on
+	// individual resources
+	IdActions = action.ActionSet{
+		action.NoOp,
+		action.Read,
+		action.Update,
+		action.Delete,
+	}
+
+	// CollectionActions contains the set of actions that can be performed on
+	// this collection
+	CollectionActions = action.ActionSet{
+		action.Create,
+		action.List,
+	}
+)
+
+func init() {
+	var err error
+	if maskManager, err = handlers.NewMaskManager(&store.CredentialStore{}, &pb.CredentialStore{}, &pb.VaultCredentialStoreAttributes{}); err != nil {
+		panic(err)
+	}
+}
---
+++ internal/servers/controller/handlers/credentialstores/credentialstore_service_test.go
@@ -0,0 +1,7 @@
+package credentialstores
+
+import "testing"
+
+func TestDummy(t *testing.T) {
+	// This is currently added to uncover incorrect Mask Mapping.
+}
