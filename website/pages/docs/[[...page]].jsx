d1d35e827cfb9a192e73561d5a1035b3bebe8680
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-05-06 13:32:41 -0400

    website: support hidden pages in nav-data (#1201)
    * website: bump to docs-page prerelease w hidden page support
    
    * website: replace hot fix for hidden /docs pages
    
    * website: remove cruft from old hidden page hot fix
    
    * website: bump to stable docs-page, w next-mdx-remote bump

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -3,16 +3,8 @@ import DocsPage from '@hashicorp/react-docs-page'
 import {
   generateStaticPaths,
   generateStaticProps,
-} from 'components/_temp-enable-hidden-pages'
+} from '@hashicorp/react-docs-page/server'
 
-/**
- * DEBT: short term patch for "hidden" docs-sidenav items.
- * See components/_temp-enable-hidden-pages for details.
- * Revert to importing from @hashicorp/react-docs-page/server
- * once https://app.asana.com/0/1100423001970639/1200197752405255/f
- * is complete.
- **/
-const NAV_DATA_FILE_HIDDEN = 'data/docs-nav-data-hidden.json'
 const NAV_DATA_FILE = 'data/docs-nav-data.json'
 const CONTENT_DIR = 'content/docs'
 const basePath = 'docs'
@@ -32,7 +24,6 @@ export async function getStaticPaths() {
     fallback: false,
     paths: await generateStaticPaths({
       navDataFile: NAV_DATA_FILE,
-      navDataFileHidden: NAV_DATA_FILE_HIDDEN,
       localContentDir: CONTENT_DIR,
     }),
   }
@@ -42,7 +33,6 @@ export async function getStaticProps({ params }) {
   return {
     props: await generateStaticProps({
       navDataFile: NAV_DATA_FILE,
-      navDataFileHidden: NAV_DATA_FILE_HIDDEN,
       localContentDir: CONTENT_DIR,
       product: { name: productName, slug: productSlug },
       params,
