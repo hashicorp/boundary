dc33ee3505566aaae8b4cb47ccdeedf6a1daca4e
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-05-07 12:45:10 -0400

    patch: clean up css override marked as debt

--- website/pages/home/index.jsx
+++ website/pages/home/index.jsx
@@ -21,6 +21,7 @@ export default function HomePage() {
           {
             title: 'Download macOS Client',
             url: '/downloads#desktop',
+            linkType: 'inbound',
           },
         ]}
         uiVideo={{
