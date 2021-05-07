13ef1c47b1f0f9444dc3cc1ad2118a2aeba4ad45
Jim Lambert <jlambert@hashicorp.com>
2021-05-07 10:00:32 -0400

    a working version of event.WriteInfo(...)

--- internal/obs/events/context.go
+++ internal/obs/events/context.go
@@ -8,15 +8,38 @@ import (
 
 type key int
 
-var eventerKey key
-var requestInfoKey key
+const (
+	EventerKey key = iota
+	RequestInfoKey
+)
 
 type RequestInfo struct {
-	ID string
+	Id string
 }
 
 func NewEventerContext(ctx context.Context, eventer *Eventer) (context.Context, error) {
-	return context.WithValue(ctx, eventerKey, eventer), nil
+	const op = "event.NewEventerContext"
+	if ctx == nil {
+		return nil, errors.New(errors.InvalidParameter, op, "missing context")
+	}
+	if eventer == nil {
+		return nil, errors.New(errors.InvalidParameter, op, "missing eventer")
+	}
+	return context.WithValue(ctx, EventerKey, eventer), nil
+}
+
+func NewRequestInfoContext(ctx context.Context, info *RequestInfo) (context.Context, error) {
+	const op = "event.NewRequestInfoContext"
+	if ctx == nil {
+		return nil, errors.New(errors.InvalidParameter, op, "missing context")
+	}
+	if info == nil {
+		return nil, errors.New(errors.InvalidParameter, op, "missing request info")
+	}
+	if info.Id == "" {
+		return nil, errors.New(errors.InvalidParameter, op, "missing request info ID")
+	}
+	return context.WithValue(ctx, RequestInfoKey, info), nil
 }
 
 func WriteInfo(ctx context.Context, caller Op, opt ...Option) error {
@@ -25,15 +48,15 @@ func WriteInfo(ctx context.Context, caller Op, opt ...Option) error {
 	if opts.withDetails == nil && opts.withHeader == nil {
 		return errors.New(errors.InvalidParameter, op, "you must specify either header or details options for an event payload")
 	}
-	eventer, ok := ctx.Value(eventerKey).(*Eventer)
+	eventer, ok := ctx.Value(EventerKey).(*Eventer)
 	if !ok {
 		return errors.New(errors.InvalidParameter, op, "context missing eventer")
 	}
-	reqInfo, ok := ctx.Value(requestInfoKey).(*RequestInfo)
+	reqInfo, ok := ctx.Value(RequestInfoKey).(*RequestInfo)
 	if !ok {
 		return errors.New(errors.InvalidParameter, op, "context missing request info")
 	}
-	e, err := NewInfo(reqInfo.ID, caller, opt...)
+	e, err := NewInfo(reqInfo.Id, caller, opt...)
 	if err != nil {
 		return errors.Wrap(err, op)
 	}
---
+++ internal/obs/events/context_test.go
@@ -0,0 +1,63 @@
+package event_test
+
+import (
+	"context"
+	"testing"
+
+	event "github.com/hashicorp/boundary/internal/obs/events"
+	"github.com/hashicorp/go-hclog"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_WriteInfo(t *testing.T) {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Name: "test",
+	})
+	c := event.Config{
+		InfoEnabled: true,
+		Sinks: []event.SinkConfig{
+			{
+				Name:       "tmp.txt",
+				EventTypes: []event.Type{event.EveryType},
+				Format:     event.JSONSinkFormat,
+				Path:       "./",
+				FileName:   "tmp.txt",
+			},
+		},
+	}
+	e, err := event.NewEventer(logger, c)
+	require.NoError(t, err)
+
+	info := &event.RequestInfo{Id: "867-5309"}
+
+	testHdr := map[string]interface{}{
+		"name": "bar",
+		"list": []string{"1", "2"},
+	}
+
+	ctx, err := event.NewEventerContext(context.Background(), e)
+	require.NoError(t, err)
+	ctx, err = event.NewRequestInfoContext(ctx, info)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name    string
+		header  map[string]interface{}
+		details map[string]interface{}
+		ctx     context.Context
+	}{
+		{
+			name:   "simple",
+			ctx:    ctx,
+			header: testHdr,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+			err := event.WriteInfo(tt.ctx, "Test_WriteInfo", event.WithHeader(tt.header))
+			require.NoError(err)
+		})
+	}
+
+}
