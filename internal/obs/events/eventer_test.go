68a1e980741bba1ecaef5c78ecc8d0f0f44c2a36
Jim Lambert <jlambert@hashicorp.com>
2021-05-09 07:43:53 -0400

    refactored to deal with how event ids are handled

--- internal/obs/events/eventer_test.go
+++ internal/obs/events/eventer_test.go
@@ -21,14 +21,11 @@ func Test_NewEventer(t *testing.T) {
 	e, err := NewEventer(logger, c)
 	require.NoError(err)
 
-	id, err := newId(string(InfoType))
-	require.NoError(err)
-
 	m := map[string]interface{}{
 		"name": "bar",
 		"list": []string{"1", "2"},
 	}
-	infoEvent, err := NewInfo(id, "Test_NewEventer", WithHeader(m))
+	infoEvent, err := NewInfo("Test_NewEventer", WithHeader(m))
 	require.NoError(err)
 
 	require.NoError(e.Info(context.Background(), infoEvent))
