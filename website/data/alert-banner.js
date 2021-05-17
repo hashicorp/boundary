9906ae02a4c6f7b779cc76ab97e7940589eb671a
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2021-05-17 15:42:36 -0500

    Add HashiConf alert banner data (#1232)

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -1,7 +1,10 @@
-export const ALERT_BANNER_ACTIVE = false
+export const ALERT_BANNER_ACTIVE = true
 
 export default {
-  tag: 'ANNOUNCING',
-  text: 'Some details about an announcement here',
-  url: 'https://hashicorp.com',
+  tag: 'June 8-11',
+  text:
+    'The countdown to HashiConf Europe is on, and the full schedule is now live.',
+  linkText: 'View Schedule',
+  url: 'https://hashiconf.com/europe/?utm_source=DocsBanner',
+  expirationDate: null,
 }
