2da72039b94d3dee7b3184291d34c6e0105ddc28
Jim Lambert <jlambert@hashicorp.com>
2021-05-23 13:13:38 -0400

    refactor to use SimpleGatedPayload

--- internal/obs/events/event_observation.go
+++ internal/obs/events/event_observation.go
@@ -1,15 +1,16 @@
 package event
 
-import "github.com/hashicorp/boundary/internal/errors"
+import (
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/eventlogger"
+)
 
 // fields are intentionally alphabetically ordered so they will match output
 // from marshaling event json
 type Observation struct {
-	Details     map[string]interface{} `json:"details,omitempty"`
-	Header      map[string]interface{} `json:"header,omitempty"`
-	Id          Id                     `json:"id,omitempty"`
-	Op          Op                     `json:"op,omitempty"`
-	RequestInfo *RequestInfo           `json:"request_info,omitempty"`
+	eventlogger.SimpleGatedPayload
+	Op          Op           `json:"op,omitempty"`
+	RequestInfo *RequestInfo `json:"request_info,omitempty"`
 }
 
 func NewObservation(fromOperation Op, opt ...Option) (*Observation, error) {
@@ -26,11 +27,13 @@ func NewObservation(fromOperation Op, opt ...Option) (*Observation, error) {
 		}
 	}
 	i := &Observation{
-		Id:          Id(opts.withId),
+		SimpleGatedPayload: eventlogger.SimpleGatedPayload{
+			ID:     opts.withId,
+			Header: opts.withHeader,
+			Detail: opts.withDetails,
+		},
 		Op:          fromOperation,
 		RequestInfo: opts.withRequestInfo,
-		Header:      opts.withHeader,
-		Details:     opts.withDetails,
 	}
 	if err := i.validate(); err != nil {
 		return nil, errors.Wrap(err, op)
@@ -43,7 +46,7 @@ func (i *Observation) EventType() string { return string(ObservationType) }
 
 func (i *Observation) validate() error {
 	const op = "event.(Observation).validate"
-	if i.Id == "" {
+	if i.ID == "" {
 		return errors.New(errors.InvalidParameter, op, "missing event id")
 	}
 	if i.Op == "" {
