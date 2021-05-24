9e2bd5d432f6261c108288c914778a867b252188
Jim Lambert <jlambert@hashicorp.com>
2021-05-24 14:30:27 -0400

    just use an empty struct for now until the RFC is done

--- internal/observability/events/event_audit.go
+++ internal/observability/events/event_audit.go
@@ -3,10 +3,4 @@ package event
 // fields are intentionally alphabetically ordered so they will match output
 // from marshaling event json
 type Audit struct {
-	Id      Id     `json:"id,omitempty"`
-	Op      Op     `json:"op,omitempty"`
-	Version string `json:"version"`
-	Type    string `json:"type"`
-
-	RequestInfo *RequestInfo `json:"request_info,omitempty"`
 }
