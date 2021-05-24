b8e3f92268c4d0fb90cc92b49309df6a5b1660d2
Jim Lambert <jlambert@hashicorp.com>
2021-05-24 19:31:00 -0400

    create precedence when "discoverying" eventers and add some godocs

--- internal/observability/events/context.go
+++ internal/observability/events/context.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/go-hclog"
 )
 
 type key int
@@ -38,11 +39,19 @@ func NewRequestInfoContext(ctx context.Context, info *RequestInfo) (context.Cont
 	return context.WithValue(ctx, RequestInfoKey, info), nil
 }
 
+// WriteObservation will write an observation event.  It will first check the
+// ctx for an eventer, then try event.SysEventer() and if no eventer can be
+// found an error is returned.  At least one and any combination of the
+// supported options may be used: WithHeader, WithDetails, WithId, WithFlush and
+// WithRequestInfo. All other options are ignored.
 func WriteObservation(ctx context.Context, caller Op, opt ...Option) error {
 	const op = "event.WriteObservation"
 	eventer, ok := ctx.Value(EventerKey).(*Eventer)
 	if !ok {
-		return errors.New(errors.InvalidParameter, op, "context missing eventer")
+		eventer = SysEventer()
+		if eventer == nil {
+			return errors.New(errors.InvalidParameter, op, "missing both context and system eventer")
+		}
 	}
 	opts := getOpts(opt...)
 	if opts.withDetails == nil && opts.withHeader == nil && !opts.withFlush {
@@ -58,33 +67,47 @@ func WriteObservation(ctx context.Context, caller Op, opt ...Option) error {
 	if err != nil {
 		return errors.Wrap(err, op)
 	}
-	if err := eventer.writeObservation(ctx, e, opt...); err != nil {
+	if err := eventer.writeObservation(ctx, e); err != nil {
 		return errors.Wrap(err, op)
 	}
 	return nil
 }
 
-func WriteError(ctx context.Context, caller Op, e error, opt ...Option) error {
+// WriteError will write an error event.  It will first check the
+// ctx for an eventer, then try event.SysEventer() and if no eventer can be
+// found an hclog.Logger will be created and used. The options WithId and
+// WithRequestInfo are supported and all other options are ignored.
+func WriteError(ctx context.Context, caller Op, e error, opt ...Option) {
 	const op = "event.WriteError"
 	eventer, ok := ctx.Value(EventerKey).(*Eventer)
 	if !ok {
-		return errors.New(errors.InvalidParameter, op, "context missing eventer")
+		eventer = SysEventer()
+		if eventer == nil {
+			logger := hclog.New(nil)
+			logger.Error("%s: no eventer available to write error: %w", e)
+			return
+		}
 	}
 	opts := getOpts(opt...)
 	if opts.withRequestInfo == nil {
 		var err error
 		if opt, err = addCtxOptions(ctx, opt...); err != nil {
-			return errors.Wrap(err, op)
+			eventer.logger.Error("%s: %w", errors.Wrap(err, op))
+			eventer.logger.Error("%s: unable to process context options to write error: %w", e)
+			return
 		}
 	}
 	ev, err := newError(caller, e, opt...)
 	if err != nil {
-		return errors.Wrap(err, op)
+		eventer.logger.Error("%s: %w", errors.Wrap(err, op))
+		eventer.logger.Error("%s: unable to create new error to write error: %w", e)
+		return
 	}
 	if err := eventer.writeError(ctx, ev); err != nil {
-		return errors.Wrap(err, op)
+		eventer.logger.Error("%s: %w", errors.Wrap(err, op))
+		eventer.logger.Error("%s: unable to write error: %w", e)
+		return
 	}
-	return nil
 }
 
 func WriteAudit(ctx context.Context, caller Op, opt ...Option) error {
