0612cc344f4bdadb96086270a1e4b60c62819b81
Jim Lambert <jlambert@hashicorp.com>
2021-05-24 14:00:35 -0400

    begin defining structs

--- internal/observability/events/event_audit.go
+++ internal/observability/events/event_audit.go
@@ -3,7 +3,10 @@ package event
 // fields are intentionally alphabetically ordered so they will match output
 // from marshaling event json
 type Audit struct {
-	Id          Id           `json:"id,omitempty"`
-	Op          Op           `json:"op,omitempty"`
+	Id      Id     `json:"id,omitempty"`
+	Op      Op     `json:"op,omitempty"`
+	Version string `json:"version"`
+	Type    string `json:"type"`
+
 	RequestInfo *RequestInfo `json:"request_info,omitempty"`
 }
