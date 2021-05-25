f7a56192068f408172c0b6af2ec84811e431a74e
Jim Lambert <jlambert@hashicorp.com>
2021-05-24 20:01:39 -0400

    add version to error event

--- internal/observability/events/event_error.go
+++ internal/observability/events/event_error.go
@@ -2,11 +2,14 @@ package event
 
 import "github.com/hashicorp/boundary/internal/errors"
 
+const ErrorVersion = "v0.1"
+
 // fields are intentionally alphabetically ordered so they will match output
 // from marshaling event json
 type err struct {
 	Error       error        `json:"error"`
 	Id          Id           `json:"id,omitempty"`
+	Version     string       `json:"version"`
 	Op          Op           `json:"op,omitempty"`
 	RequestInfo *RequestInfo `json:"request_info,omitempty"`
 }
@@ -27,6 +30,7 @@ func newError(fromOperation Op, e error, opt ...Option) (*err, error) {
 	newErr := &err{
 		Id:          Id(opts.withId),
 		Op:          fromOperation,
+		Version:     ErrorVersion,
 		RequestInfo: opts.withRequestInfo,
 		Error:       e,
 	}
