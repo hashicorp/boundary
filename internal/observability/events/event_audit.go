af8930c4ee57a53b63def6c700a4e93c7c9aadfb
Jim Lambert <jlambert@hashicorp.com>
2021-05-25 14:00:09 -0400

    add GatedFilter support and remove Op

--- internal/observability/events/event_audit.go
+++ internal/observability/events/event_audit.go
@@ -1,9 +1,11 @@
 package event
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/eventlogger"
 )
 
 const AuditVersion = "v0.1"
@@ -14,10 +16,9 @@ const (
 	ApiRequest AuditEventType = "APIRequest"
 )
 
-type audit struct {
+type Audit struct {
 	Id             string       `json:"id"`                     // std audit/boundary field
 	Version        string       `json:"version"`                // std audit/boundary field
-	Op             Op           `json:"op"`                     // std boundary field
 	Type           string       `json:"type"`                   // std audit field
 	Timestamp      time.Time    `json:"timestamp"`              // std audit field
 	RequestInfo    *RequestInfo `json:"request_info,omitempty"` // boundary field
@@ -29,7 +30,7 @@ type audit struct {
 	Flush          bool         `json:"-"`
 }
 
-func newAudit(fromOperation Op, opt ...Option) (*audit, error) {
+func newAudit(fromOperation Op, opt ...Option) (*Audit, error) {
 	const op = "event.newAudit"
 	if fromOperation == "" {
 		return nil, errors.New(errors.InvalidParameter, op, "missing from operation")
@@ -50,9 +51,8 @@ func newAudit(fromOperation Op, opt ...Option) (*audit, error) {
 		dtm = time.Now()
 	}
 
-	a := &audit{
+	a := &Audit{
 		Id:          opts.withId,
-		Op:          fromOperation,
 		Version:     AuditVersion,
 		Type:        string(ApiRequest),
 		Timestamp:   dtm,
@@ -60,6 +60,7 @@ func newAudit(fromOperation Op, opt ...Option) (*audit, error) {
 		Auth:        opts.withAuth,
 		Request:     opts.withRequest,
 		Response:    opts.withResponse,
+		Flush:       opts.withFlush,
 	}
 	if err := a.validate(); err != nil {
 		return nil, errors.Wrap(err, op)
@@ -68,15 +69,58 @@ func newAudit(fromOperation Op, opt ...Option) (*audit, error) {
 }
 
 // EventType is required for all event types by the eventlogger broker
-func (a *audit) EventType() string { return string(AuditType) }
+func (a *Audit) EventType() string { return string(AuditType) }
 
-func (a *audit) validate() error {
-	const op = "event.(Audti).validate"
+func (a *Audit) validate() error {
+	const op = "event.(audit).validate"
 	if a.Id == "" {
 		return errors.New(errors.InvalidParameter, op, "missing event id")
 	}
-	if a.Op == "" {
-		return errors.New(errors.InvalidParameter, op, "missing operation which raised event")
-	}
 	return nil
+}
+
+// GetID is part of the eventlogger.Gateable interface and returns the audit
+// event's id.
+func (a *Audit) GetID() string {
+	return a.Id
+}
+
+// FlushEvent is part of the eventlogger.Gateable interface and returns the
+// value of the audit event's flush field
+func (a *Audit) FlushEvent() bool {
+	return a.Flush
+}
+
+func (a *Audit) ComposeFrom(events []*eventlogger.Event) (eventlogger.EventType, interface{}, error) {
+	const op = "event.(audit).ComposedFrom"
+	if len(events) == 0 {
+		return "", nil, errors.New(errors.InvalidParameter, op, "missing events")
+
+	}
+	payload := Audit{}
+	for i, v := range events {
+		gated, ok := v.Payload.(*Audit)
+		if !ok {
+			return "", nil, errors.New(errors.InvalidParameter, op, fmt.Sprintf("event %d is not a simple gated payload", i))
+		}
+		if gated.RequestInfo != nil {
+			payload.RequestInfo = gated.RequestInfo
+		}
+		if gated.Auth != nil {
+			payload.Auth = gated.Auth
+		}
+		if gated.Request != nil {
+			payload.Request = gated.Request
+		}
+		if gated.Response != nil {
+			payload.Response = gated.Response
+		}
+		if !gated.Timestamp.IsZero() {
+			payload.Timestamp = gated.Timestamp
+		}
+		payload.Id = gated.Id
+		payload.Version = gated.Version
+		payload.Type = gated.Type
+	}
+	return eventlogger.EventType(a.EventType()), payload, nil
 }
