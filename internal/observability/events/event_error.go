0fb466410bb7788b1e39c426bde52db776232341
Jim Lambert <jlambert@hashicorp.com>
2021-06-01 10:06:27 -0400

    better validation for err event type

--- internal/observability/events/event_error.go
+++ internal/observability/events/event_error.go
@@ -15,7 +15,10 @@ type err struct {
 func newError(fromOperation Op, e error, opt ...Option) (*err, error) {
 	const op = "event.newError"
 	if fromOperation == "" {
-		return nil, errors.New(errors.InvalidParameter, op, "missing from operation")
+		return nil, errors.New(errors.InvalidParameter, op, "missing operation")
+	}
+	if e == nil {
+		return nil, errors.New(errors.InvalidParameter, op, "missing error")
 	}
 	opts := getOpts(opt...)
 	if opts.withId == "" {
@@ -44,10 +47,13 @@ func (e *err) EventType() string { return string(ErrorType) }
 func (e *err) validate() error {
 	const op = "event.(err).validate"
 	if e.Id == "" {
-		return errors.New(errors.InvalidParameter, op, "missing event id")
+		return errors.New(errors.InvalidParameter, op, "missing id")
 	}
 	if e.Op == "" {
-		return errors.New(errors.InvalidParameter, op, "missing operation which raised event")
+		return errors.New(errors.InvalidParameter, op, "missing operation")
+	}
+	if e.Error == nil {
+		return errors.New(errors.InvalidParameter, op, "missing error")
 	}
 	return nil
 }
