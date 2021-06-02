e3b4440269a4dd57660aaf51e359860f094aec7a
Jim Lambert <jlambert@hashicorp.com>
2021-06-02 16:14:28 -0400

    add a testBroker

--- internal/observability/events/testing.go
+++ internal/observability/events/testing.go
@@ -1,13 +1,16 @@
 package event
 
 import (
+	"context"
 	"io/ioutil"
 	"os"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/boundary/internal/gen/controller/api/resources/groups"
 	pbs "github.com/hashicorp/boundary/internal/gen/controller/api/services"
+	"github.com/hashicorp/eventlogger"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
@@ -31,7 +34,7 @@ type TestConfig struct {
 func TestEventerConfig(t *testing.T, testName string) TestConfig {
 	t.Helper()
 	require := require.New(t)
-	tmpAllFile, err := ioutil.TempFile("./", "tmp-observations-"+testName)
+	tmpAllFile, err := ioutil.TempFile("./", "tmp-all-events-"+testName)
 	require.NoError(err)
 
 	tmpErrFile, err := ioutil.TempFile("./", "tmp-errors-"+testName)
@@ -116,4 +119,50 @@ func testResponse(t *testing.T) *Response {
 			},
 		},
 	}
+}
+
+// testWithBroker is an unexported and a test option for passing in an optional broker
+func testWithBroker(b broker) Option {
+	return func(o *options) {
+		o.withBroker = b
+	}
+}
+
+type testBroker struct {
+	reopened          bool
+	stopTimeAt        time.Time
+	registeredNodeIds []eventlogger.NodeID
+	successThresholds map[eventlogger.EventType]int
+	pipelines         []eventlogger.Pipeline
+}
+
+func (b *testBroker) Reopen(ctx context.Context) error {
+	b.reopened = true
+	return nil
+}
+
+func (b *testBroker) RegisterPipeline(def eventlogger.Pipeline) error {
+	b.pipelines = append(b.pipelines, def)
+	return nil
+}
+
+func (b *testBroker) Send(ctx context.Context, t eventlogger.EventType, payload interface{}) (eventlogger.Status, error) {
+	panic("not-implemented")
+}
+
+func (b *testBroker) StopTimeAt(t time.Time) {
+	b.stopTimeAt = t
+}
+
+func (b *testBroker) RegisterNode(id eventlogger.NodeID, node eventlogger.Node) error {
+	b.registeredNodeIds = append(b.registeredNodeIds, id)
+	return nil
+}
+
+func (b *testBroker) SetSuccessThreshold(t eventlogger.EventType, successThreshold int) error {
+	if b.successThresholds == nil {
+		b.successThresholds = map[eventlogger.EventType]int{}
+	}
+	b.successThresholds[t] = successThreshold
+	return nil
 }
