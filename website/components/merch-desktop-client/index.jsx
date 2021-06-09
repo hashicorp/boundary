86e6b476cf26428ec2af387959d65facbe845745
Brandon Romano <brandon@hashicorp.com>
2021-06-09 11:44:16 -0700

    Support multiple Desktop OS

--- website/components/merch-desktop-client/index.jsx
+++ website/components/merch-desktop-client/index.jsx
@@ -2,29 +2,68 @@ import s from './merch-desktop-client.module.css'
 import InlineSvg from '@hashicorp/react-inline-svg'
 
 export default function MerchDesktopDownload({ version, releases }) {
-  const { builds } = releases.versions[version]
+  var { builds } = releases.versions[version]
+
+  // Calculate all Operating Systems that we have versions for
+  // and place their respective builds under them
+  const operatingSystems = builds
+    .map((build) => {
+      return build.os
+    })
+    .filter((value, index, self) => {
+      // Parse out duplicates
+      return self.indexOf(value) === index
+    })
+    .map((os) => {
+      // Add the respective builds under their OS
+      return {
+        os: os,
+        builds: builds.filter((build) => {
+          return build.os === os
+        }),
+      }
+    })
 
   return (
     <div className={s.container} id="desktop">
       <div className={s.wrapper}>
-        <span className={s.title}>Desktop Client</span>
-        <div className={s.platformVersion}>
-          <InlineSvg
-            className={s.logo}
-            src={require('./img/apple-logo.svg?include')}
-          />
-          <span className={s.version}>{version}</span>
-        </div>
-        <div className={s.downloadLinks}>
-          {builds.map((build) => (
-            <a key={build.filename} className={s.downloadLink} href={build.url}>
-              .{getFileExtension(build.filename)}
-            </a>
-          ))}
-        </div>
+        <span className={s.title}>Desktop Client - {version}</span>
+        <ul className={s.downloadsList}>
+          {operatingSystems.map((operatingSystem) => {
+            return (
+              <li key={operatingSystem.os}>
+                <InlineSvg
+                  src={require(`./img/${operatingSystem.os}.svg?include`)}
+                />
+                <ul className={s.versionsList}>
+                  {operatingSystem.builds.map((build) => {
+                    return (
+                      <li key={build.filename} className={s.versionDownload}>
+                        <a href={build.url}>
+                          .{getFileExtension(build.filename)} (
+                          {humanArch(build.arch)})
+                        </a>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </div>
   )
+}
+
+function humanArch(arch) {
+  if (arch === '386') {
+    return '32-bit'
+  }
+  if (arch === 'amd64') {
+    return '64-bit'
+  }
+  return arch
 }
 
 function getFileExtension(filename) {
