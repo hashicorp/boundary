352718223f095da2b0d402efec3205919519fa27
Jim Lambert <jlambert@hashicorp.com>
2021-06-14 07:33:00 -0400

    add better docs to PointerTag and Taggable

--- internal/observability/event/node/map.go
+++ internal/observability/event/node/map.go
@@ -4,13 +4,23 @@ package node
 // within a map[string]interface{} along with its DataClassification and
 // FilterOperation.
 type PointerTag struct {
-	Pointer        string
+
+	// Pointer is the pointerstructure pointer string to get/set a key within a
+	// map[string]interface{}  See: https://github.com/mitchellh/pointerstructure
+	Pointer string
+
+	// Classification is the DataClassification of data pointed to by the
+	// Pointer
 	Classification DataClassification
-	Filter         FilterOperation
+
+	// Filter is the FilterOperation to apply to the data pointed to by the
+	// Pointer
+	Filter FilterOperation
 }
 
 // Taggable defines an interface for taggable maps
 type Taggable interface {
+
 	// Tags will return a set of pointer tags for the map
 	Tags() ([]PointerTag, error)
 }
