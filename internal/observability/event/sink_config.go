99d5d20165f73ce4ae432641a41e469f8961da23
Jim <jlambert@hashicorp.com>
2021-06-21 15:38:46 -0400

    add hcl tags

--- internal/observability/event/sink_config.go
+++ internal/observability/event/sink_config.go
@@ -8,15 +8,15 @@ import (
 
 // SinkConfig defines the configuration for a Eventer sink
 type SinkConfig struct {
-	Name           string        // Name defines a name for the sink.
-	EventTypes     []Type        // EventTypes defines a list of event types that will be sent to the sink. See the docs for EventTypes for a list of accepted values.
-	SinkType       SinkType      // SinkType defines the type of sink (StderrSink or FileSink)
-	Format         SinkFormat    // Format defines the format for the sink (JSONSinkFormat)
-	Path           string        // Path defines the file path for the sink
-	FileName       string        // FileName defines the file name for the sink
-	RotateBytes    int           // RotateByes defines the number of bytes that should trigger rotation of a FileSink
-	RotateDuration time.Duration // RotateDuration defines how often a FileSink should be rotated
-	RotateMaxFiles int           // RotateMaxFiles defines how may historical rotated files should be kept for a FileSink
+	Name           string        `hcl:"name"`             // Name defines a name for the sink.
+	EventTypes     []Type        `hcl:"event_types"`      // EventTypes defines a list of event types that will be sent to the sink. See the docs for EventTypes for a list of accepted values.
+	SinkType       SinkType      `hcl:"sink_type"`        // SinkType defines the type of sink (StderrSink or FileSink)
+	Format         SinkFormat    `hcl:"format"`           // Format defines the format for the sink (JSONSinkFormat)
+	Path           string        `hcl:"path"`             // Path defines the file path for the sink
+	FileName       string        `hcl:"file_name"`        // FileName defines the file name for the sink
+	RotateBytes    int           `hcl:"rotate_bytes"`     // RotateByes defines the number of bytes that should trigger rotation of a FileSink
+	RotateDuration time.Duration `hcl:"rotate_duration"`  // RotateDuration defines how often a FileSink should be rotated
+	RotateMaxFiles int           `hcl:"rotate_max_files"` // RotateMaxFiles defines how may historical rotated files should be kept for a FileSink
 }
 
 func (sc *SinkConfig) validate() error {
