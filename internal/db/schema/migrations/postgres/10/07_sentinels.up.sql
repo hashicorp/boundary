26a0303b38acb5312d10da448acd270fa8a2c237
Louis Ruch <louisruch@gmail.com>
2021-06-25 13:30:03 -0700

    Add sentinel and sanitize package (#1353)
    * Add sentinel migrations
    
    * Add sentinel and sanitize package

---
+++ internal/db/schema/migrations/postgres/10/07_sentinels.up.sql
@@ -0,0 +1,36 @@
+begin;
+
+update credential_vault_credential
+   set external_id = concat(external_id, u&'\ffff')
+ where wt_is_sentinel(external_id)
+   and not starts_with(reverse(external_id), u&'\ffff');
+
+alter domain wt_sentinel
+    drop constraint wt_sentinel_not_valid;
+
+drop function wt_is_sentinel;
+
+create function wt_is_sentinel(string text)
+    returns bool
+as $$
+select starts_with(string, u&'\fffe') and starts_with(reverse(string), u&'\ffff');
+$$ language sql
+    immutable
+    returns null on null input;
+comment on function wt_is_sentinel is
+    'wt_is_sentinel returns true if string is a sentinel value';
+
+alter domain wt_sentinel
+    add constraint wt_sentinel_not_valid
+        check(
+                wt_is_sentinel(value)
+                or
+                length(trim(trailing u&'\ffff' from trim(leading u&'\fffe ' from value))) > 0
+            );
+
+comment on domain wt_sentinel is
+    'A non-empty string with a Unicode prefix of U+FFFE and suffix of U+FFFF to indicate it is a sentinel value';
+
+drop function wt_to_sentinel; -- wt_to_sentinel is not needed, dropping and not re-creating
+
+commit;
---
+++ internal/db/sentinel/sentinel.go
@@ -0,0 +1,31 @@
+package sentinel
+
+const (
+	// Start is the Unicode special non-character U+FFFE, and the prefix included in all
+	// Boundary defined sentinel values.
+	Start = '\ufffe'
+
+	// End is the Unicode special non-character U+FFFF, and the suffix included in all
+	// Boundary defined sentinel values.
+	End = '\uffff'
+)
+
+const (
+	// ExternalIdNone is a Boundary sentinel indicating that no id was provided by an
+	// external system.
+	ExternalIdNone = "\ufffenone\uffff"
+)
+
+// Is returns true if s is a valid sentinel.
+func Is(s string) bool {
+	// A valid sentinel must be at least 6 bytes in length, 3 bytes for '\ufffe' and 3
+	// bytes for '\uffff'.
+	if len(s) < 6 {
+		return false
+	}
+	sr := []rune(s)
+	if sr[0] == Start && sr[len(sr)-1] == End {
+		return true
+	}
+	return false
+}
