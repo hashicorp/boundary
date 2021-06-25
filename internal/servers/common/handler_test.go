b1f6c28f7602142585d5a78d682752e89e3de4d0
Jim <jlambert@hashicorp.com>
2021-06-25 07:32:59 -0400

    add assertions for event data in common.Test_WrapWithEventsHandler(...)

--- internal/servers/common/handler_test.go
+++ internal/servers/common/handler_test.go
@@ -2,10 +2,13 @@ package common
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/boundary/internal/db"
@@ -174,9 +177,15 @@ func Test_WrapWithEventsHandler(t *testing.T) {
 	t.Parallel()
 	wrapper := db.TestWrapper(t)
 	conn, _ := db.TestSetup(t, "postgres")
-	testEventer, err := event.NewEventer(hclog.Default(), *event.DefaultEventerConfig())
+	c := event.TestEventerConfig(t, "Test_WrapWithEventsHandler", event.TestWithAuditSink(t), event.TestWithObservationSink(t))
+	testEventer, err := event.NewEventer(hclog.Default(), c.EventerConfig)
 	require.NoError(t, err)
 	testKms := kms.TestKms(t, conn, wrapper)
+
+	testHander := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusTeapot)
+		fmt.Fprintln(w, "I'm a little teapot short and stout")
+	})
 	tests := []struct {
 		name            string
 		h               http.Handler
@@ -205,24 +214,27 @@ func Test_WrapWithEventsHandler(t *testing.T) {
 			wantErrContains: "missing eventer",
 		},
 		{
-			name: "missing logger",
-			h: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintln(w, "Hello, client")
-			}),
+			name:            "missing logger",
+			h:               testHander,
 			e:               testEventer,
 			kms:             testKms,
 			wantErrMatch:    errors.T(errors.InvalidParameter),
 			wantErrContains: "missing logger",
 		},
 		{
-			name: "missing kms",
-			h: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintln(w, "Hello, client")
-			}),
+			name:            "missing kms",
+			h:               testHander,
 			e:               testEventer,
 			logger:          hclog.Default(),
 			wantErrMatch:    errors.T(errors.InvalidParameter),
 			wantErrContains: "missing kms",
+		},
+		{
+			name:   "success",
+			h:      testHander,
+			e:      testEventer,
+			logger: hclog.Default(),
+			kms:    testKms,
 		},
 	}
 	for _, tt := range tests {
@@ -240,7 +252,140 @@ func Test_WrapWithEventsHandler(t *testing.T) {
 			}
 			require.NoError(err)
 			assert.NotNil(got)
+
+			req, err := http.NewRequest("GET", "/greeting", nil)
+			require.NoError(err)
+			rr := httptest.NewRecorder()
+			got.ServeHTTP(rr, req)
+			assert.Equal(http.StatusTeapot, rr.Code)
+
+			{ // test that the got observation is what we wanted.
+				require.NotNil(c.ObservationEvents)
+				defer func() { _ = os.WriteFile(c.ObservationEvents.Name(), nil, 0o666) }()
+				b, err := ioutil.ReadFile(c.ObservationEvents.Name())
+				assert.NoError(err)
+
+				got := &eventJson{}
+				err = json.Unmarshal(b, got)
+				require.NoErrorf(err, "json: %s", string(b))
+
+				actualJson, err := json.Marshal(got)
+				require.NoError(err)
+
+				// set the got values to the wanted values that are either
+				// static or calculated in real-time
+				info := event.RequestInfo{
+					Method: "GET",
+					Path:   "/greeting",
+					Id:     got.Payload["id"].(string),
+				}
+				hdr := map[string]interface{}{
+					"status":     http.StatusTeapot,
+					"start":      got.Payload["header"].(map[string]interface{})["start"].(string),
+					"stop":       got.Payload["header"].(map[string]interface{})["stop"].(string),
+					"latency-ms": got.Payload["header"].(map[string]interface{})["latency-ms"].(float64),
+				}
+				wantJson := testJson(t, event.ObservationType, &info, event.Op(tt.name), got, hdr, nil)
+				assert.JSONEq(string(wantJson), string(actualJson))
+			}
+
+			{ // test that the got audit is what we wanted.
+				require.NotNil(c.AuditEvents)
+				defer func() { _ = os.WriteFile(c.AuditEvents.Name(), nil, 0o666) }()
+				b, err := ioutil.ReadFile(c.AuditEvents.Name())
+				assert.NoError(err)
+
+				got := &eventJson{}
+				err = json.Unmarshal(b, got)
+				require.NoErrorf(err, "json: %s", string(b))
+
+				actualJson, err := json.Marshal(got)
+				require.NoError(err)
+
+				// set the got values to the wanted values that are either
+				// static or calculated in real-time
+				info := event.RequestInfo{
+					Method: "GET",
+					Path:   "/greeting",
+					Id:     got.Payload["id"].(string),
+				}
+				hdr := map[string]interface{}{
+					"id":              got.Payload["id"].(string),
+					"timestamp":       got.Payload["timestamp"].(string),
+					"serialized_hmac": got.Payload["serialized_hmac"].(string),
+				}
+				wantJson := testJson(t, event.AuditType, &info, event.Op(tt.name), got, hdr, nil)
+				assert.JSONEq(string(wantJson), string(actualJson))
+
+			}
 		})
 	}
 
+}
+
+type eventJson struct {
+	CreatedAt string                 `json:"created_at"`
+	EventType string                 `json:"event_type"`
+	Payload   map[string]interface{} `json:"payload"`
+}
+
+func testJson(t *testing.T, eventType event.Type, reqInfo *event.RequestInfo, caller event.Op, got *eventJson, hdr, details map[string]interface{}) []byte {
+	t.Helper()
+	const (
+		testAuditVersion       = "v0.1"
+		testErrorVersion       = "v0.1"
+		testObservationVersion = "v0.1"
+	)
+
+	require := require.New(t)
+
+	var payload map[string]interface{}
+	switch eventType {
+	case event.ObservationType:
+		payload = map[string]interface{}{
+			event.IdField: got.Payload[event.IdField].(string),
+			event.HeaderField: map[string]interface{}{
+				event.RequestInfoField: reqInfo,
+				event.VersionField:     testObservationVersion,
+			},
+		}
+		h := payload[event.HeaderField].(map[string]interface{})
+		for k, v := range hdr {
+			h[k] = v
+		}
+	case event.AuditType:
+		payload = map[string]interface{}{
+			event.IdField:          got.Payload[event.IdField].(string),
+			event.RequestInfoField: reqInfo,
+			event.VersionField:     testAuditVersion,
+			event.TypeField:        event.ApiRequest,
+		}
+		for k, v := range hdr {
+			payload[k] = v
+		}
+	}
+	j := eventJson{
+		CreatedAt: got.CreatedAt,
+		EventType: string(eventType),
+		Payload:   payload,
+	}
+
+	if details != nil {
+		details[event.OpField] = string(caller)
+		d := got.Payload[event.DetailsField].([]interface{})[0].(map[string]interface{})
+		j.Payload[event.DetailsField] = []struct {
+			CreatedAt string                 `json:"created_at"`
+			Type      string                 `json:"type"`
+			Payload   map[string]interface{} `json:"payload"`
+		}{
+			{
+				CreatedAt: d[event.CreatedAtField].(string),
+				Type:      d[event.TypeField].(string),
+				Payload:   details,
+			},
+		}
+	}
+	b, err := json.Marshal(j)
+	require.NoError(err)
+	return b
 }
