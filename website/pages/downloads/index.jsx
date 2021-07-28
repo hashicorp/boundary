d3bdada0fd2783c29ec0f3f5cc9d557f8b702daa
Bryce Kalow <bkalow@hashicorp.com>
2021-07-27 20:48:14 -0500

    remove unused prop

--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -10,52 +10,6 @@ export default function DownloadsPage({ binaryReleases, desktopReleases }) {
   return (
     <ProductDownloadsPage
       releases={binaryReleases}
-      packageManagers={[
-        {
-          label: 'Homebrew',
-          commands: [
-            'brew tap hashicorp/tap',
-            'brew install hashicorp/tap/boundary',
-          ],
-          os: 'darwin',
-        },
-        {
-          label: 'Ubuntu/Debian',
-          commands: [
-            'curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -',
-            'sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"',
-            'sudo apt-get update && sudo apt-get install boundary',
-          ],
-          os: 'linux',
-        },
-        {
-          label: 'CentOS/RHEL',
-          commands: [
-            'sudo yum install -y yum-utils',
-            'sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo',
-            'sudo yum -y install boundary',
-          ],
-          os: 'linux',
-        },
-        {
-          label: 'Fedora',
-          commands: [
-            'sudo dnf install -y dnf-plugins-core',
-            'sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo',
-            'sudo dnf -y install boundary',
-          ],
-          os: 'linux',
-        },
-        {
-          label: 'Amazon Linux',
-          commands: [
-            'sudo yum install -y yum-utils',
-            'sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo',
-            'sudo yum -y install boundary',
-          ],
-          os: 'linux',
-        },
-      ]}
       latestVersion={VERSION}
       getStartedLinks={[
         {
