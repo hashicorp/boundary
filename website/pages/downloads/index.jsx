8599b55f7ab9a588aacf5737648aeedd14b8e480
Bryce Kalow <bkalow@hashicorp.com>
2021-08-02 11:34:03 -0500

    web: migrate to web platform- packages (#1405)
    * migrate usages of nextjs-scripts to platform packages
    
    * upgrade hashi packages
    
    * use new downloads page component
    
    * adds missing dep
    
    * remove redundant head component
    
    * remove styles
    
    * remove unused prop

--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -1,7 +1,6 @@
 import { VERSION, DESKTOP_VERSION } from 'data/version.js'
-import HashiHead from '@hashicorp/react-head'
-import { productName, productSlug } from 'data/metadata'
-import ProductDownloader from '@hashicorp/react-product-downloader'
+import { productSlug } from 'data/metadata'
+import ProductDownloadsPage from '@hashicorp/react-product-downloads-page'
 import MerchDesktopClient from 'components/merch-desktop-client'
 import styles from './style.module.css'
 
@@ -9,95 +8,46 @@ const DESKTOP_BINARY_SLUG = 'boundary-desktop'
 
 export default function DownloadsPage({ binaryReleases, desktopReleases }) {
   return (
-    <div className={styles.root}>
-      <HashiHead title={`Downloads | ${productName} by HashiCorp`} />
-      <ProductDownloader
-        releases={binaryReleases}
-        packageManagers={[
-          {
-            label: 'Homebrew',
-            commands: [
-              'brew tap hashicorp/tap',
-              'brew install hashicorp/tap/boundary',
-            ],
-            os: 'darwin',
-          },
-          {
-            label: 'Ubuntu/Debian',
-            commands: [
-              'curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -',
-              'sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"',
-              'sudo apt-get update && sudo apt-get install boundary',
-            ],
-            os: 'linux',
-          },
-          {
-            label: 'CentOS/RHEL',
-            commands: [
-              'sudo yum install -y yum-utils',
-              'sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo',
-              'sudo yum -y install boundary',
-            ],
-            os: 'linux',
-          },
-          {
-            label: 'Fedora',
-            commands: [
-              'sudo dnf install -y dnf-plugins-core',
-              'sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo',
-              'sudo dnf -y install boundary',
-            ],
-            os: 'linux',
-          },
-          {
-            label: 'Amazon Linux',
-            commands: [
-              'sudo yum install -y yum-utils',
-              'sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo',
-              'sudo yum -y install boundary',
-            ],
-            os: 'linux',
-          },
-        ]}
-        latestVersion={VERSION}
-        getStartedLinks={[
-          {
-            label: 'Install Boundary',
-            href:
-              'https://learn.hashicorp.com/tutorials/boundary/getting-started-install',
-          },
-          {
-            label: 'Introduction to Boundary',
-            href:
-              'https://learn.hashicorp.com/tutorials/boundary/getting-started-intro',
-          },
-          {
-            label: 'Start a Development Environment',
-            href:
-              'https://learn.hashicorp.com/tutorials/boundary/getting-started-dev',
-          },
-        ]}
-        logo={
-          <img
-            className={styles.logo}
-            alt="Boundary"
-            src={require('@hashicorp/mktg-logos/product/boundary/primary/color.svg')}
-          />
-        }
-        product="boundary"
-        tutorialLink={{
-          label: 'View Tutorial on HashiCorp Learn',
+    <ProductDownloadsPage
+      releases={binaryReleases}
+      latestVersion={VERSION}
+      getStartedLinks={[
+        {
+          label: 'Install Boundary',
           href:
             'https://learn.hashicorp.com/tutorials/boundary/getting-started-install',
-        }}
-        merchandisingSlot={
-          <MerchDesktopClient
-            version={DESKTOP_VERSION}
-            releases={desktopReleases}
-          />
-        }
-      />
-    </div>
+        },
+        {
+          label: 'Introduction to Boundary',
+          href:
+            'https://learn.hashicorp.com/tutorials/boundary/getting-started-intro',
+        },
+        {
+          label: 'Start a Development Environment',
+          href:
+            'https://learn.hashicorp.com/tutorials/boundary/getting-started-dev',
+        },
+      ]}
+      logo={
+        <img
+          className={styles.logo}
+          alt="Boundary"
+          src={require('@hashicorp/mktg-logos/product/boundary/primary/color.svg')}
+        />
+      }
+      product="boundary"
+      tutorialLink={{
+        label: 'View Tutorial on HashiCorp Learn',
+        href:
+          'https://learn.hashicorp.com/tutorials/boundary/getting-started-install',
+      }}
+      merchandisingSlot={
+        <MerchDesktopClient
+          version={DESKTOP_VERSION}
+          releases={desktopReleases}
+        />
+      }
+    />
   )
 }
 
