ebaab38536925ea0bd15fe524fa7d8ee7c37fbec
Jim <jlambert@hashicorp.com>
2021-08-03 08:08:52 -0400

    add unit test for newHclogFormatterFilter(...)

--- internal/observability/event/hclog_formatter_node_test.go
+++ internal/observability/event/hclog_formatter_node_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 func TestHclogFormatter_Process(t *testing.T) {
+	t.Parallel()
 	ctx := context.Background()
 	tests := []struct {
 		name            string
@@ -194,6 +195,7 @@ func TestHclogFormatter_Process(t *testing.T) {
 }
 
 func Test_hclogFormatterFilter_Name(t *testing.T) {
+	t.Parallel()
 	t.Run("simple", func(t *testing.T) {
 		ff := &hclogFormatterFilter{}
 		assert.Equal(t, hclogNodeName, ff.Name())
@@ -201,6 +203,7 @@ func Test_hclogFormatterFilter_Name(t *testing.T) {
 }
 
 func Test_hclogFormatterFilter_Reopen(t *testing.T) {
+	t.Parallel()
 	t.Run("simple", func(t *testing.T) {
 		ff := &hclogFormatterFilter{}
 		assert.Equal(t, nil, ff.Reopen())
@@ -208,8 +211,104 @@ func Test_hclogFormatterFilter_Reopen(t *testing.T) {
 }
 
 func Test_hclogFormatterFilter_Type(t *testing.T) {
+	t.Parallel()
 	t.Run("simple", func(t *testing.T) {
 		ff := &hclogFormatterFilter{}
 		assert.Equal(t, eventlogger.NodeTypeFormatterFilter, ff.Type())
 	})
+}
+
+func Test_newHclogFormatterFilter(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name            string
+		jsonFormat      bool
+		opt             []Option
+		wantErr         bool
+		wantIsError     error
+		wantErrContains string
+		wantAllow       []string
+		wantDeny        []string
+	}{
+		{
+			name: "no-opts",
+		},
+		{
+			name:       "bad-allow-filter",
+			jsonFormat: true,
+			opt: []Option{
+				WithAllow("foo=;22", "foo==bar"),
+			},
+			wantErr:         true,
+			wantErrContains: "invalid allow filter 'foo=;22'",
+		},
+		{
+			name:       "bad-deny-filter",
+			jsonFormat: true,
+			opt: []Option{
+				WithDeny("foo=;22", "foo==bar"),
+			},
+			wantErr:         true,
+			wantErrContains: "invalid deny filter 'foo=;22'",
+		},
+		{
+			name:       "empty-allow-filter",
+			jsonFormat: true,
+			opt: []Option{
+				WithAllow(""),
+			},
+			wantErr:         true,
+			wantErrContains: "missing filter",
+		},
+		{
+			name:       "empty-deny-filter",
+			jsonFormat: true,
+			opt: []Option{
+				WithDeny(""),
+			},
+			wantErr:         true,
+			wantErrContains: "missing filter",
+		},
+		{
+			name:       "valid-filters",
+			jsonFormat: true,
+			opt: []Option{
+				WithAllow("alice==friend", "bob==friend"),
+				WithDeny("eve==acquaintance", "fido!=dog"),
+			},
+			wantAllow: []string{"alice==friend", "bob==friend"},
+			wantDeny:  []string{"eve==acquaintance", "fido!=dog"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			got, err := newHclogFormatterFilter(tt.jsonFormat, tt.opt...)
+			if tt.wantErr {
+				require.Error(err)
+				assert.Nil(got)
+				if tt.wantIsError != nil {
+					assert.ErrorIs(err, tt.wantIsError)
+				}
+				if tt.wantErrContains != "" {
+					assert.Contains(err.Error(), tt.wantErrContains)
+				}
+				return
+			}
+			require.NoError(err)
+			assert.NotNil(got)
+
+			assert.Equal(tt.jsonFormat, got.jsonFormat)
+
+			assert.Len(got.allow, len(tt.wantAllow))
+			for _, f := range got.allow {
+				assert.Contains(tt.wantAllow, f.raw)
+			}
+			assert.Len(got.deny, len(tt.wantDeny))
+			for _, f := range got.deny {
+				assert.Contains(tt.wantDeny, f.raw)
+			}
+		})
+	}
 }
