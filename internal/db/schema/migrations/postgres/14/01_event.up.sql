23a6e48ca9224f1078d670b65186aaeadbffa8ab
Sarah Christoff <schristoff@hashicorp.com>
2021-08-10 12:00:37 -0600

    first pass

--- internal/db/schema/migrations/postgres/14/01_event.up.sql
+++ internal/db/schema/migrations/postgres/14/01_event.up.sql
@@ -71,20 +71,6 @@ comment on table event_type_enm is
 'event_type_enm is an enumeration table for the valid event types within '
 'the domain';
 
-create table event_sink_type_enm (
-    name text primary key
-        constraint only_predefined_event_sink_types_allowed
-        check (
-            name in (
-                'stderr',
-                'file'
-            )
-        )
-);
-comment on table event_sink_type_enm is
-'event_sink_type_enm is an enumeration table for the valid sink types within '
-'the domain';
-
 create table event_format_type_enm (
     name text primary key
         constraint only_predefined_event_format_types_allowed
@@ -120,6 +106,15 @@ comment on table event_config is
 'event_config is a table where each row defines the event configuration for '
 'a scope.  Currently, the only supported scope is global';
 
+  create trigger update_version_column after update on event_config
+    for each row execute procedure update_version_column();
+
+  create trigger update_time_column before update on event_config
+    for each row execute procedure update_time_column();
+
+  create trigger default_create_time_column before insert on event_config
+    for each row execute procedure default_create_time();
+
 create table event_type_enabled (
     config_id wt_public_id
         constraint event_config_fkey
@@ -144,21 +139,6 @@ create table event_sink(
         constraint event_config_fkey
         references event_config(public_id)
         on delete cascade
-        on update cascade,
-    event_type text not null
-        constraint event_type_enm_fkey
-        references event_type_enm(name)
-        on delete restrict
-        on update cascade,
-    sink_type text not null
-        constraint event_sink_type_enm_fkey
-        references event_sink_type_enm(name)
-        on delete restrict
-        on update cascade,
-    format_type text not null
-        constraint event_format_type_enm_fkey
-        references event_format_type_enm(name)
-        on delete restrict
         on update cascade
 );
 comment on table event_sink is 
@@ -167,10 +147,6 @@ comment on table event_sink is
 create table event_file_sink(
     public_id wt_public_id primary key,
     sink_id wt_public_id not null
-        constraint event_sink_type_fkey
-            references event_sink(public_id)
-            on delete cascade
-            on update cascade,
     path text not null 
         constraint path_not_empty
         check (
@@ -203,11 +179,33 @@ create table event_file_sink(
             rotate_max_files > 0
         ),
     constraint path_filename_uq
-        unique(path, filename) -- ensure each sink is writing to a unique file
+        unique(path, filename), -- ensure each sink is writing to a unique file
+    allow_filter wt_bexprfilter,
+    deny_filter wt_bexprfilter
 );
 comment on table event_file_sink is 
 'event_file_sink is a table where each entry represents a configured event file '
 'sink';
 
+create table event_stderr_sink(
+    public_id wt_public_id primary key,
+    sink_id wt_public_id not null
+    event_type text not null
+        constraint event_type_enm_fkey
+            references event_type_enm (name)
+            on delete restrict
+            on update cascade,
+    format_type text not null
+    constraint event_format_type_enm_fkey
+        references event_format_type_enm(name)
+        on delete restrict
+        on update cascade,
+    allow_filter wt_bexprfilter,
+    deny_filter wt_bexprfilter
+);
+
+comment on table event_stderr_sink is
+'event_stderr_sink is a table where each entry represents a configured stderr '
+'sink';
 
 commit;
