2ca7f1544b1221b566f75ca321a5e0fbce56b462
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2021-08-11 12:19:14 -0400

    Update aead plugin

--- sdk/kms/plugins/aead/go.mod
+++ sdk/kms/plugins/aead/go.mod
@@ -2,4 +2,4 @@ module github.com/hashicorp/boundary/sdk/kms/plugins/aead
 
 go 1.16
 
-require github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2 v2.0.0-20210810171353-2a298deaeb2d
+require github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2 v2.0.0-20210811161017-d54b12be8805
--- sdk/kms/plugins/aead/main.go
+++ sdk/kms/plugins/aead/main.go
@@ -1,7 +1,16 @@
 package main
 
-import aead "github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2"
+import (
+	"fmt"
+	"os"
+
+	aead "github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2"
+)
 
 func main() {
-	aead.ServePlugin()
+	if err := aead.ServePlugin(); err != nil {
+		fmt.Println("Error serving plugin", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
