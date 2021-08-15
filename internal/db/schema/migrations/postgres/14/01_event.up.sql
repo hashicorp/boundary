96ec9b1d48d95bbd5eae460815cc8b10c1cef80d
s-christoff <schristoff@hashicorp.com>
2021-08-15 13:44:26 -0600

    Update internal/db/schema/migrations/postgres/14/01_event.up.sql
    Co-authored-by: Michael Gaffney <mgaffney@users.noreply.github.com>

--- internal/db/schema/migrations/postgres/14/01_event.up.sql
+++ internal/db/schema/migrations/postgres/14/01_event.up.sql
@@ -295,7 +295,11 @@ after delete on event_file_sink
 
 create table event_stderr_sink(
     public_id wt_public_id primary key,
-    config_id wt_public_id not null,
+    config_id wt_public_id not null
+        constraint event_config_fkey
+            references event_config(public_id)
+            on delete cascade
+            on update cascade,
     event_type text not null
         constraint event_type_enm_fkey
             references event_type_enm(name)
