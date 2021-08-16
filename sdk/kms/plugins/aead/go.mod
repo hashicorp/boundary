8f7bb5d983136f291f7e8292770e65180f7dcb10
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2021-08-16 17:43:53 -0400

    Update plugin dep

--- sdk/kms/plugins/aead/go.mod
+++ sdk/kms/plugins/aead/go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/hashicorp/go-hclog v0.16.2
-	github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2 v2.0.0-20210812153428-cc6f3f5d1dd7
+	github.com/hashicorp/go-kms-wrapping/plugin/v2 v2.0.0-20210816213628-b5d47ca9ce92
+	github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2 v2.0.0-20210816213628-b5d47ca9ce92
 )
--- sdk/kms/plugins/aead/main.go
+++ sdk/kms/plugins/aead/main.go
@@ -5,11 +5,12 @@ import (
 	"os"
 
 	"github.com/hashicorp/go-hclog"
-	aead "github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2"
+	gkwp "github.com/hashicorp/go-kms-wrapping/plugin/v2"
+	"github.com/hashicorp/go-kms-wrapping/wrappers/aead/v2"
 )
 
 func main() {
-	if err := aead.ServePlugin(aead.WithLogger(hclog.NewNullLogger())); err != nil {
+	if err := gkwp.ServePlugin(aead.NewWrapper(), gkwp.WithLogger(hclog.NewNullLogger())); err != nil {
 		fmt.Println("Error serving plugin", err)
 		os.Exit(1)
 	}
