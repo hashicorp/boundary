9d329ea483fbf85f2a19d3b8f01552e4c49a3e07
Todd Knight <T.Alan.Knight@gmail.com>
2021-09-08 22:39:29 -0700

    Change some context.Background to use existing ctx vars.

--- internal/host/plugin/host_catalog_test.go
+++ internal/host/plugin/host_catalog_test.go
@@ -17,6 +17,7 @@ import (
 )
 
 func TestHostCatalog_Create(t *testing.T) {
+	ctx := context.Background()
 	conn, _ := db.TestSetup(t, "postgres")
 	wrapper := db.TestWrapper(t)
 	_, prj := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
@@ -140,7 +141,7 @@ func TestHostCatalog_Create(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewHostCatalog(context.Background(), tt.args.pluginId, tt.args.scopeId, tt.args.opts...)
+			got, err := NewHostCatalog(ctx, tt.args.pluginId, tt.args.scopeId, tt.args.opts...)
 			require.NoError(t, err)
 			require.NotNil(t, got)
 
@@ -154,7 +155,7 @@ func TestHostCatalog_Create(t *testing.T) {
 			got.PublicId = id
 
 			w := db.New(conn)
-			err = w.Create(context.Background(), got)
+			err = w.Create(ctx, got)
 			if tt.wantCreateErr {
 				assert.Error(t, err)
 			} else {
@@ -164,7 +165,7 @@ func TestHostCatalog_Create(t *testing.T) {
 						PublicId: id,
 					},
 				}
-				require.NoError(t, w.LookupById(context.Background(), found))
+				require.NoError(t, w.LookupById(ctx, found))
 				assert.Empty(t, cmp.Diff(got.HostCatalog, found.HostCatalog, protocmp.Transform()), "%q compared to %q", got.Attributes, found.Attributes)
 			}
 		})
@@ -181,7 +182,7 @@ func TestHostCatalog_Create_DuplicateNames(t *testing.T) {
 	plg := host.TestPlugin(t, conn, "test1", "prefix1")
 	plg2 := host.TestPlugin(t, conn, "test2", "prefix2")
 
-	got, err := NewHostCatalog(context.Background(), plg.GetPublicId(), prj.GetPublicId(), WithName("duplicate"))
+	got, err := NewHostCatalog(ctx, plg.GetPublicId(), prj.GetPublicId(), WithName("duplicate"))
 	require.NoError(t, err)
 	got.PublicId, err = newHostCatalogId(plg.GetIdPrefix())
 	require.NoError(t, err)
@@ -193,14 +194,14 @@ func TestHostCatalog_Create_DuplicateNames(t *testing.T) {
 	assert.Error(t, w.Create(ctx, got))
 
 	// Can't create another resource with same pluginName in same scope even for different plugin
-	got, err = NewHostCatalog(context.Background(), plg2.GetPublicId(), prj.GetPublicId(), WithName("duplicate"))
+	got, err = NewHostCatalog(ctx, plg2.GetPublicId(), prj.GetPublicId(), WithName("duplicate"))
 	require.NoError(t, err)
 	got.PublicId, err = newHostCatalogId(plg2.GetIdPrefix())
 	require.NoError(t, err)
 	assert.Error(t, w.Create(ctx, got))
 
 	// Can create another resource with same pluginName in different scope even for same plugin
-	got, err = NewHostCatalog(context.Background(), plg.GetPublicId(), prj2.GetPublicId(), WithName("duplicate"))
+	got, err = NewHostCatalog(ctx, plg.GetPublicId(), prj2.GetPublicId(), WithName("duplicate"))
 	require.NoError(t, err)
 	got.PublicId, err = newHostCatalogId(plg.GetIdPrefix())
 	require.NoError(t, err)
