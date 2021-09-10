1d4b0ac5a0a4e56fb20b7c6c7797fb0b635b44cc
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-10 13:39:42 +0000

    backport of commit 1d2378b63bb8b93c171547ac0babf836fb7fd13e

--- website/pages/api-docs/[[...page]].jsx
+++ website/pages/api-docs/[[...page]].jsx
@@ -1,10 +1,12 @@
 import { productName, productSlug } from 'data/metadata'
-import path from 'path'
-import parseSwagger from '../../lib/swagger-parser'
 import OpenApiPage, {
   getPathsFromSchema,
   getPropsForPage,
-} from '../../components/openapi-page'
+} from '@hashicorp/react-open-api-page'
+/* Used server-side only */
+import fs from 'fs'
+import path from 'path'
+import parseSwagger from '@hashicorp/react-open-api-page/parse-swagger'
 
 const targetFile = '../internal/gen/controller.swagger.json'
 const pathFromRoot = 'api-docs'
@@ -21,13 +23,15 @@ export default function OpenApiDocsPage(props) {
 }
 
 export async function getStaticPaths() {
-  const schema = await parseSwagger(path.join(process.cwd(), targetFile))
+  const swaggerFile = path.join(process.cwd(), targetFile)
+  const schema = await parseSwagger(fs.readFileSync(swaggerFile))
   const paths = getPathsFromSchema(schema)
   return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }) {
-  const schema = await parseSwagger(path.join(process.cwd(), targetFile))
+  const swaggerFile = path.join(process.cwd(), targetFile)
+  const schema = await parseSwagger(fs.readFileSync(swaggerFile))
   const props = getPropsForPage(schema, params)
   return { props }
 }
