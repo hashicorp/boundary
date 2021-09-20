8edef8050c7352fcf507a10114b47e113738ddda
Bryce Kalow <bkalow@hashicorp.com>
2021-09-20 16:16:03 -0500

    Upgrades deps and migrate to css modules

--- website/components/homepage-hero/index.jsx
+++ website/components/homepage-hero/index.jsx
@@ -17,6 +17,7 @@ export default function HomepageHero({
   return (
     <div className={styles.homepageHero}>
       <Hero
+        className={styles.hero}
         data={{
           product: 'boundary',
           title: title,
