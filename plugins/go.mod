146486530286b4b904b88445a6b1ac0a3359ad17
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2021-10-04 17:12:05 -0400

    Add shared host plugin service code

---
+++ plugins/go.mod
@@ -0,0 +1,10 @@
+module github.com/hashicorp/boundary/plugins
+
+go 1.16
+
+require (
+	github.com/hashicorp/boundary/sdk v0.0.11-0.20210930043937-08ce7ac8890d
+	github.com/hashicorp/go-hclog v0.16.2
+	github.com/hashicorp/go-plugin v1.4.3
+	google.golang.org/grpc v1.40.0
+)
---
+++ plugins/host/options.go
@@ -0,0 +1,52 @@
+package external_host_plugins
+
+import (
+	"errors"
+
+	"github.com/hashicorp/go-hclog"
+)
+
+// getOpts iterates the inbound Options and returns a struct
+func getOpts(opt ...Option) (*options, error) {
+	opts := getDefaultOptions()
+	for _, o := range opt {
+		if o == nil {
+			continue
+		}
+		iface := o()
+		switch to := iface.(type) {
+		case OptionFunc:
+			to(opts)
+		default:
+			return nil, errors.New("option passed in that does not belong to this package")
+		}
+	}
+	return opts, nil
+}
+
+type options struct {
+	withLogger hclog.Logger
+}
+
+// Option - a type that wraps an interface for compile-time safety but can
+// contain an option for this package or for wrappers implementing this
+// interface.
+type Option func() interface{}
+
+// OptionFunc - a type for funcs that operate on the shared Options struct. The
+// options below explicitly wrap this so that we can switch on it when parsing
+// opts for various wrappers.
+type OptionFunc func(*options)
+
+func getDefaultOptions() *options {
+	return &options{}
+}
+
+// WithLogger allows passing a logger to the plugin library for debugging
+func WithLogger(logger hclog.Logger) Option {
+	return func() interface{} {
+		return OptionFunc(func(o *options) {
+			o.withLogger = logger
+		})
+	}
+}
