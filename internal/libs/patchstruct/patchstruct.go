565d9bfbe5116f4911c247b3df394de2e1e9ce64
Chris Marchesi <chrism@vancluevertech.com>
2021-10-13 10:39:21 -0700

    Add doc comment for patchstruct.Patch

--- internal/libs/patchstruct/patchstruct.go
+++ internal/libs/patchstruct/patchstruct.go
@@ -2,6 +2,30 @@ package patchstruct
 
 import "google.golang.org/protobuf/types/known/structpb"
 
+// Patch updates the struct found in dst with the values found in src. The
+// intent of this helper is to provide a fallback mechanism for subtype
+// attributes when the actual schema of the subtype attributes are unknown. As
+// such, it's preferred to use other methods (such as mask mapping) when an
+// actual message for the subtype is known.
+//
+// The following rules apply:
+//
+// * The source (src) map is merged into the destination map (dst).
+//
+// * Values are overwritten by the source map if they exist in both.a
+//
+// * Values are deleted from the destination if they are set to null in the
+//   source.
+//
+// * Maps are recursively applied, meaning that a nested map at key "foo" in
+//   the destination would be patched with a map at key "foo" in the
+//   source.
+//
+// * A map in the destination is overwritten by a non-map in the source,
+//   and a non-map in the destination is overwritten by a map in the
+//   source.
+//
+// Patch returns the updated map as a copy, dst and src are not altered.
 func Patch(dst, src *structpb.Struct) *structpb.Struct {
 	result, err := structpb.NewStruct(patchM(dst.AsMap(), src.AsMap()))
 	if err != nil {
