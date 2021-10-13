cee614298bf107e3483dada427fd5a6eb5aa2e50
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2021-10-13 11:03:47 -0400

    Use analytics package instead of direct impl

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,13 +1,12 @@
 import './style.css'
 import '@hashicorp/platform-util/nprogress/style.css'
 
-import { useEffect } from 'react'
-import * as Fathom from 'fathom-client'
+import useFathomAnalytics from '@hashicorp/platform-analytics'
 import NProgress from '@hashicorp/platform-util/nprogress'
 import createConsentManager from '@hashicorp/react-consent-manager/loader'
 import useAnchorLinkAnalytics from '@hashicorp/platform-util/anchor-link-analytics'
 import HashiStackMenu from '@hashicorp/react-hashi-stack-menu'
-import Router, { useRouter } from 'next/router'
+import Router from 'next/router'
 import HashiHead from '@hashicorp/react-head'
 import { ErrorBoundary } from '@hashicorp/platform-runtime-error-monitoring'
 import ProductSubnav from '../components/subnav'
@@ -26,27 +25,7 @@ const description =
   'Boundary is an open source solution that automates a secure identity-based user access to hosts and services across environments.'
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    // Load Fathom analytics
-    Fathom.load('AQKEGSAG', {
-      includedDomains: ['boundaryproject.io', 'www.boundaryproject.io'],
-    })
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview()
-    }
-
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
-
+  useFathomAnalytics()
   useAnchorLinkAnalytics()
 
   return (
