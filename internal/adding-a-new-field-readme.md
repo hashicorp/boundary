a679300b506fb843ffcbf70917e4cff1b350b54b
Jim <jlambert@hashicorp.com>
2021-10-27 09:37:14 -0400

    feature (events): Classify auth method request/resp messages for audit events. (#1640)
    * refactor (oidc): Stop emitting error for not found token id
    
    Stop emitting errors when authtoken repo.IssueAuthToken is called and
    there's if no pending token.  It's not an "normal" state and not
    an error condition.
    
    * feature (audit tagging): Add tags for auth method requests/responses
    
    Including testing functions for asserting audit events created
    when a service is called

--- internal/adding-a-new-field-readme.md
+++ internal/adding-a-new-field-readme.md
@@ -7,13 +7,16 @@ Once you've figured out that you need an additional field in Boundary's domain m
 
 * Make schema changes:
   * Define the new column and provide for the migration of existing data
-    * Create a new migration under `internal/db/schema/migrations/postgres`
-  * Run `make migrations` after modifying DDL in SQL files.
+  
+    * Create a new migration under `internal/db/schema/migrations/oss/postgres`
 
 * Add the new field to the storage protobuf 
   * storage protobufs are under: `internal/proto/local/controller/storage`
-  * Define a gorm tag for the new field via `@inject_tag`
+
+  * Define a gorm tag for the new field via `@gotags` (`@inject_tag` has been deprecated)
+
   * Define a `custom_options.v1.mask_mapping` for the field which maps the storage `this` field to the API `that` field (yes, it's the opposite of how it's defined for the API protobuf)
+
   * Run `make proto` after modifying storage protobuf
 
 * Extend the existing repository function for Updating the resource to incorporate the new field. This could/may entail defining new options for the Update function.
@@ -28,6 +31,12 @@ Now that the repository supports the new field, you can move on to adding this n
 * Define a `custom_options.v1.mask_mapping` tag for the field which maps the API `this` field to the storage `that` field (yes, it's the opposite of how it's defined for a storage protobuf)
 
 * Define a `custom_options.v1.generate_sdk_option` tag for the SDK
+
+* Define a data classification/filter tag for the field via `@gotags` or the
+  `encrypt.Taggable` interface which specifies how sensitive/secret/public data
+  will be handled for the API's audit events.  Please write unit tests to verify
+  the audit event is properly "redacted" (see the unit tests of
+  `TestAuthenticate_Tags` and `TestAuthenticate` for examples).
 
 * Run `make proto` and `make api` after modifying the API/SDK protobufs 
 
--- internal/tests/api/authmethods/authenticate_test.go
+++ internal/tests/api/authmethods/authenticate_test.go
@@ -4,19 +4,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"sync"
 	"testing"
 
 	"github.com/hashicorp/boundary/api"
 	"github.com/hashicorp/boundary/api/authmethods"
 	"github.com/hashicorp/boundary/api/authtokens"
+	"github.com/hashicorp/boundary/internal/cmd/config"
+	"github.com/hashicorp/boundary/internal/observability/event"
 	"github.com/hashicorp/boundary/internal/servers/controller"
+	tests_api "github.com/hashicorp/boundary/internal/tests/api"
+	"github.com/hashicorp/go-hclog"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+// TestAuthenticate tests the api calls and the audit events it should produce
 func TestAuthenticate(t *testing.T) {
+	// this cannot run in parallel because it relies on envvar
+	// globals.BOUNDARY_DEVELOPER_ENABLE_EVENTS
+	event.TestEnableEventing(t, true)
+
 	assert, require := assert.New(t), require.New(t)
-	tc := controller.NewTestController(t, nil)
+	eventConfig := event.TestEventerConfig(t, "TestAuthenticateAuditEntry", event.TestWithAuditSink(t))
+	testLock := &sync.Mutex{}
+	testLogger := hclog.New(&hclog.LoggerOptions{
+		Mutex: testLock,
+		Name:  "test",
+	})
+	require.NoError(event.InitSysEventer(testLogger, testLock, "TestAuthenticateAuditEntry", event.WithEventerConfig(&eventConfig.EventerConfig)))
+	tcConfig, err := config.DevController()
+	require.NoError(err)
+	tcConfig.Eventing = &eventConfig.EventerConfig
+
+	tc := controller.NewTestController(t, &controller.TestControllerOpts{Config: tcConfig})
 	defer tc.Shutdown()
 
 	client := tc.Client()
@@ -49,4 +71,30 @@ func TestAuthenticate(t *testing.T) {
 	token := new(authtokens.AuthToken)
 	require.NoError(json.Unmarshal(result.GetRawAttributes(), token))
 	require.NotEmpty(token.Token)
+
+	require.NotNil(eventConfig.AuditEvents)
+	_ = os.WriteFile(eventConfig.AuditEvents.Name(), nil, 0o666) // clean out audit events from previous calls
+
+	tok, err = methods.Authenticate(tc.Context(), tc.Server().DevPasswordAuthMethodId, "login", map[string]interface{}{"login_name": "user", "password": "passpass"})
+	require.NoError(err)
+	assert.NotNil(tok)
+	got := tests_api.CloudEventFromFile(t, eventConfig.AuditEvents.Name())
+
+	reqDetails := tests_api.GetEventDetails(t, got, "request")
+	tests_api.AssertRedactedValues(t, reqDetails)
+	tests_api.AssertRedactedValues(t, reqDetails["attributes"], "password")
+
+	respDetails := tests_api.GetEventDetails(t, got, "response")
+	tests_api.AssertRedactedValues(t, respDetails)
+	tests_api.AssertRedactedValues(t, respDetails["attributes"], "token")
+
+	_ = os.WriteFile(eventConfig.AuditEvents.Name(), nil, 0o666) // clean out audit events from previous calls
+	tok, err = methods.Authenticate(tc.Context(), tc.Server().DevPasswordAuthMethodId, "login", map[string]interface{}{"login_name": "user", "password": "bad-pass"})
+	require.Error(err)
+	assert.Nil(tok)
+	got = tests_api.CloudEventFromFile(t, eventConfig.AuditEvents.Name())
+
+	reqDetails = tests_api.GetEventDetails(t, got, "request")
+	tests_api.AssertRedactedValues(t, reqDetails)
+	tests_api.AssertRedactedValues(t, reqDetails["attributes"], "password")
 }
