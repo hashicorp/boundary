e348720b62fced90388009d82a0f13a6c02b9940
Chris Marchesi <chrism@vancluevertech.com>
2021-11-04 15:33:46 -0700

    WIP

--- internal/host/plugin/host_catalog_secret_test.go
+++ internal/host/plugin/host_catalog_secret_test.go
@@ -18,7 +18,7 @@ import (
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-func TestHostCatalogSecret_New(t *testing.T) {
+func TestHostCatalogSecret_New_Create(t *testing.T) {
 	conn, _ := db.TestSetup(t, "postgres")
 	wrapper := db.TestWrapper(t)
 	kkms := kms.TestKms(t, conn, wrapper)
@@ -95,7 +95,7 @@ func TestHostCatalogSecret_New(t *testing.T) {
 			require.NoError(t, err)
 			require.NotNil(t, databaseWrapper)
 
-			got, err := newHostCatalogSecret(context.Background(), tt.args.catalogId, tt.args.attrs)
+			got, err := newHostCatalogSecret(ctx, tt.args.catalogId, tt.args.attrs)
 			if tt.wantErr {
 				assert.Error(t, err)
 				require.Nil(t, got)
@@ -113,9 +113,107 @@ func TestHostCatalogSecret_New(t *testing.T) {
 				return
 			}
 			require.NoError(t, err)
+
+			w := db.New(conn)
+			err = w.Create(ctx, got)
+			assert.NoError(t, err)
+			found := &HostCatalogSecret{
+				HostCatalogSecret: &store.HostCatalogSecret{
+					CatalogId: tt.args.catalogId,
+				},
+			}
+			require.NoError(t, w.LookupById(ctx, found))
+			assert.Empty(t, cmp.Diff(got.HostCatalogSecret, found.HostCatalogSecret, protocmp.Transform()), "%q compared to %q", got.HostCatalogSecret, found.HostCatalogSecret)
+
+			// Do a decrypt test
 			require.NoError(t, got.decrypt(ctx, databaseWrapper))
+			require.NoError(t, found.decrypt(ctx, databaseWrapper))
+			assert.Empty(t, cmp.Diff(got.Secret, found.Secret, protocmp.Transform()), "%q compared to %q", got.Secret, found.Secret)
 		})
 	}
+}
+
+func TestHostCatalogSecret_Create_Upsert_Update_Delete(t *testing.T) {
+	conn, _ := db.TestSetup(t, "postgres")
+	wrapper := db.TestWrapper(t)
+	kkms := kms.TestKms(t, conn, wrapper)
+
+	_, prj := iam.TestScopes(t, iam.TestRepo(t, conn, wrapper))
+	plg := host.TestPlugin(t, conn, "test")
+	cat := TestCatalog(t, conn, prj.GetPublicId(), plg.GetPublicId())
+	ctx := context.Background()
+
+	secret, err := newHostCatalogSecret(ctx, cat.GetPublicId(), mustStruct(map[string]interface{}{
+		"foo": "bar",
+	}))
+	require.NoError(t, err)
+	require.NotNil(t, secret)
+	require.Empty(t, secret.CtSecret)
+
+	databaseWrapper, err := kkms.GetWrapper(ctx, prj.PublicId, kms.KeyPurposeDatabase)
+	require.NoError(t, err)
+	require.NotNil(t, databaseWrapper)
+	require.NoError(t, secret.encrypt(ctx, databaseWrapper))
+
+	// Create
+	w := db.New(conn)
+	require.NoError(t, w.Create(ctx, secret))
+
+	// Upsert
+	newStructUpsert := mustMarshal(map[string]interface{}{
+		"baz": "qux",
+	})
+	newSecretUpsert := secret.clone()
+	newSecretUpsert.Secret = newStructUpsert
+	require.NoError(t, newSecretUpsert.encrypt(ctx, databaseWrapper))
+	require.NoError(t, w.Create(ctx, newSecretUpsert, db.WithOnConflict(&db.OnConflict{
+		Target: db.Columns{"catalog_id"},
+		Action: db.SetColumnValues(map[string]interface{}{
+			"secret": newSecretUpsert.CtSecret,
+			"key_id": newSecretUpsert.KeyId,
+		}),
+	})))
+	found := &HostCatalogSecret{
+		HostCatalogSecret: &store.HostCatalogSecret{
+			CatalogId: cat.GetPublicId(),
+		},
+	}
+	require.NoError(t, w.LookupById(ctx, found))
+	assert.Empty(t, cmp.Diff(newSecretUpsert.HostCatalogSecret, found.HostCatalogSecret, protocmp.Transform()))
+	require.NoError(t, found.decrypt(ctx, databaseWrapper))
+	assert.Empty(t, cmp.Diff(newStructUpsert, found.Secret, protocmp.Transform()))
+
+	// Update
+	newStructUpdate := mustMarshal(map[string]interface{}{
+		"one": "two",
+	})
+	newSecretUpdate := newSecretUpsert.clone()
+	newSecretUpdate.Secret = newStructUpdate
+	require.NoError(t, newSecretUpdate.encrypt(ctx, databaseWrapper))
+	rowsUpdated, err := w.Update(ctx, newSecretUpdate, []string{"secret"}, []string{})
+	require.NoError(t, err)
+	require.Equal(t, 1, rowsUpdated)
+	found = &HostCatalogSecret{
+		HostCatalogSecret: &store.HostCatalogSecret{
+			CatalogId: cat.GetPublicId(),
+		},
+	}
+	require.NoError(t, w.LookupById(ctx, found))
+	assert.Empty(t, cmp.Diff(newSecretUpdate.HostCatalogSecret, found.HostCatalogSecret, protocmp.Transform()))
+	require.NoError(t, found.decrypt(ctx, databaseWrapper))
+	assert.Empty(t, cmp.Diff(newStructUpdate, found.Secret, protocmp.Transform()))
+
+	// Delete
+	rowsDeleted, err := w.Delete(ctx, found)
+	require.NoError(t, err)
+	require.Equal(t, 1, rowsDeleted)
+	err = w.LookupById(ctx, &HostCatalogSecret{
+		HostCatalogSecret: &store.HostCatalogSecret{
+			CatalogId: cat.GetPublicId(),
+		},
+	})
+	require.Error(t, err)
+	require.True(t, errors.IsNotFoundError(err))
 }
 
 func TestHostCatalogSecret_Custom_Queries(t *testing.T) {
