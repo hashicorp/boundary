aa175370c61158faa656c6f920d921d247178ee8
Chris Marchesi <chrism@vancluevertech.com>
2021-12-07 11:02:36 -0800

    still wip

--- internal/db/schema/migrations/oss/postgres/22/01_host_catalog_secret_ttl.up.sql
+++ internal/db/schema/migrations/oss/postgres/22/01_host_catalog_secret_ttl.up.sql
@@ -1,6 +1,7 @@
 begin;
   alter table host_plugin_catalog_secret (
-    add column ttl wt_timestamp;
+    add column ttl_seconds int
+      constraint ttl_seconds_not_less_than_zero
+        check(sync_interval_seconds >= 0)
   );
 commit;
-
