5faf62eb87c0440c4c4beb42d58b57e9e2fa5dcb
Jim <jlambert@hashicorp.com>
2021-12-09 16:39:28 -0500

    fixup! feature (workers): Add support for user client ip in tcp protocol

--- internal/db/schema/migrations/oss/postgres/21/01_session.up.sql
+++ internal/db/schema/migrations/oss/postgres/21/01_session.up.sql
@@ -1,6 +1,9 @@
 begin;
 
 alter table session_connection
-  add column user_client_ip inet not null default '::';
+  -- intentionally null because we don't know the user_client_ip when the
+  -- session is created by the controller.  The worker will update the session
+  -- with the user_client_ip after the user establishes a connection.
+  add column user_client_ip inet; 
 
 commit;
