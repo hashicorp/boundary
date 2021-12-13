d3043d5e6e70ad0c4ebba27335574eee529300fa
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-12-13 09:47:51 -0500

    feat: WIP start website split

---
+++ website/scripts/website-preview-start.sh
@@ -0,0 +1,16 @@
+# Set the subdirectory name for the dev-portal app
+PREVIEW_DIR=website-preview
+# Clone the dev-portal project, if needed
+if [ ! -d "$PREVIEW_DIR" ]; then
+    echo "⏳ Cloning the dev-portal repo, this might take a while..."
+    git clone https://github.com/hashicorp/dev-portal.git "$PREVIEW_DIR"
+fi
+# cd into the dev-portal project
+cd "$PREVIEW_DIR"
+# Ensure we're on the branch intended for preview mode
+# TODO: this should always be main,
+# TODO: but for now, checking out a specific
+# TODO: branch is useful for dev
+git checkout main
+# Run the dev-portal content-repo start script
+REPO=waypoint PREVIEW_DIR="$PREVIEW_DIR" ./scripts/content-repo-preview/start.sh
