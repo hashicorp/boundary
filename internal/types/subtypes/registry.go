fe5071adc9084f65fc13d361219f86a248e8d27d
Timothy Messier <tim.messier@gmail.com>
2022-03-18 15:11:45 -0400

    feat(subtype): Add global registry

--- internal/types/subtypes/registry.go
+++ internal/types/subtypes/registry.go
@@ -16,6 +16,28 @@ const (
 	UnknownSubtype Subtype = "unknown"
 )
 
+// global registry
+var registry = NewRegistry()
+
+// SubtypeFromType returns the Subtype from the provided string or if
+// no Subtype was registered with that string Unknown is returned.
+func SubtypeFromType(t string) Subtype {
+	return registry.SubtypeFromType(t)
+}
+
+// SubtypeFromId returns the Subtype from the provided id if the id's prefix
+// was registered with a Subtype. Otherwise Unknown is returned.
+func SubtypeFromId(id string) Subtype {
+	return registry.SubtypeFromId(id)
+}
+
+// Register registers all the prefixes for a provided Subtype.  Register returns
+// an error if the subtype has already been registered or if any of the
+// prefixes are associated with another subtype.
+func Register(subtype Subtype, prefixes ...string) error {
+	return registry.Register(subtype, prefixes...)
+}
+
 // String returns the string representation of a Subtype
 func (t Subtype) String() string {
 	return string(t)
