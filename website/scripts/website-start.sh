5c9a14b0e6caace74d7e7c6ed271b24aefd02b27
Bryce Kalow <bkalow@hashicorp.com>
2022-03-22 17:39:17 -0500

    tweak scripts to handle updating

--- website/scripts/website-start.sh
+++ website/scripts/website-start.sh
@@ -1,5 +1,7 @@
 # Set the subdirectory name for the dev-portal app
 PREVIEW_DIR=website-preview
+# The product for which we are running local preview mode
+PRODUCT=boundary
 
 should_pull=true
 
@@ -14,9 +16,10 @@ fi
 cd "$PREVIEW_DIR"
 
 # If the directory already existed, pull to ensure the dev-portal clone is fresh
-if [ "$should_pull" = true ]; then
-    git pull origin main
+if [ "$should_pull" = true ] && [ -d ".git" ]; then
+    git fetch origin main
+    git reset --hard origin/main
 fi
 
 # Run the dev-portal content-repo start script
-REPO=boundary PREVIEW_DIR="$PREVIEW_DIR" npm run start:local-preview
+REPO=$PRODUCT PREVIEW_DIR="$PREVIEW_DIR" npm run start:local-preview
