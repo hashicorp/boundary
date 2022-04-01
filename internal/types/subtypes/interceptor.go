42b45f973c06d2230d92aa16971044b319fa6058
Timothy Messier <tim.messier@gmail.com>
2022-04-01 13:38:58 -0400

    feat(subtypes): Add grpc interceptor for transforming attributes
    This interceptor is responsible for maintaining compatability with the
    JSON API with the change to the protobuf messages. The messages are
    changing from a single attribue field to a oneof to explicitly associate
    subtype attribute messages with the resource message. The interceptor
    transforms the request prior to the auditReqeust interceptor, and
    transforms the response after the auditResponse interceptor. This allows
    the audit interceptors to use all the necessary classification
    information when creating the audit events.

---
+++ internal/types/subtypes/interceptor.go
@@ -0,0 +1,297 @@
+package subtypes
+
+import (
+	"context"
+
+	"github.com/hashicorp/boundary/internal/servers/controller/handlers"
+	"github.com/hashicorp/boundary/sdk/pbs/controller/protooptions"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+func messageDomain(m proto.Message) string {
+	r := m.ProtoReflect()
+	fd := r.Descriptor().ParentFile()
+	if fd == nil {
+		return ""
+	}
+
+	opts, ok := fd.Options().(*descriptorpb.FileOptions)
+	if !ok {
+		return ""
+	}
+
+	domain := proto.GetExtension(opts, protooptions.E_Domain).(string)
+	return domain
+}
+
+// transformRequestAttributes will modify the request proto.Message, setting
+// any subtype attribute fields into the corresponding strongly-typed struct
+// for the subtype. It looks for some specific structure in the proto.Message
+// to identify the correct subtype and apply the transformation.
+//
+// The first structure is a message that contains a single "item" that is a
+// message that has a "type" and oneof for attributes:
+//
+//    message CreateFooRequest {
+//      item Foo = 1;
+//    }
+//    message Foo {
+//      string type = 1;
+//      // other fields
+//      oneof attrs {
+//        google.protobuf.Struct attributes = 10 [(custom_options.v1.subtype) = "default"];
+//        // other subtype attributes types
+//      }
+//    }
+//
+// The second structure is similar to the first, but rather then the type field
+// being provided, an id field is set. Note this is a different id from the
+// third structure. In this case it is an id for a related resource and the id
+// is marked with the "subtype_source_id" custom option:
+//
+//    message CreateFooRequest {
+//      item Foo = 1;
+//    }
+//    message Foo {
+//      string bar_id = 1 [(custom_options.v1.subtype_source_id) = true];
+//      // other fields
+//      oneof attrs {
+//        google.protobuf.Struct attributes = 10 [(custom_options.v1.subtype) = "default"];
+//        // other subtype attributes types
+//      }
+//    }
+//
+// The third structure is a message that contains an id string and an "item"
+// that is a message that has a oneof for attributes:
+//
+//    message UpdateFooRequest {
+//      string id = 1;
+//      item Foo  = 2;
+//    }
+//    message Foo {
+//      // other fields
+//      oneof attrs {
+//        google.protobuf.Struct attributes = 10 [(custom_options.v1.subtype) = "default"];
+//        // other subtype attributes types
+//      }
+//    }
+//
+// The forth structure is a message that contains an id string and a oneof for
+// attributes:
+//
+//    message FooActionRequest {
+//      string id = 1;
+//      oneof attrs {
+//        google.protobuf.Struct attributes = 10 [(custom_options.v1.subtype) = "default"];
+//        // other subtype attributes types
+//      }
+//    }
+//
+// Also note that for any of the id based lookups to function, the file that contains
+// the proto.Message definition must set the "domain" custom option.
+func transformRequestAttributes(req proto.Message) (proto.Message, error) {
+	domain := messageDomain(req)
+
+	r := req.ProtoReflect()
+	fields := r.Descriptor().Fields()
+
+	itemField := fields.ByName("item")
+	idField := fields.ByName("id")
+	attributesField := fields.ByName("attributes")
+
+	fieldValue := func(m protoreflect.Message, fd protoreflect.FieldDescriptor) string {
+		if fd == nil {
+			return ""
+		}
+		return m.Get(fd).String()
+	}
+
+	var st Subtype
+	switch {
+	case itemField != nil:
+		itemR := itemField.Message()
+		if itemR == nil {
+			return req, nil
+		}
+		id := fieldValue(r, idField)
+
+		item := r.Get(itemField).Message().Interface()
+		itemFields := itemR.Fields()
+
+		typeField := itemFields.ByName("type")
+		t := fieldValue(item.ProtoReflect(), typeField)
+
+		sourceIdField := sourceIdFieldDescriptor(item.ProtoReflect().Descriptor())
+		sourceId := fieldValue(item.ProtoReflect(), sourceIdField)
+
+		switch {
+		case idField != nil && id != "":
+			st = SubtypeFromId(domain, id)
+		case sourceIdField != nil && sourceId != "":
+			st = SubtypeFromId(domain, sourceId)
+		case typeField != nil && t != "":
+			st = Subtype(t)
+		default: // need either type or id
+			return req, nil
+		}
+		if err := convertAttributesToSubtype(item, st); err != nil {
+			return req, err
+		}
+	case idField != nil && attributesField != nil:
+		id := r.Get(idField).String()
+		st = SubtypeFromId(domain, id)
+		if err := convertAttributesToSubtype(req, st); err != nil {
+			return req, err
+		}
+	}
+	return req, nil
+}
+
+func transformResponseItemAttributes(item proto.Message) error {
+	r := item.ProtoReflect()
+
+	itemFields := r.Descriptor().Fields()
+	typeField := itemFields.ByName("type")
+	if typeField == nil {
+		// not an item with subtypes
+		return nil
+	}
+
+	st := Subtype(item.ProtoReflect().Get(typeField).String())
+	return convertAttributesToDefault(item, st)
+}
+
+// transformResponseAttributes will modify the response proto.Message, setting
+// any subtype attribute fields into the default structpb.Struct field It looks
+// for some specific structure in the proto.Message to identify that it is a
+// message that needs transformation.
+//
+// The first structure is a message that contains a single "item" that is a
+// message that has a oneof for attributes:
+//
+//    message CreateFooResponse {
+//      item Foo = 1;
+//    }
+//    message Foo {
+//      oneof attrs {
+//        google.protobuf.Struct attributes = 10 [(custom_options.v1.subtype) = "default"];
+//        // other subtype attributes types
+//      }
+//    }
+//
+// The second structure is a message that contains a single "items" that is a
+// slice of item messages that have a oneof for attributes:
+//
+//    message ListFooResponse {
+//      items []Foo = 1;
+//    }
+//    message Foo {
+//      oneof attrs {
+//        google.protobuf.Struct attributes = 10 [(custom_options.v1.subtype) = "default"];
+//        // other subtype attributes types
+//      }
+//    }
+func transformResponseAttributes(res proto.Message) (proto.Message, error) {
+	r := res.ProtoReflect()
+	fields := r.Descriptor().Fields()
+
+	itemField := fields.ByName("item")
+	itemsField := fields.ByName("items")
+	switch {
+	case itemField != nil:
+		if itemR := itemField.Message(); itemR == nil {
+			return res, nil
+		}
+
+		item := r.Get(itemField).Message().Interface()
+		if err := transformResponseItemAttributes(item); err != nil {
+			return res, err
+		}
+	case itemsField != nil:
+		if !itemsField.IsList() {
+			return res, nil
+		}
+		items := r.Get(itemsField).List()
+
+		for i := 0; i < items.Len(); i++ {
+			item := items.Get(i).Message().Interface()
+			if err := transformResponseItemAttributes(item); err != nil {
+				return res, err
+			}
+		}
+	}
+	return res, nil
+}
+
+// AttributeTransformerInterceptor is a grpc server interceptor that will
+// transform subtype attributes for requests and responses. This will only
+// modify requests and responses that adhere to a specific structure and is done
+// to support the use of a oneof for attributes to strongly type the attributes
+// while allowing the JSON API to provide attributes via a single key.
+//
+// For example with a protobuf message definition like:
+//
+//     message Account {
+//       string id = 1;
+//       string type = 2;
+//       oneof attrs {
+//         google.protobuf.Struct attributes = 10 [(controller.custom_options.v1.subtype) = "default"];
+//         PasswordAttributes password_attributes = 20 [(controller.custom_options.v1.subtype) = "password"];
+//       }
+//     }
+//
+//     message PasswordAttributes {
+//        string login_name = 1;
+//     }
+//
+//     message AccountCreateRequest {
+//        Account item = 1;
+//     }
+//
+//     message AccountCreateResponse {
+//        Account item = 1;
+//     }
+//
+// And a create request with JSON request body like:
+//     {
+//        "type": "password",
+//        "attributes": {
+//           "login_name": "tim"
+//        }
+//     }
+//
+// Will result in a proto request like:
+//
+//     type:"password" attributes:{fields:{key:"login_name" value:{string_value:"tim"}}}
+//
+// This request will be transformed into:
+//
+//     type:"password" password_attributes:{login_name:"tim"}
+func AttributeTransformerInterceptor(_ context.Context) grpc.UnaryServerInterceptor {
+	const op = "subtypes.AttributeTransformInterceptor"
+	return func(interceptorCtx context.Context, req interface{}, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		var err error
+		if reqMsg, ok := req.(proto.Message); ok {
+			req, err = transformRequestAttributes(reqMsg)
+			if err != nil {
+				return nil, handlers.InvalidArgumentErrorf("Error in provided request.",
+					map[string]string{"attributes": "Attribute fields do not match the expected format."})
+			}
+		}
+
+		res, handlerErr := handler(interceptorCtx, req)
+		newRes := res
+
+		if res, ok := res.(proto.Message); ok {
+			newRes, err = transformResponseAttributes(res)
+			if err != nil {
+				return nil, handlers.ApiErrorWithCodeAndMessage(codes.Internal, "failed building attribute struct: %v", err)
+			}
+		}
+		return newRes, handlerErr
+	}
+}
