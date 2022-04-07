6ad9aba50570c5e1e819c3e956f5f48e2987b5bd
Timothy Messier <tim.messier@gmail.com>
2022-04-07 15:24:31 -0400

    feat(credentialstore): Classify resources for audit events

---
+++ sdk/pbs/controller/api/resources/credentialstores/credential_store_taggable.go
@@ -0,0 +1,20 @@
+package credentialstores
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/eventlogger/filters/encrypt"
+)
+
+// Tags implements the encrypt.Taggable interface which allows
+// CredentialStore map fields to be classified for the encrypt filter.
+func (req *CredentialStore) Tags() ([]encrypt.PointerTag, error) {
+	tags := make([]encrypt.PointerTag, 0, len(req.AuthorizedCollectionActions))
+	for k := range req.AuthorizedCollectionActions {
+		tags = append(tags, encrypt.PointerTag{
+			Pointer:        fmt.Sprintf("/AuthorizedCollectionActions/%s", k),
+			Classification: encrypt.PublicClassification,
+		})
+	}
+	return tags, nil
+}
