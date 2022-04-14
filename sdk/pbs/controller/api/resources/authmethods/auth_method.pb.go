38b9acb578dbcb1cb6b599fcfd406bb2d40a23e9
Timothy Messier <tim.messier@gmail.com>
2022-04-14 16:29:16 -0400

    fixup(authmethod): Implement taggable for authorized collection actions

--- sdk/pbs/controller/api/resources/authmethods/auth_method.pb.go
+++ sdk/pbs/controller/api/resources/authmethods/auth_method.pb.go
@@ -62,7 +62,7 @@ type AuthMethod struct {
 	// Output only. The available actions on this resource for this user.
 	AuthorizedActions []string `protobuf:"bytes,300,rep,name=authorized_actions,proto3" json:"authorized_actions,omitempty" class:"public"` // @gotags: `class:"public"`
 	// Output only. The authorized actions for the scope's collections.
-	AuthorizedCollectionActions map[string]*structpb.ListValue `protobuf:"bytes,310,rep,name=authorized_collection_actions,proto3" json:"authorized_collection_actions,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+	AuthorizedCollectionActions map[string]*structpb.ListValue `protobuf:"bytes,310,rep,name=authorized_collection_actions,proto3" json:"authorized_collection_actions,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"` // classified as public via taggable implementation
 }
 
 func (x *AuthMethod) Reset() {
