e865f0672898a2d6ce77d68fad8ef6f32b46971b
Cameron Perera <cameron.perera@hashicorp.com>
2022-05-11 12:00:25 -0500

    chore: refactor uiupdate.sh to use UI_COMMITISH instead of main

--- scripts/uiupdate.sh
+++ scripts/uiupdate.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
 if [ -z "$UI_VERSION_FILE" ]; then
 	echo "Must set UI_VERSION_FILE"; exit 1
 fi
+if [ -z "$UI_COMMITISH" ]; then
+	echo "Must set UI_COMMITISH"; exit 1
+fi
 
 shafileabs="$(pwd)/${UI_VERSION_FILE}"
-V="$(git ls-remote https://github.com/hashicorp/boundary-ui main)"
+V="$(git ls-remote https://github.com/hashicorp/boundary-ui ${UI_COMMITISH})"
+
+if [ -z "$V" ]; then
+	V=$UI_COMMITISH;
+fi
+
 SHA=${V:0:40}
 echo "==> Setting UI version to: $SHA"
 
