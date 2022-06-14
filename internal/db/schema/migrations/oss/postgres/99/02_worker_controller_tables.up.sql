8f80cad80df4b5ddf17ee19fccf8994a91746ef7
Todd <github@quaddi.com>
2022-06-14 14:39:00 -0700

    tweaking the constraints on last_status_time and address.

--- internal/db/schema/migrations/oss/postgres/99/02_worker_controller_tables.up.sql
+++ internal/db/schema/migrations/oss/postgres/99/02_worker_controller_tables.up.sql
@@ -56,21 +56,21 @@ create table server_worker (
   description wt_description,
   name wt_name -- server_worker_scope_id_name_uq defines an appropriate uniqueness constraint for name
     constraint worker_name_must_be_set_by_status
-    check (
-        type != 'kms' or name is not null
-      )
-  constraint kms_name_must_be_lowercase
-    check (lower(trim(name)) = name and type = 'kms')
-  constraint kms_name_only_has_printable_characters
-    check (name !~ '[^[:print:]]' and type = 'kms'),
+      check (
+          type != 'kms' or name is not null
+        )
+    constraint kms_name_must_be_lowercase
+      check (lower(trim(name)) = name and type = 'kms')
+    constraint kms_name_only_has_printable_characters
+      check (name !~ '[^[:print:]]' and type = 'kms'),
   address wt_network_address
     constraint address_must_be_set_by_status
-    check (
-        -- address can be null only if a non kms worker has not received a status update yet.
-        (type != 'kms')
-        or
-        (last_status_time is not null and address is not null)
-      ),
+      check (
+          -- address can be null only no status update has been received yet.
+          (last_status_time is not null and address is not null)
+          or
+          (last_status_time is null and address is null)
+        ),
   create_time wt_timestamp,
   update_time wt_timestamp,
   version wt_version,
@@ -79,9 +79,11 @@ create table server_worker (
       references server_worker_type_enm (name)
       on delete restrict
       on update cascade,
-  last_status_time timestamp with time zone,
-  constraint last_status_time_not_before_create_time
-    check (last_status_time >= create_time),
+  last_status_time timestamp with time zone
+    constraint last_status_time_not_before_create_time
+      check (last_status_time >= create_time)
+    constraint last_status_time_always_set_for_kms
+      check (type != 'kms' or last_status_time is not null),
   constraint server_worker_scope_id_name_uq
     unique(scope_id, name)
 );
