9a9c0d748cb4083ef0e063f827b1cc15289ad745
Louis Ruch <louisruch@gmail.com>
2022-06-22 10:02:14 -0700

    fixup! bug(scheduler): Fix controller id length check

--- internal/db/schema/migrations/oss/postgres/35/01_job_migrations.up.sql
+++ internal/db/schema/migrations/oss/postgres/35/01_job_migrations.up.sql
@@ -5,14 +5,24 @@ begin;
 -- domain type, and therefore does not have a minimum length of 10 characters.
 
 -- Drop constraint that requires controller_id to be at least 10 chars
+-- Removes constraint defined in 34/02_worker_controller_tables.up.sql 
 alter table job_run
   drop constraint controller_id_must_be_at_least_10_characters;
 
+create domain wt_not_empty as text
+  check(
+    length(trim(value)) > 0
+  );
+comment on domain wt_not_empty is
+'A text column that can only be null or not empty.';
+
 -- Add a not empty contraint to the controller_id
 alter table job_run
-  add constraint controller_id_must_not_be_empty
-    check(
-      length(trim(controller_id)) > 0
-    );
+    alter column controller_id type wt_not_empty;
+
+-- Add a not empty contraint to the private_id
+-- Updates column defined in 34/02_worker_controller_tables.up.sql 
+alter table server_controller
+    alter column private_id type wt_not_empty;
 
 commit;
