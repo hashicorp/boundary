da0645ebe838644df92f1e03c414e361510e8b94
Louis Ruch <louisruch@gmail.com>
2022-06-22 11:03:15 -0700

    fixup! bug(scheduler): Fix controller id length check

--- internal/db/schema/migrations/oss/postgres/35/01_job_migrations.up.sql
+++ internal/db/schema/migrations/oss/postgres/35/01_job_migrations.up.sql
@@ -4,25 +4,22 @@ begin;
 -- While this column is called a prviate_id it does not implement the wt_private_id
 -- domain type, and therefore does not have a minimum length of 10 characters.
 
--- Drop constraint that requires controller_id to be at least 10 chars
--- Removes constraint defined in 34/02_worker_controller_tables.up.sql 
-alter table job_run
-  drop constraint controller_id_must_be_at_least_10_characters;
-
-create domain wt_not_empty as text
+create domain wt_controller_id as text
   check(
     length(trim(value)) > 0
   );
-comment on domain wt_not_empty is
-'A text column that can only be null or not empty.';
+comment on domain wt_controller_id is
+'A text column that can only be null or not empty representing the id of the controller.';
 
--- Add a not empty contraint to the controller_id
+-- Drop constraint that requires controller_id to be at least 10 chars
+-- Alters controller_id column defined in 34/02_worker_controller_tables.up.sql
 alter table job_run
-    alter column controller_id type wt_not_empty;
+  drop constraint controller_id_must_be_at_least_10_characters,
+  alter column controller_id type wt_controller_id;
 
 -- Add a not empty contraint to the private_id
--- Updates column defined in 34/02_worker_controller_tables.up.sql 
+-- Updates column private_id defined in 34/02_worker_controller_tables.up.sql
 alter table server_controller
-    alter column private_id type wt_not_empty;
+    alter column private_id type wt_controller_id;
 
 commit;
