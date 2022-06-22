57a1f726b7856357d06664f8491cd82ffb01b1b0
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2022-06-22 18:42:54 -0500

    Update query

--- internal/db/schema/migrations/oss/postgres/35/01_cleanup.up.sql
+++ internal/db/schema/migrations/oss/postgres/35/01_cleanup.up.sql
@@ -1,22 +1,74 @@
 begin;
 
--- This adds additional cleanup to what went into 0.8.x. A forgotten case could
--- mean that sessions can persist in canceled state even if there are no
--- connections for that session (possibly due to cleanup) and will never be
--- transitioned because connection state cannot be determined.
+-- This adds additional cleanup to what went into 0.8.x. There could be a case
+-- where sessions can persist in canceled state even if there are no connections
+-- for that session (possibly due to cleanup) and will never be transitioned
+-- because connection state cannot be determined.
 --
--- There is no reason to transition to terminated state as we remove those
--- periodically anyways, and there is no useful information to be gleaned from
--- having it in that state.
---
--- Note that this only cleans up those in canceling state. In pending we may
--- well not have connections yet; in active our fixed logic should ensure that
--- when it transitions the right things happen; and in terminated those will be
--- cleaned up periodically.
-delete from session
-where public_id not in 
-  (select session_id from session_connection)
-and
-  termination_reason = 'canceled';
+-- If they have previously been marked terminated in the warehouse we delete
+-- them; if not we mark them terminated so it's recorded in the warehouse and
+-- the cleanup job should eventually clear these out.
+with
+  canceling_sessions(session_id) as (
+    select session_id
+      from session_state
+     where state = 'canceling'
+       and end_time is null
+  ),
+  no_connection_canceling_sessions as (
+    select session_id
+      from canceling_sessions
+     where session_id not in
+             (
+               select session_id
+                 from session_connection
+             )
+  ),
+  has_termination_reason (session_id) as (
+        select public_id
+          from session
+         where public_id in
+                 (
+                   select session_id
+                     from no_connection_canceling_sessions
+                 )
+           and termination_reason is not null
+  ),
+  terminated_in_warehouse (session_id) as (
+        select session_id
+          from has_termination_reason
+         where session_id in
+                (
+                  select session_id
+                    from wh_session_accumulating_fact
+                   where session_terminated_date_key <> -1
+                )
+  ),
+  not_terminated_in_warehouse (session_id) as (
+        select session_id
+          from has_termination_reason
+         where session_id not in
+                 (
+                   select session_id
+                     from terminated_in_warehouse
+                 )
+  ),
+  delete_terminated_in_warehouse_from_session as (
+        delete from session
+         where public_id in
+                 (
+                   select session_id
+                     from terminated_in_warehouse
+                 )
+  )
+  update session
+    set
+      version = version + 1,
+      termination_reason = 'canceled'
+    where public_id in
+            (
+              select session_id
+              from not_terminated_in_warehouse
+            );
 
 commit;
