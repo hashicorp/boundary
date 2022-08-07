fc4231503f8fa15af23b8e590cae6da625452f9d
Louis Ruch <louisruch@gmail.com>
2022-08-06 23:00:53 -0700

    Display brokered ssh_private_key if it contains a passphrase

--- internal/cmd/commands/connect/ssh.go
+++ internal/cmd/commands/connect/ssh.go
@@ -71,7 +71,7 @@ func (s *sshFlags) buildArgs(c *Command, port, ip, addr string, creds credential
 		var password string
 		if len(retCreds.usernamePassword) > 0 {
 			// For now just grab the first username password credential brokered
-			// Mark credential as consumed so it is not printed to user
+			// Mark credential as consumed so that it is not printed to user
 			retCreds.usernamePassword[0].consumed = true
 
 			username = retCreds.usernamePassword[0].Username
@@ -109,11 +109,16 @@ func (s *sshFlags) buildArgs(c *Command, port, ip, addr string, creds credential
 		// If we want to consume check if we have a private key available first
 		case len(creds.sshPrivateKey) > 0:
 			// For now just grab the first ssh private key credential brokered
-			// Mark credential as consumed so it is not printed to user
-			retCreds.sshPrivateKey[0].consumed = true
+			cred := retCreds.sshPrivateKey[0]
 
-			username = retCreds.sshPrivateKey[0].Username
-			privateKey := retCreds.sshPrivateKey[0].PrivateKey
+			if cred.Passphrase == "" {
+				// Credential does not contain a passphrase, mark it as consumed so that it
+				// is not printed to user.
+				retCreds.sshPrivateKey[0].consumed = true
+			}
+
+			username = cred.Username
+			privateKey := cred.PrivateKey
 
 			pkFile, err := ioutil.TempFile("", "*")
 			if err != nil {
@@ -126,7 +131,7 @@ func (s *sshFlags) buildArgs(c *Command, port, ip, addr string, creds credential
 				return nil
 			})
 			// SSH requires the private key file to end with a newline.
-			// When injesting a ssh_private_key from a file:// Boundary calls strings.TrimSpace
+			// When ingesting an ssh_private_key from a file:// Boundary calls strings.TrimSpace
 			// which will also trim newlines.
 			if !strings.HasSuffix(privateKey, "\n") {
 				privateKey = fmt.Sprintln(privateKey)
@@ -142,7 +147,7 @@ func (s *sshFlags) buildArgs(c *Command, port, ip, addr string, creds credential
 
 		// Next check if we have a username password credential
 		case len(creds.usernamePassword) > 0:
-			// We cannot use the password of the credential outside of sshpass but we
+			// We cannot use the password of the credential outside of sshpass, but we
 			// can use the username.
 			// N.B. Do not mark credential as consumed, as user will still need enter
 			// the password when prompted.
