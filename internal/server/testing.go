8f992c7884fa12fcfd75e9bc202c6fad49e39ef0
irenarindos <irena.rindos@gmail.com>
2022-08-18 10:23:37 -0400

    fix make gen deltas

--- internal/server/testing.go
+++ internal/server/testing.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"crypto/rand"
-	"github.com/hashicorp/boundary/version"
 	mathRand "math/rand"
 	"strings"
 	"testing"
@@ -16,6 +15,7 @@ import (
 	"github.com/hashicorp/boundary/internal/kms"
 	"github.com/hashicorp/boundary/internal/server/store"
 	"github.com/hashicorp/boundary/internal/types/scope"
+	"github.com/hashicorp/boundary/version"
 	wrapping "github.com/hashicorp/go-kms-wrapping/v2"
 	"github.com/hashicorp/nodeenrollment"
 	"github.com/hashicorp/nodeenrollment/registration"
