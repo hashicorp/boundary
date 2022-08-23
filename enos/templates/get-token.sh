3d6eb869b48d2fcb5369a3919e59c4d8b10cb3b8
Josh Brand <jbrand@hashicorp.com>
2022-08-23 13:29:14 -0400

    Fix boundary path for auth-methods check

--- enos/templates/get-token.sh
+++ enos/templates/get-token.sh
@@ -21,11 +21,8 @@ function retry {
   return 0
 }
 
-# make sure the ALB is up and passing healthchecks
-retry 10 curl -s -o /dev/null ${BOUNDARY_ADDR}
-
 # make sure boundary is connected to DB (unauthenticated endpoint)
-retry 10 boundary auth-methods list > /dev/null
+retry 10 ${BOUNDARY_PATH}/boundary auth-methods list > /dev/null
 
 export BP="${PASSWORD}"
 ${BOUNDARY_PATH}/boundary authenticate password -auth-method-id=${METHOD_ID} -login-name=${LOGIN_NAME} -password=env://BP -token-name=none -format=json -keyring-type=none
