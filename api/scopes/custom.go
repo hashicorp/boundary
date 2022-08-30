e0d8c383d92004b993bfa4e39123bfb852c9395c
Danielle <29378233+DanielleMiu@users.noreply.github.com>
2022-08-30 11:33:01 -0400

    Add Rotate Keys CLI Command (#2395)
    * add cli command and client
    
    * add bats tests
    
    * fix some info text and pr comments
    
    * write a new rotate keys description

--- api/scopes/custom.go
+++ api/scopes/custom.go
@@ -21,6 +21,14 @@ func (n KeyListResult) GetResponse() *api.Response {
 	return n.response
 }
 
+type KeysRotateResult struct {
+	response *api.Response
+}
+
+func (n KeysRotateResult) GetResponse() *api.Response {
+	return n.response
+}
+
 func (c *Client) ListKeys(ctx context.Context, scopeId string, opt ...Option) (*KeyListResult, error) {
 	if scopeId == "" {
 		return nil, fmt.Errorf("empty scopeId value passed into ListKeys request")
@@ -58,5 +66,42 @@ func (c *Client) ListKeys(ctx context.Context, scopeId string, opt ...Option) (*
 		return nil, apiErr
 	}
 	target.response = resp
+	return target, nil
+}
+
+func (c *Client) RotateKeys(ctx context.Context, scopeId string, rewrapKeys bool, opt ...Option) (*KeysRotateResult, error) {
+	if scopeId == "" {
+		return nil, fmt.Errorf("empty scopeId value passed into RotateKeys request")
+	}
+	if c.client == nil {
+		return nil, fmt.Errorf("nil client")
+	}
+
+	opts, apiOpts := getOpts(opt...)
+
+	opts.postMap["scope_id"] = scopeId
+	opts.postMap["rewrap"] = rewrapKeys
+
+	req, err := c.client.NewRequest(ctx, "POST", "scopes:rotate-keys", opts.postMap, apiOpts...)
+	if err != nil {
+		return nil, fmt.Errorf("error creating RotateKeys request: %w", err)
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error performing client request during RotateKeys call: %w", err)
+	}
+
+	apiErr, err := resp.Decode(nil)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding RotateKeys response: %w", err)
+	}
+	if apiErr != nil {
+		return nil, apiErr
+	}
+
+	target := &KeysRotateResult{
+		response: resp,
+	}
 	return target, nil
 }
