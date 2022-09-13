82a0d573720996a9f129196888c7cc76d267dde6
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2022-09-12 17:32:51 -0700

    Store key_id and scope_id with oplog entries, re-type columns referencing data key version (#2431)
    * fix(db): Correct types of data key version referencers
    
    * fix(oplog): Fix missing error handling
    
    * feat(oplog): Store key_id and scope_id with oplog entries
    
    This migration truncates all existing oplog entries, as
    migrating existing data would have been complex and
    likely unnecessary.
    
    * Update view references
    
    * Always delete oplog entries when dropping keys

---
+++ internal/db/schema/migrations/oss/postgres/51/01_oplog_key_id_scope_id_truncation.up.sql
@@ -0,0 +1,34 @@
+begin;
+
+  truncate table oplog_entry, oplog_metadata;
+
+  alter table oplog_entry
+    add column key_id kms_private_id not null
+      constraint kms_data_key_version_fkey
+        references kms_data_key_version (private_id)
+        on delete restrict
+        on update cascade,
+    add column scope_id wt_scope_id not null
+      constraint iam_scope_fkey
+        references iam_scope (public_id)
+        on delete cascade
+        on update cascade;
+
+  create function insert_oplog_entry() returns trigger
+  as $$
+  begin
+    select scope_id into new.scope_id
+      from kms_root_key
+      inner join kms_data_key on kms_root_key.private_id = kms_data_key.root_key_id
+      inner join kms_data_key_version on kms_data_key.private_id = kms_data_key_version.data_key_id
+      where kms_data_key_version.private_id = new.key_id;
+    return new;
+  end;
+  $$ language plpgsql;
+  comment on function insert_oplog_entry() is
+    'insert_oplog_entry sets the scope_id based on the key_id';
+
+  create trigger insert_oplog_entry before insert on oplog_entry
+    for each row execute procedure insert_oplog_entry();
+
+commit;
