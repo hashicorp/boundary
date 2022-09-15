527216653a8369a3e2e347542c2088c6a6e69987
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2022-09-15 14:48:04 -0400

    Update internal/db/schema/migrations/oss/postgres/51/04_mutable_root_key_version.up.sql
    Co-authored-by: Michael Gaffney <mgaffney@users.noreply.github.com>

--- internal/db/schema/migrations/oss/postgres/51/04_mutable_root_key_version.up.sql
+++ internal/db/schema/migrations/oss/postgres/51/04_mutable_root_key_version.up.sql
@@ -7,9 +7,7 @@ begin;
 -- rewrapping the data key version.
 drop trigger kms_immutable_columns on kms_data_key_version;
 
-create trigger kms_immutable_columns
-before
-update on kms_data_key_version
+create trigger kms_immutable_columns before update on kms_data_key_version
   for each row execute procedure kms_immutable_columns('private_id', 'data_key_id', 'create_time');
 
 commit;
