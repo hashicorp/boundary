a2ef14283bb059c579464bec87865bba9055b5a7
Timothy Messier <tim.messier@gmail.com>
2022-09-19 09:47:52 -0400

    fix(org): Address bug that prevented deleting an org (#2465)
    The schema changes introduced in 0.10.2 introduced a bug where an org
    could not be deleted if there was a session for an org scoped user. It
    would result in an error like:
    
        scope.(Service).deleteFromRepo: unable to delete scope: \
            iam.(Repository).DeleteScope: failed for o_VkNy46KwfS: \
            iam.(Repository).delete: db.DoTx: iam.(Repository).delete: \
            db.Delete: insert or update on table "session" violates foreign key constraint "target_fkey": integrity violation: error #1003
    
    A worker around would be to first delete all projects in the org, then
    finally delete the org.
    
    This fix adds a regression test to the sqltests and updates several
    constraints on the session table to be deferrable. Since the org delete
    is a single transaction, this allows all of the necessary resources to
    be deleted before attempting to enforce these constrains.
    
    See: https://www.postgresql.org/docs/current/sql-set-constraints.html
    Blame: 3a28ab614

---
+++ internal/db/sqltest/tests/org/org-delete.sql
@@ -0,0 +1,15 @@
+-- org-delete tests:
+--  deleting an org
+
+begin;
+  select plan(1);
+
+  prepare delete_org as
+    delete from iam_scope
+     where type = 'org'
+       and public_id = 'o__foodtruck';
+
+  select lives_ok('delete_org');
+
+  select * from finish();
+rollback;
