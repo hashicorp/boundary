7f8b62eb1a27ca0dc65a7ea98911c2bad8158d47
Danielle Miu <29378233+DanielleMiu@users.noreply.github.com>
2022-09-20 12:53:19 -0400

    add migration to fix immutable error

--- internal/db/schema/migrations/oss/postgres/47/01_key_rewrap.up.sql
+++ internal/db/schema/migrations/oss/postgres/47/01_key_rewrap.up.sql
@@ -17,6 +17,7 @@ create trigger kms_immutable_columns before update on kms_root_key_version
 -- rewrapping the data key version.
 drop trigger kms_immutable_columns on kms_data_key_version;
 
+-- trigger updated again in migration 51 file 03
 create trigger kms_immutable_columns before update on kms_data_key_version
   for each row execute procedure kms_immutable_columns('private_id', 'data_key_id', 'root_key_version_id', 'create_time');
 
