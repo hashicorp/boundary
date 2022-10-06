88c49abcf6ed5c6888baed8b135d85b37c437919
Michael Li <michael.li@hashicorp.com>
2022-10-06 18:27:21 -0400

    fix(bats): Fix flakiness in sessions.bats tests
    The sessions.bats tests were seeing some flaky behavior when listing sessions but not getting the expected amount back. This is due to the connect_nc command that is currently used, which will immediately set the session to be "canceled" once it returns. This can cause subsequent operations to  get unexpected results (sometimes, listing sessions would return an empty list if the created sessions go to a "terminated" state).

--- internal/tests/cli/boundary/_sessions.bash
+++ internal/tests/cli/boundary/_sessions.bash
@@ -1,11 +1,11 @@
 load _authorized_actions
 
-function list_sessions() {
-  boundary sessions list -scope-id $1 -format json
+function list_sessions_include_terminated() {
+  boundary sessions list -scope-id $1 -include-terminated -format json
 }
 
-function count_sessions() {
-  list_sessions $1 | jq '.items | length'
+function count_sessions_include_terminated() {
+  list_sessions_include_terminated $1 | jq '.items | length'
 }
 
 function cancel_session() {
