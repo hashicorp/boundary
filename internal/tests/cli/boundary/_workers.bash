c9d902ebd0e4138b07c27647f2a34ee458ee0871
Todd <github@quaddi.com>
2022-10-10 12:37:17 -0700

    Add worker create, list, read, update, and delete cli ui tests. (#2384)

---
+++ internal/tests/cli/boundary/_workers.bash
@@ -0,0 +1,40 @@
+load _authorized_actions
+
+function read_worker() {
+  boundary workers read -id $1 -format json
+}
+
+function create_worker() {
+  boundary workers create controller-led \
+    -name $1 \
+    -description 'test worker'
+}
+
+function update_worker() {
+  boundary workers update -id $1 -name $2 -version 0
+}
+
+function delete_worker() {
+  boundary workers delete -id $1
+}
+
+function list_workers() {
+  boundary workers list -format json
+}
+
+function worker_id() {
+  local id=$1
+  strip $(list_workers | jq -c ".items[] | select(.name | contains(\"$id\")) | .[\"id\"]")
+}
+
+function has_default_worker_actions() {
+  local out=$1
+  local actions=('read' 'update' 'delete' 'add-worker-tags' 'set-worker-tags' 'remove-worker-tags')
+
+  for action in ${actions[@]}; do
+    $(has_authorized_action "$out" "$action") || {
+      echo "failed to find $action action in output: $out"
+      return 1 
+    } 
+  done
+}
