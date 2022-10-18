3da2c1c7de27634a76019524dc9e97fbce82c885
Danielle Miu <29378233+DanielleMiu@users.noreply.github.com>
2022-10-18 14:48:14 -0400

    forgot a comment

--- internal/authtoken/rewrapping.go
+++ internal/authtoken/rewrapping.go
@@ -15,7 +15,7 @@ func init() {
 func authTokenRewrapFn(ctx context.Context, dataKeyVersionId, scopeId string, reader db.Reader, writer db.Writer, kmsRepo *kms.Kms) error {
 	const op = "authtoken.authTokenRewrapFn"
 	var credentials []*AuthToken
-	// indexes on public id and token, and the only reference to public id is session (unreliable join).
+	// Indexes exist on public id and token, and the only reference to public id is session, which may not exist for all rows we need.
 	// This is the fastest query we can use without creating a new index on key_id.
 	if err := reader.SearchWhere(ctx, &credentials, "key_id=?", []interface{}{dataKeyVersionId}, db.WithLimit(-1)); err != nil {
 		return errors.Wrap(ctx, err, op, errors.WithMsg("failed to query sql for rows that need rewrapping"))
