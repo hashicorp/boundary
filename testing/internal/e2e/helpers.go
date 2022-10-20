ab223493a394c2ab17c03190c88a612bff3d351c
Michael Li <michael.li@hashicorp.com>
2022-10-20 10:36:08 -0400

    fix(e2e): Remove unused code

--- testing/internal/e2e/helpers.go
+++ testing/internal/e2e/helpers.go
@@ -10,32 +10,7 @@ import (
 	"testing"
 )
 
-// Option is a func that sets optional attributes for a call. This does not need
-// to be used directly, but instead option arguments are built from the
-// functions in this package. WithX options set a value to that given in the
-// argument; DefaultX options indicate that the value should be set to its
-// default. When an API call is made options are processed in ther order they
-// appear in the function call, so for a given argument X, a succession of WithX
-// or DefaultX calls will result in the last call taking effect.
-type Option func(*options)
-
-type options struct {
-	withArgs []string
-	withPipe []string
-}
-
-func getOpts(opt ...Option) options {
-	opts := options{}
-	for _, o := range opt {
-		if o != nil {
-			o(&opts)
-		}
-	}
-
-	return opts
-}
-
-// CommandResult encapsulates the output from running an external command
+// CommandResult captures the output from running an external command
 type CommandResult struct {
 	Stdout   []byte
 	Stderr   []byte
@@ -72,22 +47,6 @@ func RunCommand(ctx context.Context, name string, args ...string) *CommandResult
 		Stderr:   errbuf.Bytes(),
 		ExitCode: exitCode,
 		Err:      err,
-	}
-}
-
-// WithArgs is an option to RunCommand that allows the user to specify arguments
-// for the provided command
-func WithArgs(args ...string) Option {
-	return func(o *options) {
-		o.withArgs = args
-	}
-}
-
-// WithPipe is an option to RunCommand that allows the user to specify a command+arguments
-// to pipe to
-func WithPipe(command ...string) Option {
-	return func(o *options) {
-		o.withPipe = command
 	}
 }
 
