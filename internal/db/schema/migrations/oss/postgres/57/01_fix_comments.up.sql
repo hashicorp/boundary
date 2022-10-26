c9cfb0e5644af45662f4925b23b694cf0c4f6423
Michael Gaffney <mike@gaffney.cc>
2022-10-26 08:01:14 -0400

    docs(db): fix comments on oidc database tables
    Fixes the comments on the auth_oidc_method and auth_oidc_account tables.

--- internal/db/schema/migrations/oss/postgres/57/01_fix_comments.up.sql
+++ internal/db/schema/migrations/oss/postgres/57/01_fix_comments.up.sql
@@ -9,4 +9,10 @@ begin;
   comment on function wt_sub_seconds is 'wt_sub_seconds returns ts - sec.';
   comment on function wt_sub_seconds_from_now is 'wt_sub_seconds_from_now returns current_timestamp - sec.';
 
+  -- Fixes incorrect comments in 2/04_oidc.up.sql
+  comment on table auth_oidc_method is
+    'auth_oidc_method entries are the current oidc auth methods configured for existing scopes.';
+  comment on table auth_oidc_account is
+    'auth_oidc_account entries are subtypes of auth_account and represent an oidc account.';
+
 commit;
