cede1aec93ceb3c140563ac58a1aa7cf7667a202
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2022-10-27 09:01:05 -0400

    Add initial templating support to Vault credential libraries (#2569) (#2575)
    Add initial templating support to Vault credential libraries
    
    This adds support for templating various account and user values into
    Vault dynamic credential library POST bodies and GET/POST paths.
    
    It also unifies how parameters are used in grants, with backwards
    compatibility.

---
+++ internal/util/template/generate.go
@@ -0,0 +1,69 @@
+package template
+
+import (
+	"context"
+	"strings"
+	"text/template"
+
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/util"
+)
+
+// Parsed contains information about a template parsed via New. Technically
+// `raw` and `funcMap` are not required to be cached here as they are part of
+// the `template.Template` object but it is useful for tests.
+type Parsed struct {
+	raw     string
+	tmpl    *template.Template
+	funcMap template.FuncMap
+}
+
+// New creates a Parsed struct. It requires the raw string.
+func New(ctx context.Context, raw string) (*Parsed, error) {
+	const op = "util.template.New"
+
+	if raw == "" {
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "empty raw template")
+	}
+
+	ret := &Parsed{
+		raw: raw,
+		funcMap: map[string]interface{}{
+			"truncateFrom": truncateFrom,
+		},
+	}
+
+	tmpl, err := template.New("template").
+		Funcs(ret.funcMap).
+		Parse(ret.raw)
+	if err != nil {
+		return nil, errors.Wrap(ctx, err, op, errors.WithMsg("error parsing template"))
+	}
+	ret.tmpl = tmpl
+
+	return ret, nil
+}
+
+// Generate based on the provided template
+func (p *Parsed) Generate(ctx context.Context, data interface{}) (string, error) {
+	const op = "util.template.(Parsed).Generate"
+
+	switch {
+	case p.tmpl == nil:
+		return "", errors.New(ctx, errors.InvalidParameter, op, "parsed template not initialized")
+	case util.IsNil(data):
+		return "", errors.New(ctx, errors.InvalidParameter, op, "input data is nil")
+	}
+
+	str := &strings.Builder{}
+	if err := p.tmpl.Execute(str, data); err != nil {
+		return "", errors.Wrap(ctx, err, op, errors.WithMsg("error executing template"))
+	}
+
+	out := str.String()
+	if strings.Contains(out, "<nil>") {
+		return "", errors.New(ctx, errors.InvalidParameter, op, "template execution resulted in nil value")
+	}
+
+	return out, nil
+}
---
+++ internal/util/template/generate_test.go
@@ -0,0 +1,131 @@
+package template
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/boundary/internal/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// NOTE: Although this does a basic test of template generation, other tests
+// that are closer to where this library is used can and should also be enhanced
+// over time. For instance, TestAuthorizeSession from target_service_test.go has
+// been enhanced to use this library to check output against Vault.
+
+func TestErrors(t *testing.T) {
+	require, assert := require.New(t), assert.New(t)
+	ctx := context.Background()
+
+	// No template
+	parsed, err := New(ctx, "")
+	assert.Error(err)
+	assert.Nil(parsed)
+
+	// Bad template
+	parsed, err = New(ctx, "{{ foobar")
+	assert.Error(err)
+	assert.Nil(parsed)
+
+	// Good template
+	ts := "Foobar:{{ .User.Name }}"
+	parsed, err = New(ctx, ts)
+	assert.NoError(err)
+	require.NotNil(parsed)
+	assert.Equal(ts, parsed.raw)
+	assert.NotNil(parsed.tmpl)
+	assert.Len(parsed.funcMap, 1)
+
+	// Test out errors on the parsed value
+
+	// Nil template
+	oldTmpl := parsed.tmpl
+	parsed.tmpl = nil
+	out, err := parsed.Generate(ctx, Data{})
+	assert.Error(err)
+	assert.Empty(out)
+	parsed.tmpl = oldTmpl
+
+	// Nil
+	out, err = parsed.Generate(ctx, nil)
+	assert.Error(err)
+	assert.Empty(out)
+
+	// Nil pointer
+	var nilData *Data
+	out, err = parsed.Generate(ctx, nilData)
+	assert.Error(err)
+	assert.Empty(out)
+
+	// Nil internal data
+	_, err = parsed.Generate(ctx, Data{})
+	assert.Error(err)
+
+	// Good data
+	out, err = parsed.Generate(ctx, Data{User: User{Name: util.Pointer("name")}})
+	require.NoError(err)
+	assert.Equal("Foobar:name", out)
+}
+
+func TestGenerate(t *testing.T) {
+	require, assert := require.New(t), assert.New(t)
+	ctx := context.Background()
+	data := Data{
+		User: User{
+			Id:       util.Pointer("userId"),
+			Name:     util.Pointer("userName"),
+			FullName: util.Pointer("userFullName"),
+			Email:    util.Pointer("user@email.com"),
+		},
+		Account: Account{
+			Id:        util.Pointer("accountId"),
+			Name:      util.Pointer("accountName"),
+			LoginName: util.Pointer("accountLoginName"),
+			Subject:   util.Pointer("accountSubject"),
+			Email:     util.Pointer("account@email.com"),
+		},
+	}
+	raw := strings.TrimSpace(`
+{{ .User.Id }}
+{{ .User.Name }}
+{{ .User.FullName }}
+{{ .User.Email }}
+{{ truncateFrom .User.Email "@" }}
+{{ .Account.Id }}
+{{ .Account.Name }}
+{{ .Account.LoginName }}
+{{ .Account.Subject }}
+{{ .Account.Email }}
+{{ truncateFrom .Account.Email "@" }}
+`)
+
+	parsed, err := New(ctx, raw)
+	require.NoError(err)
+	require.NotNil(parsed)
+
+	// Ensure we error with required data
+	_, err = parsed.Generate(ctx, Data{})
+	require.Error(err)
+
+	// Do again with non-empty data
+	out, err := parsed.Generate(ctx, data)
+	require.NoError(err)
+
+	exp := strings.TrimSpace(`
+userId
+userName
+userFullName
+user@email.com
+user
+accountId
+accountName
+accountLoginName
+accountSubject
+account@email.com
+account
+`)
+
+	assert.Equal(exp, out)
+}
