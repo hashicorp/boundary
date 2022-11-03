80cdbc305dfff43f8f53cb8e01ca1bebf9673b9a
Michael Li <michael.li@hashicorp.com>
2022-11-02 20:05:15 -0400

    test(e2e): Check that an active session ends if the user is deleted (#2588)
    Adds a new test to the e2e test suite that checks that an active session ends once the user is deleted.

--- testing/internal/e2e/tests/static/session_cancel_user_test.go
+++ testing/internal/e2e/tests/static/session_cancel_user_test.go
@@ -3,12 +3,9 @@ package static_test
 import (
 	"context"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"testing"
 	"time"
 
-	"github.com/cenkalti/backoff/v4"
 	"github.com/hashicorp/boundary/api/roles"
 	"github.com/hashicorp/boundary/api/sessions"
 	"github.com/hashicorp/boundary/api/users"
@@ -63,7 +60,7 @@ func TestCliSessionCancelUser(t *testing.T) {
 	var response e2e.CliError
 	err = json.Unmarshal(output.Stderr, &response)
 	require.NoError(t, err)
-	require.Equal(t, 403, response.Status)
+	require.Equal(t, 403, int(response.Status))
 	t.Log("Successfully received an error when connecting to target as a user without permissions")
 
 	// Create a role for user
@@ -97,68 +94,12 @@ func TestCliSessionCancelUser(t *testing.T) {
 		)
 	}()
 	t.Cleanup(cancel)
-
-	// Wait for session to be active
-	var session *sessions.Session
-	err = backoff.RetryNotify(
-		func() error {
-			// List sessions
-			output := e2e.RunCommand(ctx, "boundary",
-				e2e.WithArgs("sessions", "list", "-scope-id", newProjectId, "-format", "json"),
-			)
-			if output.Err != nil {
-				return backoff.Permanent(errors.New(string(output.Stderr)))
-			}
-			var sessionListResult sessions.SessionListResult
-			err := json.Unmarshal(output.Stdout, &sessionListResult)
-			if err != nil {
-				return backoff.Permanent(err)
-			}
-
-			// Check if there is one session
-			sessionCount := len(sessionListResult.Items)
-			if sessionCount == 0 {
-				return errors.New("No items are appearing in the session list")
-			}
-			t.Logf("Found %d session(s)", sessionCount)
-			if sessionCount != 1 {
-				return backoff.Permanent(errors.New("Only one session was expected to be found"))
-			}
-
-			// Check if session is active
-			session = sessionListResult.Items[0]
-			output = e2e.RunCommand(ctx, "boundary",
-				e2e.WithArgs("sessions", "read", "-id", session.Id, "-format", "json"),
-			)
-			if output.Err != nil {
-				return backoff.Permanent(errors.New(string(output.Stderr)))
-			}
-
-			var sessionReadResult sessions.SessionReadResult
-			err = json.Unmarshal(output.Stdout, &sessionReadResult)
-			if err != nil {
-				return backoff.Permanent(err)
-			}
-
-			if sessionReadResult.Item.Status != "active" {
-				return errors.New(fmt.Sprintf("Waiting for session to be active... Expected: %s, Actual: %s",
-					"active",
-					sessionReadResult.Item.Status,
-				))
-			}
-
-			return nil
-		},
-		backoff.WithMaxRetries(backoff.NewConstantBackOff(3*time.Second), 5),
-		func(err error, td time.Duration) {
-			t.Logf("%s. Retrying...", err.Error())
-		},
-	)
-	require.NoError(t, err)
+	session := boundary.WaitForSessionToBeActiveCli(t, ctx, newProjectId)
 	assert.Equal(t, newTargetId, session.TargetId)
 	assert.Equal(t, newHostId, session.HostId)
 
 	// Cancel session
+	t.Log("Canceling session...")
 	output = e2e.RunCommand(ctx, "boundary",
 		e2e.WithArgs("sessions", "cancel", "-id", session.Id),
 	)
