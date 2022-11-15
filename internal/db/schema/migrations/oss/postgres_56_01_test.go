d8812eba070c0a186b7736a0c04a3f5a5742066f
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2022-11-14 17:47:58 -0800

    fix(migrations): Rewrite migration 56 tests
    These tests can no longer use the normal test helpers
    as they try to use the new oplog table structure, so we
    have to manually write all the interactions.

--- internal/db/schema/migrations/oss/postgres_56_01_test.go
+++ internal/db/schema/migrations/oss/postgres_56_01_test.go
@@ -75,53 +75,143 @@ func TestMigrations_DeleteOrphanedAccounts(t *testing.T) {
 	iamRepo := iam.TestRepo(t, conn, wrapper)
 	usr, err := iam.NewUser(scope.Global.String())
 	require.NoError(t, err)
-	usr, err = iamRepo.CreateUser(ctx, usr)
+	usr.PublicId = "u_1234567890"
+	num, err := rw.Exec(ctx, `
+insert into iam_user
+	(public_id, scope_id)
+values
+	(?, ?)
+	`, []any{usr.PublicId, scope.Global.String()})
 	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 
 	// Create the accounts we will delete and assert their behavior
-	pwRepo, err := password.NewRepository(rw, rw, kmsCache)
-	require.NoError(t, err)
 
 	pwAm1, err := password.NewAuthMethod(scope.Global.String())
 	require.NoError(t, err)
-	pwAm1, err = pwRepo.CreateAuthMethod(ctx, pwAm1)
+	pwAm1.PublicId = "ampw_1234567890"
+	_, err = rw.DoTx(ctx, 0, db.ExpBackoff{}, func(r db.Reader, w db.Writer) error {
+		_, err := w.Exec(ctx, `
+		insert into auth_password_argon2_conf
+			(private_id, password_method_id)
+		values
+			(?, ?)
+			`, []any{"arg2conf_1234567890", pwAm1.PublicId})
+		if err != nil {
+			return err
+		}
+		_, err = w.Exec(ctx, `
+		insert into auth_password_method
+			(public_id, password_conf_id, scope_id)
+		values
+			(?, ?, ?)
+			`, []any{pwAm1.PublicId, "arg2conf_1234567890", pwAm1.ScopeId})
+		return err
+	})
 	require.NoError(t, err)
 	pwAcct1, err := password.NewAccount(pwAm1.GetPublicId(), password.WithLoginName("account1"), password.WithPassword("password"))
 	require.NoError(t, err)
-	pwAcct1, err = pwRepo.CreateAccount(ctx, scope.Global.String(), pwAcct1)
+	pwAcct1.PublicId = "acctpw_1234567890"
+	num, err = rw.Exec(ctx, `
+insert into auth_password_account
+	(public_id, auth_method_id, scope_id, login_name)
+values
+	(?, ?, ?, ?)
+	`, []any{pwAcct1.PublicId, pwAm1.PublicId, pwAm1.ScopeId, pwAcct1.LoginName})
 	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 
 	pwAm2, err := password.NewAuthMethod(scope.Global.String())
 	require.NoError(t, err)
-	pwAm2, err = pwRepo.CreateAuthMethod(ctx, pwAm2)
-	require.NoError(t, err)
+	pwAm2.PublicId = "ampw_0123456789"
+	_, err = rw.DoTx(ctx, 0, db.ExpBackoff{}, func(r db.Reader, w db.Writer) error {
+		_, err := w.Exec(ctx, `
+		insert into auth_password_argon2_conf
+			(private_id, password_method_id)
+		values
+			(?, ?)
+			`, []any{"arg2conf_0123456789", pwAm2.PublicId})
+		if err != nil {
+			return err
+		}
+		_, err = w.Exec(ctx, `
+		insert into auth_password_method
+			(public_id, password_conf_id, scope_id)
+		values
+			(?, ?, ?)
+			`, []any{pwAm2.PublicId, "arg2conf_0123456789", pwAm2.ScopeId})
+		return err
+	})
 	pwAcct2, err := password.NewAccount(pwAm2.GetPublicId(), password.WithLoginName("account2"), password.WithPassword("password"))
 	require.NoError(t, err)
-	pwAcct2, err = pwRepo.CreateAccount(ctx, scope.Global.String(), pwAcct2)
+	pwAcct2.PublicId = "acctpw_0123456789"
+	num, err = rw.Exec(ctx, `
+insert into auth_password_account
+	(public_id, auth_method_id, scope_id, login_name)
+values
+	(?, ?, ?, ?)
+	`, []any{pwAcct2.PublicId, pwAm2.PublicId, pwAm2.ScopeId, pwAcct2.LoginName})
 	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 
-	oidcRepo, err := oidc.NewRepository(ctx, rw, rw, kmsCache)
-	require.NoError(t, err)
 	databaseWrapper, err := kmsCache.GetWrapper(context.Background(), scope.Global.String(), kms.KeyPurposeDatabase)
 	oidcAm1 := oidc.TestAuthMethod(t, conn, databaseWrapper, scope.Global.String(), oidc.InactiveState, "alice_rp1",
 		"my-dogs-name", oidc.WithIssuer(oidc.TestConvertToUrls(t, "https://alice1.com")[0]), oidc.WithApiUrl(oidc.TestConvertToUrls(t, "https://api1.com")[0]))
+	oidcAm1.PublicId = "amoidc_1234567890"
+	num, err = rw.Exec(ctx, `
+	insert into auth_oidc_method
+		(public_id, scope_id, state, key_id)
+	values
+		(?, ?, ?, ?)
+		`, []any{oidcAm1.PublicId, oidcAm1.ScopeId, oidcAm1.OperationalState, oidcAm1.KeyId})
+	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 	oidcAcct1, err := oidc.NewAccount(ctx, oidcAm1.GetPublicId(), "oidcAcct1")
 	require.NoError(t, err)
-	oidcAcct1, err = oidcRepo.CreateAccount(ctx, scope.Global.String(), oidcAcct1)
+	oidcAcct1.PublicId = "acctoidc_0123456789"
+	num, err = rw.Exec(ctx, `
+insert into auth_oidc_account
+	(public_id, auth_method_id, scope_id, issuer, subject)
+values
+	(?, ?, ?, ?, ?)
+	`, []any{oidcAcct1.PublicId, oidcAm1.PublicId, oidcAm1.ScopeId, oidcAm1.Issuer, oidcAcct1.Subject})
 	require.NoError(t, err)
+	assert.Equal(t, 1, num)
+
 	oidcAm2 := oidc.TestAuthMethod(t, conn, databaseWrapper, scope.Global.String(), oidc.InactiveState, "alice_rp2",
 		"my-dogs-name", oidc.WithIssuer(oidc.TestConvertToUrls(t, "https://alice2.com")[0]), oidc.WithApiUrl(oidc.TestConvertToUrls(t, "https://api2.com")[0]))
+	oidcAm2.PublicId = "amoidc_0123456789"
+	num, err = rw.Exec(ctx, `
+	insert into auth_oidc_method
+		(public_id, scope_id, state, key_id)
+	values
+		(?, ?, ?, ?)
+		`, []any{oidcAm2.PublicId, oidcAm2.ScopeId, oidcAm2.OperationalState, oidcAm2.KeyId})
+	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 	oidcAcct2, err := oidc.NewAccount(ctx, oidcAm2.GetPublicId(), "oidcAcct2")
 	require.NoError(t, err)
-	oidcAcct2, err = oidcRepo.CreateAccount(ctx, scope.Global.String(), oidcAcct2)
+	oidcAcct2.PublicId = "acctoidc_1234567890"
+	num, err = rw.Exec(ctx, `
+insert into auth_oidc_account
+	(public_id, auth_method_id, scope_id, issuer, subject)
+values
+	(?, ?, ?, ?, ?)
+	`, []any{oidcAcct2.PublicId, oidcAm2.PublicId, oidcAm2.ScopeId, oidcAm2.Issuer, oidcAcct2.Subject})
 	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 
-	_, err = iamRepo.AddUserAccounts(ctx, usr.GetPublicId(), usr.GetVersion(),
-		[]string{
-			pwAcct1.GetPublicId(), pwAcct2.GetPublicId(),
-			oidcAcct1.GetPublicId(), oidcAcct2.GetPublicId(),
-		})
+	num, err = rw.Exec(ctx, `
+update auth_account set 
+	iam_user_id=?
+where
+	public_id in (?, ?, ?, ?)
+	`, []any{
+		usr.PublicId,
+		pwAcct1.PublicId, pwAcct2.PublicId, oidcAcct1.PublicId, oidcAcct2.PublicId,
+	})
 	require.NoError(t, err)
+	assert.Equal(t, 4, num)
 	usr, accts, err := iamRepo.LookupUser(ctx, usr.GetPublicId())
 	require.NoError(t, err)
 	assert.ElementsMatch(t, []string{
@@ -129,10 +219,20 @@ func TestMigrations_DeleteOrphanedAccounts(t *testing.T) {
 		oidcAcct1.GetPublicId(), oidcAcct2.GetPublicId(),
 	}, accts)
 
-	_, err = pwRepo.DeleteAccount(ctx, scope.Global.String(), pwAcct1.GetPublicId())
+	num, err = rw.Exec(ctx, `
+delete from auth_password_account
+where
+	public_id=?
+	`, []any{pwAcct1.PublicId})
 	require.NoError(t, err)
-	_, err = oidcRepo.DeleteAccount(ctx, scope.Global.String(), oidcAcct1.GetPublicId())
+	assert.Equal(t, 1, num)
+	num, err = rw.Exec(ctx, `
+delete from auth_oidc_account
+where
+	public_id=?
+	`, []any{oidcAcct1.PublicId})
 	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 
 	// pwAcct1 is still listed as an account for this user!  oh no!
 	_, accts, err = iamRepo.LookupUser(ctx, usr.GetPublicId())
@@ -170,13 +270,23 @@ func TestMigrations_DeleteOrphanedAccounts(t *testing.T) {
 	require.NoError(t, err)
 	assert.ElementsMatch(t, []string{pwAcct2.GetPublicId(), oidcAcct2.GetPublicId()}, accts)
 
-	_, err = pwRepo.DeleteAccount(ctx, scope.Global.String(), pwAcct2.GetPublicId())
+	num, err = rw.Exec(ctx, `
+delete from auth_password_account
+where
+	public_id=?
+	`, []any{pwAcct2.PublicId})
 	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 	_, accts, err = iamRepo.LookupUser(ctx, usr.GetPublicId())
 	require.NoError(t, err)
 	assert.ElementsMatch(t, []string{oidcAcct2.GetPublicId()}, accts)
-	_, err = oidcRepo.DeleteAccount(ctx, scope.Global.String(), oidcAcct2.GetPublicId())
+	num, err = rw.Exec(ctx, `
+delete from auth_oidc_account
+where
+	public_id=?
+	`, []any{oidcAcct2.PublicId})
 	require.NoError(t, err)
+	assert.Equal(t, 1, num)
 
 	// Deleting the account now removes the association with the user
 	_, accts, err = iamRepo.LookupUser(ctx, usr.GetPublicId())
