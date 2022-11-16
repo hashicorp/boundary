80e55991906a9428055d7ef4cf0ab5b417335581
irenarindos <irena.rindos@gmail.com>
2022-11-16 15:05:26 -0500

    fix failing tests

--- internal/server/testing.go
+++ internal/server/testing.go
@@ -113,7 +113,7 @@ func TestKmsWorker(t *testing.T, conn *db.DB, wrapper wrapping.Wrapper, opt ...O
 		address = opts.withAddress
 	}
 	versionInfo := version.Get()
-	relVer := versionInfo.VersionNumber()
+	relVer := versionInfo.FullVersionNumber(false)
 	wrk := NewWorker(scope.Global.String(),
 		WithName(name),
 		WithAddress(address),
