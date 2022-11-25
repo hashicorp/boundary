df02501dcc63349137a0bd1fc93384f953e1edfc
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2022-11-24 19:07:37 -0600

    Create new "previous-root" key purpose for root key migrations (#2639) (#2648)
    * Update go-kms-wrapping dependency
    
    * Apply changes suggested by Jeff
    
    * Fix listener test
    
    * Add note about config fix to CHANGELOG
    
    * Review comments
    
    Co-authored-by: Dan Heath <76443935+Dan-Heath@users.noreply.github.com>
    
    Co-authored-by: Dan Heath <76443935+Dan-Heath@users.noreply.github.com>

--- globals/kms.go
+++ globals/kms.go
@@ -2,6 +2,7 @@ package globals
 
 const (
 	KmsPurposeRoot              = "root"
+	KmsPurposePreviousRoot      = "previous-root"
 	KmsPurposeWorkerAuth        = "worker-auth"
 	KmsPurposeWorkerAuthStorage = "worker-auth-storage"
 	KmsPurposeRecovery          = "recovery"
