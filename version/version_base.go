7cd5b5a320cb9dabc53952ca02fbee61480b89a1
emily neil <63985869+emilymianeil@users.noreply.github.com>
2022-12-09 13:08:02 -0800

    Onboard boundary to use new version/VERSION file and reproducible actions-go-build (#2662)
    * Onboard boundary to use new .release/VERSION file and reproducible actions-go-build
    
    * Added automatic patch version bumps to end of release pipeline
    
    * Added back VersionMetadata to internal directory
    
    * Moved VERSION file to version/
    
    * Update to current version
    
    * Using prepare workflow for pre-stable channel workflow
    
    * Test prepare workflow
    
    * PR feedback: allow builds to run builds locally without setting BIN_PATH
    
    * PR feedback: revert Enos changes
    
    * PR Feedback
    
    * Remove current branch from ci pipeline
    
    * Run make fmt

--- version/version_base.go
+++ version/version_base.go
@@ -1,17 +1,20 @@
 package version
 
 var (
-	// The git commit that was compiled. This will be filled in by the compiler.
+	// GitCommit is the git commit that was compiled. This will be filled in by the compiler.
 	GitCommit   string
 	GitDescribe string
 
-	// Whether cgo is enabled or not; set at build time
+	// CgoEnabled is whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
+	// Version is the base version
+	// Default values - set when building locally (at build time)
 	Version = "0.12.0"
 
 	// VersionPrerelease is also set at compile time, similarly to Version.
 	VersionPrerelease = ""
 
+	// VersionMetadata is also set at compile time.
 	VersionMetadata = ""
 )
