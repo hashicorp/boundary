3e4045a2e22d29ada499d125e3130c46cc053310
Jim <jlambert@hashicorp.com>
2022-12-11 07:53:39 -0500

    tests (kms): add MockWrapper and MockGetWrapperer
    Make it a bit easier to write unhappy path tests which depend on
    a Wrapper or GetWrapperer

--- internal/kms/testing.go
+++ internal/kms/testing.go
@@ -39,3 +39,92 @@ func TestKmsDeleteAllKeys(t testing.TB, conn *db.DB) {
 type dataKey struct{}
 
 func (*dataKey) TableName() string { return "kms_data_key" }
+
+// MockGetWrapperer provides a mock for returning a set of mock values for a
+// GetWrapperer
+type MockGetWrapperer struct {
+	// Kms is the underlying kms which is used to provide the mock's default
+	// behavior
+	Kms *Kms
+
+	// GetErr is a mock value to return for the GetWrapper(...) operation
+	GetErr error
+
+	// ReturnWrapper is a mock value to return for the GetWrapper(...) operation
+	ReturnWrapper wrapping.Wrapper
+}
+
+// GetWrapper returns a wrapper for a given scope and purpose.  Supports mock
+// values: ReturnWrapper, GetErr
+func (m *MockGetWrapperer) GetWrapper(ctx context.Context, scopeId string, purpose KeyPurpose, opt ...Option) (wrapping.Wrapper, error) {
+	switch {
+	case m.ReturnWrapper != nil:
+		return m.ReturnWrapper, nil
+	case m.GetErr != nil:
+		return nil, m.GetErr
+	default:
+		return m.Kms.GetWrapper(ctx, scopeId, purpose, opt...)
+	}
+}
+
+// MockWrapper provides a mock for returning a set of mock values for a Wrapper
+type MockWrapper struct {
+	// Wrapper is the underlying wrapping.Wrapper which is used to provide the
+	// mock's default behavior
+	Wrapper wrapping.Wrapper
+
+	// EncryptErr is a mock value to return for the Encrypt(...) operation
+	EncryptErr error
+
+	// DecryptErr is a mock value to return for the Decrypt(...) operation
+	DecryptErr error
+
+	// KeyIdErr is a mock value to return for the KeyId(...) operation
+	KeyIdErr error
+
+	// KeyIdReturned is a mock value to return for the KeyId(...) operation
+	KeyIdReturned string
+}
+
+// Type of the wrapper.  No mock values supported
+func (w *MockWrapper) Type(ctx context.Context) (wrapping.WrapperType, error) {
+	return w.Wrapper.Type(ctx)
+}
+
+// KeyId is the id of the key currently used for encryption operations. Mock
+// values supported: KeyIdErr, KeyIdReturned
+func (w *MockWrapper) KeyId(ctx context.Context) (string, error) { // nolint
+	switch {
+	case w.KeyIdErr != nil:
+		return "", w.KeyIdErr
+	case w.KeyIdReturned != "":
+		return w.KeyIdReturned, nil
+	default:
+		return w.Wrapper.KeyId(ctx)
+	}
+}
+
+// SetConfig applies the given options to a wrapper and returns
+// configuration information.  No mock values supported.
+func (w *MockWrapper) SetConfig(ctx context.Context, options ...wrapping.Option) (*wrapping.WrapperConfig, error) {
+	return w.Wrapper.SetConfig(ctx, options...)
+}
+
+// / Encrypt encrypts the given byte slice and stores the resulting information
+// in the returned blob info.  Mock values supported: EncryptErr
+func (w *MockWrapper) Encrypt(ctx context.Context, plaintext []byte, options ...wrapping.Option) (*wrapping.BlobInfo, error) {
+	if w.EncryptErr != nil {
+		return nil, w.EncryptErr
+	}
+
+	return w.Wrapper.Encrypt(ctx, plaintext, options...)
+}
+
+// Decrypt decrypts the given byte slice and stores the resulting information in
+// the returned byte slice. Mock values supported: DecryptErr
+func (w *MockWrapper) Decrypt(ctx context.Context, ciphertext *wrapping.BlobInfo, options ...wrapping.Option) ([]byte, error) {
+	if w.DecryptErr != nil {
+		return nil, w.DecryptErr
+	}
+	return w.Wrapper.Decrypt(ctx, ciphertext, options...)
+}
