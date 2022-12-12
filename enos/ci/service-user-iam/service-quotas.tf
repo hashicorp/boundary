d3e93914ab3c8fe9a7107b7483ed0f5624da8cdb
Mike Baum <mike.baum@hashicorp.com>
2022-12-12 13:16:29 -0500

    [QT-396] Address review comments

--- enos/ci/service-user-iam/service-quotas.tf
+++ enos/ci/service-user-iam/service-quotas.tf
@@ -1,9 +1,11 @@
 locals {
+  // This is the code of the service quota to request a change for. Each adjustable limit has a
+  // unique code. See, https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/servicequotas_service_quota#quota_code
   subnets_per_vpc          = "L-F678F1CE"
   rds_max_db_subnet_groups = "L-48C6BF61"
 }
 
-resource "aws_servicequotas_service_quota" "vpcs_pre_region_us_east_1" {
+resource "aws_servicequotas_service_quota" "vpcs_per_region_us_east_1" {
   provider     = aws.us_east_1
   quota_code   = local.subnets_per_vpc
   service_code = "vpc"
@@ -17,7 +19,7 @@ resource "aws_servicequotas_service_quota" "rds_subnet_groups_us_east_1" {
   value        = 50
 }
 
-resource "aws_servicequotas_service_quota" "vpcs_pre_region_us_east_2" {
+resource "aws_servicequotas_service_quota" "vpcs_per_region_us_east_2" {
   provider     = aws.us_east_2
   quota_code   = local.subnets_per_vpc
   service_code = "vpc"
@@ -31,7 +33,7 @@ resource "aws_servicequotas_service_quota" "rds_subnet_groups_us_east_2" {
   value        = 50
 }
 
-resource "aws_servicequotas_service_quota" "vpcs_pre_region_us_west_1" {
+resource "aws_servicequotas_service_quota" "vpcs_per_region_us_west_1" {
   provider     = aws.us_west_1
   quota_code   = local.subnets_per_vpc
   service_code = "vpc"
@@ -45,7 +47,7 @@ resource "aws_servicequotas_service_quota" "rds_subnet_groups_us_west_1" {
   value        = 50
 }
 
-resource "aws_servicequotas_service_quota" "vpcs_pre_region_us_west_2" {
+resource "aws_servicequotas_service_quota" "vpcs_per_region_us_west_2" {
   provider     = aws.us_west_2
   quota_code   = local.subnets_per_vpc
   service_code = "vpc"
