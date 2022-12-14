2d4442569cba93fd1ddcde17854809f1b2f3de0f
Jim <jlambert@hashicorp.com>
2022-12-14 14:29:54 -0500

    feature (auth/ldap): define AuthMethod and all its value objects (#2703)
    * feature (auth/ldap): storage protos
    * feature (auth/ldap): define AuthMethod and all its value objects

---
+++ internal/auth/ldap/bind_credential_test.go
@@ -0,0 +1,280 @@
+package ldap
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/boundary/internal/auth/ldap/store"
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/kms"
+	wrapping "github.com/hashicorp/go-kms-wrapping/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestNewBindCredential(t *testing.T) {
+	t.Parallel()
+	testCtx := context.Background()
+	tests := []struct {
+		name            string
+		ctx             context.Context
+		authMethodId    string
+		dn              string
+		password        []byte
+		want            *BindCredential
+		wantErr         bool
+		wantErrCode     errors.Code
+		wantErrContains string
+	}{
+		{
+			name:         "valid",
+			ctx:          testCtx,
+			authMethodId: "test-id",
+			dn:           "dn",
+			password:     []byte("password"),
+			want: &BindCredential{
+				BindCredential: &store.BindCredential{
+					LdapMethodId: "test-id",
+					Dn:           "dn",
+					Password:     []byte("password"),
+				},
+			},
+		},
+		{
+			name:            "missing-auth-method-id",
+			ctx:             testCtx,
+			dn:              "dn",
+			password:        []byte("password"),
+			wantErr:         true,
+			wantErrCode:     errors.InvalidParameter,
+			wantErrContains: "missing auth method id",
+		},
+		{
+			name:            "missing-dn",
+			ctx:             testCtx,
+			authMethodId:    "test-id",
+			password:        []byte("password"),
+			wantErr:         true,
+			wantErrCode:     errors.InvalidParameter,
+			wantErrContains: "missing dn",
+		},
+		{
+			name:            "missing-password",
+			ctx:             testCtx,
+			authMethodId:    "test-id",
+			dn:              "dn",
+			wantErr:         true,
+			wantErrCode:     errors.InvalidParameter,
+			wantErrContains: "missing password",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			got, err := NewBindCredential(tc.ctx, tc.authMethodId, tc.dn, tc.password)
+			if tc.wantErr {
+				require.Error(err)
+				assert.Nil(got)
+				if tc.wantErrCode != errors.Unknown {
+					assert.True(errors.Match(errors.T(tc.wantErrCode), err))
+				}
+				if tc.wantErrContains != "" {
+					assert.Contains(err.Error(), tc.wantErrContains)
+				}
+				return
+			}
+			require.NoError(err)
+			assert.Equal(tc.want, got)
+		})
+	}
+}
+
+func TestBindCredential_SetTableName(t *testing.T) {
+	t.Parallel()
+	defaultTableName := bindCredentialTableName
+	tests := []struct {
+		name      string
+		setNameTo string
+		want      string
+	}{
+		{
+			name:      "new-name",
+			setNameTo: "new-name",
+			want:      "new-name",
+		},
+		{
+			name:      "reset to default",
+			setNameTo: "",
+			want:      defaultTableName,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			def := allocBindCredential()
+			require.Equal(defaultTableName, def.TableName())
+			m := allocBindCredential()
+			m.SetTableName(tt.setNameTo)
+			assert.Equal(tt.want, m.TableName())
+		})
+	}
+}
+
+func TestBindCredential_clone(t *testing.T) {
+	t.Parallel()
+	testCtx := context.Background()
+	t.Run("valid", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		bc, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password"))
+		require.NoError(err)
+		cp := bc.clone()
+		assert.True(proto.Equal(cp.BindCredential, bc.BindCredential))
+	})
+	t.Run("not-equal", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		bc, err := NewBindCredential(testCtx, "test-id", "dn2", []byte("password"))
+		require.NoError(err)
+
+		bc2, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password2"))
+		require.NoError(err)
+
+		cp := bc.clone()
+		assert.True(!proto.Equal(cp.BindCredential, bc2.BindCredential))
+	})
+}
+
+func TestBindCredential_encrypt_decrypt(t *testing.T) {
+	t.Parallel()
+	testCtx := context.Background()
+	testWrapper := db.TestWrapper(t)
+	tests := []struct {
+		name            string
+		ctx             context.Context
+		cipher          wrapping.Wrapper
+		bc              *BindCredential
+		wantErr         bool
+		wantErrCode     errors.Code
+		wantErrContains string
+	}{
+		{
+			name:   "valid",
+			ctx:    testCtx,
+			cipher: testWrapper,
+			bc: func() *BindCredential {
+				testBindCred, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password"))
+				require.NoError(t, err)
+				return testBindCred
+			}(),
+		},
+		{
+			name: "missing-cipher",
+			ctx:  testCtx,
+			bc: func() *BindCredential {
+				testBindCred, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password"))
+				require.NoError(t, err)
+				return testBindCred
+			}(),
+			wantErr:         true,
+			wantErrCode:     errors.InvalidParameter,
+			wantErrContains: "missing cipher",
+		},
+		{
+			name: "encrypt-err",
+			ctx:  testCtx,
+			cipher: &kms.MockWrapper{
+				Wrapper:    testWrapper,
+				EncryptErr: fmt.Errorf("test encrypt error"),
+			},
+			bc: func() *BindCredential {
+				testBindCred, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password"))
+				require.NoError(t, err)
+				return testBindCred
+			}(),
+			wantErr:         true,
+			wantErrCode:     errors.Encrypt,
+			wantErrContains: "test encrypt error",
+		},
+		{
+			name: "keyId-err",
+			ctx:  testCtx,
+			cipher: &kms.MockWrapper{
+				Wrapper:  testWrapper,
+				KeyIdErr: fmt.Errorf("test key id error"),
+			},
+			bc: func() *BindCredential {
+				testBindCred, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password"))
+				require.NoError(t, err)
+				return testBindCred
+			}(),
+			wantErr:         true,
+			wantErrCode:     errors.Encrypt,
+			wantErrContains: "test key id error",
+		},
+		{
+			name: "unknown-wrapper-type-err",
+			ctx:  testCtx,
+			cipher: &kms.MockWrapper{
+				Wrapper: testWrapper,
+			},
+			bc: func() *BindCredential {
+				testBindCred, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password"))
+				require.NoError(t, err)
+				return testBindCred
+			}(),
+			wantErr:         true,
+			wantErrCode:     errors.Encrypt,
+			wantErrContains: "failed to hmac password",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			require.Empty(tc.bc.CtPassword)
+			require.Empty(tc.bc.PasswordHmac)
+
+			err := tc.bc.encrypt(tc.ctx, tc.cipher)
+			if tc.wantErr {
+				require.Error(err)
+				if tc.wantErrCode != errors.Unknown {
+					assert.True(errors.Match(errors.T(tc.wantErrCode), err))
+				}
+				if tc.wantErrContains != "" {
+					assert.Contains(err.Error(), tc.wantErrContains)
+				}
+				return
+			}
+			require.NoError(err)
+			assert.NotEmpty(tc.bc.GetPasswordHmac())
+			assert.NotEmpty(tc.bc.GetCtPassword())
+
+			origPass := make([]byte, len(tc.bc.Password))
+			copy(origPass, tc.bc.Password)
+			tc.bc.Password = nil
+			require.NoError(tc.bc.decrypt(tc.ctx, tc.cipher))
+			assert.NotEmpty(tc.bc.Password)
+			assert.Equal(origPass, tc.bc.Password)
+		})
+	}
+	t.Run("decrypt-missing-cipher", func(t *testing.T) {
+		testBindCred, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password"))
+		require.NoError(t, err)
+		err = testBindCred.decrypt(testCtx, nil)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "missing cipher")
+	})
+	t.Run("decrypt-err", func(t *testing.T) {
+		w := &kms.MockWrapper{
+			Wrapper:    testWrapper,
+			DecryptErr: fmt.Errorf("test decrypt error"),
+		}
+		testBindCred, err := NewBindCredential(testCtx, "test-id", "dn", []byte("password"))
+		require.NoError(t, err)
+		require.NoError(t, testBindCred.encrypt(testCtx, testWrapper))
+		err = testBindCred.decrypt(testCtx, w)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "test decrypt error")
+	})
+}
---
+++ internal/auth/ldap/client_certificate.go
@@ -0,0 +1,117 @@
+package ldap
+
+import (
+	"context"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+
+	"github.com/hashicorp/boundary/internal/auth/ldap/store"
+	"github.com/hashicorp/boundary/internal/errors"
+	wrapping "github.com/hashicorp/go-kms-wrapping/v2"
+	"github.com/hashicorp/go-kms-wrapping/v2/extras/structwrapping"
+	"google.golang.org/protobuf/proto"
+)
+
+const clientCertificateTableName = "auth_ldap_client_certificate"
+
+// ClientCertificate represents a set of optional configuration fields used for
+// specifying a mTLS client cert for LDAP connections. ClientCertificates are
+// value objects of an AuthMethod, therefore there's no need for oplog metadata,
+// since only the AuthMethod will have metadata because it's the root aggregate.
+type ClientCertificate struct {
+	*store.ClientCertificate
+	tableName string
+}
+
+// NewClientCertificate creates a new in memory ClientCertificate. No options
+// are currently supported.  PrivKey must be in PKCS #8, ASN.1 DER form. certPem
+// must be in ASN.1 DER form encoded as PEM.
+func NewClientCertificate(ctx context.Context, authMethodId string, privKey []byte, certPem string, _ ...Option) (*ClientCertificate, error) {
+	const op = "ldap.NewClientCertificate"
+	switch {
+	case authMethodId == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing auth method id")
+	case len(privKey) == 0:
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing key")
+	case certPem == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing certificate")
+	}
+	if _, err := x509.ParsePKCS8PrivateKey(privKey); err != nil {
+		return nil, errors.Wrap(ctx, err, op, errors.WithMsg("failed to parse key in PKCS #8, ASN.1 DER form"), errors.WithCode(errors.InvalidParameter))
+	}
+	blk, _ := pem.Decode([]byte(certPem))
+	if blk == nil {
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "failed to parse certificate: invalid PEM encoding")
+	}
+	if _, err := x509.ParseCertificate(blk.Bytes); err != nil {
+		return nil, errors.New(ctx, errors.InvalidParameter, op, fmt.Sprintf("failed to parse certificate: invalid block: %s", err.Error()), errors.WithWrap(err))
+	}
+	return &ClientCertificate{
+		ClientCertificate: &store.ClientCertificate{
+			LdapMethodId:   authMethodId,
+			CertificateKey: privKey,
+			Certificate:    []byte(certPem),
+		},
+	}, nil
+}
+
+// allocClientCertificate makes an empty one in memory
+func allocClientCertificate() *ClientCertificate {
+	return &ClientCertificate{
+		ClientCertificate: &store.ClientCertificate{},
+	}
+}
+
+// clone a ClientCertificate
+func (cc *ClientCertificate) clone() *ClientCertificate {
+	cp := proto.Clone(cc.ClientCertificate)
+	return &ClientCertificate{
+		ClientCertificate: cp.(*store.ClientCertificate),
+	}
+}
+
+// TableName returns the table name
+func (cc *ClientCertificate) TableName() string {
+	if cc.tableName != "" {
+		return cc.tableName
+	}
+	return clientCertificateTableName
+}
+
+// SetTableName sets the table name.
+func (cc *ClientCertificate) SetTableName(n string) {
+	cc.tableName = n
+}
+
+// encrypt the client certificate before writing it to the database
+func (cc *ClientCertificate) encrypt(ctx context.Context, cipher wrapping.Wrapper) error {
+	const op = "ldap.(ClientCertificate).encrypt"
+	if cipher == nil {
+		return errors.New(ctx, errors.InvalidParameter, op, "missing cipher")
+	}
+	if err := structwrapping.WrapStruct(ctx, cipher, cc.ClientCertificate); err != nil {
+		return errors.Wrap(ctx, err, op, errors.WithCode(errors.Encrypt))
+	}
+	var err error
+	if cc.KeyId, err = cipher.KeyId(ctx); err != nil {
+		return errors.Wrap(ctx, err, op, errors.WithCode(errors.Encrypt), errors.WithMsg("failed to read cipher key id"))
+	}
+	if cc.CertificateKeyHmac, err = hmacField(ctx, cipher, cc.CertificateKey, cc.LdapMethodId); err != nil {
+		return errors.Wrap(ctx, err, op, errors.WithCode(errors.Encrypt), errors.WithMsg("failed to hmac client certificate"))
+	}
+
+	return nil
+}
+
+// decrypt the client certificate after reading it from the database
+func (cc *ClientCertificate) decrypt(ctx context.Context, cipher wrapping.Wrapper) error {
+	const op = "ldap.(ClientCertificate).decrypt"
+	if cipher == nil {
+		return errors.New(ctx, errors.InvalidParameter, op, "missing cipher")
+	}
+	if err := structwrapping.UnwrapStruct(ctx, cipher, cc.ClientCertificate); err != nil {
+		return errors.Wrap(ctx, err, op, errors.WithCode(errors.Decrypt))
+	}
+	return nil
+}
