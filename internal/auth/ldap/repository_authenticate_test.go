e6ab9741565fa0d7bd19cc61eb18ff446f7a7845
Jim <jlambert@hashicorp.com>
2022-12-27 15:06:37 -0500

    feature (auth/ldap): add Authenticate(...)

---
+++ internal/auth/ldap/repository_authenticate_test.go
@@ -0,0 +1,295 @@
+package ldap
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/boundary/internal/auth/ldap/store"
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/iam"
+	"github.com/hashicorp/boundary/internal/kms"
+	"github.com/hashicorp/cap/ldap"
+	"github.com/hashicorp/go-hclog"
+	"github.com/jimlambrt/gldap"
+	"github.com/jimlambrt/gldap/testdirectory"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestRepository_authenticate(t *testing.T) {
+	t.Parallel()
+	testCtx := context.Background()
+
+	rootWrapper := db.TestWrapper(t)
+
+	testConn, _ := db.TestSetup(t, "postgres")
+	testRw := db.New(testConn)
+
+	testKms := kms.TestKms(t, testConn, rootWrapper)
+
+	testRepo, err := NewRepository(testCtx, testRw, testRw, testKms)
+	require.NoError(t, err)
+
+	iamRepo := iam.TestRepo(t, testConn, rootWrapper)
+	org, _ := iam.TestScopes(t, iamRepo)
+	orgDbWrapper, err := testKms.GetWrapper(testCtx, org.PublicId, kms.KeyPurposeDatabase)
+	require.NoError(t, err)
+
+	logger := hclog.New(&hclog.LoggerOptions{
+		Name:  "test-logger",
+		Level: hclog.Error,
+	})
+	td := testdirectory.Start(t,
+		testdirectory.WithDefaults(t, &testdirectory.Defaults{AllowAnonymousBind: true}),
+		testdirectory.WithLogger(t, logger),
+	)
+	tdCerts, err := ParseCertificates(testCtx, td.Cert())
+	require.NoError(t, err)
+
+	testAm := TestAuthMethod(t, testConn, orgDbWrapper, org.PublicId,
+		[]string{fmt.Sprintf("ldaps://127.0.0.1:%d", td.Port())},
+		WithCertificates(testCtx, tdCerts...),
+		WithDiscoverDn(testCtx),
+		WithEnableGroups(testCtx),
+		WithUserDn(testCtx, testdirectory.DefaultUserDN),
+		WithGroupDn(testCtx, testdirectory.DefaultGroupDN),
+	)
+
+	const (
+		testLoginName = "alice"
+		testPassword  = "password"
+	)
+
+	testAccount := TestAccount(t, testConn, testAm, testLoginName)
+
+	groups := []*gldap.Entry{
+		testdirectory.NewGroup(t, "admin", []string{"alice"}),
+		testdirectory.NewGroup(t, "admin", []string{"eve"}, testdirectory.WithDefaults(t, &testdirectory.Defaults{UPNDomain: "example.com"})),
+	}
+	tokenGroups := map[string][]*gldap.Entry{
+		"S-1-1": {
+			testdirectory.NewGroup(t, "admin-token-group", []string{"alice"}),
+		},
+	}
+	sidBytes, err := ldap.SIDBytes(1, 1)
+	require.NoError(t, err)
+	users := testdirectory.NewUsers(t, []string{"alice", "bob"}, testdirectory.WithMembersOf(t, "admin"), testdirectory.WithTokenGroups(t, sidBytes))
+	users = append(
+		users,
+		testdirectory.NewUsers(
+			t,
+			[]string{"eve"},
+			testdirectory.WithDefaults(t, &testdirectory.Defaults{UPNDomain: "example.com"}),
+			testdirectory.WithMembersOf(t, "admin"))...,
+	)
+	// add some attributes that we always want to filter out of an AuthResult,
+	// so if we ever start seeing tests fail because of them; we know that we've
+	// messed up the default filtering
+	for _, u := range users {
+		u.Attributes = append(u.Attributes,
+			gldap.NewEntryAttribute(ldap.DefaultADUserPasswordAttribute, []string{"password"}),
+			gldap.NewEntryAttribute(ldap.DefaultOpenLDAPUserPasswordAttribute, []string{"password"}),
+			gldap.NewEntryAttribute("fullName", []string{"test-full-name"}),
+		)
+	}
+	td.SetUsers(users...)
+	td.SetGroups(groups...)
+	td.SetTokenGroups(tokenGroups)
+
+	tests := []struct {
+		name            string
+		ctx             context.Context
+		repo            *Repository
+		authMethodId    string
+		loginName       string
+		password        string
+		want            func(got *Account) *Account
+		wantErrMatch    *errors.Template
+		wantErrContains string
+	}{
+		{
+			name:         "success-existing-account",
+			ctx:          testCtx,
+			repo:         testRepo,
+			authMethodId: testAm.PublicId,
+			loginName:    testAccount.LoginName,
+			password:     testPassword,
+			want: func(got *Account) *Account {
+				a := &Account{Account: &store.Account{
+					AuthMethodId:   testAm.PublicId,
+					ScopeId:        testAccount.ScopeId,
+					PublicId:       testAccount.PublicId,
+					Version:        testAccount.Version,
+					Dn:             "cn=alice,ou=people,dc=example,dc=org",
+					Email:          "alice@example.com",
+					FullName:       "test-full-name",
+					LoginName:      "alice",
+					MemberOfGroups: "[\"cn=admin,ou=groups,dc=example,dc=org\"]",
+				}}
+				return a
+			},
+		},
+		{
+			name:         "success-new-account-with-no-groups",
+			ctx:          testCtx,
+			repo:         testRepo,
+			authMethodId: testAm.PublicId,
+			loginName:    "bob",
+			password:     testPassword,
+			want: func(got *Account) *Account {
+				a := &Account{Account: &store.Account{
+					AuthMethodId: testAm.PublicId,
+					ScopeId:      testAm.ScopeId,
+					PublicId:     got.PublicId,
+					Version:      got.Version,
+					Dn:           "cn=bob,ou=people,dc=example,dc=org",
+					Email:        "bob@example.com",
+					FullName:     "test-full-name",
+					LoginName:    "bob",
+				}}
+				return a
+			},
+		},
+		{
+			name:            "missing-auth-method-id",
+			ctx:             testCtx,
+			repo:            testRepo,
+			loginName:       "alice",
+			password:        testPassword,
+			wantErrMatch:    errors.T(errors.InvalidParameter),
+			wantErrContains: "missing auth method id",
+		},
+		{
+			name:            "missing-login-name",
+			ctx:             testCtx,
+			repo:            testRepo,
+			authMethodId:    testAm.PublicId,
+			password:        testPassword,
+			wantErrMatch:    errors.T(errors.InvalidParameter),
+			wantErrContains: "missing login name",
+		},
+		{
+			name:            "missing-password",
+			ctx:             testCtx,
+			repo:            testRepo,
+			authMethodId:    testAm.PublicId,
+			loginName:       "alice",
+			wantErrMatch:    errors.T(errors.InvalidParameter),
+			wantErrContains: "missing password",
+		},
+		{
+			name:            "auth-method-id-not-found",
+			ctx:             testCtx,
+			repo:            testRepo,
+			authMethodId:    "auth-method-id-not-found",
+			loginName:       "alice",
+			password:        testPassword,
+			wantErrMatch:    errors.T(errors.RecordNotFound),
+			wantErrContains: "auth method id \"auth-method-id-not-found\" not found",
+		},
+		{
+			name: "auth-method-id-lookup-err",
+			ctx:  testCtx,
+			repo: func() *Repository {
+				conn, mock := db.TestSetupWithMock(t)
+				mock.ExpectQuery(`SELECT`).WillReturnError(fmt.Errorf("auth-method-id-lookup-err"))
+				rw := db.New(conn)
+				r, err := NewRepository(testCtx, rw, rw, testKms)
+				require.NoError(t, err)
+				return r
+			}(),
+			authMethodId:    testAm.PublicId,
+			loginName:       "alice",
+			password:        testPassword,
+			wantErrMatch:    errors.T(errors.Unknown),
+			wantErrContains: "auth-method-id-lookup-err",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			got, err := tc.repo.Authenticate(tc.ctx, tc.authMethodId, tc.loginName, tc.password)
+			if tc.wantErrMatch != nil {
+				require.Error(err)
+				assert.Nil(got)
+				assert.Truef(errors.Match(tc.wantErrMatch, err), "unexpected error: %s", err.Error())
+				if tc.wantErrContains != "" {
+					assert.Contains(err.Error(), tc.wantErrContains)
+				}
+				return
+			}
+			require.NoError(err)
+			require.NotNil(tc.want)
+			assert.NotEmpty(got.UpdateTime)
+			assert.NotEmpty(got.CreateTime)
+			w := tc.want(got)
+			w.UpdateTime = got.UpdateTime
+			w.CreateTime = got.CreateTime
+			assert.Empty(cmp.Diff(w, got, protocmp.Transform()))
+		})
+	}
+	t.Run("use-token-groups", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+		amWithTokenGroups := TestAuthMethod(t, testConn, orgDbWrapper, org.PublicId,
+			[]string{fmt.Sprintf("ldaps://127.0.0.1:%d", td.Port())},
+			WithCertificates(testCtx, tdCerts...),
+			WithDiscoverDn(testCtx),
+			WithEnableGroups(testCtx),
+			WithUserDn(testCtx, testdirectory.DefaultUserDN),
+			WithUseTokenGroups(testCtx),
+		)
+
+		got, err := testRepo.Authenticate(testCtx, amWithTokenGroups.PublicId, testLoginName, testPassword)
+		require.NoError(err)
+		assert.NotNil(got)
+		assert.NotEmpty(got.UpdateTime)
+		assert.NotEmpty(got.CreateTime)
+		w := &Account{Account: &store.Account{
+			AuthMethodId:   amWithTokenGroups.PublicId,
+			ScopeId:        amWithTokenGroups.ScopeId,
+			PublicId:       got.PublicId,
+			Version:        got.Version,
+			Dn:             "cn=alice,ou=people,dc=example,dc=org",
+			Email:          "alice@example.com",
+			FullName:       "test-full-name",
+			LoginName:      "alice",
+			MemberOfGroups: "[\"cn=admin-token-group,ou=groups,dc=example,dc=org\"]",
+		}}
+		w.UpdateTime = got.UpdateTime
+		w.CreateTime = got.CreateTime
+		assert.Empty(cmp.Diff(w, got, protocmp.Transform()))
+	})
+	t.Run("authenticate-err", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+
+		amWithNoCerts := TestAuthMethod(t, testConn, orgDbWrapper, org.PublicId,
+			[]string{fmt.Sprintf("ldaps://127.0.0.1:%d", td.Port())},
+			WithDiscoverDn(testCtx),
+			WithEnableGroups(testCtx),
+			WithUserDn(testCtx, testdirectory.DefaultUserDN),
+			WithGroupDn(testCtx, testdirectory.DefaultGroupDN),
+		)
+		got, err := testRepo.Authenticate(testCtx, amWithNoCerts.PublicId, testLoginName, testPassword)
+		require.Error(err)
+		assert.Contains(err.Error(), "authenticate failed")
+		assert.Contains(err.Error(), "failed to connect")
+		assert.Nil(got)
+	})
+}
+
+func Test_caseInsensitiveAttributeSearch(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+
+	found, values := caseInsensitiveAttributeSearch("fullName", map[string][]string{"fullName": {"eve"}})
+	assert.True(found)
+	assert.Equal([]string{"eve"}, values)
+
+	found, values = caseInsensitiveAttributeSearch("preferredName", map[string][]string{"fullName": {"eve"}})
+	assert.False(found)
+	assert.Empty(values)
+}
---
+++ internal/auth/ldap/service_authenticate_test.go
@@ -0,0 +1,331 @@
+package ldap
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/boundary/internal/authtoken"
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/iam"
+	"github.com/hashicorp/boundary/internal/kms"
+	"github.com/hashicorp/go-hclog"
+	"github.com/jimlambrt/gldap"
+	"github.com/jimlambrt/gldap/testdirectory"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAuthenticate(t *testing.T) {
+	t.Parallel()
+	testCtx := context.Background()
+	testConn, _ := db.TestSetup(t, "postgres")
+	testRw := db.New(testConn)
+	rootWrapper := db.TestWrapper(t)
+	testKms := kms.TestKms(t, testConn, rootWrapper)
+
+	// some standard factories for unit tests
+	authenticatorFn := func() (Authenticator, error) {
+		return NewRepository(testCtx, testRw, testRw, testKms)
+	}
+	lookupUserWithFn := func() (LookupUser, error) {
+		return iam.NewRepository(testRw, testRw, testKms)
+	}
+	tokenCreatorFn := func() (AuthTokenCreator, error) {
+		return authtoken.NewRepository(testRw, testRw, testKms)
+	}
+	iamRepo := iam.TestRepo(t, testConn, rootWrapper)
+	org, _ := iam.TestScopes(t, iamRepo)
+	orgDbWrapper, err := testKms.GetWrapper(testCtx, org.PublicId, kms.KeyPurposeDatabase)
+	require.NoError(t, err)
+
+	logger := hclog.New(&hclog.LoggerOptions{
+		Name:  "test-logger",
+		Level: hclog.Error,
+	})
+	td := testdirectory.Start(t,
+		testdirectory.WithDefaults(t, &testdirectory.Defaults{AllowAnonymousBind: true}),
+		testdirectory.WithLogger(t, logger),
+	)
+	tdCerts, err := ParseCertificates(testCtx, td.Cert())
+	require.NoError(t, err)
+
+	groups := []*gldap.Entry{
+		testdirectory.NewGroup(t, "admin", []string{"alice"}),
+	}
+	users := testdirectory.NewUsers(t, []string{"alice", "bob"}, testdirectory.WithMembersOf(t, "admin"))
+	td.SetUsers(users...)
+	td.SetGroups(groups...)
+
+	testPrimaryAuthMethod := TestAuthMethod(t, testConn, orgDbWrapper, org.PublicId,
+		[]string{fmt.Sprintf("ldaps://127.0.0.1:%d", td.Port())},
+		WithCertificates(testCtx, tdCerts...),
+		WithDiscoverDn(testCtx),
+		WithEnableGroups(testCtx),
+		WithUserDn(testCtx, testdirectory.DefaultUserDN),
+		WithGroupDn(testCtx, testdirectory.DefaultGroupDN),
+	)
+	iam.TestSetPrimaryAuthMethod(t, iamRepo, org, testPrimaryAuthMethod.PublicId)
+
+	testNotPrimaryAuthMethod := TestAuthMethod(t, testConn, orgDbWrapper, org.PublicId,
+		[]string{fmt.Sprintf("ldaps://127.0.0.1:%d", td.Port())},
+		WithCertificates(testCtx, tdCerts...),
+		WithDiscoverDn(testCtx),
+		WithEnableGroups(testCtx),
+		WithUserDn(testCtx, testdirectory.DefaultUserDN),
+		WithGroupDn(testCtx, testdirectory.DefaultGroupDN),
+	)
+
+	const (
+		testLoginName  = "alice"
+		testPassword   = "password"
+		testLoginName2 = "bob"
+	)
+
+	testExistingAcct := TestAccount(t, testConn, testNotPrimaryAuthMethod, testLoginName2)
+	iam.TestUser(t, iamRepo, org.PublicId, iam.WithAccountIds(testExistingAcct.PublicId))
+
+	// order is important in these tests, so we're using a slice
+	tests := []struct {
+		order                 int
+		name                  string
+		ctx                   context.Context
+		authenticatorFn       AuthenticatorFactory
+		lookupUserWithLoginFn LookupUserFactory
+		tokenCreatorFn        AuthTokenCreatorFactory
+		authMethodId          string
+		loginName             string
+		password              string
+		wantErrMatch          *errors.Template
+		wantErrContains       string
+	}{
+		{
+			order:                 0,
+			name:                  "success-with-primary-auth-method-auto-create",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName,
+			password:              testPassword,
+		},
+		{
+			order:                 1,
+			name:                  "success-with-primary-auth-method-existing-account",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName,
+			password:              testPassword,
+		},
+		{
+			order:                 2,
+			name:                  "success-with-non-primary-auth-method",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testNotPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName2,
+			password:              testPassword,
+		},
+		{
+			order:                 3,
+			name:                  "err-not-primary-refused-to-create-user",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testNotPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName,
+			password:              testPassword,
+			wantErrMatch:          errors.T(errors.RecordNotFound),
+			wantErrContains:       "auth method is not primary for the scope so refusing to auto-create user",
+		},
+		{
+			order:                 4,
+			name:                  "missing-authenticator-fn",
+			ctx:                   testCtx,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testNotPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName,
+			password:              testPassword,
+			wantErrMatch:          errors.T(errors.InvalidParameter),
+			wantErrContains:       "missing authenticator factory",
+		},
+		{
+			order:           5,
+			name:            "missing-lookup-user-fn",
+			ctx:             testCtx,
+			authenticatorFn: authenticatorFn,
+			tokenCreatorFn:  tokenCreatorFn,
+			authMethodId:    testNotPrimaryAuthMethod.PublicId,
+			loginName:       testLoginName,
+			password:        testPassword,
+			wantErrMatch:    errors.T(errors.InvalidParameter),
+			wantErrContains: "missing lookup user factory",
+		},
+		{
+			order:                 6,
+			name:                  "missing-at-creator-fn",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			authMethodId:          testNotPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName,
+			password:              testPassword,
+			wantErrMatch:          errors.T(errors.InvalidParameter),
+			wantErrContains:       "missing auth token creator factory",
+		},
+		{
+			order:                 7,
+			name:                  "missing-auth-method-id",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			loginName:             testLoginName,
+			password:              testPassword,
+			wantErrMatch:          errors.T(errors.InvalidParameter),
+			wantErrContains:       "missing auth method id",
+		},
+		{
+			order:                 8,
+			name:                  "missing-login-name",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testNotPrimaryAuthMethod.PublicId,
+			password:              testPassword,
+			wantErrMatch:          errors.T(errors.InvalidParameter),
+			wantErrContains:       "missing login name",
+		},
+		{
+			order:                 9,
+			name:                  "missing-password",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testNotPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName,
+			wantErrMatch:          errors.T(errors.InvalidParameter),
+			wantErrContains:       "missing password",
+		},
+		{
+			order: 10,
+			name:  "authenticator-fn-err",
+			ctx:   testCtx,
+			authenticatorFn: func() (Authenticator, error) {
+				return nil, errors.New(testCtx, errors.Internal, "test", "authenticator-fn-err")
+			},
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName,
+			password:              testPassword,
+			wantErrMatch:          errors.T(errors.Internal),
+			wantErrContains:       "authenticator-fn-err",
+		},
+		{
+			order:           11,
+			name:            "lookup-user-fn-err",
+			ctx:             testCtx,
+			authenticatorFn: authenticatorFn,
+			lookupUserWithLoginFn: func() (LookupUser, error) {
+				return nil, errors.New(testCtx, errors.Internal, "test", "lookup-user-fn-err")
+			},
+			tokenCreatorFn:  tokenCreatorFn,
+			authMethodId:    testPrimaryAuthMethod.PublicId,
+			loginName:       testLoginName,
+			password:        testPassword,
+			wantErrMatch:    errors.T(errors.Internal),
+			wantErrContains: "lookup-user-fn-err",
+		},
+		{
+			order:                 12,
+			name:                  "at-fn-err",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn: func() (AuthTokenCreator, error) {
+				return nil, errors.New(testCtx, errors.Internal, "test", "at-fn-err")
+			},
+			authMethodId:    testPrimaryAuthMethod.PublicId,
+			loginName:       testLoginName,
+			password:        testPassword,
+			wantErrMatch:    errors.T(errors.Internal),
+			wantErrContains: "at-fn-err",
+		},
+		{
+			order: 13,
+			name:  "authenticate-err",
+			ctx:   testCtx,
+			authenticatorFn: func() (Authenticator, error) {
+				return &mockAuthenticator{authErr: errors.New(testCtx, errors.Internal, "test", "authenticate-err")}, nil
+			},
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn:        tokenCreatorFn,
+			authMethodId:          testPrimaryAuthMethod.PublicId,
+			loginName:             testLoginName,
+			password:              testPassword,
+			wantErrMatch:          errors.T(errors.Internal),
+			wantErrContains:       "authenticate-err",
+		},
+		{
+			order:                 14,
+			name:                  "token-err",
+			ctx:                   testCtx,
+			authenticatorFn:       authenticatorFn,
+			lookupUserWithLoginFn: lookupUserWithFn,
+			tokenCreatorFn: func() (AuthTokenCreator, error) {
+				return &mockTokenCreator{tokenErr: errors.New(testCtx, errors.Internal, "test", "token-err")}, nil
+			},
+			authMethodId:    testPrimaryAuthMethod.PublicId,
+			loginName:       testLoginName,
+			password:        testPassword,
+			wantErrMatch:    errors.T(errors.Internal),
+			wantErrContains: "token-err",
+		},
+	}
+	for idx, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			require.Equal(tc.order, idx)
+			got, err := Authenticate(tc.ctx, tc.authenticatorFn, tc.lookupUserWithLoginFn, tc.tokenCreatorFn, tc.authMethodId, tc.loginName, tc.password)
+			if tc.wantErrMatch != nil {
+				require.Error(err)
+				assert.Empty(got)
+				assert.Truef(errors.Match(tc.wantErrMatch, err), "unexpected error")
+				if tc.wantErrContains != "" {
+					assert.Contains(err.Error(), tc.wantErrContains)
+				}
+				return
+			}
+			require.NoError(err)
+			assert.NotEmpty(got)
+		})
+	}
+}
+
+type mockAuthenticator struct {
+	authErr error
+}
+
+func (m *mockAuthenticator) Authenticate(ctx context.Context, authMethodId, loginName, password string) (*Account, error) {
+	return nil, m.authErr
+}
+
+type mockTokenCreator struct {
+	tokenErr error
+}
+
+func (m *mockTokenCreator) CreateAuthToken(ctx context.Context, withIamUser *iam.User, withAuthAccountId string, opt ...authtoken.Option) (*authtoken.AuthToken, error) {
+	return nil, m.tokenErr
+}
