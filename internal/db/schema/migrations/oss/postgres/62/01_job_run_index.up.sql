55259029f1085ae3f4f691384e82938f7c006469
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-01-31 17:41:33 -0800

    Add job run cleaner (#2866)
    * chore: remove duplicate import
    
    * feat: Add job run cleaner
    
    The new job cleans job runs older than a configured interval.
    Configure the job to clean jobs older than 1 minute.

---
+++ internal/db/schema/migrations/oss/postgres/62/01_job_run_index.up.sql
@@ -0,0 +1,12 @@
+begin;
+
+  -- Delete any existing completed job_runs to make index
+  -- creation faster and avoid one very expensive first cleaner job.
+  delete from job_run where status='completed';
+
+  -- Create index for faster deletes.
+  create index job_run_status_ix on job_run (status);
+  comment on index job_run_status_ix is
+    'the job_run_status_ix is used by the job run cleaner job';
+
+commit;
