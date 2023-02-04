6d03fee184eba2dcb3f0e51082ee54ff9c8b1eca
Jim <jlambert@hashicorp.com>
2023-02-04 16:01:06 +0100

    feature (wh/ldap) add tests for new ldap auth method and accounts

--- internal/db/schema/migrations/oss/postgres/14/01_wh_user_dimension_oidc.up.sql
+++ internal/db/schema/migrations/oss/postgres/14/01_wh_user_dimension_oidc.up.sql
@@ -36,6 +36,7 @@ begin;
     alter column auth_account_email       type wh_dim_text
   ;
 
+-- Replaced in ???/01_wh_user_dimension_ldap.up.sql
   drop view whx_user_dimension_source;
   create view whx_user_dimension_source as
        select -- id is the first column in the target view
