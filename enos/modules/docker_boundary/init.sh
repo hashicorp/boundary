f47eeb07ffc2ceb1812f5e487779376a3699d9f6
Michael Li <michael.li@hashicorp.com>
2023-02-06 18:34:33 -0500

    refact(enos): Use docker for infrastructure

---
+++ enos/modules/docker_boundary/init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# This script initializes a postgres database to work with Boundary by spinning up a temporary
+# Boundary docker container pointed to the specified database and invoking `boundary database init`.
+#
+# This script must only output the JSON that comes from `boundary database init` as the output is
+# consumed by other scripts.
+
+TEST_BOUNDARY_IMAGE=$1
+TEST_DATABASE_ADDRESS=$2
+TEST_CONTAINER_NAME=boundary-init
+SOURCE=$(realpath $(dirname ${BASH_SOURCE[0]})) # get directory of this script
+
+docker run \
+    -d \
+    --name $TEST_CONTAINER_NAME \
+    -e "BOUNDARY_POSTGRES_URL=$TEST_DATABASE_ADDRESS" \
+    -e "SKIP_CHOWN=true" \
+    --cap-add IPC_LOCK \
+    --mount type=bind,src=$SOURCE,dst=/boundary/ \
+    --network e2e_network \
+    $TEST_BOUNDARY_IMAGE \
+    boundary database init -config /boundary/boundary-config.hcl -format json \
+    >/dev/null 2>&1 # suppresses output
+
+docker wait $TEST_CONTAINER_NAME >/dev/null 2>&1
+echo $(docker logs $TEST_CONTAINER_NAME)
+docker rm -v $TEST_CONTAINER_NAME >/dev/null 2>&1 || true
---
+++ enos/modules/docker_boundary/main.tf
@@ -0,0 +1,131 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+
+    enos = {
+      source = "app.terraform.io/hashicorp-qti/enos"
+    }
+  }
+}
+
+variable "image_name" {
+  description = "Name of Docker Image"
+  type        = string
+}
+variable "network_name" {
+  description = "Name of Docker Network"
+  type        = string
+}
+variable "container_name" {
+  description = "Name of Docker Container"
+  type        = string
+  default     = "boundary"
+}
+variable "postgres_address" {
+  description = "Address to postgres database"
+  type        = string
+}
+
+
+resource "docker_image" "boundary" {
+  name         = var.image_name
+  keep_locally = true
+}
+
+resource "enos_local_exec" "init_database" {
+  inline = ["bash ./${path.module}/init.sh \"${var.image_name}\" \"${var.postgres_address}\""]
+}
+
+locals {
+  test           = jsondecode(enos_local_exec.init_database.stdout)
+  auth_method_id = local.test["auth_method"]["auth_method_id"]
+  login_name     = local.test["auth_method"]["login_name"]
+  password       = local.test["auth_method"]["password"]
+}
+
+resource "docker_container" "boundary" {
+  depends_on = [
+    enos_local_exec.init_database,
+  ]
+  image   = docker_image.boundary.image_id
+  name    = var.container_name
+  command = ["boundary", "server", "-config", "/boundary/boundary-config.hcl"]
+  env = [
+    "BOUNDARY_POSTGRES_URL=${var.postgres_address}",
+    "HOSTNAME=boundary",
+    "SKIP_CHOWN=true",
+  ]
+  ports {
+    internal = 9200
+    external = 9200
+  }
+  ports {
+    internal = 9201
+    external = 9201
+  }
+  ports {
+    internal = 9202
+    external = 9202
+  }
+  ports {
+    internal = 9203
+    external = 9203
+  }
+  capabilities {
+    add = ["IPC_LOCK"]
+  }
+  volumes {
+    host_path      = abspath(path.module)
+    container_path = "/boundary/"
+  }
+  volumes {
+    host_path      = "${abspath(path.module)}/auditlogs"
+    container_path = "/logs"
+  }
+  healthcheck {
+    test     = ["CMD", "wget", "--quiet", "-O", "/dev/null", "http://boundary:9203/health"]
+    interval = "3s"
+    timeout  = "5s"
+    retries  = 5
+  }
+  wait     = true
+  must_run = true
+  networks_advanced {
+    name = var.network_name
+  }
+}
+
+resource "enos_local_exec" "wait" {
+  depends_on = [
+    docker_container.boundary
+  ]
+
+  inline = ["timeout 10s bash -c 'until curl http://0.0.0.0:9200; do sleep 2; done'"]
+}
+
+output "address" {
+  value = "http://0.0.0.0:9200"
+}
+
+output "auth_method_id" {
+  value = local.auth_method_id
+}
+
+output "login_name" {
+  value = local.login_name
+}
+
+output "password" {
+  value = local.password
+}
