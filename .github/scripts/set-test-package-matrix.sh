e8b147836304467c972107701ab08daebdca35e4
Sarah <sthompson@hashicorp.com>
2023-02-22 13:16:00 +0000

    fix matrix

--- .github/scripts/set-test-package-matrix.sh
+++ .github/scripts/set-test-package-matrix.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 # set matrix var to list of unique packages containing tests
-matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath | split("\n")')"
+matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath')"
 
 echo "matrix=${matrix}" | tee -a "${GITHUB_OUTPUT}"
