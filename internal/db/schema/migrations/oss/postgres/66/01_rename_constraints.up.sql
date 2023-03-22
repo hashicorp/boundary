498758bba84f5b90a70b3c4c2b5c564dfbca34c2
Haotian Zeng <htzeng@msn.com>
2023-03-22 10:42:08 -0700

    fixup sql formatting

--- internal/db/schema/migrations/oss/postgres/66/01_rename_constraints.up.sql
+++ internal/db/schema/migrations/oss/postgres/66/01_rename_constraints.up.sql
@@ -6,47 +6,46 @@ begin;
   -- procedurally rename unique constraints to follow style guide `tablename_col1_colx_uq`
   -- if constraint name format is under 63 characters
   do $$
-  declare 
+  declare
     const record;
-    i int;
+    i     int;
   begin 
     for const in
-    select
-      pc.conname as constraint_name,
-      pgc.relname as table_name,
-      pgc.relname || '_' ||
-        (select string_agg(attname, '_')
-        from (
-          select pa.attname
-          from pg_catalog.pg_attribute pa
-          where pa.attrelid = pc.conrelid
-            and pa.attnum = any(pc.conkey)
-          order by pa.attname asc
-        ) as attname
-      ) || '_uq' as new_name,
-      pc.conrelid as table_num,
-      pc.conkey as attr_keys,
-      regexp_replace(regexp_replace(pc.conname, '^' || pgc.relname || '_', ''), 'uq$', '') as attrs_concat
-  from pg_catalog.pg_constraint pc
-    join pg_catalog.pg_class pgc on pc.conrelid = pgc.oid
-  where pgc.relname not like 'pg_%'
-    and contype = 'u'
-  order by pgc.relname, pc.conname
-  loop foreach i in array const.attr_keys 
-    loop const.attrs_concat := regexp_replace(
-      const.attrs_concat,
-      concat((
-          select pa.attname
-          from pg_catalog.pg_attribute pa
-          where pa.attnum = i
-            and pa.attrelid = const.table_num
-        ), '_'), '');
-    end loop;
-    if length(const.attrs_concat) > 0 and length(const.new_name) <= 63
-      then
-        execute format('alter table %I rename constraint %I to %I', const.table_name, const.constraint_name, const.new_name);
-    end if;
-  end loop;
+        select pc.conname  as constraint_name,
+               pgc.relname as table_name,
+               pgc.relname || '_' || (
+                select string_agg(attname, '_')
+                  from (
+                    select pa.attname
+                      from pg_catalog.pg_attribute pa
+                     where pa.attrelid = pc.conrelid
+                       and pa.attnum = any(pc.conkey)
+                  order by pa.attname asc
+                  ) as attname
+               ) || '_uq'  as new_name,
+               pc.conrelid as table_num,
+               pc.conkey   as attr_keys,
+               regexp_replace(regexp_replace(pc.conname, '^' || pgc.relname || '_', ''), 'uq$', '') as attrs_concat
+          from pg_catalog.pg_constraint pc
+          join pg_catalog.pg_class      pgc on pc.conrelid = pgc.oid
+         where pgc.relname not like 'pg_%'
+           and contype = 'u'
+      order by pgc.relname, pc.conname
+      loop foreach i in array const.attr_keys 
+        loop const.attrs_concat := regexp_replace(
+          const.attrs_concat,
+          concat((
+              select pa.attname
+                from pg_catalog.pg_attribute pa
+               where pa.attnum = i
+                 and pa.attrelid = const.table_num
+            ), '_'), '');
+        end loop;
+        if length(const.attrs_concat) > 0 and length(const.new_name) <= 63
+          then
+            execute format('alter table %I rename constraint %I to %I', const.table_name, const.constraint_name, const.new_name);
+        end if;
+      end loop;
   end;
   $$ language plpgsql;
 
@@ -67,26 +66,34 @@ begin;
   do $$
   declare
     const record;
-    i int;
+    i     int;
   begin
-  for const in select pc.conname as constraint_name,
-    pgc.relname as table_name,
-    pgc.relname || '_' as new_name,
-    pc.conrelid as conrelid,
-    pc.conkey as conkey,
-    pgc2.relname as con_table
-  from pg_catalog.pg_constraint pc
-    join pg_catalog.pg_class pgc on pc.confrelid = pgc.oid
-    join pg_catalog.pg_class pgc2 on pc.conrelid = pgc2.oid
-	where pgc.relname not like 'pg_%'
-    and contype = 'f'
-	  and (pc.conname !~ ('^' || pgc.relname || '_')
-        or pc.conname !~ 'fkey[1]?$')
-	order by pgc.relname, pc.conname
+  for const in
+      select pc.conname         as constraint_name,
+             pgc.relname        as table_name,
+             pgc.relname || '_' as new_name,
+             pc.conrelid        as conrelid,
+             pc.conkey          as conkey,
+             pgc2.relname       as con_table
+        from pg_catalog.pg_constraint pc
+        join pg_catalog.pg_class      pgc on pc.confrelid = pgc.oid
+        join pg_catalog.pg_class      pgc2 on pc.conrelid = pgc2.oid
+	     where pgc.relname not like 'pg_%'
+         and contype = 'f'
+	       and (pc.conname !~ ('^' || pgc.relname || '_')
+          or pc.conname !~ 'fkey[1]?$')
+	  order by pgc.relname, pc.conname
   loop
-    i = (select max(x) from unnest(const.conkey) as x);
-    const.new_name = const.new_name || (select pa.attname from pg_catalog.pg_attribute pa where 
-      const.conrelid = pa.attrelid and pa.attnum = i) || '_fkey';
+    i = (
+      select max(x)
+        from unnest(const.conkey) as x
+    );
+    const.new_name = const.new_name || (
+      select pa.attname
+        from pg_catalog.pg_attribute pa
+       where const.conrelid = pa.attrelid
+         and pa.attnum = i
+    ) || '_fkey';
     if length(const.new_name) <= 63
       then
       begin
