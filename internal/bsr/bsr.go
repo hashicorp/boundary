397a6977318138d6c23093ca8c21d7ec9e84f9b0
Timothy Messier <tim.messier@gmail.com>
2023-03-24 20:07:55 +0000

    feat(bsr): Define stubs for meta structures
    These are used to provide the metadata about a session or connection
    when writing a BSR and will be written to the corresponding .meta files
    within a BSR.

---
+++ internal/bsr/bsr.go
@@ -0,0 +1,14 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package bsr
+
+// SessionMeta contains metadata about a session in a BSR.
+type SessionMeta struct {
+	Id string
+}
+
+// ConnectionMeta contains metadata about a connection in a BSR.
+type ConnectionMeta struct {
+	Id string
+}
