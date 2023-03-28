bc31892f291562ab0912301b395bc092fe692183
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-03-28 10:35:26 -0700

    internal/db: add set_once_columns function (#3117)
    * internal/db: add set_once_columns function
    
    Also replace existing before_soft_delete_credential_vault_store
    function and trigger with the new function.
    
    * internal/db/schema: add note to replaced definitions

---
+++ internal/db/schema/migrations/oss/postgres/66/02_use_set_once_columns.up.sql
@@ -0,0 +1,12 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: MPL-2.0
+
+begin;
+
+  drop trigger before_soft_delete_credential_vault_store on credential_vault_store;
+  drop function before_soft_delete_credential_vault_store;
+
+  create trigger set_once_columns before update on credential_vault_store
+    for each row execute procedure set_once_columns('delete_time');
+
+commit;
