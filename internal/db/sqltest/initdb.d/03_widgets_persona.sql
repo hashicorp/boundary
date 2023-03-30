15922d71f47700e06e42fd36ec7a59cb359bce3c
Danielle Miu <29378233+DanielleMiu@users.noreply.github.com>
2023-03-30 15:26:46 -0400

    fix sql

--- internal/db/sqltest/initdb.d/03_widgets_persona.sql
+++ internal/db/sqltest/initdb.d/03_widgets_persona.sql
@@ -270,10 +270,15 @@ begin;
      where h.catalog_id = s.catalog_id
        and h.address like '%.widget';
 
-    insert into plugin_host
+    insert into plugin
       (scope_id, public_id, name)
     values
       ('global', 'plg___wb-hplg', 'Short Host Plugin');
+
+    insert into plugin_host_supported
+      (public_id)
+    values
+      ('plg___wb-hplg');
 
     insert into host_plugin_catalog
       (project_id, public_id, plugin_id, name, attributes)
