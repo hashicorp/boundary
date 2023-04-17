cf7356254f69493a0be893b46e19d6be5494eb11
Stan Ryzhov <60649800+stasryzhov@users.noreply.github.com>
2023-04-17 12:32:59 -0400

    test(e2e): Rename file and enhance comments

--- testing/internal/e2e/tests/static/auth_token_delete_test.go
+++ testing/internal/e2e/tests/static/auth_token_delete_test.go
@@ -15,7 +15,12 @@ import (
 
 const testAccountName = "test-account"
 
-// TestUserIsLoggedOutWhenAuthTokenIsDeletedCli validates auth-token deletion by admin
+// TestUserIsLoggedOutWhenAuthTokenIsDeletedCli validates that a user
+// gets logged out if admin deletes its auth-token.
+// The test authenticates a new user and verifies that it can perform
+// operations requiring to be logged in (e.g. 'boundary auth-tokens list').
+// Then the test deletes the user's auth-token by admin and verifies that
+// the user cannot perform operations requiring to be logged in anymore.
 func TestUserIsLoggedOutWhenAuthTokenIsDeletedCli(t *testing.T) {
 	e2e.MaybeSkipTest(t)
 	bc, err := boundary.LoadConfig()
@@ -48,7 +53,8 @@ func TestUserIsLoggedOutWhenAuthTokenIsDeletedCli(t *testing.T) {
 		e2e.WithArgs("-token-name", testAccountName),
 	)
 
-	// Check if user is logged in
+	// As the user, execute the command requiring to be logged in.
+	// Provide the user's token name to the command so that it is executed by the user, not admin
 	output := e2e.RunCommand(ctx, "boundary",
 		e2e.WithArgs(
 			"auth-tokens", "list",
@@ -57,7 +63,7 @@ func TestUserIsLoggedOutWhenAuthTokenIsDeletedCli(t *testing.T) {
 	)
 	require.NoError(t, output.Err, string(output.Stderr))
 
-	// Delete user's auth token by admin
+	// As admin, delete the user's auth token
 	userAuthTokenID := boundary.GetAuthenticationTokenIdCli(t, ctx, testAccountName)
 	boundary.AuthenticateAdminCli(t, ctx)
 	output = e2e.RunCommand(ctx, "boundary",
@@ -69,13 +75,15 @@ func TestUserIsLoggedOutWhenAuthTokenIsDeletedCli(t *testing.T) {
 	require.NoError(t, output.Err, string(output.Stderr))
 	t.Logf("Sucessfully deleted user auth-token: %s", userAuthTokenID)
 
-	// Check if user is logged out
+	// As the user, try to execute the command requiring to be logged in again
 	output = e2e.RunCommand(ctx, "boundary",
 		e2e.WithArgs(
 			"auth-tokens", "list",
 			"-token-name", testAccountName,
 		),
 	)
+
+	// Expect error as the user got logged out
 	require.Error(t, output.Err, fmt.Sprintf("User '%s' is still logged in", testAccountName))
 	t.Logf("User is logged out: %s", testAccountName)
 }
