8b8b66548314f2795fdbe11978dfc5988683899a
Josh Brand <jbrand@hashicorp.com>
2023-05-04 18:16:00 -0400

    chore(e2e): Update CI user for testing (#3203)

--- enos/ci/service-user-iam/outputs.tf
+++ enos/ci/service-user-iam/outputs.tf
@@ -2,16 +2,9 @@
 # SPDX-License-Identifier: MPL-2.0
 
 output "ci_roles" {
-  value = local.is_ent ? [for role in aws_iam_role.github_actions_doormat_role : {
-    name = role.name
-    arn  = role.arn
-    policy = [for policy in role.inline_policy : {
-      name   = policy.name
-      policy = jsondecode(policy.policy)
-    }][0]
-    }] : [{
-    name   = aws_iam_role.role[0].name
-    arn    = aws_iam_role.role[0].arn
-    policy = aws_iam_role_policy.role_policy[0].policy
+  value = [{
+    name   = aws_iam_role.role.name
+    arn    = aws_iam_role.role.arn
+    policy = aws_iam_role_policy.role_policy.policy
   }]
 }
