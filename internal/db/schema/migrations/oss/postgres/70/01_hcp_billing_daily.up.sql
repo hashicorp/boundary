9c1e2a699f623926593ff17bb395a35e557ca048
Michael Gaffney <mike@gaffney.cc>
2023-06-03 18:23:35 -0400

    feat(db): Add function to update billing snapshot table
    This adds the `update_sessions_pending_daily_snapshot` SQL function
    which updates the `sessions_pending_daily_snapshot` table by inserting
    values for any days missing from the table.

--- internal/db/schema/migrations/oss/postgres/70/01_hcp_billing_daily.up.sql
+++ internal/db/schema/migrations/oss/postgres/70/01_hcp_billing_daily.up.sql
@@ -96,4 +96,69 @@ begin;
     'the sum of pending sessions for all days excluding the current day. '
     'All timestamps returned are in UTC.';
 
+  create function update_sessions_pending_daily_snapshot()
+    returns setof sessions_pending_daily_snapshot
+  as $$
+  begin
+
+    -- already ran for today
+      if (date_trunc('day', now(), 'utc') - '1 day'::interval) = (select max(snapshot_date) from sessions_pending_daily_snapshot)
+    then return;
+    end if;
+
+    -- never run before and there are only sessions starting from today
+      if (select count(*) from sessions_pending_daily_snapshot) = 0
+     and date_trunc('day', now(), 'utc') = (select min(session_pending_time) from wh_session_accumulating_fact)
+    then return query
+           insert into sessions_pending_daily_snapshot
+             (snapshot_date, sessions_pending_count)
+           values
+             (date_trunc('day', now(), 'utc') - '1 day'::interval, 0)
+           returning *;
+         return;
+    end if;
+
+    return query
+    with
+    daily_counts (day, sessions_pending_count) as (
+        select date_trunc('day', session_pending_time), count(*)
+          from wh_session_accumulating_fact
+         where session_pending_time < date_trunc('day', now()) -- before midnight today
+           and session_pending_time >= coalesce((select max(snapshot_date) from sessions_pending_daily_snapshot), '-infinity')
+      group by date_trunc('day', session_pending_time)
+    ),
+    daily_range (day) as (
+        select bucket
+          from generate_series(
+                  coalesce(date_trunc('day', (select min(session_pending_time) from wh_session_accumulating_fact), 'utc'),
+                           date_trunc('day', now(), 'utc') - '1 day'::interval),
+                  now() - '1 day'::interval,
+                  '1 day'::interval
+               ) as bucket
+    ),
+    missing (day, sessions_pending_count) as (
+           select daily_range.day::timestamp with time zone,
+                  coalesce(daily_counts.sessions_pending_count, 0)
+             from daily_range
+        left join daily_counts on daily_range.day = daily_counts.day
+    ),
+    final (day, sessions_pending_count) as (
+      insert into sessions_pending_daily_snapshot
+        (snapshot_date, sessions_pending_count)
+      select day::date, sessions_pending_count
+        from missing
+      returning *
+    )
+      select day, sessions_pending_count
+        from final
+      order by day desc;
+  end;
+  $$ language plpgsql
+     set timezone to 'UTC';
+  comment on function update_sessions_pending_daily_snapshot is
+    'update_sessions_pending_daily_snapshot is a function that updates the sessions_pending_daily_snapshot table by '
+    'querying the data warehouse and inserting the session pending counts for any days since the max snapshot_date '
+    'and yesterday. '
+    'update_sessions_pending_daily_snapshot returns the rows inserted or null if no rows are inserted.';
+
 commit;
