hashicorp/boundary

commit 41b1b5b4b74b38141ddf934ff274dfb5c5b53d0e

feat(db): Introduce migrations for ssh targets (#2960)
Adds the schema for ssh targets. This also changes the target listing
func to not error out when it encounters a target with an unknown type.
Instead, it now ignores it and continues to the next one.

internal/db/schema/migrations/oss/postgres/64/02_hcp_billing_hourly.up.sql
@@ -1,0 +1,158 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: MPL-2.0
+
+begin;
+  drop index if exists wh_session_accumulating_fact_session_pending_time_idx;
+  create index wh_session_accumulating_fact_session_pending_time_idx on wh_session_accumulating_fact (session_pending_time);
+  analyze wh_session_accumulating_fact;
+
+/*
+  Implementation Note 1
+
+  SQL is very flexible. I'm 87% sure there are approximately 1,962 different
+  ways to write the queries for each of the views below (plus or minus 2). I
+  chose the ones below based on the explain plans for each view after filling
+  the wh_session_accumulating_fact table with more than 250,000 rows of test
+  data.
+
+  The query I used to populate the test data is at the bottom of this file. If
+  you need to alter these views, please generate test data and use the explain
+  plan to guide your decisions.
+*/
+
+  drop view if exists hcp_billing_hourly_sessions_last_3_hours;
+  create view hcp_billing_hourly_sessions_last_3_hours as
+  with
+  hourly_counts (hour, sessions_pending_count) as (
+      select date_trunc('hour', session_pending_time), count(*)
+        from wh_session_accumulating_fact
+       where session_pending_time >= date_trunc('hour', now() - '3 hours'::interval)
+    group by date_trunc('hour', session_pending_time)
+  ),
+  hourly_range (hour) as (
+      select date_trunc('hour',time)
+        from generate_series(now() - '3 hours'::interval, now(), '1 hour'::interval) as time
+  ),
+  final (hour, sessions_pending_count) as (
+      select hourly_range.hour, coalesce(hourly_counts.sessions_pending_count, 0)
+        from hourly_range
+   left join hourly_counts on hourly_range.hour = hourly_counts.hour
+  )
+    select hour, sessions_pending_count
+      from final
+  order by hour desc;
+  comment on view hcp_billing_hourly_sessions_last_3_hours is
+    'hcp_billing_hourly_sessions_last_3_hours is a view where each row contains the timestamp for an hour and the sum of the pending sessions created in that hour. '
+      '4 rows are returned: 1 for the current hour plus 3 for the previous 3 hours. '
+      'Rows are sorted by the hour in descending order.';
+
+  drop view if exists hcp_billing_hourly_sessions_last_7_days;
+  create view hcp_billing_hourly_sessions_last_7_days as
+  with
+  hourly_counts (hour, sessions_pending_count) as (
+      select date_trunc('hour', session_pending_time), count(*)
+        from wh_session_accumulating_fact
+       where session_pending_time >= date_trunc('hour', now() - '7 days'::interval)
+    group by date_trunc('hour', session_pending_time)
+  ),
+  hourly_range (hour) as (
+      select date_trunc('hour',time)
+        from generate_series(now() - '7 days'::interval, now(), '1 hour'::interval) as time
+  ),
+  final (hour, sessions_pending_count) as (
+      select hourly_range.hour, coalesce(hourly_counts.sessions_pending_count, 0)
+        from hourly_range
+   left join hourly_counts on hourly_range.hour = hourly_counts.hour
+  )
+    select hour, sessions_pending_count
+      from final
+  order by hour desc;
+  comment on view hcp_billing_hourly_sessions_last_7_days is
+    'hcp_billing_hourly_sessions_last_7_days is a view where each row contains the timestamp for an hour and the sum of the pending sessions created in that hour. '
+      '169 rows are returned: 1 for the current hour plus 168 for the previous 7 days. '
+      'Rows are sorted by the hour in descending order.';
+
+  drop view if exists hcp_billing_hourly_sessions_all;
+  create view hcp_billing_hourly_sessions_all as
+  with
+  hourly_counts (hour, sessions_pending_count) as (
+      select date_trunc('hour', session_pending_time), count(*)
+        from wh_session_accumulating_fact
+    group by date_trunc('hour', session_pending_time)
+  ),
+  hourly_range (hour) as (
+      select date_trunc('hour',time)
+        from generate_series(
+                 (select min(session_pending_time) from wh_session_accumulating_fact),
+                 now(),
+                 '1 hour'::interval
+             ) as time
+  ),
+  final (hour, sessions_pending_count) as (
+      select hourly_range.hour, coalesce(hourly_counts.sessions_pending_count, 0)
+        from hourly_range
+   left join hourly_counts on hourly_range.hour = hourly_counts.hour
+  )
+    select hour, sessions_pending_count
+      from final
+  order by hour desc;
+  comment on view hcp_billing_hourly_sessions_all is
+    'hcp_billing_hourly_sessions_all is a view where each row contains the timestamp for an hour and the sum of the pending sessions created in that hour. '
+      'A row is returned for each hour since the first session was created up to and including the current hour. '
+      'Rows are sorted by the hour in descending order.';
+
+/*
+  Implementation Note 2: Generating test data
+
+  Step 1: Start and connect to the sqltest docker container (see sqltest/README.md).
+
+    The sqltest container initializes with a few sessions already in the data
+    warehouse. These sessions will populate rows in the wh_host_dimension and
+    wh_user_dimension tables which will be used in the query below.
+
+  Step 2: Truncate the warehouse fact tables:
+
+      truncate wh_session_connection_accumulating_fact, wh_session_accumulating_fact;
+
+  Step 3: Fill the wh_session_accumulating_fact table with 261961 rows of data:
+
+      with
+      dim_keys (host_key, user_key, credential_group_key) as (
+        select h.key, u.key, 'no credentials'
+          from (select key from wh_host_dimension limit 1) as h,
+               (select key from wh_user_dimension limit 1) as u
+      ),
+      time_series (date_key, time_key, time) as (
+        select wh_date_key(time), wh_time_key(time), time
+          from generate_series(
+                  now() - interval '6 months',
+                  now() - interval '2 hours',
+                  interval '1 minute'
+               ) as time
+      ),
+      fake_sessions (session_id, auth_token_id,
+                     host_key, user_key, credential_group_key,
+                     session_pending_date_key, session_pending_time_key, session_pending_time) as (
+        select substr(md5(random()::text), 0, 15), substr(md5(random()::text), 0, 15),
+               k.host_key, k.user_key, k.credential_group_key,
+               t.date_key, t.time_key,t.time
+          from dim_keys as k,
+               time_series as t
+      )
+      insert into wh_session_accumulating_fact
+            (session_id, auth_token_id,
+             host_key, user_key, credential_group_key,
+             session_pending_date_key, session_pending_time_key, session_pending_time
+            )
+      select session_id, auth_token_id,
+             host_key, user_key, credential_group_key,
+             session_pending_date_key, session_pending_time_key, session_pending_time
+        from fake_sessions;
+
+  Step 4: Collect the execution plan for each view to establish a baseline before making changes:
+
+      explain (analyze, buffers) select * from hcp_billing_hourly_sessions_last_3_hours;
+      explain (analyze, buffers) select * from hcp_billing_hourly_sessions_last_7_days;
+      explain (analyze, buffers) select * from hcp_billing_hourly_sessions_all;
+*/
+commit;

internal/db/schema/migrations/oss/postgres/64/03_hcp_billing_monthly.up.sql
@@ -1,0 +1,110 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: MPL-2.0
+
+begin;
+  drop view if exists hcp_billing_monthly_sessions_current_month;
+  create view hcp_billing_monthly_sessions_current_month as
+  with
+  monthly_counts (month, sessions_pending_count) as (
+      select date_trunc('month', session_pending_time), count(*)
+        from wh_session_accumulating_fact
+       where session_pending_time >= date_trunc('month', now())
+         and session_pending_time < date_trunc('hour', now())
+    group by date_trunc('month', session_pending_time)
+  ),
+  monthly_range (month) as (
+      select date_trunc('month',now())
+  ),
+  final (start_time, end_time, sessions_pending_count) as (
+      select monthly_range.month, -- start
+             case when monthly_range.month = date_trunc('month', now())
+                    then date_trunc('hour', now())
+                  else monthly_range.month + interval '1 month'
+             end,
+             coalesce(monthly_counts.sessions_pending_count, 0)
+        from monthly_range
+   left join monthly_counts on monthly_range.month = monthly_counts.month
+  )
+    select start_time, end_time, sessions_pending_count
+      from final
+  order by start_time desc;
+  comment on view hcp_billing_monthly_sessions_current_month is
+    'hcp_billing_monthly_sessions_current_month is a view that contains '
+    'the sum of pending sessions '
+    'from the beginning of the current month '
+    'until the start of the current hour (exclusive).';
+
+  drop view if exists hcp_billing_monthly_sessions_last_2_months;
+  create view hcp_billing_monthly_sessions_last_2_months as
+  with
+  monthly_counts (month, sessions_pending_count) as (
+      select date_trunc('month', session_pending_time), count(*)
+        from wh_session_accumulating_fact
+       where session_pending_time >= date_trunc('month', now() - interval '1 month')
+         and session_pending_time < date_trunc('hour', now())
+    group by date_trunc('month', session_pending_time)
+  ),
+  monthly_range (month) as (
+      select date_trunc('month', time)
+        from generate_series(
+                 date_trunc('month', now() - interval '1 month'),
+                 now(),
+                 '1 month'::interval
+             ) as time
+  ),
+  final (start_time, end_time, sessions_pending_count) as (
+      select monthly_range.month, -- start
+             case when monthly_range.month = date_trunc('month', now())
+                    then date_trunc('hour', now())
+                  else monthly_range.month + interval '1 month'
+             end,
+             coalesce(monthly_counts.sessions_pending_count, 0)
+        from monthly_range
+   left join monthly_counts on monthly_range.month = monthly_counts.month
+  )
+    select start_time, end_time, sessions_pending_count
+      from final
+  order by start_time desc;
+  comment on view hcp_billing_monthly_sessions_last_2_months is
+    'hcp_billing_monthly_sessions_last_2_months is a view that contains '
+    'the sum of pending sessions for the current month and the previous month. '
+    'The current month is a sum from the beginning of the current month '
+    'until the start of the current hour (exclusive).';
+
+  drop view if exists hcp_billing_monthly_sessions_all;
+  create view hcp_billing_monthly_sessions_all as
+  with
+  monthly_counts (month, sessions_pending_count) as (
+      select date_trunc('month', session_pending_time), count(*)
+        from wh_session_accumulating_fact
+       where session_pending_time < date_trunc('hour', now())
+    group by date_trunc('month', session_pending_time)
+  ),
+  monthly_range (month) as (
+      select date_trunc('month',time)
+        from generate_series(
+                 (select min(session_pending_time) from wh_session_accumulating_fact),
+                 now(),
+                 '1 month'::interval
+             ) as time
+  ),
+  final (start_time, end_time, sessions_pending_count) as (
+      -- select monthly_range.month - interval '1 month', -- start
+      select monthly_range.month, -- start
+             case when monthly_range.month = date_trunc('month', now())
+                    then date_trunc('hour', now())
+                  else monthly_range.month + interval '1 month'
+             end,
+             coalesce(monthly_counts.sessions_pending_count, 0)
+        from monthly_range
+   left join monthly_counts on monthly_range.month = monthly_counts.month
+  )
+    select start_time, end_time, sessions_pending_count
+      from final
+  order by start_time desc;
+  comment on view hcp_billing_monthly_sessions_all is
+    'hcp_billing_monthly_sessions_all is a view that contains '
+    'the sum of pending sessions for the current month and all previous months. '
+    'The current month is a sum from the beginning of the current month '
+    'until the start of the current hour (exclusive).';
+commit;

commit 47ddd2e40a4d974618ecfd370763c38333b92d18

fix(db): Fix generated series to yield consistent results
The hcp billing `_all` views built a range of buckets using the
`generate_series` function using the minimum value of the
`session_pending_time` column from the `wh_session_accumulating_fact`
table as the start time and `now()` as the end time. It then truncated
values in the series using the `date_trunc` function. This allowed the
generated series of buckets returned to vary and could result in one
additional bucket being included in the series depending on the current
time and the start time.

As an illustrative example, the `hourly_all` query used:

    select date_trunc('hour',time)
      from generate_series(
             (select min(session_pending_time) -- start time
                from wh_session_accumulating_fact),
              now(),
              '1 hour'::interval
           ) as time;

Substituting the start time with a hard coded value to show the issue:

    -- Show current time, where the minutes portion is 55
    select now();
                  now
    -------------------------------
     2023-05-22 14:55:40.567605+00
    (1 row)

    -- generate series with start time's minutes portion at 57
    select date_trunc('hour',time)
      from generate_series(
              '2023-05-22 01:57:00'::timestamptz,
              now(),
              '1 hour'::interval
           ) as time;

           date_trunc
    ------------------------
     2023-05-22 01:00:00+00
     2023-05-22 02:00:00+00
     2023-05-22 03:00:00+00
     2023-05-22 04:00:00+00
     2023-05-22 05:00:00+00
     2023-05-22 06:00:00+00
     2023-05-22 07:00:00+00
     2023-05-22 08:00:00+00
     2023-05-22 09:00:00+00
     2023-05-22 10:00:00+00
     2023-05-22 11:00:00+00
     2023-05-22 12:00:00+00
     2023-05-22 13:00:00+00
    (13 rows)

    -- Wait a few minutes
    select now();
                 now
    ------------------------------
     2023-05-22 14:58:48.75287+00
    (1 row)

    -- Now run again and get an extra result, even though still
    -- within the same hour
    select date_trunc('hour',time)
      from generate_series(
              '2023-05-22 01:57:00'::timestamptz,
              now(),
              '1 hour'::interval
           ) as time;

           date_trunc
    ------------------------
     2023-05-22 01:00:00+00
     2023-05-22 02:00:00+00
     2023-05-22 03:00:00+00
     2023-05-22 04:00:00+00
     2023-05-22 05:00:00+00
     2023-05-22 06:00:00+00
     2023-05-22 07:00:00+00
     2023-05-22 08:00:00+00
     2023-05-22 09:00:00+00
     2023-05-22 10:00:00+00
     2023-05-22 11:00:00+00
     2023-05-22 12:00:00+00
     2023-05-22 13:00:00+00
     2023-05-22 14:00:00+00
    (14 rows)

To fix this, we call the `generate_series` function truncating the start
time argument to the bucket size:

    -- Hourly
    select bucket
      from generate_series(
              date_trunc('hour', select min(session_pending_time)
                                   from wh_session_accumulating_fact)),
              now(),
              '1 hour'::interval
           ) as bucket;

    -- Monthly
    select bucket
      from generate_series(
              date_trunc('month', select min(session_pending_time)
                                    from wh_session_accumulating_fact)),
              now(),
              '1 month'::interval
           ) as bucket;

Fixes: 41b1b5b4b74b38141ddf934ff274dfb5c5b53d0e

internal/db/schema/migrations/oss/postgres/64/02_hcp_billing_hourly.up.sql
@@ -72,6 +72,7 @@ begin;
       '169 rows are returned: 1 for the current hour plus 168 for the previous 7 days. '
       'Rows are sorted by the hour in descending order.';
 
+  -- replaced in 70/02_hcp_billing_hourly.up.sql
   drop view if exists hcp_billing_hourly_sessions_all;
   create view hcp_billing_hourly_sessions_all as
   with

internal/db/schema/migrations/oss/postgres/64/03_hcp_billing_monthly.up.sql
@@ -71,6 +71,7 @@ begin;
     'The current month is a sum from the beginning of the current month '
     'until the start of the current hour (exclusive).';
 
+  -- replaced in 70/03_hcp_billing_monthly.up.sql
   drop view if exists hcp_billing_monthly_sessions_all;
   create view hcp_billing_monthly_sessions_all as
   with
@@ -107,4 +108,5 @@ begin;
     'the sum of pending sessions for the current month and all previous months. '
     'The current month is a sum from the beginning of the current month '
     'until the start of the current hour (exclusive).';
+
 commit;

internal/db/schema/migrations/oss/postgres/70/02_hcp_billing_hourly.up.sql
@@ -1,0 +1,36 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: MPL-2.0
+
+begin;
+
+  -- replaces view from 64/02_hcp_billing_hourly.up.sql
+  drop view hcp_billing_hourly_sessions_all;
+  create view hcp_billing_hourly_sessions_all as
+  with
+  hourly_counts (hour, sessions_pending_count) as (
+      select date_trunc('hour', session_pending_time), count(*)
+        from wh_session_accumulating_fact
+    group by date_trunc('hour', session_pending_time)
+  ),
+  hourly_range (hour) as (
+      select bucket
+        from generate_series(
+               date_trunc('hour', ( select min(session_pending_time) from wh_session_accumulating_fact ) ),
+               now(),
+               '1 hour'::interval
+           ) as bucket
+  ),
+  final (hour, sessions_pending_count) as (
+      select hourly_range.hour, coalesce(hourly_counts.sessions_pending_count, 0)
+        from hourly_range
+   left join hourly_counts on hourly_range.hour = hourly_counts.hour
+  )
+    select hour, sessions_pending_count
+      from final
+  order by hour desc;
+  comment on view hcp_billing_hourly_sessions_all is
+    'hcp_billing_hourly_sessions_all is a view where each row contains the timestamp for an hour and the sum of the pending sessions created in that hour. '
+      'A row is returned for each hour since the first session was created up to and including the current hour. '
+      'Rows are sorted by the hour in descending order.';
+
+commit;

internal/db/schema/migrations/oss/postgres/70/03_hcp_billing_monthly.up.sql
@@ -1,0 +1,44 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: MPL-2.0
+
+begin;
+
+  -- replaces view from 64/03_hcp_billing_monthly.up.sql
+  drop view hcp_billing_monthly_sessions_all;
+  create view hcp_billing_monthly_sessions_all as
+  with
+  monthly_counts (month, sessions_pending_count) as (
+      select date_trunc('month', session_pending_time), count(*)
+        from wh_session_accumulating_fact
+       where session_pending_time < date_trunc('hour', now())
+    group by date_trunc('month', session_pending_time)
+  ),
+  monthly_range (month) as (
+      select bucket
+        from generate_series(
+               date_trunc('month', ( select min(session_pending_time) from wh_session_accumulating_fact ) ),
+               now(),
+               '1 month'::interval
+           ) as bucket
+  ),
+  final (start_time, end_time, sessions_pending_count) as (
+      -- select monthly_range.month - interval '1 month', -- start
+      select monthly_range.month, -- start
+             case when monthly_range.month = date_trunc('month', now())
+                    then date_trunc('hour', now())
+                  else monthly_range.month + interval '1 month'
+             end,
+             coalesce(monthly_counts.sessions_pending_count, 0)
+        from monthly_range
+   left join monthly_counts on monthly_range.month = monthly_counts.month
+  )
+    select start_time, end_time, sessions_pending_count
+      from final
+  order by start_time desc;
+  comment on view hcp_billing_monthly_sessions_all is
+    'hcp_billing_monthly_sessions_all is a view that contains '
+    'the sum of pending sessions for the current month and all previous months. '
+    'The current month is a sum from the beginning of the current month '
+    'until the start of the current hour (exclusive).';
+
+commit;