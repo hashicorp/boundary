a85096d5f9f828f37e64626b7a45bb2ffec19b55
Jim <jlambert@hashicorp.com>
2023-06-14 06:27:17 -0400

    feat: add a "cache" CLI command
    This new command will support both managing and searching the
    local CLI cache daemon.

---
+++ internal/cmd/commands/cache/options.go
@@ -0,0 +1,38 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package cache
+
+import (
+	"context"
+)
+
+type options struct {
+	withDebug bool
+}
+
+// Option - how options are passed as args
+type Option func(*options) error
+
+func getDefaultOptions() options {
+	return options{}
+}
+
+func getOpts(opt ...Option) (options, error) {
+	opts := getDefaultOptions()
+
+	for _, o := range opt {
+		if err := o(&opts); err != nil {
+			return opts, err
+		}
+	}
+	return opts, nil
+}
+
+// WithDebug provides an optional debug flag.
+func WithDebug(_ context.Context, debug bool) Option {
+	return func(o *options) error {
+		o.withDebug = debug
+		return nil
+	}
+}
