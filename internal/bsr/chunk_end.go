be074ecbb3678e181ccc1c316ecd48c9ea87957d
Irena Rindos <irenarindos@users.noreply.github.com>
2023-06-28 08:38:57 -0400

    chore: fix op for end chunk (#3370)

--- internal/bsr/chunk_end.go
+++ internal/bsr/chunk_end.go
@@ -31,7 +31,7 @@ func (c *EndChunk) MarshalData(_ context.Context) ([]byte, error) {
 
 // NewEnd creates an EndChunk.
 func NewEnd(ctx context.Context, p Protocol, d Direction, t *Timestamp) (*EndChunk, error) {
-	const op = "bsr.NewHeader"
+	const op = "bsr.NewEnd"
 
 	bc, err := NewBaseChunk(ctx, p, d, t, ChunkEnd)
 	if err != nil {
