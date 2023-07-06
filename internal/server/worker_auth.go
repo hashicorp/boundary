a8e363888a5b12b9da3a24c6246a6658d20aac9e
Irena Rindos <irenarindos@users.noreply.github.com>
2023-07-06 15:07:28 -0400

    bug(workerAuth): allow duplicate workerAuth inserts if records match (#3389)
    * bug(workerAuth): allow duplicate workerAuth inserts if records match

--- internal/server/worker_auth.go
+++ internal/server/worker_auth.go
@@ -4,6 +4,7 @@
 package server
 
 import (
+	"bytes"
 	"context"
 
 	"github.com/hashicorp/boundary/internal/errors"
@@ -48,6 +49,21 @@ func (w *WorkerAuth) decrypt(ctx context.Context, cipher wrapping.Wrapper) error
 		return errors.Wrap(ctx, err, op, errors.WithCode(errors.Decrypt))
 	}
 	return nil
+}
+
+func (w *WorkerAuth) compare(other *WorkerAuth) bool {
+	switch {
+	case w.WorkerId != other.WorkerId:
+		return false
+	case !bytes.Equal(w.WorkerEncryptionPubKey, other.WorkerEncryptionPubKey):
+		return false
+	case !bytes.Equal(w.WorkerSigningPubKey, other.WorkerSigningPubKey):
+		return false
+	case !bytes.Equal(w.Nonce, other.Nonce):
+		return false
+	default:
+		return true
+	}
 }
 
 // WorkerAuthSet is intended to store a set of WorkerAuth records
