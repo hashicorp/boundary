e8ecf04346d4a85cb7ac3523d79280b3115b0c76
Todd <github@quaddi.com>
2023-08-09 13:34:09 -0700

    Cache refreshing now uses all stored personas. (#3545)

--- internal/daemon/cache/store.go
+++ internal/daemon/cache/store.go
@@ -67,24 +67,26 @@ begin;
 create table if not exists cache_persona (
   boundary_addr text not null,
   token_name text not null,
+  keyring_type text not null,
   auth_token_id text not null, -- Expected token id stored in the token name
   -- the timestamp has this default in order for the db to store fractional seconds. 
   last_accessed_time timestamp not null default (strftime('%Y-%m-%d %H:%M:%f','now')),
-  primary key (boundary_addr, token_name)
+  primary key (boundary_addr, keyring_type, token_name)
 );
 
 create table if not exists cache_target (
   boundary_addr text not null,
   token_name text not null,
+  keyring_type text not null,
   id text not null,
   name text,
   description text,
   address text,
   item text,
-  foreign key (boundary_addr, token_name)
-	references cache_persona(boundary_addr, token_name)
+  foreign key (boundary_addr, keyring_type, token_name)
+	references cache_persona(boundary_addr, keyring_type, token_name)
 	on delete cascade,
-  primary key (boundary_addr, token_name, id)
+  primary key (boundary_addr, keyring_type, token_name, id)
 );
 
 create table if not exists cache_api_error (
