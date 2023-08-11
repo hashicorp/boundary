6cb74e606d8885c61db8b153ddf7ac91de1ffd71
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-08-11 16:03:33 -0700

    Relicense as BUSL-1.1

--- internal/db/sqltest/tests/purge/deleted_table_tests.sql
+++ internal/db/sqltest/tests/purge/deleted_table_tests.sql
@@ -1,5 +1,5 @@
 -- Copyright (c) HashiCorp, Inc.
--- SPDX-License-Identifier: MPL-2.0
+-- SPDX-License-Identifier: BUSL-1.1
 
 begin;
   create function op_table(deletion_table_name name) returns text
