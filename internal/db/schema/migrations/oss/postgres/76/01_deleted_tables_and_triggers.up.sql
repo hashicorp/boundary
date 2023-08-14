9ff501513a15c7e6e67926923d67d681644c592a
Michael Milton <mikemountain@users.noreply.github.com>
2023-08-14 14:38:05 -0400

    feat: add job to purge pagination deletion tables (#3568)
    * prototyping
    
    * feat: add deletion tables and cleanup job
    
    * clean up comments and imports
    
    * address pr comments
    
    * move to subdirectory
    
    * prototyping
    
    * feat: add deletion tables and cleanup job
    
    * clean up comments and imports
    
    * make gen
    
    * pushing changes, confused about why rows scan is only returning one row in testing
    
    * fix query
    
    * rename cleanup to purge
    
    * rename sql tests dir from cleanup to purge
    
    * remove moved cleanup folder
    
    * address pr comments
    
    * Relicense as BUSL-1.1
    
    * schema: add on conflict clause to deleted table trigger
    
    The deleted table trigger now updates the delete time
    of the existing entry if there is already an entry with
    the same ID. This guarantees all consumers will see
    the deleted ID appropriately without the need to add
    a surrogate key to the tables.
    
    ---------
    
    Co-authored-by: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>

---
+++ internal/db/schema/migrations/oss/postgres/76/01_deleted_tables_and_triggers.up.sql
@@ -0,0 +1,129 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+  create table target_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table session_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table auth_method_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table auth_token_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table credential_library_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table credential_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table credential_store_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table iam_group_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table host_catalog_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table host_set_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table host_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table iam_role_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table iam_scope_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table storage_plugin_storage_bucket_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table iam_user_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table recording_session_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table auth_managed_group_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table server_worker_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create table auth_account_deleted (
+    public_id wt_public_id primary key,
+    delete_time wt_timestamp not null
+  );
+
+  create or replace function insert_deleted_id() returns trigger
+  as $$
+  begin
+    execute format('insert into %I (public_id, delete_time) values ($1, now()) on conflict (public_id) do update set delete_time = excluded.delete_time', tg_argv[0]) using old.public_id;
+    return old;
+  end;
+  $$ language plpgsql;
+
+  create function get_deletion_tables() returns setof name
+  as $$
+    select c.relname
+      from pg_catalog.pg_class c
+     where c.relkind in ('r')
+       and c.relname operator(pg_catalog.~) '^(.+_deleted)$' collate pg_catalog.default
+       and pg_catalog.pg_table_is_visible(c.oid);
+  $$ language sql;
+
+  do $$
+  declare
+    deletion_table_name text;
+    table_name          text;
+  begin
+    for deletion_table_name in select get_deletion_tables() loop
+      table_name := split_part(deletion_table_name, '_deleted', 1);
+      execute format('create trigger trigger_insert_on_deletion before delete on %I for each row execute function insert_deleted_id(''%I'')', table_name, deletion_table_name);
+      execute format('create index %I_delete_time_idx on %I (delete_time)', deletion_table_name, deletion_table_name);
+    end loop;
+  end;
+  $$ language plpgsql;
+commit;
