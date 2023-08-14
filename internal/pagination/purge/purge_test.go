c67a88ab56bdccf88a46e89fc4f9b605d8964b94
Michael Milton <mikemountain@users.noreply.github.com>
2023-08-14 14:38:05 -0400

    feat: add job to purge pagination deletion tables (#3568)
    * prototyping
    
    * feat: add deletion tables and cleanup job
    
    * clean up comments and imports
    
    * address pr comments
    
    * move to subdirectory
    
    * prototyping
    
    * feat: add deletion tables and cleanup job
    
    * clean up comments and imports
    
    * make gen
    
    * pushing changes, confused about why rows scan is only returning one row in testing
    
    * fix query
    
    * rename cleanup to purge
    
    * rename sql tests dir from cleanup to purge
    
    * remove moved cleanup folder
    
    * address pr comments
    
    * Relicense as BUSL-1.1
    
    * schema: add on conflict clause to deleted table trigger
    
    The deleted table trigger now updates the delete time
    of the existing entry if there is already an entry with
    the same ID. This guarantees all consumers will see
    the deleted ID appropriately without the need to add
    a surrogate key to the tables.
    
    ---------
    
    Co-authored-by: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>

---
+++ internal/pagination/purge/purge_test.go
@@ -0,0 +1,126 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package purge
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPurgeTables(t *testing.T) {
+	ctx := context.Background()
+
+	conn, _ := db.TestSetup(t, "postgres")
+	rw := db.New(conn)
+
+	db, err := conn.SqlDB(ctx)
+	if err != nil {
+		t.Errorf("error getting db connection %s", err)
+	}
+
+	rows, err := db.Query("select get_deletion_tables()")
+	if err != nil {
+		t.Errorf("unable to query for deletion tables %s", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var table string
+		err = rows.Scan(&table)
+		if err != nil {
+			t.Errorf("unable to scan rows for deletion tables %s", err)
+		}
+		_, err = db.Exec(fmt.Sprintf("insert into %s (public_id, delete_time) values ('p1234567890', $1)", table), time.Now())
+		if err != nil {
+			t.Errorf("error updating %s %s", table, err)
+		}
+		_, err = db.Exec(fmt.Sprintf("insert into %s (public_id, delete_time) values ('p9876543210', $1)", table), time.Now().AddDate(0, -2, 0))
+		if err != nil {
+			t.Errorf("error updating %s %s", table, err)
+		}
+
+		query := fmt.Sprintf("delete from %s where delete_time < now() - interval '30 days'", table)
+		sJob := purgeJob{
+			w:     rw,
+			table: table,
+			query: query,
+		}
+
+		err = sJob.Run(ctx)
+		require.NoError(t, err)
+
+		var count int
+		err = db.QueryRowContext(ctx, fmt.Sprintf("select count(public_id) from %s", table)).Scan(&count)
+		if err != nil {
+			t.Errorf("error checking %s table %s", table, err)
+		}
+		require.Equal(t, 1, count)
+	}
+}
+
+func TestNewPurgeJob(t *testing.T) {
+	ctx := context.Background()
+
+	conn, _ := db.TestSetup(t, "postgres")
+	rw := db.New(conn)
+
+	type args struct {
+		w     db.Writer
+		table string
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantIsErr  errors.Code
+		wantErrMsg string
+	}{
+		{
+			name: "valid",
+			args: args{
+				w:     rw,
+				table: "valid-table",
+			},
+		},
+		{
+			name: "nil-writer",
+			args: args{
+				w:     nil,
+				table: "valid-table",
+			},
+			wantIsErr:  errors.InvalidParameter,
+			wantErrMsg: "purgeJob.newPurgeJob: missing db.Writer: parameter violation: error #100",
+		},
+		{
+			name: "no table",
+			args: args{
+				w:     rw,
+				table: "",
+			},
+			wantIsErr:  errors.InvalidParameter,
+			wantErrMsg: "purgeJob.newPurgeJob: missing table: parameter violation: error #100",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			got, err := newPurgeJob(ctx, tt.args.w, tt.args.table)
+			if tt.wantIsErr != 0 {
+				assert.Truef(errors.Match(errors.T(tt.wantIsErr), err), "Unexpected error %s", err)
+				assert.Equal(tt.wantErrMsg, err.Error())
+				return
+			}
+			assert.NoError(err)
+			require.NotNil(got)
+		})
+	}
+}
