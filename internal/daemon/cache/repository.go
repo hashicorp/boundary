2a6a3e52f4cb2536a01a13b02f7b7f324e3e3647
Todd <github@quaddi.com>
2023-08-16 06:54:50 -0700

    Moved search to use mql and added tests for the search command (#3589)
    * search uses hashicorp/mql

--- internal/daemon/cache/repository.go
+++ internal/daemon/cache/repository.go
@@ -8,9 +8,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	stdErrors "errors"
-	"reflect"
-	"regexp"
-	"strings"
+	"fmt"
 	"time"
 
 	"github.com/hashicorp/boundary/api/authtokens"
@@ -19,7 +17,7 @@ import (
 	"github.com/hashicorp/boundary/internal/errors"
 	"github.com/hashicorp/boundary/internal/types/resource"
 	"github.com/hashicorp/boundary/internal/util"
-	"github.com/seldonio/goven/sql_adaptor"
+	"github.com/hashicorp/mql"
 )
 
 const (
@@ -269,6 +267,25 @@ func (r *Repository) RefreshTargets(ctx context.Context, p *Persona, targets []*
 	return nil
 }
 
+func (r *Repository) ListTargets(ctx context.Context, p *Persona) ([]*targets.Target, error) {
+	const op = "cache.(Repository).ListTargets"
+	switch {
+	case p == nil:
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "persona is missing")
+	case p.TokenName == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "token name is missing")
+	case p.KeyringType == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "keyring type is missing")
+	case p.BoundaryAddr == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "boundary address is missing")
+	}
+	ret, err := r.searchTargets(ctx, p, "true", nil)
+	if err != nil {
+		return nil, errors.Wrap(ctx, err, op)
+	}
+	return ret, nil
+}
+
 func (r *Repository) QueryTargets(ctx context.Context, p *Persona, query string) ([]*targets.Target, error) {
 	const op = "cache.(Repository).QueryTargets"
 	switch {
@@ -276,114 +293,47 @@ func (r *Repository) QueryTargets(ctx context.Context, p *Persona, query string)
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "persona is missing")
 	case p.TokenName == "":
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "token name is missing")
+	case p.KeyringType == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "keyring type is missing")
 	case p.BoundaryAddr == "":
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "boundary address is missing")
 	case query == "":
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "query is missing")
 	}
-	var cachedTargets []*Target
 
-	reflection := reflect.ValueOf(&Target{})
-	defaultFields := sql_adaptor.FieldParseValidatorFromStruct(reflection)
-	delete(defaultFields, "item")
-	delete(defaultFields, "create_time")
-	delete(defaultFields, "token_name")
-
-	matchers := map[*regexp.Regexp]sql_adaptor.ParseValidateFunc{}
-	fieldMappings := map[string]string{}
-	queryAdaptor := sql_adaptor.NewSqlAdaptor(fieldMappings, defaultFields, matchers)
-
-	parsedQuery, err := queryAdaptor.Parse(query)
+	w, err := mql.Parse(query, Target{}, mql.WithIgnoredFields("BoundaryAddr", "KeyringType", "TokenName", "Item"))
+	if err != nil {
+		return nil, errors.Wrap(ctx, err, op, errors.WithCode(errors.InvalidParameter))
+	}
+	ret, err := r.searchTargets(ctx, p, w.Condition, w.Args)
 	if err != nil {
 		return nil, errors.Wrap(ctx, err, op)
 	}
-	if err := r.rw.SearchWhere(ctx, &cachedTargets, parsedQuery.Raw, sql_adaptor.StringSliceToInterfaceSlice(parsedQuery.Values), db.WithLimit(-1)); err != nil {
-		return nil, errors.Wrap(ctx, err, op)
-	}
-	retTargets := make([]*targets.Target, 0, len(cachedTargets))
-	for _, cachedTar := range cachedTargets {
-		var tar targets.Target
-		if err := json.Unmarshal([]byte(cachedTar.Item), &tar); err != nil {
-			return nil, errors.Wrap(ctx, err, op)
-		}
-		retTargets = append(retTargets, &tar)
-	}
-	return retTargets, nil
+	return ret, nil
 }
 
-func (r *Repository) FindTargets(ctx context.Context, p *Persona, opt ...Option) ([]*targets.Target, error) {
-	const op = "cache.(Repository).FindTargets"
+func (r *Repository) searchTargets(ctx context.Context, p *Persona, condition string, searchArgs []any) ([]*targets.Target, error) {
+	const op = "cache.(Repository).searchTargets"
 	switch {
 	case p == nil:
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "persona is missing")
 	case p.TokenName == "":
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "token name is missing")
+	case p.KeyringType == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "keyring type is missing")
 	case p.BoundaryAddr == "":
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "boundary address is missing")
+	case condition == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "condition is missing")
 	}
+
+	condition = fmt.Sprintf("%s and (boundary_addr = ? and keyring_type = ? and token_name = ?)", condition)
+	args := append(searchArgs, p.BoundaryAddr, p.KeyringType, p.TokenName)
 	var cachedTargets []*Target
-
-	opts, err := getOpts(opt...)
-	if err != nil {
+	if err := r.rw.SearchWhere(ctx, &cachedTargets, condition, args); err != nil {
 		return nil, errors.Wrap(ctx, err, op)
 	}
-	whereClause := []string{"boundary_addr = @boundary_addr and token_name = @token_name"}
-	whereParameters := []any{sql.Named("boundary_addr", p.BoundaryAddr), sql.Named("token_name", p.TokenName)}
 
-	if opts.withIdContains != "" {
-		whereClause = append(whereClause, "id like @contains_id")
-		whereParameters = append(whereParameters, sql.Named("contains_id", "%"+opts.withIdContains+"%"))
-	}
-	if opts.withNameContains != "" {
-		whereClause = append(whereClause, "name like @contains_name")
-		whereParameters = append(whereParameters, sql.Named("contains_name", "%"+opts.withNameContains+"%"))
-	}
-	if opts.withDescriptionContains != "" {
-		whereClause = append(whereClause, "description like @contains_description")
-		whereParameters = append(whereParameters, sql.Named("contains_description", "%"+opts.withDescriptionContains+"%"))
-	}
-	if opts.withAddressContains != "" {
-		whereClause = append(whereClause, "address like @contains_address")
-		whereParameters = append(whereParameters, sql.Named("contains_address", "%"+opts.withAddressContains+"%"))
-	}
-
-	if opts.withIdStartsWith != "" {
-		whereClause = append(whereClause, "id like @starts_with_id")
-		whereParameters = append(whereParameters, sql.Named("starts_with_id", opts.withIdStartsWith+"%"))
-	}
-	if opts.withNameStartsWith != "" {
-		whereClause = append(whereClause, "name like @starts_with_name")
-		whereParameters = append(whereParameters, sql.Named("starts_with_name", opts.withNameStartsWith+"%"))
-	}
-	if opts.withDescriptionStartsWith != "" {
-		whereClause = append(whereClause, "description like @starts_with_description")
-		whereParameters = append(whereParameters, sql.Named("starts_with_description", opts.withDescriptionStartsWith+"%"))
-	}
-	if opts.withAddressStartsWith != "" {
-		whereClause = append(whereClause, "address like @starts_with_address")
-		whereParameters = append(whereParameters, sql.Named("starts_with_address", opts.withAddressStartsWith+"%"))
-	}
-
-	if opts.withIdEndsWith != "" {
-		whereClause = append(whereClause, "id like @ends_with_id")
-		whereParameters = append(whereParameters, sql.Named("ends_with_id", "%"+opts.withIdEndsWith))
-	}
-	if opts.withNameEndsWith != "" {
-		whereClause = append(whereClause, "name like @ends_with_name")
-		whereParameters = append(whereParameters, sql.Named("ends_with_name", "%"+opts.withNameEndsWith))
-	}
-	if opts.withDescriptionEndsWith != "" {
-		whereClause = append(whereClause, "description like @ends_with_description")
-		whereParameters = append(whereParameters, sql.Named("ends_with_description", "%"+opts.withDescriptionEndsWith))
-	}
-	if opts.withAddressEndsWith != "" {
-		whereClause = append(whereClause, "address like @ends_with_address")
-		whereParameters = append(whereParameters, sql.Named("ends_with_address", "%"+opts.withAddressEndsWith))
-	}
-
-	if err := r.rw.SearchWhere(ctx, &cachedTargets, strings.Join(whereClause, " and "), whereParameters, db.WithLimit(-1)); err != nil {
-		return nil, errors.Wrap(ctx, err, op)
-	}
 	retTargets := make([]*targets.Target, 0, len(cachedTargets))
 	for _, cachedTar := range cachedTargets {
 		var tar targets.Target
