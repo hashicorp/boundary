7ac4c105701cecec5de9f97fca50af4aea830b15
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-08-28 16:51:32 -0700

    internal/target: add support for pagination

--- internal/target/tcp/repository_test.go
+++ internal/target/tcp/repository_test.go
@@ -141,7 +141,93 @@ func TestRepository_LookupTarget(t *testing.T) {
 	}
 }
 
-func TestRepository_ListRoles_Multiple_Scopes(t *testing.T) {
+func TestRepository_ListTargets(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	conn, _ := db.TestSetup(t, "postgres")
+	wrapper := db.TestWrapper(t)
+	testKms := kms.TestKms(t, conn, wrapper)
+	iamRepo := iam.TestRepo(t, conn, wrapper)
+	_, proj1 := iam.TestScopes(t, iamRepo)
+
+	total := 5
+	for i := 0; i < total; i++ {
+		tcp.TestTarget(ctx, t, conn, proj1.GetPublicId(), fmt.Sprintf("proj1-%d", i))
+	}
+
+	rw := db.New(conn)
+	repo, err := target.NewRepository(ctx, rw, rw, testKms,
+		target.WithPermissions([]perms.Permission{
+			{
+				ScopeId:  proj1.PublicId,
+				Resource: resource.Target,
+				Action:   action.List,
+				All:      true,
+			},
+		}),
+	)
+	require.NoError(t, err)
+
+	t.Run("no-options", func(t *testing.T) {
+		got, err := repo.ListTargets(ctx)
+		require.NoError(t, err)
+		assert.Equal(t, total, len(got))
+	})
+
+	t.Run("withStartPageAfter", func(t *testing.T) {
+		assert, require := assert.New(t), require.New(t)
+
+		page1, err := repo.ListTargets(
+			context.Background(),
+			target.WithLimit(2),
+		)
+		require.NoError(err)
+		require.Len(page1, 2)
+		page2, err := repo.ListTargets(
+			context.Background(),
+			target.WithLimit(2),
+			target.WithStartPageAfterItem(page1[1].GetPublicId(), page1[1].GetUpdateTime().AsTime()),
+		)
+		require.NoError(err)
+		require.Len(page2, 2)
+		for _, item := range page1 {
+			assert.NotEqual(item.GetPublicId(), page2[0].GetPublicId())
+			assert.NotEqual(item.GetPublicId(), page2[1].GetPublicId())
+		}
+		page3, err := repo.ListTargets(
+			context.Background(),
+			target.WithLimit(2),
+			target.WithStartPageAfterItem(page2[1].GetPublicId(), page2[1].GetUpdateTime().AsTime()),
+		)
+		require.NoError(err)
+		require.Len(page3, 1)
+		for _, item := range append(page1, page2...) {
+			assert.NotEqual(item.GetPublicId(), page3[0].GetPublicId())
+		}
+		page4, err := repo.ListTargets(
+			context.Background(),
+			target.WithLimit(2),
+			target.WithStartPageAfterItem(page3[0].GetPublicId(), page3[0].GetUpdateTime().AsTime()),
+		)
+		require.NoError(err)
+		require.Empty(page4)
+
+		// Update the first session and check that it gets listed subsequently
+		page1[0].SetName("new-name")
+		_, _, err = repo.UpdateTarget(ctx, page1[0], page1[0].GetVersion(), []string{"name"})
+		require.NoError(err)
+		page5, err := repo.ListTargets(
+			context.Background(),
+			target.WithLimit(2),
+			target.WithStartPageAfterItem(page3[0].GetPublicId(), page3[0].GetUpdateTime().AsTime()),
+		)
+		require.NoError(err)
+		require.Len(page5, 1)
+		require.Equal(page5[0].GetPublicId(), page1[0].GetPublicId())
+	})
+}
+
+func TestRepository_ListTargets_Multiple_Scopes(t *testing.T) {
 	t.Parallel()
 	ctx := context.Background()
 	conn, _ := db.TestSetup(t, "postgres")
@@ -309,4 +395,70 @@ func TestRepository_ListRoles_Above_Default_Count(t *testing.T) {
 	for _, tar := range got {
 		assert.Equal(t, "1.2.3.4", tar.GetAddress())
 	}
+}
+
+func TestListDeletedIds(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	conn, _ := db.TestSetup(t, "postgres")
+	wrapper := db.TestWrapper(t)
+	testKms := kms.TestKms(t, conn, wrapper)
+	iamRepo := iam.TestRepo(t, conn, wrapper)
+	_, proj1 := iam.TestScopes(t, iamRepo)
+
+	rw := db.New(conn)
+	repo, err := target.NewRepository(ctx, rw, rw, testKms)
+	require.NoError(t, err)
+
+	// Expect no entries at the start
+	deletedIds, err := repo.ListDeletedIds(ctx, time.Now().AddDate(-1, 0, 0))
+	require.NoError(t, err)
+	require.Empty(t, deletedIds)
+
+	// Delete a session
+	tg := tcp.TestTarget(ctx, t, conn, proj1.GetPublicId(), "deleteme")
+	_, err = repo.DeleteTarget(ctx, tg.GetPublicId())
+	require.NoError(t, err)
+
+	// Expect a single entry
+	deletedIds, err = repo.ListDeletedIds(ctx, time.Now().AddDate(-1, 0, 0))
+	require.NoError(t, err)
+	require.Equal(t, []string{tg.GetPublicId()}, deletedIds)
+
+	// Try again with the time set to now, expect no entries
+	deletedIds, err = repo.ListDeletedIds(ctx, time.Now())
+	require.NoError(t, err)
+	require.Empty(t, deletedIds)
+}
+
+func TestGetTotalItems(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	conn, _ := db.TestSetup(t, "postgres")
+	wrapper := db.TestWrapper(t)
+	testKms := kms.TestKms(t, conn, wrapper)
+	iamRepo := iam.TestRepo(t, conn, wrapper)
+	_, proj1 := iam.TestScopes(t, iamRepo)
+
+	rw := db.New(conn)
+	repo, err := target.NewRepository(ctx, rw, rw, testKms)
+	require.NoError(t, err)
+
+	// Check total entries at start, expect 0
+	numItems, err := repo.GetTotalItems(ctx)
+	require.NoError(t, err)
+	assert.Equal(t, 0, numItems)
+
+	// Create a session, expect 1 entries
+	tg := tcp.TestTarget(ctx, t, conn, proj1.GetPublicId(), "target1")
+	numItems, err = repo.GetTotalItems(ctx)
+	require.NoError(t, err)
+	assert.Equal(t, 1, numItems)
+
+	// Delete the session, expect 0 again
+	_, err = repo.DeleteTarget(ctx, tg.GetPublicId())
+	require.NoError(t, err)
+	numItems, err = repo.GetTotalItems(ctx)
+	require.NoError(t, err)
+	assert.Equal(t, 0, numItems)
 }
