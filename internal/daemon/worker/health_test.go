1465cd179104156bfea7b0787a3e8450f9f49e12
Elim Tsiagbey <elim.tsiagbey@hashicorp.com>
2023-08-31 18:23:57 -0400

    fix space change

--- internal/daemon/worker/health_test.go
+++ internal/daemon/worker/health_test.go
@@ -64,7 +64,6 @@ func TestGetHealth(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			path := "/health"
 			if tt.queryParams != "" {
 				path = fmt.Sprintf("%s?%s", path, tt.queryParams)
