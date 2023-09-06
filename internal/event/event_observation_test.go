16d83945ce0a6032e06ceffcb955d0d6af1cff0c
April Ayres <april.ayres@hashicorp.com>
2023-09-06 15:38:11 -0700

    observation(event): refine telemetry filter, and WithTelemetry()

--- internal/event/event_observation_test.go
+++ internal/event/event_observation_test.go
@@ -4,14 +4,13 @@
 package event
 
 import (
-	"encoding/json"
 	"testing"
 	"time"
 
-	"github.com/hashicorp/boundary/internal/gen/controller/servers"
-	"github.com/hashicorp/boundary/internal/gen/controller/servers/services"
+	"github.com/hashicorp/eventlogger"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
 func Test_newObservation(t *testing.T) {
@@ -143,48 +142,236 @@ func Test_observationEventType(t *testing.T) {
 	assert.Equal(t, string(ObservationType), e.EventType())
 }
 
-func Test_iterateProto(t *testing.T) {
-	assert, _ := assert.New(t), require.New(t)
-	input := Request{
-		Operation: "",
-		Endpoint:  "",
-		Details: &services.StatusRequest{
-			Jobs: []*services.JobStatus{
-				{Job: &services.Job{
-					Type:    1,
-					JobInfo: nil,
-				}},
+func Test_composeFromTelemetryFiltering(t *testing.T) {
+	t.Parallel()
+
+	now := time.Now()
+
+	tests := []struct {
+		name                 string
+		fromOp               Op
+		opts                 []Option
+		wantObservation      *observation
+		wantErrIs            error
+		wantErrContains      string
+		wantFilteredRequest  *Request
+		wantFilteredResponse *Response
+	}{
+		{
+			name:   "with-request-no-telemetry",
+			fromOp: Op("with-request-no-telemetry"),
+			opts: []Option{
+				WithId("with-request-no-telemetry"),
+				WithRequestInfo(TestRequestInfo(t)),
+				WithFlush(),
+				WithRequest(&Request{
+					Operation: "op",
+					Endpoint:  "/worker-status/<id>",
+					Details:   testWorkerStatus(t),
+				}),
 			},
-			UpdateTags: false,
-			WorkerStatus: &servers.ServerWorkerStatus{
-				PublicId:    "testID",
-				Name:        "w_1234567890",
-				Description: "A default worker created in",
-				Address:     "127.0.0.1:9202",
-				Tags: []*servers.TagPair{
+			wantObservation: &observation{
+				ID:          "with-request-no-telemetry",
+				Flush:       true,
+				Version:     errorVersion,
+				Op:          Op("with-request-no-telemetry"),
+				RequestInfo: TestRequestInfo(t),
+			},
+		},
+		{
+			name:   "with-response-no-telemetry",
+			fromOp: Op("with-response-no-telemetry"),
+			opts: []Option{
+				WithId("with-response-no-telemetry"),
+				WithRequestInfo(TestRequestInfo(t)),
+				WithFlush(),
+				WithResponse(&Response{
+					StatusCode: 200,
+					Details:    testWorkerStatus(t),
+				}),
+			},
+			wantObservation: &observation{
+				ID:          "with-response-no-telemetry",
+				Flush:       true,
+				Version:     errorVersion,
+				Op:          Op("with-response-no-telemetry"),
+				RequestInfo: TestRequestInfo(t),
+			},
+		},
+		{
+			name:   "with-request-with-telemetry",
+			fromOp: Op("with-request-with-telemetry"),
+			opts: []Option{
+				WithId("with-request-with-telemetry"),
+				WithRequestInfo(TestRequestInfo(t)),
+				WithFlush(),
+				WithRequest(&Request{
+					Operation: "op",
+					Endpoint:  "/worker-status/<id>",
+					Details:   testWorkerStatus(t),
+				}),
+				WithTelemetry(),
+			},
+			wantObservation: &observation{
+				ID:          "with-request-with-telemetry",
+				Flush:       true,
+				Version:     errorVersion,
+				Op:          Op("with-request-with-telemetry"),
+				RequestInfo: TestRequestInfo(t),
+				Request: &Request{
+					Operation: "op",
+					Endpoint:  "/worker-status/<id>",
+					Details:   testWorkerStatus(t),
+				},
+			},
+			wantFilteredRequest: &Request{
+				Operation: "op",
+				Endpoint:  "/worker-status/<id>",
+				Details:   testWorkerStatusObservable(t),
+			},
+		},
+		{
+			name:   "with-response-with-telemetry",
+			fromOp: Op("with-response-with-telemetry"),
+			opts: []Option{
+				WithId("with-response-with-telemetry"),
+				WithRequestInfo(TestRequestInfo(t)),
+				WithFlush(),
+				WithResponse(&Response{
+					StatusCode: 200,
+					Details:    testWorkerStatus(t),
+				}),
+				WithTelemetry(),
+			},
+			wantObservation: &observation{
+				ID:          "with-response-with-telemetry",
+				Flush:       true,
+				Version:     errorVersion,
+				Op:          Op("with-response-with-telemetry"),
+				RequestInfo: TestRequestInfo(t),
+				Response: &Response{
+					StatusCode: 200,
+					Details:    testWorkerStatus(t),
+				},
+			},
+			wantFilteredResponse: &Response{
+				StatusCode: 200,
+				Details:    testWorkerStatusObservable(t),
+			},
+		},
+		{
+			name:   "nil-request-details-with-telemetry",
+			fromOp: Op("nil-request-details-with-telemetry"),
+			opts: []Option{
+				WithId("nil-request-details-with-telemetry"),
+				WithRequestInfo(TestRequestInfo(t)),
+				WithFlush(),
+				WithRequest(&Request{
+					Operation: "op",
+					Endpoint:  "/worker-status/<id>",
+					Details:   nil,
+				}),
+				WithTelemetry(),
+			},
+			wantObservation: &observation{
+				ID:          "nil-request-details-with-telemetry",
+				Flush:       true,
+				Version:     errorVersion,
+				Op:          Op("nil-request-details-with-telemetry"),
+				RequestInfo: TestRequestInfo(t),
+				Request: &Request{
+					Operation: "op",
+					Endpoint:  "/worker-status/<id>",
+					Details:   nil,
+				},
+			},
+			wantFilteredRequest: &Request{
+				Operation: "op",
+				Endpoint:  "/worker-status/<id>",
+				Details:   nil,
+			},
+		},
+		{
+			name:   "nil-response-details-with-telemetry",
+			fromOp: Op("nil-response-details-with-telemetry"),
+			opts: []Option{
+				WithId("nil-response-details-with-telemetry"),
+				WithRequestInfo(TestRequestInfo(t)),
+				WithFlush(),
+				WithResponse(&Response{
+					StatusCode: 200,
+					Details:    nil,
+				}),
+				WithTelemetry(),
+			},
+			wantObservation: &observation{
+				ID:          "nil-response-details-with-telemetry",
+				Flush:       true,
+				Version:     errorVersion,
+				Op:          Op("nil-response-details-with-telemetry"),
+				RequestInfo: TestRequestInfo(t),
+				Response: &Response{
+					StatusCode: 200,
+					Details:    nil,
+				},
+			},
+			wantFilteredResponse: &Response{
+				StatusCode: 200,
+				Details:    nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			got, err := newObservation(tt.fromOp, tt.opts...)
+			require.NoError(err)
+			require.NotNil(got)
+			opts := getOpts(tt.opts...)
+			if opts.withId == "" {
+				tt.wantObservation.ID = got.ID
+			}
+			assert.Equal(tt.wantObservation, got)
+			// feed the event through ComposeFrom which will filter any request/response data
+			_, ev, err := got.ComposeFrom(
+				[]*eventlogger.Event{
 					{
-						Key:   "type",
-						Value: "dev",
+						Type:      "observation",
+						CreatedAt: now,
+						Payload:   got,
 					},
 				},
-				KeyId:            "ovary-valid-curler-scrambled-glutinous-alias-rework-debit",
-				ReleaseVersion:   "Boundary v0.13.1",
-				OperationalState: "active",
-			},
-			ConnectedWorkerKeyIdentifiers:         nil,
-			ConnectedUnmappedWorkerKeyIdentifiers: nil,
-			ConnectedWorkerPublicIds:              nil,
-		},
-		DetailsUpstreamMessage: nil,
+			)
+			if tt.wantErrIs != nil {
+				require.Error(err)
+				assert.Nil(got)
+				assert.ErrorIs(err, tt.wantErrIs)
+				if tt.wantErrContains != "" {
+					assert.Contains(err.Error(), tt.wantErrContains)
+				}
+				return
+			}
+			payload, ok := ev.(map[string]any)
+			assert.True(ok)
+			assert.NotNil(payload)
+			if tt.wantFilteredRequest != nil {
+				req, ok := payload["request"]
+				assert.True(ok)
+				assert.NotNil(req)
+				pmsg, ok := req.(*Request)
+				assert.True(ok)
+				assert.NotNil(pmsg)
+				assert.True(proto.Equal(tt.wantFilteredRequest.Details, pmsg.Details))
+			}
+			if tt.wantFilteredResponse != nil {
+				req, ok := payload["response"]
+				assert.True(ok)
+				assert.NotNil(req)
+				pmsg, ok := req.(*Response)
+				assert.True(ok)
+				assert.NotNil(pmsg)
+				assert.True(proto.Equal(tt.wantFilteredResponse.Details, pmsg.Details))
+			}
+		})
 	}
-
-	res := recurseStructureWithTagFilter(
-		input.Details,
-		map[string]string{
-			"eventstream": "observation",
-		},
-		false,
-	)
-	data, _ := json.Marshal(res)
-	assert.NotNil(data)
 }
