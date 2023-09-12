151fa44d47748f62eb781f96ff1768ef51e6f757
irenarindos <irena.rindos@gmail.com>
2023-09-12 10:22:30 -0400

    fixup! chore: update go-eventlogger

--- internal/daemon/common/handler_test.go
+++ internal/daemon/common/handler_test.go
@@ -490,6 +490,7 @@ func (b *testMockBroker) StopTimeAt(t time.Time) {}
 func (b *testMockBroker) RegisterNode(id eventlogger.NodeID, node eventlogger.Node, opt ...eventlogger.Option) error {
 	return nil
 }
+
 func (b *testMockBroker) SetSuccessThreshold(t eventlogger.EventType, successThreshold int) error {
 	return nil
 }
