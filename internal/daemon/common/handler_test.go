bf595f8599d560430014f527c39b6c8935dc6634
Irena Rindos <irenarindos@users.noreply.github.com>
2023-09-14 16:06:10 -0400

    feat(events): add deregister pipeline (#3729)
    * feat(events): add deregister pipeline

--- internal/daemon/common/handler_test.go
+++ internal/daemon/common/handler_test.go
@@ -491,6 +491,10 @@ func (b *testMockBroker) RegisterNode(id eventlogger.NodeID, node eventlogger.No
 	return nil
 }
 
+func (b *testMockBroker) RemovePipelineAndNodes(ctx context.Context, t eventlogger.EventType, id eventlogger.PipelineID) (bool, error) {
+	return true, nil
+}
+
 func (b *testMockBroker) SetSuccessThreshold(t eventlogger.EventType, successThreshold int) error {
 	return nil
 }
