4a91d3c34db3ed96333a1462e9591c9d97abc47b
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-09-21 14:51:03 -0700

    db: add TransactionTimestamp
    The TransactionTimestamp method returns the timestamp
    of the current transaction.

--- internal/db/read_writer.go
+++ internal/db/read_writer.go
@@ -70,6 +70,9 @@ type Reader interface {
 
 	// ScanRows will scan sql rows into the interface provided
 	ScanRows(ctx context.Context, rows *sql.Rows, result any) error
+
+	// TransactionTimestamp returns the current transaction timestamp.
+	TransactionTimestamp(ctx context.Context) (time.Time, error)
 }
 
 // Writer interface defines create, update and retryable transaction handlers
@@ -523,6 +526,22 @@ func (rw *Db) SearchWhere(ctx context.Context, resources any, where string, args
 		return wrapError(ctx, err, op)
 	}
 	return nil
+}
+
+// TransactionTimestamp returns the current transaction timestamp.
+func (rw *Db) TransactionTimestamp(ctx context.Context) (time.Time, error) {
+	const op = "db.(*Db).TransactionTimestamp"
+	rows, err := rw.Query(ctx, "select current_timestamp", nil)
+	if err != nil {
+		return time.Time{}, errors.Wrap(ctx, err, op, errors.WithMsg("failed to query current timestamp"))
+	}
+	var now time.Time
+	for rows.Next() {
+		if err := rw.ScanRows(ctx, rows, &now); err != nil {
+			return time.Time{}, errors.Wrap(ctx, err, op, errors.WithMsg("failed to query current timestamp"))
+		}
+	}
+	return now, nil
 }
 
 func isNil(i any) bool {
