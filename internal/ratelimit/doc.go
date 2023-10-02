59449df661052dd0a0786acecc4c31ed698f49a2
Timothy Messier <tim.messier@gmail.com>
2023-10-02 20:56:35 +0000

    feat(ratelimit): Define initial ratelimit package and Config

---
+++ internal/ratelimit/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+// Package ratelimit provides the rate limit configuration and http middleware
+// for use by the controller's http API.
+package ratelimit
