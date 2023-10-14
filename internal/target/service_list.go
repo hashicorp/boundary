c64c2b503b7d265b1aabadea48b4c81ee1a58a8c
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-10-13 22:17:33 -0700

    internal/target: migrate to generic pagination

--- internal/target/service_list.go
+++ internal/target/service_list.go
@@ -6,89 +6,31 @@ package target
 import (
 	"context"
 
-	"github.com/hashicorp/boundary/internal/errors"
-	"github.com/hashicorp/boundary/internal/refreshtoken"
+	"github.com/hashicorp/boundary/internal/pagination"
 )
-
-// This function is a callback passed down from the application service layer
-// used to filter out protobuf targets that don't match any user-supplied filter.
-type ListFilterFunc func(Target) (bool, error)
 
 // List lists targets according to the page size,
 // filtering out entries that do not
-// pass the filter item fn. It returns a new refresh token
+// pass the filter item function. It returns a new refresh token
 // based on the grants hash and the returned targets.
 func List(
 	ctx context.Context,
 	repo *Repository,
 	grantsHash []byte,
 	pageSize int,
-	filterItemFn ListFilterFunc,
-) (*ListResponse, error) {
-	const op = "target.List"
-
-	limit := pageSize + 1
-	opts := []Option{
-		WithLimit(limit),
-	}
-
-	targets := make([]Target, 0, limit)
-dbLoop:
-	for {
-		// Request another page from the DB until we fill the final items
-		page, err := repo.listTargets(ctx, opts...)
-		if err != nil {
-			return nil, errors.Wrap(ctx, err, op)
-		}
-		for _, item := range page {
-			ok, err := filterItemFn(item)
-			if err != nil {
-				return nil, err
-			}
-			if ok {
-				targets = append(targets, item)
-				// If we filled the items after filtering,
-				// we're done.
-				if len(targets) == cap(targets) {
-					break dbLoop
-				}
-			}
-		}
-		// If the current page was shorter than the limit, stop iterating
-		if len(page) < limit {
-			break dbLoop
-		}
-
-		opts = []Option{
+	filterItemFn pagination.ListFilterFunc[Target],
+) (*pagination.ListResponse2[Target], error) {
+	listItemsFn := func(ctx context.Context, lastPageItem Target, limit int) ([]Target, error) {
+		opts := []Option{
 			WithLimit(limit),
-			WithStartPageAfterItem(page[len(page)-1].GetPublicId(), page[len(page)-1].GetUpdateTime().AsTime()),
 		}
-	}
-	// If we couldn't fill the items, it was a complete listing.
-	completeListing := len(targets) < cap(targets)
-	totalItems := len(targets)
-	if !completeListing {
-		// Items is of size pageSize+1, so
-		// truncate if it was filled.
-		targets = targets[:pageSize]
-		// If this was not a complete listing, get an estimate
-		// of the total items from the DB.
-		var err error
-		totalItems, err = repo.estimatedCount(ctx)
-		if err != nil {
-			return nil, errors.Wrap(ctx, err, op)
+		if lastPageItem != nil {
+			opts = append(opts,
+				WithStartPageAfterItem(lastPageItem.GetPublicId(), lastPageItem.GetUpdateTime().AsTime()),
+			)
 		}
+		return repo.listTargets(ctx, opts...)
 	}
 
-	resp := &ListResponse{
-		Items:               targets,
-		EstimatedTotalItems: totalItems,
-		CompleteListing:     completeListing,
-	}
-
-	if len(targets) > 0 {
-		resp.RefreshToken = refreshtoken.FromResource(targets[len(targets)-1], grantsHash)
-	}
-
-	return resp, nil
+	return pagination.List(ctx, grantsHash, pageSize, filterItemFn, listItemsFn, repo.estimatedCount)
 }
