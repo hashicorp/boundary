af69228152bfd88f9da8b591d6928adbb354f57a
Michael Li <michael.li@hashicorp.com>
2023-10-16 16:32:03 -0400

    CR: Add an additional check for user auto-creation

--- testing/internal/e2e/tests/base/auth_method_password_test.go
+++ testing/internal/e2e/tests/base/auth_method_password_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/hashicorp/boundary/api/authmethods"
 	"github.com/hashicorp/boundary/api/scopes"
+	"github.com/hashicorp/boundary/api/users"
 	"github.com/hashicorp/boundary/testing/internal/e2e"
 	"github.com/hashicorp/boundary/testing/internal/e2e/boundary"
 	"github.com/stretchr/testify/require"
@@ -58,18 +59,7 @@ func TestCliAuthMethodPassword(t *testing.T) {
 	testAccountName := "test-account"
 	newAccountId, acctPassword := boundary.CreateNewAccountCli(t, ctx, newAuthMethodId, testAccountName)
 
-	// Create user and attach new account
-	newUserId := boundary.CreateNewUserCli(t, ctx, newOrgId)
-	t.Cleanup(func() {
-		boundary.AuthenticateAdminCli(t, context.Background())
-		output := e2e.RunCommand(ctx, "boundary",
-			e2e.WithArgs("users", "delete", "-id", newUserId),
-		)
-		require.NoError(t, output.Err, string(output.Stderr))
-	})
-	boundary.SetAccountToUserCli(t, ctx, newUserId, newAccountId)
-
-	// Set new account as primary auth method for the new org
+	// Set new auth method as primary auth method for the new org
 	output = e2e.RunCommand(ctx, "boundary",
 		e2e.WithArgs(
 			"scopes", "update",
@@ -84,7 +74,56 @@ func TestCliAuthMethodPassword(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, newAuthMethodId, updateScopeResult.Item.PrimaryAuthMethodId)
 
-	// Log in as the new user
+	// Log in with the new account
+	output = e2e.RunCommand(ctx, "boundary",
+		e2e.WithArgs(
+			"authenticate", "password",
+			"-scope-id", newOrgId,
+			"-login-name", testAccountName,
+			"-password", "env://E2E_TEST_BOUNDARY_PASSWORD",
+			"-format", "json",
+		),
+		e2e.WithEnv("E2E_TEST_BOUNDARY_PASSWORD", acctPassword),
+	)
+	require.NoError(t, output.Err, string(output.Stderr))
+
+	// Verify that a new user was automatically created with the new account attached
+	boundary.AuthenticateAdminCli(t, ctx)
+	output = e2e.RunCommand(ctx, "boundary",
+		e2e.WithArgs(
+			"users", "list",
+			"-scope-id", newOrgId,
+			"-format", "json",
+		),
+	)
+	require.NoError(t, err, string(output.Stderr))
+	var usersListResult users.UserListResult
+	err = json.Unmarshal(output.Stdout, &usersListResult)
+	require.NoError(t, err)
+	require.Equal(t, newAccountId, usersListResult.Items[0].PrimaryAccountId)
+
+	userId := usersListResult.Items[0].Id
+	output = e2e.RunCommand(ctx, "boundary",
+		e2e.WithArgs(
+			"users", "read",
+			"-id", userId,
+			"-format", "json",
+		),
+	)
+	require.NoError(t, err, string(output.Stderr))
+	var usersReadResult users.UserReadResult
+	err = json.Unmarshal(output.Stdout, &usersReadResult)
+	require.NoError(t, err)
+	require.Equal(t, newAccountId, usersReadResult.Item.PrimaryAccountId)
+	require.Contains(t, usersReadResult.Item.AccountIds, newAccountId)
+
+	// Create a new account and manually attach it to a new user
+	newUserId := boundary.CreateNewUserCli(t, ctx, newOrgId)
+	testAccountName = "test-account2"
+	newAccountId, acctPassword = boundary.CreateNewAccountCli(t, ctx, newAuthMethodId, testAccountName)
+	boundary.SetAccountToUserCli(t, ctx, newUserId, newAccountId)
+
+	// Log in with the new account
 	output = e2e.RunCommand(ctx, "boundary",
 		e2e.WithArgs(
 			"authenticate", "password",
