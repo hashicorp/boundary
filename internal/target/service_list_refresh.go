1b5087f97e1b9ad93aeae0b7ad7c77fe9578c2a8
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-10-20 09:53:56 -0700

    New generic pagination (#3844)
    * internal/pagination: add new pagination logic
    
    This unifies the core of the pagination logic in
    the new pattern, significantly simplifying the
    domain logic and removing repetition. This also
    makes it easier to test the generic logic in
    isolation.
    
    I've established that this pattern can be applied
    both to the simpler pagination in the target
    domain and the more complicated logic in
    the auth domain.
    
    * internal/target: migrate to generic pagination
    
    * fixup! internal/pagination: add new pagination logic

--- internal/target/service_list_refresh.go
+++ internal/target/service_list_refresh.go
@@ -6,7 +6,7 @@ package target
 import (
 	"context"
 
-	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/pagination"
 	"github.com/hashicorp/boundary/internal/refreshtoken"
 )
 
@@ -21,78 +21,23 @@ func ListRefresh(
 	repo *Repository,
 	grantsHash []byte,
 	pageSize int,
-	filterItemFn func(Target) (bool, error),
-) (*ListResponse, error) {
-	const op = "target.ListRefresh"
-
-	deletedIds, transactionTimestamp, err := repo.listDeletedIds(ctx, tok.UpdatedTime)
-	if err != nil {
-		return nil, errors.Wrap(ctx, err, op)
-	}
-
-	limit := pageSize + 1
-	opts := []Option{
-		WithLimit(limit),
-		WithStartPageAfterItem(tok.LastItemId, tok.LastItemUpdatedTime),
-	}
-
-	targets := make([]Target, 0, pageSize+1)
-dbLoop:
-	for {
-		// Request another page from the DB until we fill the final items
-		page, err := repo.listTargets(ctx, opts...)
-		if err != nil {
-			return nil, errors.Wrap(ctx, err, op)
-		}
-		for _, item := range page {
-			ok, err := filterItemFn(item)
-			if err != nil {
-				return nil, errors.Wrap(ctx, err, op)
-			}
-			if ok {
-				targets = append(targets, item)
-				// If we filled the items after filtering,
-				// we're done.
-				if len(targets) == cap(targets) {
-					break dbLoop
-				}
-			}
-		}
-		// If the current page was shorter than the limit, stop iterating
-		if len(page) < limit {
-			break dbLoop
-		}
-
-		opts = []Option{
+	filterItemFn pagination.ListFilterFunc[Target],
+) (*pagination.ListResponse2[Target], error) {
+	listItemsFn := func(ctx context.Context, tok *refreshtoken.Token, lastPageItem Target, limit int) ([]Target, error) {
+		opts := []Option{
 			WithLimit(limit),
-			WithStartPageAfterItem(page[len(page)-1].GetPublicId(), page[len(page)-1].GetUpdateTime().AsTime()),
 		}
-	}
-	// If we couldn't fill the items, it was a complete listing.
-	completeListing := len(targets) < cap(targets)
-	if !completeListing {
-		// Items is of size pageSize+1, so
-		// truncate if it was filled.
-		targets = targets[:pageSize]
-	}
-
-	totalItems, err := repo.estimatedCount(ctx)
-	if err != nil {
-		return nil, errors.Wrap(ctx, err, op)
+		if lastPageItem != nil {
+			opts = append(opts,
+				WithStartPageAfterItem(lastPageItem.GetPublicId(), lastPageItem.GetUpdateTime().AsTime()),
+			)
+		} else {
+			opts = append(opts,
+				WithStartPageAfterItem(tok.LastItemId, tok.LastItemUpdatedTime),
+			)
+		}
+		return repo.listTargets(ctx, opts...)
 	}
 
-	resp := &ListResponse{
-		Items:               targets,
-		DeletedIds:          deletedIds,
-		EstimatedTotalItems: totalItems,
-		CompleteListing:     completeListing,
-	}
-
-	if len(targets) > 0 {
-		resp.RefreshToken = tok.RefreshLastItem(targets[len(targets)-1], transactionTimestamp)
-	} else {
-		resp.RefreshToken = tok.Refresh(transactionTimestamp)
-	}
-
-	return resp, nil
+	return pagination.ListRefresh(ctx, grantsHash, pageSize, filterItemFn, listItemsFn, repo.estimatedCount, repo.listDeletedIds, tok)
 }
