546086d5e404bb39535c0bcc987b594a2417fb4c
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-10-23 16:49:32 -0700

    api: add automatic list pagination
    Updates all List endpoints to automatically paginate
    through results to preserve backwards compatibility.
    Adding support for user controllable pagination could
    be considered at a later time.

--- api/authmethods/option.gen.go
+++ api/authmethods/option.gen.go
@@ -26,6 +26,7 @@ type options struct {
 	withAutomaticVersioning bool
 	withSkipCurlOutput      bool
 	withFilter              string
+	withRefreshToken        string
 	withRecursive           bool
 }
 
@@ -50,6 +51,9 @@ func getOpts(opt ...Option) (options, []api.Option) {
 	if opts.withFilter != "" {
 		opts.queryMap["filter"] = opts.withFilter
 	}
+	if opts.withRefreshToken != "" {
+		opts.queryMap["refresh_token"] = opts.withRefreshToken
+	}
 	if opts.withRecursive {
 		opts.queryMap["recursive"] = strconv.FormatBool(opts.withRecursive)
 	}
@@ -71,6 +75,14 @@ func WithAutomaticVersioning(enable bool) Option {
 func WithSkipCurlOutput(skip bool) Option {
 	return func(o *options) {
 		o.withSkipCurlOutput = true
+	}
+}
+
+// WithRefreshToken tells the API to use the provided refresh token
+// for listing operations on this resource.
+func WithRefreshToken(refreshToken string) Option {
+	return func(o *options) {
+		o.withRefreshToken = refreshToken
 	}
 }
 
--- api/managedgroups/managedgroups.gen.go
+++ api/managedgroups/managedgroups.gen.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"slices"
 	"time"
 
 	"github.com/hashicorp/boundary/api"
@@ -62,12 +63,32 @@ func (n ManagedGroupDeleteResult) GetResponse() *api.Response {
 }
 
 type ManagedGroupListResult struct {
-	Items    []*ManagedGroup
-	response *api.Response
+	Items        []*ManagedGroup `json:"items,omitempty"`
+	EstItemCount uint            `json:"est_item_count,omitempty"`
+	RemovedIds   []string        `json:"removed_ids,omitempty"`
+	RefreshToken string          `json:"refresh_token,omitempty"`
+	ResponseType string          `json:"response_type,omitempty"`
+	response     *api.Response
 }
 
 func (n ManagedGroupListResult) GetItems() []*ManagedGroup {
 	return n.Items
+}
+
+func (n ManagedGroupListResult) GetEstItemCount() uint {
+	return n.EstItemCount
+}
+
+func (n ManagedGroupListResult) GetRemovedIds() []string {
+	return n.RemovedIds
+}
+
+func (n ManagedGroupListResult) GetRefreshToken() string {
+	return n.RefreshToken
+}
+
+func (n ManagedGroupListResult) GetResponseType() string {
+	return n.ResponseType
 }
 
 func (n ManagedGroupListResult) GetResponse() *api.Response {
@@ -320,5 +341,77 @@ func (c *Client) List(ctx context.Context, authMethodId string, opt ...Option) (
 		return nil, apiErr
 	}
 	target.response = resp
+	if target.ResponseType == "complete" || target.ResponseType == "" {
+		return target, nil
+	}
+	// If there are more results, automatically fetch the rest of the results.
+	// idToIndex keeps a map from the ID of an item to its index in target.Items.
+	// This is used to update updated items in-place and remove deleted items
+	// from the result after pagination is done.
+	idToIndex := map[string]int{}
+	for i, item := range target.Items {
+		idToIndex[item.Id] = i
+	}
+	for {
+		req, err := c.client.NewRequest(ctx, "GET", "managed-groups", nil, apiOpts...)
+		if err != nil {
+			return nil, fmt.Errorf("error creating List request: %w", err)
+		}
+
+		opts.queryMap["refresh_token"] = target.RefreshToken
+		if len(opts.queryMap) > 0 {
+			q := url.Values{}
+			for k, v := range opts.queryMap {
+				q.Add(k, v)
+			}
+			req.URL.RawQuery = q.Encode()
+		}
+
+		resp, err := c.client.Do(req)
+		if err != nil {
+			return nil, fmt.Errorf("error performing client request during List call: %w", err)
+		}
+
+		page := new(ManagedGroupListResult)
+		apiErr, err := resp.Decode(page)
+		if err != nil {
+			return nil, fmt.Errorf("error decoding List response: %w", err)
+		}
+		if apiErr != nil {
+			return nil, apiErr
+		}
+		for _, item := range page.Items {
+			if i, ok := idToIndex[item.Id]; ok {
+				// Item has already been seen at index i, update in-place
+				target.Items[i] = item
+			} else {
+				target.Items = append(target.Items, item)
+				idToIndex[item.Id] = len(target.Items) - 1
+			}
+		}
+		target.RemovedIds = append(target.RemovedIds, page.RemovedIds...)
+		target.EstItemCount = page.EstItemCount
+		target.RefreshToken = page.RefreshToken
+		target.ResponseType = page.ResponseType
+		target.response = resp
+		if target.ResponseType == "complete" {
+			break
+		}
+	}
+	for _, removedId := range target.RemovedIds {
+		if i, ok := idToIndex[removedId]; ok {
+			// Remove the item at index i without preserving order
+			// https://github.com/golang/go/wiki/SliceTricks#delete-without-preserving-order
+			target.Items[i] = target.Items[len(target.Items)-1]
+			target.Items = target.Items[:len(target.Items)-1]
+			// Update the index of the last element
+			idToIndex[target.Items[i].Id] = i
+		}
+	}
+	// Finally, sort the results again since in-place updates and deletes
+	// may have shuffled items.
+	slices.SortFunc(target.Items, func(i, j *ManagedGroup) int {
+		return i.UpdatedTime.Compare(j.UpdatedTime)
+	})
 	return target, nil
 }
