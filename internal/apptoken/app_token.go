5e79b6865fb1a22f438bb439a27b810dc586868c
Jim <jlambert@hashicorp.com>
2023-10-25 13:56:23 -0400

    feat (apptoken): add AppToken type

--- internal/apptoken/app_token.go
+++ internal/apptoken/app_token.go
@@ -2,3 +2,71 @@
 // SPDX-License-Identifier: BUSL-1.1
 
 package apptoken
+
+import (
+	"context"
+	"time"
+
+	"github.com/hashicorp/boundary/internal/apptoken/store"
+	"github.com/hashicorp/boundary/internal/db/timestamp"
+	"github.com/hashicorp/boundary/internal/errors"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+const appTokenTableName = "app_token"
+
+// AppToken defines an app token for storage.  It is the root aggregate for
+// other entities: AppTokenGrant
+type AppToken struct {
+	*store.AppToken
+	tableName string
+}
+
+// NewAppToken creates an in-memory app token with options.  Supported options:
+// WithName, WithDescription
+func NewAppToken(ctx context.Context, scopeId string, expirationTime time.Time, createdByUserCurrentHistoryId string, opt ...Option) (*AppToken, error) {
+	const op = "apptoken.NewAppToken"
+	switch {
+	case createdByUserCurrentHistoryId == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing created by user (history id)")
+	case scopeId == "":
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing scope id")
+	case expirationTime.IsZero():
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing expiration time")
+	}
+
+	opts, err := getOpts(opt...)
+	if err != nil {
+		return nil, errors.Wrap(ctx, err, op)
+	}
+	return &AppToken{
+		AppToken: &store.AppToken{
+			ScopeId:        scopeId,
+			ExpirationTime: &timestamp.Timestamp{Timestamp: timestamppb.New(expirationTime.Truncate(time.Second))},
+			Name:           opts.withName,
+			Description:    opts.withDescription,
+		},
+	}, nil
+}
+
+// clone an AppToken.
+func (at *AppToken) clone() *AppToken {
+	cp := proto.Clone(at.AppToken)
+	return &AppToken{
+		AppToken: cp.(*store.AppToken),
+	}
+}
+
+// TableName returns the table name.
+func (at *AppToken) TableName() string {
+	if at.tableName != "" {
+		return at.tableName
+	}
+	return appTokenTableName
+}
+
+// SetTableName sets the table name.
+func (at *AppToken) SetTableName(n string) {
+	at.tableName = n
+}
