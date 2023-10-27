aafe86e7a287ecc7963577e9d5409d279edf8dc7
Jim <jlambert@hashicorp.com>
2023-10-27 10:19:59 -0400

    feat (app token): add AppToken type (#3915)
    * feat (apptoken): added proto storage for apptoken
    
    This includes addition to Makefile for injecting tags

---
+++ internal/apptoken/options.go
@@ -0,0 +1,47 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package apptoken
+
+import (
+	"context"
+)
+
+type options struct {
+	withName        string
+	withDescription string
+}
+
+// Option - how options are passed as args
+type Option func(*options) error
+
+func getDefaultOptions() options {
+	return options{}
+}
+
+func getOpts(opt ...Option) (options, error) {
+	opts := getDefaultOptions()
+
+	for _, o := range opt {
+		if err := o(&opts); err != nil {
+			return opts, err
+		}
+	}
+	return opts, nil
+}
+
+// WithName provides an optional name.
+func WithName(_ context.Context, n string) Option {
+	return func(o *options) error {
+		o.withName = n
+		return nil
+	}
+}
+
+// WithDescription provides an optional description.
+func WithDescription(_ context.Context, desc string) Option {
+	return func(o *options) error {
+		o.withDescription = desc
+		return nil
+	}
+}
