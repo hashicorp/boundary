bdc3c051bc1d267906571eb5a4321fb34dc43a40
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-10-30 14:43:12 -0700

    internal/host: add plugin host set pagination

--- internal/host/plugin/query.go
+++ internal/host/plugin/query.go
@@ -342,4 +342,264 @@ final as (
     from final
 order by update_time desc, public_id asc;
 `
+
+	estimateCountHostSets = `
+select sum(reltuples::bigint) as estimate from pg_class where oid in ('host_plugin_set'::regclass)
+`
+
+	listSetsTemplate = `
+with host_sets as (
+    select public_id,
+           catalog_id,
+           name,
+           description,
+           create_time,
+           update_time,
+           last_sync_time,
+           need_sync,
+           sync_interval_seconds,
+           version,
+           attributes
+      from host_plugin_set
+     where catalog_id = @catalog_id
+  order by create_time desc, public_id asc
+     limit %d
+),
+host_catalog as (
+  select plugin_id,
+         public_id
+    from host_plugin_catalog
+   where public_id = @catalog_id
+),
+preferred_endpoints as (
+    select string_agg(distinct concat_ws('=', priority, condition), '|') as preferred_endpoints,
+           host_set_id            
+      from host_set_preferred_endpoint
+     where host_set_id in (select public_id from host_sets)
+  group by host_set_id
+),
+host_set_members as (
+    select string_agg(distinct host_id, '|') as host_ids,
+           set_id            
+      from host_plugin_set_member
+     where set_id in (select public_id from host_sets)
+  group by set_id
+), 
+final as (
+           select hs.public_id,
+                  hs.catalog_id,
+                  hc.plugin_id,
+                  hs.name,
+                  hs.description,
+                  hs.create_time,
+                  hs.update_time,
+                  hs.last_sync_time,
+                  hs.need_sync,
+                  hs.sync_interval_seconds,
+                  hs.version,
+                  hs.attributes,
+                  ppe.preferred_endpoints,
+                  hsm.host_ids
+             from host_sets hs
+             join host_catalog hc         on hc.public_id = hs.catalog_id
+  left outer join preferred_endpoints ppe on ppe.host_set_id = hs.public_id
+  left outer join host_set_members hsm    on hsm.set_id = hs.public_id
+)
+  select *  
+    from final
+order by create_time desc, public_id asc;
+`
+
+	listSetsPageTemplate = `
+with host_sets as (
+    select public_id,
+           catalog_id,
+           name,
+           description,
+           create_time,
+           update_time,
+           last_sync_time,
+           need_sync,
+           sync_interval_seconds,
+           version,
+           attributes
+      from host_plugin_set
+     where catalog_id = @catalog_id
+       and (create_time, public_id) < (@last_item_create_time, @last_item_id)
+  order by create_time desc, public_id asc
+     limit %d
+),
+host_catalog as (
+  select plugin_id,
+         public_id
+    from host_plugin_catalog
+   where public_id = @catalog_id
+),
+preferred_endpoints as (
+    select string_agg(distinct concat_ws('=', priority, condition), '|') as preferred_endpoints,
+           host_set_id            
+      from host_set_preferred_endpoint
+     where host_set_id in (select public_id from host_sets)
+  group by host_set_id
+),
+host_set_members as (
+    select string_agg(distinct host_id, '|') as host_ids,
+           set_id            
+      from host_plugin_set_member
+     where set_id in (select public_id from host_sets)
+  group by set_id
+), 
+final as (
+           select hs.public_id,
+                  hs.catalog_id,
+                  hc.plugin_id,
+                  hs.name,
+                  hs.description,
+                  hs.create_time,
+                  hs.update_time,
+                  hs.last_sync_time,
+                  hs.need_sync,
+                  hs.sync_interval_seconds,
+                  hs.version,
+                  hs.attributes,
+                  ppe.preferred_endpoints,
+                  hsm.host_ids
+             from host_sets hs
+             join host_catalog hc         on hc.public_id = hs.catalog_id
+  left outer join preferred_endpoints ppe on ppe.host_set_id = hs.public_id
+  left outer join host_set_members hsm    on hsm.set_id = hs.public_id
+)
+  select *  
+    from final
+order by create_time desc, public_id asc;
+`
+
+	listSetsRefreshTemplate = `
+with host_sets as (
+    select public_id,
+           catalog_id,
+           name,
+           description,
+           create_time,
+           update_time,
+           last_sync_time,
+           need_sync,
+           sync_interval_seconds,
+           version,
+           attributes
+      from host_plugin_set
+     where catalog_id = @catalog_id
+       and update_time > @updated_after_time
+  order by update_time desc, public_id asc
+     limit %d
+),
+host_catalog as (
+  select plugin_id,
+         public_id
+    from host_plugin_catalog
+   where public_id = @catalog_id
+),
+preferred_endpoints as (
+    select string_agg(distinct concat_ws('=', priority, condition), '|') as preferred_endpoints,
+           host_set_id            
+      from host_set_preferred_endpoint
+     where host_set_id in (select public_id from host_sets)
+  group by host_set_id
+),
+host_set_members as (
+    select string_agg(distinct host_id, '|') as host_ids,
+           set_id            
+      from host_plugin_set_member
+     where set_id in (select public_id from host_sets)
+  group by set_id
+), 
+final as (
+           select hs.public_id,
+                  hs.catalog_id,
+                  hc.plugin_id,
+                  hs.name,
+                  hs.description,
+                  hs.create_time,
+                  hs.update_time,
+                  hs.last_sync_time,
+                  hs.need_sync,
+                  hs.sync_interval_seconds,
+                  hs.version,
+                  hs.attributes,
+                  ppe.preferred_endpoints,
+                  hsm.host_ids
+             from host_sets hs
+             join host_catalog hc         on hc.public_id = hs.catalog_id
+  left outer join preferred_endpoints ppe on ppe.host_set_id = hs.public_id
+  left outer join host_set_members hsm    on hsm.set_id = hs.public_id
+)
+  select *  
+    from final
+order by update_time desc, public_id asc;
+`
+
+	listSetsRefreshPageTemplate = `
+with host_sets as (
+    select public_id,
+           catalog_id,
+           name,
+           description,
+           create_time,
+           update_time,
+           last_sync_time,
+           need_sync,
+           sync_interval_seconds,
+           version,
+           attributes
+      from host_plugin_set
+     where catalog_id = @catalog_id
+       and update_time > @updated_after_time
+       and (update_time, public_id) < (@last_item_update_time, @last_item_id)
+  order by update_time desc, public_id asc
+     limit %d
+),
+host_catalog as (
+  select plugin_id,
+         public_id
+    from host_plugin_catalog
+   where public_id = @catalog_id
+),
+preferred_endpoints as (
+    select string_agg(distinct concat_ws('=', priority, condition), '|') as preferred_endpoints,
+           host_set_id            
+      from host_set_preferred_endpoint
+     where host_set_id in (select public_id from host_sets)
+  group by host_set_id
+),
+host_set_members as (
+    select string_agg(distinct host_id, '|') as host_ids,
+           set_id            
+      from host_plugin_set_member
+     where set_id in (select public_id from host_sets)
+  group by set_id
+), 
+final as (
+           select hs.public_id,
+                  hs.catalog_id,
+                  hc.plugin_id,
+                  hs.name,
+                  hs.description,
+                  hs.create_time,
+                  hs.update_time,
+                  hs.last_sync_time,
+                  hs.need_sync,
+                  hs.sync_interval_seconds,
+                  hs.version,
+                  hs.attributes,
+                  ppe.preferred_endpoints,
+                  hsm.host_ids
+             from host_sets hs
+             join host_catalog hc         on hc.public_id = hs.catalog_id
+  left outer join preferred_endpoints ppe on ppe.host_set_id = hs.public_id
+  left outer join host_set_members hsm    on hsm.set_id = hs.public_id
+)
+  select *  
+    from final
+order by update_time desc, public_id asc;
+`
 )
---
+++ internal/host/plugin/service_list_host_sets.go
@@ -0,0 +1,64 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package plugin
+
+import (
+	"context"
+	"time"
+
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/host"
+	"github.com/hashicorp/boundary/internal/pagination"
+	ppagination "github.com/hashicorp/boundary/internal/pagination/plugin"
+	"github.com/hashicorp/boundary/internal/plugin"
+)
+
+// ListHostSets lists up to page size host sets, filtering out entries that
+// do not pass the filter item function. It will automatically request
+// more host sets from the database, at page size chunks, to fill the page.
+// It returns a new list token used to continue pagination or refresh items.
+// Host sets are ordered by create time descending (most recently created first).
+func ListHostSets(
+	ctx context.Context,
+	grantsHash []byte,
+	pageSize int,
+	filterItemFn ppagination.ListPluginFilterFunc[host.Set],
+	repo *Repository,
+	hostCatalogId string,
+) (*pagination.ListResponse[host.Set], *plugin.Plugin, error) {
+	const op = "plugin.ListHostSets"
+
+	switch {
+	case len(grantsHash) == 0:
+		return nil, nil, errors.New(ctx, errors.InvalidParameter, op, "missing grants hash")
+	case pageSize < 1:
+		return nil, nil, errors.New(ctx, errors.InvalidParameter, op, "page size must be at least 1")
+	case filterItemFn == nil:
+		return nil, nil, errors.New(ctx, errors.InvalidParameter, op, "missing filter item callback")
+	case repo == nil:
+		return nil, nil, errors.New(ctx, errors.InvalidParameter, op, "missing repo")
+	case hostCatalogId == "":
+		return nil, nil, errors.New(ctx, errors.InvalidParameter, op, "missing host catalog ID")
+	}
+
+	listItemsFn := func(ctx context.Context, lastPageItem host.Set, limit int) ([]host.Set, *plugin.Plugin, time.Time, error) {
+		opts := []Option{
+			WithLimit(limit),
+		}
+		if lastPageItem != nil {
+			opts = append(opts, WithStartPageAfterItem(lastPageItem))
+		}
+		pSets, plg, listTime, err := repo.listSets(ctx, hostCatalogId, opts...)
+		if err != nil {
+			return nil, nil, time.Time{}, err
+		}
+		var sets []host.Set
+		for _, set := range pSets {
+			sets = append(sets, set)
+		}
+		return sets, plg, listTime, nil
+	}
+
+	return ppagination.ListPlugin(ctx, grantsHash, pageSize, filterItemFn, listItemsFn, repo.estimatedSetCount)
+}
