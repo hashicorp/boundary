148518dd381ff0eeed85b794286535b96150e271
Todd <github@quaddi.com>
2023-11-01 16:10:59 -0700

    fixup! Add indexes on update_time

--- internal/db/sqltest/tests/index/update_time.sql
+++ internal/db/sqltest/tests/index/update_time.sql
@@ -3,8 +3,12 @@
 
 begin;
   select plan(1);
-
-  select is(count(*), 32::bigint) from pg_indexes where schemaname = 'public' and indexname like tablename || '_update_time_ix';
+  
+  select results_eq(
+    'select count(*)::bigint from get_deletion_tables()',
+    'select count(*)::bigint from pg_indexes where schemaname = ''public'' and indexname like tablename || ''_update_time_idx''',
+    'every table with a deletion table should also have an index on the update_time column'
+  );
 
   select * from finish();
 rollback;
