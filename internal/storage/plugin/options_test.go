8c3e309816ad7644bfa0b46dcae78a6a0c13bba1
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-11-01 21:01:37 -0700

    internal/storage/plugin: run tests in parallel

--- internal/storage/plugin/options_test.go
+++ internal/storage/plugin/options_test.go
@@ -13,48 +13,56 @@ import (
 func Test_GetOpts(t *testing.T) {
 	t.Parallel()
 	t.Run("WithChunkSize", func(t *testing.T) {
+		t.Parallel()
 		opts := getOpts(WithChunkSize(1024))
 		testOpts := getDefaultOptions()
 		testOpts.withChunkSize = 1024
 		assert.Equal(t, opts, testOpts)
 	})
 	t.Run("WithAttributes", func(t *testing.T) {
+		t.Parallel()
 		opts := getOpts(WithAttributes(&structpb.Struct{Fields: map[string]*structpb.Value{"foo": structpb.NewStringValue("bar")}}))
 		testOpts := getDefaultOptions()
 		testOpts.withAttributes = &structpb.Struct{Fields: map[string]*structpb.Value{"foo": structpb.NewStringValue("bar")}}
 		assert.Equal(t, opts, testOpts)
 	})
 	t.Run("WithName", func(t *testing.T) {
+		t.Parallel()
 		opts := getOpts(WithName("test"))
 		testOpts := getDefaultOptions()
 		testOpts.withName = "test"
 		assert.Equal(t, opts, testOpts)
 	})
 	t.Run("WithDescription", func(t *testing.T) {
+		t.Parallel()
 		opts := getOpts(WithDescription("test desc"))
 		testOpts := getDefaultOptions()
 		testOpts.withDescription = "test desc"
 		assert.Equal(t, opts, testOpts)
 	})
 	t.Run("WithSecrets", func(t *testing.T) {
+		t.Parallel()
 		opts := getOpts(WithSecrets(&structpb.Struct{Fields: map[string]*structpb.Value{"foo": structpb.NewStringValue("bar")}}))
 		testOpts := getDefaultOptions()
 		testOpts.withSecrets = &structpb.Struct{Fields: map[string]*structpb.Value{"foo": structpb.NewStringValue("bar")}}
 		assert.Equal(t, opts, testOpts)
 	})
 	t.Run("WithBucketPrefix", func(t *testing.T) {
+		t.Parallel()
 		opts := getOpts(WithBucketPrefix("test prefix"))
 		testOpts := getDefaultOptions()
 		testOpts.withBucketPrefix = "test prefix"
 		assert.Equal(t, opts, testOpts)
 	})
 	t.Run("WithWorkerFilter", func(t *testing.T) {
+		t.Parallel()
 		opts := getOpts(WithWorkerFilter("test filter"))
 		testOpts := getDefaultOptions()
 		testOpts.withWorkerFilter = "test filter"
 		assert.Equal(t, opts, testOpts)
 	})
 	t.Run("WithLimit", func(t *testing.T) {
+		t.Parallel()
 		opts := getOpts(WithLimit(12345))
 		testOpts := getDefaultOptions()
 		testOpts.withLimit = 12345
