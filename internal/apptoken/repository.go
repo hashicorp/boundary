7866d896fdef1a430b5640dee2a7a1ea455fdb47
Uruemu S Aganbi <9087380+uruemu@users.noreply.github.com>
2023-11-04 07:56:06 -0400

    Create repository factory for apptoken (#3936)

--- internal/apptoken/repository.go
+++ internal/apptoken/repository.go
@@ -2,3 +2,52 @@
 // SPDX-License-Identifier: BUSL-1.1
 
 package apptoken
+
+import (
+	"context"
+
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/kms"
+)
+
+// Repository is the apptoken database repository
+type Repository struct {
+	reader db.Reader
+	writer db.Writer
+	kms    *kms.Kms
+
+	// defaultLimit provides a default for limiting the number of results returned from the repo.
+	// If defaultLimit < 0, then unlimited results are returned. If defaultLimit == 0, then boundary
+	// default limits are used for results
+	defaultLimit int
+}
+
+// NewRepository creates a new apptoken Repository. Supports the options: WithLimit
+// which sets a default limit on results returned by repo operations.
+func NewRepository(ctx context.Context, r db.Reader, w db.Writer, kms *kms.Kms, opt ...Option) (*Repository, error) {
+	const op = "apptoken.NewRepository"
+	if r == nil {
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "nil reader")
+	}
+	if w == nil {
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "nil writer")
+	}
+	if kms == nil {
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "nil kms")
+	}
+	opts, err := getOpts(opt...)
+	if err != nil {
+		return nil, errors.Wrap(ctx, err, op)
+	}
+	if opts.withLimit == 0 {
+		// zero signals the boundary defaults should be used.
+		opts.withLimit = db.DefaultLimit
+	}
+	return &Repository{
+		reader:       r,
+		writer:       w,
+		kms:          kms,
+		defaultLimit: opts.withLimit,
+	}, nil
+}
