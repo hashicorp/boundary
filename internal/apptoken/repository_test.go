c5b6776e50a00b188f0ad08896dec0116811dc2f
Uruemu S Aganbi <9087380+uruemu@users.noreply.github.com>
2023-11-04 07:56:06 -0400

    Create repository factory for apptoken (#3936)

---
+++ internal/apptoken/repository_test.go
@@ -0,0 +1,96 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package apptoken
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/kms"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewRepository(t *testing.T) {
+	t.Parallel()
+	conn, _ := db.TestSetup(t, "postgres")
+	rw := db.New(conn)
+	wrapper := db.TestWrapper(t)
+	testKms := kms.TestKms(t, conn, wrapper)
+	type args struct {
+		r   db.Reader
+		w   db.Writer
+		kms *kms.Kms
+	}
+	tests := []struct {
+		name          string
+		args          args
+		want          *Repository
+		wantErr       bool
+		wantErrString string
+	}{
+		{
+			name: "valid",
+			args: args{
+				r:   rw,
+				w:   rw,
+				kms: testKms,
+			},
+			want: &Repository{
+				reader:       rw,
+				writer:       rw,
+				kms:          testKms,
+				defaultLimit: db.DefaultLimit,
+			},
+			wantErr: false,
+		},
+		{
+			name: "nil-kms",
+			args: args{
+				r:   rw,
+				w:   rw,
+				kms: nil,
+			},
+			want:          nil,
+			wantErr:       true,
+			wantErrString: "apptoken.NewRepository: nil kms: parameter violation: error #100",
+		},
+		{
+			name: "nil-writer",
+			args: args{
+				r:   rw,
+				w:   nil,
+				kms: testKms,
+			},
+			want:          nil,
+			wantErr:       true,
+			wantErrString: "apptoken.NewRepository: nil writer: parameter violation: error #100",
+		},
+		{
+			name: "nil-reader",
+			args: args{
+				r:   nil,
+				w:   rw,
+				kms: testKms,
+			},
+			want:          nil,
+			wantErr:       true,
+			wantErrString: "apptoken.NewRepository: nil reader: parameter violation: error #100",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			got, err := NewRepository(context.Background(), tt.args.r, tt.args.w, tt.args.kms)
+			if tt.wantErr {
+				require.Error(err)
+				assert.Equal(tt.wantErrString, err.Error())
+				return
+			}
+			require.NoError(err)
+			assert.Equal(tt.want, got)
+		})
+	}
+}
