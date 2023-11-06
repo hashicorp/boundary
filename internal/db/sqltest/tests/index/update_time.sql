cd547210295a5829d66844021a8b662ba64571de
Todd <github@quaddi.com>
2023-11-06 09:23:07 -0800

    fixup! Add indexes on update_time

--- internal/db/sqltest/tests/index/update_time.sql
+++ internal/db/sqltest/tests/index/update_time.sql
@@ -2,13 +2,27 @@
 -- SPDX-License-Identifier: BUSL-1.1
 
 begin;
-  select plan(1);
+  create function op_table(deletion_table_name name) returns text
+  as $$
+    select split_part(deletion_table_name, '_deleted', 1);
+  $$ language sql;
   
-  select results_eq(
-    'select count(*)::bigint from get_deletion_tables()',
-    'select count(*)::bigint from pg_indexes where schemaname = ''public'' and indexname like tablename || ''_update_time_idx''',
-    'every table with a deletion table should also have an index on the update_time column'
-  );
+  create function has_update_time_index(table_name name) returns text
+  as $$
+    select * from collect_tap(
+      has_index(table_name, table_name || '_update_time_idx', 'update_time')
+    );
+  $$ language sql;
+
+  -- the op table for each deletion table should be tested for the inded
+  select plan(a.table_count::integer)
+    from (
+      select count(*) as table_count
+        from get_deletion_tables()
+    ) as a;
+
+  select has_update_time_index(op_table(a))
+    from get_deletion_tables() a;
 
   select * from finish();
 rollback;
