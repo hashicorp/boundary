952678835c57af135735b82a284b60ae3b3e3c87
Elim Tsiagbey <elim.tsiagbey@hashicorp.com>
2023-11-10 11:46:11 -0500

    initial schema changes for `auth_oidc_prompt`

---
+++ internal/db/sqltest/tests/auth/oidc/prompt.sql
@@ -0,0 +1,32 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+  select plan(2);
+  select wtt_load('widgets', 'iam', 'kms', 'auth');  
+
+  -- Try to insert invalid auth_oidc_prompt value to test constraint
+  prepare invalid_auth_oidc_prompt_value as
+     insert into auth_oidc_prompt
+     (oidc_method_id,       prompt)
+     values
+        ('aom___widget',    'invalid');
+
+  select throws_ok(
+    'invalid_auth_oidc_prompt_value',
+    '23503',
+    'insert or update on table "auth_oidc_prompt" violates foreign key constraint "auth_oidc_prompt_enm_fkey"',
+    'inserting a row with invalid auth_oidc_prompt value'
+  );  
+
+  -- Insert valid valid_auth_oidc_prompt_value value to test constraint with a valid value
+  prepare valid_auth_oidc_prompt_value as
+     insert into auth_oidc_prompt
+     (oidc_method_id,       prompt)
+     values
+        ('aom___widget',   'select_account');
+
+  select lives_ok('valid_auth_oidc_prompt_value');
+
+  select * from finish();
+rollback;
