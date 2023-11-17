1d70cf1a5a0aaf3f4357018e69088d8d53a1ec29
Jim <jlambert@hashicorp.com>
2023-11-17 12:11:12 -0500

    stubbed out TestAppToken

--- internal/apptoken/testing.go
+++ internal/apptoken/testing.go
@@ -5,7 +5,9 @@ package apptoken
 
 import (
 	"context"
+	"testing"
 
+	"github.com/hashicorp/boundary/internal/db"
 	"github.com/hashicorp/boundary/internal/errors"
 )
 
@@ -13,4 +15,9 @@ func withOptError(ctx context.Context) Option {
 	return func(o *options) error {
 		return errors.New(ctx, errors.Unknown, "withOptErrors", "with opt error")
 	}
+}
+
+func TestAppToken(t *testing.T, conn *db.DB, scopeId, createBy string, grants []string) {
+	t.Helper()
+	panic("todo - finish creating an app token with the specified grants")
 }
