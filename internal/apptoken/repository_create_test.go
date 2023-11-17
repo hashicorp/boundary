c5df474ab2033a69c636053e852daf38241b450f
Jim <jlambert@hashicorp.com>
2023-11-17 10:15:13 -0500

    test (apptoken): add Test_CreateAppToken

---
+++ internal/apptoken/repository_create_test.go
@@ -0,0 +1,92 @@
+package apptoken_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/boundary/internal/apptoken"
+	"github.com/hashicorp/boundary/internal/apptoken/store"
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/iam"
+	"github.com/hashicorp/boundary/internal/kms"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func Test_CreateAppToken(t *testing.T) {
+	t.Parallel()
+	testCtx := context.Background()
+	testConn, _ := db.TestSetup(t, "postgres")
+	testRw := db.New(testConn)
+	testRootWrapper := db.TestWrapper(t)
+	testKms := kms.TestKms(t, testConn, testRootWrapper)
+	testIamRepo := iam.TestRepo(t, testConn, testRootWrapper)
+	testOrg, _ := iam.TestScopes(t, testIamRepo)
+	testRepo, err := apptoken.NewRepository(testCtx, testRw, testRw, testKms, testIamRepo)
+	testUser := iam.TestUser(t, testIamRepo, testOrg.GetPublicId())
+	require.NoError(t, err)
+
+	testUserHistoryId, err := testRepo.ResolveUserHistoryId(testCtx, testUser.GetPublicId())
+	require.NoError(t, err)
+
+	tests := []struct {
+		name            string
+		scopeId         string
+		expTime         time.Time
+		createdBy       string
+		grants          []string
+		opts            []apptoken.Option
+		wantToken       *apptoken.AppToken
+		wantGrants      []*apptoken.AppTokenGrant
+		wantErrContains string
+		wantErrMatch    *errors.Template
+	}{
+		{
+			name:      "success",
+			scopeId:   testOrg.GetPublicId(),
+			expTime:   time.Now().Add(10 * time.Minute),
+			createdBy: testUserHistoryId,
+			grants: []string{
+				"id=*;type=*;actions=read",
+			},
+			opts: []apptoken.Option{
+				apptoken.WithName(testCtx, "test-apptoken"),
+				apptoken.WithDescription(testCtx, "test-description"),
+			},
+			wantToken: &apptoken.AppToken{
+				AppToken: &store.AppToken{},
+			},
+			wantGrants: []*apptoken.AppTokenGrant{
+				{
+					AppTokenGrant: &store.AppTokenGrant{},
+				},
+			},
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			gotToken, gotGrants, err := testRepo.CreateAppToken(testCtx, tc.scopeId, tc.expTime, tc.createdBy, tc.grants, tc.opts...)
+			if tc.wantErrContains != "" {
+				require.Errorf(err, "we expected an error")
+				assert.Contains(err.Error(), tc.wantErrContains)
+				if tc.wantErrMatch != nil {
+					assert.Truef(errors.Match(tc.wantErrMatch, err), "want err code: %q got: %q", tc.wantErrMatch, err)
+				}
+				return
+			}
+			require.NoErrorf(err, "unexpected error")
+			require.NotEmptyf(gotToken, "we expected an app token")
+			// set fields which are set by the db
+			tc.wantToken.AppToken.CreateTime = gotToken.CreateTime
+			assert.Empty(cmp.Diff(gotToken.AppToken, tc.wantToken.AppToken, protocmp.Transform()))
+			assert.Len(gotGrants, len(tc.wantGrants))
+			// TODO: sort grants and cmp.Diff each one
+		})
+	}
+}
