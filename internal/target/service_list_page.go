e88876539ff9b55e5fe622270ff655ac7a0dd0e3
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-11-24 17:24:03 -0800

    internal/target: refactor for new pagination design
    The new design uses four separate service functions; List,
    ListPage, ListRefresh and ListRefreshPage. The first two are
    used during the initial pagination phase and the latter two
    are used for any subsequent refresh phases.

--- internal/target/service_list_page.go
+++ internal/target/service_list_page.go
@@ -8,22 +8,25 @@ import (
 	"time"
 
 	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/listtoken"
 	"github.com/hashicorp/boundary/internal/pagination"
 )
 
-// List lists up to page size targets, filtering out entries that
+// ListPage lists up to page size targets, filtering out entries that
 // do not pass the filter item function. It will automatically request
 // more targets from the database, at page size chunks, to fill the page.
+// It will start its paging based on the information in the token.
 // It returns a new list token used to continue pagination or refresh items.
 // Targets are ordered by create time descending (most recently created first).
-func List(
+func ListPage(
 	ctx context.Context,
 	grantsHash []byte,
 	pageSize int,
 	filterItemFn pagination.ListFilterFunc[Target],
+	tok *listtoken.Token,
 	repo *Repository,
 ) (*pagination.ListResponse[Target], error) {
-	const op = "target.List"
+	const op = "target.ListPage"
 
 	switch {
 	case len(grantsHash) == 0:
@@ -32,8 +35,13 @@ func List(
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "page size must be at least 1")
 	case filterItemFn == nil:
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing filter item callback")
+	case tok == nil:
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing token")
 	case repo == nil:
 		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing repo")
+	}
+	if _, ok := tok.Subtype.(*listtoken.PaginationToken); !ok {
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "token did not have a pagination token component")
 	}
 
 	listItemsFn := func(ctx context.Context, lastPageItem Target, limit int) ([]Target, time.Time, error) {
@@ -42,9 +50,15 @@ func List(
 		}
 		if lastPageItem != nil {
 			opts = append(opts, WithStartPageAfterItem(lastPageItem))
+		} else {
+			lastItem, err := tok.LastItem(ctx)
+			if err != nil {
+				return nil, time.Time{}, err
+			}
+			opts = append(opts, WithStartPageAfterItem(lastItem))
 		}
 		return repo.listTargets(ctx, opts...)
 	}
 
-	return pagination.List(ctx, grantsHash, pageSize, filterItemFn, listItemsFn, repo.estimatedCount)
+	return pagination.ListPage(ctx, grantsHash, pageSize, filterItemFn, listItemsFn, repo.estimatedCount, tok)
 }
