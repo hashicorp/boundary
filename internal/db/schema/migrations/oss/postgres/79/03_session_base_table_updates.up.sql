47f2c8bcbf911871309f763632ab08a3ad56667d
Michael Milton <mikemountain@users.noreply.github.com>
2023-12-01 16:02:43 -0500

    Sessions list pagination (#4079)
    * Session list pagination refactor
    
    * address pr comments, add explanatory comments, clean up sql queries, move tests into proper files
    
    * fix build error
    
    * fix test error

---
+++ internal/db/schema/migrations/oss/postgres/79/03_session_base_table_updates.up.sql
@@ -0,0 +1,14 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+
+  -- Add new indexes for the create time and update time queries.
+  create index session_create_time_public_id_idx
+      on session (create_time desc, public_id asc);
+  create index session_update_time_public_id_idx
+      on session (update_time desc, public_id asc);
+
+  analyze session;
+
+commit;
