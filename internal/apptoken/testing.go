206367b4b5fec097a81442637d2b7c5b0c8ef88e
Cameron Perera <cameron.perera@hashicorp.com>
2023-12-08 08:13:07 -0600

    test (apptoken): create NewAppToken fixture (#4070)
    * test (apptoken): create NewAppToken fixture
    
    * test (apptoken): update TestNewRepository to include grantfinder
    
    * test (apptoken): add db lookups for apptokens and apptoken grants

--- internal/apptoken/testing.go
+++ internal/apptoken/testing.go
@@ -5,12 +5,62 @@ package apptoken
 
 import (
 	"context"
+	"testing"
+	"time"
 
+	"github.com/hashicorp/boundary/internal/db"
 	"github.com/hashicorp/boundary/internal/errors"
+	"github.com/hashicorp/boundary/internal/perms"
+	"github.com/stretchr/testify/require"
 )
 
 func withOptError(ctx context.Context) Option {
 	return func(o *options) error {
 		return errors.New(ctx, errors.Unknown, "withOptErrors", "with opt error")
 	}
+}
+
+func TestAppToken(t *testing.T, conn *db.DB, scopeId, createdBy string, grantsStr ...string) (*AppToken, []*AppTokenGrant) {
+	t.Helper()
+	ctx := context.Background()
+	require := require.New(t)
+	require.Greaterf(len(grantsStr), 0, "missing grants")
+	w := db.New(conn)
+
+	// appToken, err := NewAppToken(ctx, scopeId, time.Now().AddDate(1, 0, 0), createdBy)
+	appToken, err := NewAppToken(ctx, scopeId, time.Now().Add(10*time.Minute), createdBy)
+	require.NoError(err)
+
+	const fakeScopeId = "o_abcd1234"
+	grants := make([]*perms.Grant, 0, len(grantsStr))
+	for _, g := range grantsStr {
+		grant, err := perms.Parse(ctx, fakeScopeId, g)
+		require.NoError(err)
+		grants = append(grants, &grant)
+	}
+
+	appToken.PublicId, err = newAppTokenId(ctx)
+	require.NoError(err)
+
+	appTokenGrants := make([]*AppTokenGrant, 0, len(grantsStr))
+	for _, g := range grants {
+		ag, err := NewAppTokenGrant(ctx, appToken.PublicId, g.CanonicalString())
+		require.NoError(err)
+		appTokenGrants = append(appTokenGrants, ag)
+	}
+
+	_, err = w.DoTx(ctx, db.StdRetryCnt, db.ExpBackoff{}, func(r db.Reader, w db.Writer) error {
+		err := w.Create(ctx, appToken)
+		require.NoError(err)
+
+		for _, atg := range appTokenGrants {
+			err := w.Create(ctx, atg)
+			require.NoError(err)
+		}
+
+		return nil
+	})
+	require.NoError(err)
+
+	return appToken, appTokenGrants
 }
---
+++ internal/apptoken/testing_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package apptoken
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/boundary/internal/db"
+	"github.com/hashicorp/boundary/internal/iam"
+	"github.com/hashicorp/boundary/internal/kms"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_TestAppToken(t *testing.T) {
+	assert, require := assert.New(t), require.New(t)
+	testCtx := context.Background()
+	testConn, _ := db.TestSetup(t, "postgres")
+	testRw := db.New(testConn)
+	testRootWrapper := db.TestWrapper(t)
+	testKms := kms.TestKms(t, testConn, testRootWrapper)
+	testIamRepo := iam.TestRepo(t, testConn, testRootWrapper)
+	testOrg, _ := iam.TestScopes(t, testIamRepo)
+	testRepo, err := NewRepository(testCtx, testRw, testRw, testKms, testIamRepo)
+	testUser := iam.TestUser(t, testIamRepo, testOrg.GetPublicId())
+	require.NoError(err)
+
+	testUserHistoryId, err := testRepo.ResolveUserHistoryId(testCtx, testUser.GetPublicId())
+	require.NoError(err)
+
+	at, atg := TestAppToken(t, testConn, testOrg.PublicId, testUserHistoryId, "id=*;type=*;actions=*")
+	assert.NotEmpty(at.GetPublicId())
+	assert.Len(atg, 1)
+
+	found := AllocAppToken()
+	found.PublicId = at.GetPublicId()
+	err = testRw.LookupById(testCtx, found)
+	require.NoError(err)
+	assert.NotNil(found)
+
+	foundGrant := AllocAppTokenGrant()
+	err = testRw.LookupWhere(testCtx, foundGrant, "app_token_id=? AND canonical_grant=?", []any{atg[0].AppTokenId, atg[0].CanonicalGrant})
+	require.NoError(err)
+	assert.NotNil(foundGrant)
+}
