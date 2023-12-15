f38c79a5cef873209b25bf343a13334c8f8d0fe9
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-12-15 10:58:30 -0800

    internal/handlers: add host catalog pagination

--- internal/daemon/controller/common/common.go
+++ internal/daemon/controller/common/common.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/boundary/internal/credential"
 	credstatic "github.com/hashicorp/boundary/internal/credential/static"
 	"github.com/hashicorp/boundary/internal/credential/vault"
+	"github.com/hashicorp/boundary/internal/host"
 	pluginhost "github.com/hashicorp/boundary/internal/host/plugin"
 	"github.com/hashicorp/boundary/internal/host/static"
 	"github.com/hashicorp/boundary/internal/iam"
@@ -25,6 +26,7 @@ type (
 	VaultCredentialRepoFactory     = func() (*vault.Repository, error)
 	StaticCredentialRepoFactory    = func() (*credstatic.Repository, error)
 	CredentialStoreRepoFactory     func() (*credential.StoreRepository, error)
+	HostCatalogRepoFactory         func() (*host.CatalogRepository, error)
 	IamRepoFactory                 = iam.IamRepoFactory
 	OidcAuthRepoFactory            = oidc.OidcRepoFactory
 	LdapAuthRepoFactory            = ldap.RepoFactory
