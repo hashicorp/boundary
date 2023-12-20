70bd67eccdada5a2fe7a721c1e315b95f6adf3d4
Todd <github@quaddi.com>
2023-12-20 10:01:48 -0800

    fixup! paginate iam user

--- internal/db/schema/migrations/oss/postgres/80/08_iam_updates.up.sql
+++ internal/db/schema/migrations/oss/postgres/80/08_iam_updates.up.sql
@@ -12,9 +12,9 @@ begin;
 
   -- Add new indexes for the create time and update time queries.
   create index iam_user_create_time_public_id_idx
-      on iam_user (create_time desc, public_id asc);
+      on iam_user (create_time desc, public_id desc);
   create index iam_user_update_time_public_id_idx
-      on iam_user (update_time desc, public_id asc);
+      on iam_user (update_time desc, public_id desc);
   analyze iam_user;
 
 commit;
