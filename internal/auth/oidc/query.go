cfbaab890dcfc52382ab42489f59f7fc03102df6
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2023-12-21 20:43:52 -0800

    internal/auth/oidc: add account pagination

--- internal/auth/oidc/query.go
+++ internal/auth/oidc/query.go
@@ -15,4 +15,8 @@ const (
 			%s
 	returning public_id, version
        `
+
+	estimateCountAccounts = `
+	select sum(reltuples::bigint) as estimate from pg_class where oid in ('auth_oidc_account'::regclass)
+	`
 )
