310c4aa0605b63a538b18a350d1f575e40868a44
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2024-01-02 17:18:47 -0800

    internal/iam: add scope list pagination

--- internal/iam/service_list.go
+++ internal/iam/service_list.go
@@ -133,3 +133,44 @@ func ListGroups(
 
 	return pagination.List(ctx, grantsHash, pageSize, filterItemFn, listItemsFn, repo.estimatedGroupCount)
 }
+
+// ListScopes lists up to page size scopes, filtering out entries that
+// do not pass the filter item function. It will automatically request
+// more scopes from the database, at page size chunks, to fill the page.
+// It returns a new list token used to continue pagination or refresh items.
+// Scopes are ordered by create time descending (most recently created first).
+func ListScopes(
+	ctx context.Context,
+	grantsHash []byte,
+	pageSize int,
+	filterItemFn pagination.ListFilterFunc[*Scope],
+	repo *Repository,
+	withParentIds []string,
+) (*pagination.ListResponse[*Scope], error) {
+	const op = "iam.ListScopes"
+
+	switch {
+	case len(grantsHash) == 0:
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing grants hash")
+	case pageSize < 1:
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "page size must be at least 1")
+	case filterItemFn == nil:
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing filter item callback")
+	case repo == nil:
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing repo")
+	case len(withParentIds) == 0:
+		return nil, errors.New(ctx, errors.InvalidParameter, op, "missing parent ids")
+	}
+
+	listItemsFn := func(ctx context.Context, lastPageItem *Scope, limit int) ([]*Scope, time.Time, error) {
+		opts := []Option{
+			WithLimit(limit),
+		}
+		if lastPageItem != nil {
+			opts = append(opts, WithStartPageAfterItem(lastPageItem))
+		}
+		return repo.listScopes(ctx, withParentIds, opts...)
+	}
+
+	return pagination.List(ctx, grantsHash, pageSize, filterItemFn, listItemsFn, repo.estimatedScopeCount)
+}
