d4b60241db17c7a3d2eb3173aeee039074cedf8a
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2024-01-03 17:29:49 -0800

    internal/db: storage bucket pagination migrations (#4213)

---
+++ internal/db/schema/migrations/oss/postgres/80/16_storage_bucket_updates.up.sql
@@ -0,0 +1,14 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+
+  -- Add new indexes for the create time and update time queries.
+  create index storage_plugin_storage_bucket_create_time_public_id_idx
+      on storage_plugin_storage_bucket (create_time desc, public_id desc);
+  create index storage_plugin_storage_bucket_update_time_public_id_idx
+      on storage_plugin_storage_bucket (update_time desc, public_id desc);
+
+  analyze storage_plugin_storage_bucket;
+
+commit;
