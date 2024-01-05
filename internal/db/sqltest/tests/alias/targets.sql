56241af8f604b160379eceb18cd44487c94f67e7
Todd <github@quaddi.com>
2024-01-04 16:44:08 -0800

    Adding alias resource

--- internal/db/sqltest/tests/alias/targets.sql
+++ internal/db/sqltest/tests/alias/targets.sql
@@ -9,9 +9,9 @@ select is(count(*), 1::bigint) from alias_target where public_id = 'alt__t____cb
 
 -- validate destination id and host id can be updated
 prepare update_target_alias_destination as
-    update alias_target
-    set (destination_id, host_id) = ('t_________cg', 'h_________cg')
-    where public_id = 'alt__t____cb';
+update alias_target
+set (destination_id, host_id) = ('t_________cg', 'h_________cg')
+where public_id = 'alt__t____cb';
 select lives_ok('update_target_alias_destination');
 
 select is(count(*), 1::bigint) from alias_target where value = 'blue.tcp.target';
@@ -20,17 +20,17 @@ prepare insert_case_insensitive_value_duplicate AS
     insert into alias_target (public_id, scope_id, value)
     values ('new_alias_for_tests', 'global', 'BLUE.TCP.TARGET');
 select throws_like(
-    'insert_case_insensitive_value_duplicate',
-    'duplicate key value violates unique constraint "alias_value_uq"'
-);
+               'insert_case_insensitive_value_duplicate',
+               'duplicate key value violates unique constraint "alias_value_uq"'
+           );
 
 select is(count(*), 0::bigint) from alias_target where public_id = 'alt__t____cb' and destination_id = 't_________cb';
 select is(count(*), 1::bigint) from alias_target where public_id = 'alt__t____cb' and destination_id = 't_________cg' and host_id = 'h_________cg';
 
 -- validate deleting a target nulls out the destination id and host id
 prepare update_target_alias as
-    delete from target_tcp
-    where public_id = 't_________cg';
+delete from target_tcp
+where public_id = 't_________cg';
 select lives_ok('update_target_alias');
 
 select is(count(*), 0::bigint) from alias_target where public_id = 'alt__t____cb' and destination_id = 't_________cb';
@@ -42,9 +42,9 @@ prepare insert_destination_id_not_set_when_host_id_is_set as
     insert into alias_target (public_id, scope_id, value, host_id)
     values ('unset_destination_id', 'global', 'unset.destination.id', 'h_________cb');
 select throws_like(
-    'insert_destination_id_not_set_when_host_id_is_set',
-    'new row for relation "alias_target" violates check constraint "destination_id_set_when_host_id_is_set"'
-);
+               'insert_destination_id_not_set_when_host_id_is_set',
+               'new row for relation "alias_target" violates check constraint "destination_id_set_when_host_id_is_set"'
+           );
 
 select * from finish();
 rollback;
--- internal/db/sqltest/tests/alias/wt_target_alias.sql
+++ internal/db/sqltest/tests/alias/wt_target_alias.sql
@@ -5,131 +5,131 @@
 --  validates the wt_controller_id domain
 
 begin;
-  select plan(16);
+select plan(16);
 
-  select has_domain('wt_target_alias');
-  
-  create table target_alias_testing (
-    v wt_target_alias
-  );
+select has_domain('wt_target_alias');
 
-  prepare empty_insert as insert into target_alias_testing (v) values ('');
-  select throws_like(
-    'empty_insert',
-    '%"wt_alias_too_short"',
-    'We should error for empty values'
-  );
+create table target_alias_testing (
+  v wt_target_alias
+);
 
-  prepare valid_inserts as insert into target_alias_testing (v) values 
-    ('a'),
-    ('A'),
-    ('192.168.1.9-9'),
-    ('foo'),
-    ('a.b.c'),
-    ('A.B.C'),
-    ('a-b-c'),
-    ('a.9-9'),
-    ('9things'),
-    ('9-things'),
-    ('hp--something.test.com'),
-    ('test-for-long-name-which-is-almost-over-the-limit-of-characters'),
-    ('TEST-FOR-LONG-NAME-WHICH-IS-ALMOST-OVER-THE-LIMIT-OF-CHARACTERS'),
-    ('test-for-long-name-which-is-almost-over-the-limit-of-characters.another-label'),
-    ('test.test-for-long-name-which-is-almost-over-the-limit-of-characters'),
-    ('test-for-long-name-which-is-almost-over-the-limit-of-characters.test-for-long-name-which-is-almost-over-the-limit-of-characters'),
-    ('9-things.9-things');
-  select lives_ok('valid_inserts');
+prepare empty_insert as insert into target_alias_testing (v) values ('');
+select throws_like(
+               'empty_insert',
+               '%"wt_alias_too_short"',
+               'We should error for empty values'
+         );
 
-  prepare label_too_long as insert into target_alias_testing (v) values
+prepare valid_inserts as insert into target_alias_testing (v) values
+  ('a'),
+  ('A'),
+  ('192.168.1.9-9'),
+  ('foo'),
+  ('a.b.c'),
+  ('A.B.C'),
+  ('a-b-c'),
+  ('a.9-9'),
+  ('9things'),
+  ('9-things'),
+  ('hp--something.test.com'),
+  ('test-for-long-name-which-is-almost-over-the-limit-of-characters'),
+  ('TEST-FOR-LONG-NAME-WHICH-IS-ALMOST-OVER-THE-LIMIT-OF-CHARACTERS'),
+  ('test-for-long-name-which-is-almost-over-the-limit-of-characters.another-label'),
+  ('test.test-for-long-name-which-is-almost-over-the-limit-of-characters'),
+  ('test-for-long-name-which-is-almost-over-the-limit-of-characters.test-for-long-name-which-is-almost-over-the-limit-of-characters'),
+  ('9-things.9-things');
+select lives_ok('valid_inserts');
+
+prepare label_too_long as insert into target_alias_testing (v) values
   ('test-for-long-name-which-is-almost-over-the-limit-of-charactersX');
-    select throws_like(
-        'label_too_long',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'label_too_long',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare label_too_long_2 as insert into target_alias_testing (v) values
+prepare label_too_long_2 as insert into target_alias_testing (v) values
   ('a.test-for-long-name-which-is-almost-over-the-limit-of-charactersX');
-    select throws_like(
-        'label_too_long_2',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'label_too_long_2',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare label_too_long_3 as insert into target_alias_testing (v) values
+prepare label_too_long_3 as insert into target_alias_testing (v) values
   ('test-for-long-name-which-is-almost-over-the-limit-of-charactersX.a');
-    select throws_like(
-        'label_too_long_3',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'label_too_long_3',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare starting_with_hyphen as insert into target_alias_testing (v) values
+prepare starting_with_hyphen as insert into target_alias_testing (v) values
   ('-test.com');
-    select throws_like(
-        'starting_with_hyphen',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'starting_with_hyphen',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare starting_with_hyphen2 as insert into target_alias_testing (v) values
+prepare starting_with_hyphen2 as insert into target_alias_testing (v) values
   ('a.-test');
-    select throws_like(
-        'starting_with_hyphen2',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'starting_with_hyphen2',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare ending_with_hyphen as insert into target_alias_testing (v) values
+prepare ending_with_hyphen as insert into target_alias_testing (v) values
   ('test-.com');
-    select throws_like(
-        'ending_with_hyphen',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'ending_with_hyphen',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare ending_with_hyphen2 as insert into target_alias_testing (v) values
+prepare ending_with_hyphen2 as insert into target_alias_testing (v) values
   ('a.test-');
-    select throws_like(
-        'ending_with_hyphen2',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'ending_with_hyphen2',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare ending_with_hyphen3 as insert into target_alias_testing (v) values
+prepare ending_with_hyphen3 as insert into target_alias_testing (v) values
   ('a.9-');
-    select throws_like(
-        'ending_with_hyphen3',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'ending_with_hyphen3',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare empty_label as insert into target_alias_testing (v) values
+prepare empty_label as insert into target_alias_testing (v) values
   ('a..com');
-    select throws_like(
-        'empty_label',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'empty_label',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare empty_label2 as insert into target_alias_testing (v) values
+prepare empty_label2 as insert into target_alias_testing (v) values
   ('.a.com');
-    select throws_like(
-        'empty_label2',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'empty_label2',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare empty_label3 as insert into target_alias_testing (v) values
+prepare empty_label3 as insert into target_alias_testing (v) values
   ('a.com.');
-    select throws_like(
-        'empty_label3',
-        '%"wt_target_alias_value_shape"'
-    );
+select throws_like(
+               'empty_label3',
+               '%"wt_target_alias_value_shape"'
+         );
 
-  prepare numeric_only_tld as insert into target_alias_testing (v) values
+prepare numeric_only_tld as insert into target_alias_testing (v) values
   ('a.123');
-    select throws_like(
-        'numeric_only_tld',
-        '%"wt_target_alias_tld_not_only_numeric"'
-    );
+select throws_like(
+               'numeric_only_tld',
+               '%"wt_target_alias_tld_not_only_numeric"'
+         );
 
-  prepare numeric_only_tld2 as insert into target_alias_testing (v) values
+prepare numeric_only_tld2 as insert into target_alias_testing (v) values
   ('a.9');
-    select throws_like(
-        'numeric_only_tld2',
-        '%"wt_target_alias_tld_not_only_numeric"'
-    );
+select throws_like(
+               'numeric_only_tld2',
+               '%"wt_target_alias_tld_not_only_numeric"'
+         );
 
-  select * from finish();
+select * from finish();
 rollback;
