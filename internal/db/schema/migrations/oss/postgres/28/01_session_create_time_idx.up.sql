b6fe9824abbb7eddeef3d5ceee8278eed82931b9
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2024-01-10 13:41:52 -0800

    internal/db: optimize session list indexes

--- internal/db/schema/migrations/oss/postgres/28/01_session_create_time_idx.up.sql
+++ internal/db/schema/migrations/oss/postgres/28/01_session_create_time_idx.up.sql
@@ -4,6 +4,7 @@
 begin;
 -- Session list queries always use an order by on create_time.
 -- This index can aide in performing this order by.
+-- Dropped in 81/05_session_base_table_updates.up.sql.
 create index
   session_create_time
 on
