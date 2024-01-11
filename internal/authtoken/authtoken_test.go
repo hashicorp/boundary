fd1988e07ff95553b0a9eba253ced4ac510f3fb1
Timothy Messier <tim.messier@gmail.com>
2024-01-11 18:05:15 +0000

    test(authtoken): Associate test account with a user
    This test started failing now that the data warehouse records a auth
    token fact when an auth token is issued and it requires that the auth
    token has a user associated with it.
    
    Since this test is testing the interaction with the database directly,
    and not via the repository, it by-passes the application checks that
    would prevent an auth token without an associated user being inserted.
    
    This test is updated to meet this same pre-condition prior to attempting
    to insert into the database.

--- internal/authtoken/authtoken_test.go
+++ internal/authtoken/authtoken_test.go
@@ -131,13 +131,16 @@ func TestAuthToken_DbCreate(t *testing.T) {
 	ctx := context.Background()
 	conn, _ := db.TestSetup(t, "postgres")
 	rootWrapper := db.TestWrapper(t)
+	iamRepo := iam.TestRepo(t, conn, rootWrapper)
 	kms := kms.TestKms(t, conn, rootWrapper)
-	org, _ := iam.TestScopes(t, iam.TestRepo(t, conn, rootWrapper))
+	org, _ := iam.TestScopes(t, iamRepo)
 	wrapper, err := kms.GetWrapper(context.Background(), org.GetPublicId(), 1)
 	require.NoError(t, err)
+	user := iam.TestUser(t, iamRepo, org.GetPublicId())
 	am := password.TestAuthMethods(t, conn, org.GetPublicId(), 1)[0]
 	acct := password.TestAccount(t, conn, am.GetPublicId(), "name1")
 	createdAuthToken := TestAuthToken(t, conn, kms, org.GetPublicId())
+	iamRepo.AddUserAccounts(ctx, user.GetPublicId(), user.GetVersion(), []string{acct.GetPublicId()})
 
 	testAuthTokenId := func() string {
 		id, err := NewAuthTokenId(ctx)
