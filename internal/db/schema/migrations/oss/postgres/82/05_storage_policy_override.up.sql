d620dcbd4f4b152b4da85ef32e51dd4cbc327284
Louis Ruch <louisruch@gmail.com>
2024-01-18 15:08:12 -0800

    feat(db): Update storage policy override default to false

---
+++ internal/db/schema/migrations/oss/postgres/82/05_storage_policy_override.up.sql
@@ -0,0 +1,10 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+
+  alter table policy_storage_policy
+    alter column retain_for_days_overridable set default false,
+    alter column delete_after_days_overridable set default false;
+
+commit;
