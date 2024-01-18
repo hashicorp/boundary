8c462546027f875c86ce8a9d39d1b62fa881dfed
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2024-01-17 22:32:07 -0800

    Remove now-incorrect comment

--- internal/db/schema/migrations/oss/postgres/0/06_iam.up.sql
+++ internal/db/schema/migrations/oss/postgres/0/06_iam.up.sql
@@ -379,7 +379,6 @@ create trigger ensure_grant_scope_id_valid before insert or update on iam_role
 
 -- define the immutable fields for iam_role (started trigger name with "a_" so
 -- it will run first)
--- Updated in 83/01_iam_role_grant_scope to include grant_scope_id
 create trigger a_immutable_columns before update on iam_role
   for each row execute procedure immutable_columns('public_id', 'create_time', 'scope_id');
 
