3963c93dcd730d2b9e4d2f20d55b4047ddd6321a
Todd <github@quaddi.com>
2024-01-23 13:10:59 -0800

    fixup! Adding alias resource

--- internal/db/schema/migrations/oss/postgres/82/01_citext_extension.up.sql
+++ internal/db/schema/migrations/oss/postgres/82/01_citext_extension.up.sql
@@ -6,6 +6,6 @@ begin;
   -- https://www.postgresql.org/docs/14/citext.html allows us to make
   -- case-insensitive uniqueness constraints which is useful to us for
   -- aliases.
-  create extension citext;
+  create extension "citext";
   
 commit;
