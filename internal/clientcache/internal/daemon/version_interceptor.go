b984326a1ede7195d10bed6e35d7b157e1c59268
Todd <github@quaddi.com>
2024-01-24 13:47:00 -0800

    Add Log Service Handler and report if the daemon is running in background mode

--- internal/clientcache/internal/daemon/version_interceptor.go
+++ internal/clientcache/internal/daemon/version_interceptor.go
@@ -12,17 +12,23 @@ import (
 
 const (
 	VersionHeaderKey = "boundary_version"
+	BackgroundKey    = "background"
 )
 
-// versionInterceptor is an interceptor which attaches the daemon's version
+// serverMetadataInterceptor is an interceptor which attaches the daemon's version
 // number to all responses that it intercepts
-func versionInterceptor(h http.Handler) http.Handler {
+func serverMetadataInterceptor(h http.Handler, inBackground bool) http.Handler {
 	if util.IsNil(h) {
 		return nil
 	}
 
+	background := "false"
+	if inBackground {
+		background = "true"
+	}
 	needVer := version.Get().VersionNumber()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add(BackgroundKey, background)
 		w.Header().Add(VersionHeaderKey, needVer)
 		h.ServeHTTP(w, r)
 	})
