672206c12b6561d88b6788e1bca7ca43d9cc2c31
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2024-01-26 16:11:36 -0800

    testing/e2e: paginate managed groups

--- testing/internal/e2e/boundary/authmethod.go
+++ testing/internal/e2e/boundary/authmethod.go
@@ -12,11 +12,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// CreateNewAuthMethodApi creates a new auth method using the Go api.
+// CreateNewAuthMethodApi creates a new password auth method using the Go api.
 // Returns the id of the new auth method
 func CreateNewAuthMethodApi(t testing.TB, ctx context.Context, client *api.Client, scopeId string) string {
 	aClient := authmethods.NewClient(client)
 	newAMResult, err := aClient.Create(ctx, "password", scopeId)
+	require.NoError(t, err)
+
+	authMethodId := newAMResult.Item.Id
+	t.Logf("Created Auth Method: %s", authMethodId)
+	return authMethodId
+}
+
+// CreateNewOidcAuthMethodApi creates a new oidc auth method using the Go api.
+// Returns the id of the new auth method
+func CreateNewOidcAuthMethodApi(t testing.TB, ctx context.Context, client *api.Client, scopeId string) string {
+	aClient := authmethods.NewClient(client)
+	newAMResult, err := aClient.Create(ctx, "oidc", scopeId,
+		authmethods.WithOidcAuthMethodApiUrlPrefix("https://some_url_prefix"),
+		authmethods.WithOidcAuthMethodClientId("some_client_id"),
+		authmethods.WithOidcAuthMethodClientSecret("some_client_secret"),
+	)
 	require.NoError(t, err)
 
 	authMethodId := newAMResult.Item.Id
