a36b43d3f8a58a5396654a49f6771c87961d970b
Todd <github@quaddi.com>
2024-01-31 09:43:24 -0800

    Add alias schema changes (#4230)
    * Adding alias resource
    
    * Update website/content/docs/install-boundary/system-requirements.mdx
    
    ---------
    
    Co-authored-by: Irena Rindos <irenarindos@users.noreply.github.com>

---
+++ internal/db/sqltest/tests/pagination/alias.sql
@@ -0,0 +1,12 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+  select plan(2);
+
+  select has_index('alias_target', 'alias_target_create_time_public_id_idx', array['create_time', 'public_id']);
+  select has_index('alias_target', 'alias_target_update_time_public_id_idx', array['update_time', 'public_id']);
+
+  select * from finish();
+
+rollback;
