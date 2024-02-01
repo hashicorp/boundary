5726eac73b38cf3f19c3a5562c54a095993974c2
irenarindos <irena.rindos@gmail.com>
2024-02-01 13:50:58 -0500

    address feedback

--- internal/target/tcp/repository_alias_test.go
+++ internal/target/tcp/repository_alias_test.go
@@ -29,13 +29,8 @@ func TestRepository_FetchAliases(t *testing.T) {
 	repo, err := target.NewRepository(ctx, rw, rw, testKms)
 	require.NoError(t, err)
 
-	tar := tcp.TestTarget(ctx, t, conn, staticProj.PublicId, "test-target")
-	aname1 := "test.alias.one"
-	aname2 := "test.alias.two"
-	aname3 := "test.alias.three"
-	aliases := make(map[string]*target2.Alias)
-
 	t.Run("target-no-aliases", func(t *testing.T) {
+		tar := tcp.TestTarget(ctx, t, conn, staticProj.PublicId, "test-target-1")
 		tar, err := repo.LookupTarget(context.Background(), tar.GetPublicId())
 		require.NoError(t, err)
 
@@ -43,17 +38,25 @@ func TestRepository_FetchAliases(t *testing.T) {
 		assert.Equal(t, len(gotAliases), 0)
 	})
 	t.Run("target-one-alias", func(t *testing.T) {
-		al := target2.TestAlias(t, rw, aname1, target2.WithDestinationId(tar.GetPublicId()))
-		aliases[aname1] = al
+		aname := "test-alias"
+		tar := tcp.TestTarget(ctx, t, conn, staticProj.PublicId, "test-target-2")
+		al := target2.TestAlias(t, rw, aname, target2.WithDestinationId(tar.GetPublicId()))
 
 		tar, err := repo.LookupTarget(context.Background(), tar.GetPublicId())
 		require.NoError(t, err)
 
 		gotAliases := tar.GetAliases()
 		assert.Equal(t, len(gotAliases), 1)
-		assert.Equal(t, al.Value, aname1)
+		assert.Equal(t, al.Value, aname)
 	})
 	t.Run("target-multiple-alias", func(t *testing.T) {
+		tar := tcp.TestTarget(ctx, t, conn, staticProj.PublicId, "test-target3")
+		aliases := make(map[string]*target2.Alias)
+		aname1 := "test.alias.one"
+		aname2 := "test.alias.two"
+		aname3 := "test.alias.three"
+		al := target2.TestAlias(t, rw, aname1, target2.WithDestinationId(tar.GetPublicId()))
+		aliases[aname1] = al
 		al2 := target2.TestAlias(t, rw, aname2, target2.WithDestinationId(tar.GetPublicId()))
 		aliases[aname2] = al2
 		al3 := target2.TestAlias(t, rw, aname3, target2.WithDestinationId(tar.GetPublicId()))
