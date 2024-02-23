f67e5a0ee76f8e39175525ebb9879c6782d52393
Stan Ryzhov <60649800+stasryzhov@users.noreply.github.com>
2024-02-23 16:24:08 +0000

    backport of commit 2c75f0e3d798d294c46c7638444e0614e68a9bc3

--- testing/internal/e2e/boundary/role.go
+++ testing/internal/e2e/boundary/role.go
@@ -6,11 +6,13 @@ package boundary
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/boundary/api"
 	"github.com/hashicorp/boundary/api/roles"
 	"github.com/hashicorp/boundary/testing/internal/e2e"
+	"github.com/hashicorp/go-secure-stdlib/base62"
 	"github.com/stretchr/testify/require"
 )
 
@@ -48,6 +50,36 @@ func CreateNewRoleCli(t testing.TB, ctx context.Context, scopeId string) string 
 	return newRoleId
 }
 
+// CreateRoleCli creates a new role using the Boundary CLI.
+// Returns the id of the new role or error
+func CreateRoleCli(t testing.TB, ctx context.Context, scopeId string) (string, error) {
+	name, err := base62.Random(16)
+	if err != nil {
+		return "", fmt.Errorf("error generating role name: %w", err)
+	}
+	output := e2e.RunCommand(ctx, "boundary",
+		e2e.WithArgs(
+			"roles", "create",
+			"-scope-id", scopeId,
+			"-name", fmt.Sprintf("e2e Role %s", name),
+			"-description", "e2e",
+			"-format", "json",
+		),
+	)
+	if output.Err != nil {
+		return "", fmt.Errorf("error creating role: %w: %s", output.Err, string(output.Stderr))
+	}
+
+	var newRoleResult roles.RoleCreateResult
+	if err := json.Unmarshal(output.Stdout, &newRoleResult); err != nil {
+		return "", fmt.Errorf("error unmarshalling role creation result: %w", err)
+	}
+
+	newRoleId := newRoleResult.Item.Id
+	t.Logf("Created Role: %s in scope %s", newRoleId, scopeId)
+	return newRoleId, nil
+}
+
 // AddGrantToRoleCli adds a grant/permission to a role using the cli
 func AddGrantToRoleCli(t testing.TB, ctx context.Context, roleId string, grant string) {
 	output := e2e.RunCommand(ctx, "boundary",
@@ -70,4 +102,59 @@ func AddPrincipalToRoleCli(t testing.TB, ctx context.Context, roleId string, pri
 		),
 	)
 	require.NoError(t, output.Err, string(output.Stderr))
+	t.Logf("Principle %s added to role: %s", principal, roleId)
+}
+
+// SetGrantScopesToRoleCli uses Boundary CLI to override grant scopes for the role with the provided ones.
+// Option WithGrantScopeId can be used multiple times to provide grant scope IDs.
+func SetGrantScopesToRoleCli(t testing.TB, ctx context.Context, roleId string, opt ...RoleOption) error {
+	opts := getRoleOpts(opt...)
+	var args []string
+
+	if len(opts.scopeIds) == 0 {
+		return fmt.Errorf("at least one grant scope id must be provided")
+	}
+	for _, id := range opts.scopeIds {
+		args = append(args, "-grant-scope-id", id)
+	}
+
+	output := e2e.RunCommand(ctx, "boundary",
+		e2e.WithArgs(
+			"roles", "set-grant-scopes",
+			"-id", roleId,
+		),
+		e2e.WithArgs(args...),
+	)
+	if output.Err != nil {
+		return fmt.Errorf("error setting grant scopes to role: %w: %s", output.Err, string(output.Stderr))
+	}
+
+	t.Logf("Grant scopes are set to role %s: %v", roleId, opts.scopeIds)
+	return nil
+}
+
+// getRoleOpts iterates the inbound RoleOptions and returns a struct.
+func getRoleOpts(opt ...RoleOption) roleOptions {
+	opts := roleOptions{
+		scopeIds: make([]string, 0),
+	}
+	for _, o := range opt {
+		o(&opts)
+	}
+	return opts
+}
+
+// RoleOption represents how Options are passed as arguments.
+type RoleOption func(*roleOptions)
+
+// roleOptions is a struct representing available options for roles.
+type roleOptions struct {
+	scopeIds []string
+}
+
+// WithGrantScopeId provides an option to set the grant scope to a role.
+func WithGrantScopeId(scopeId string) RoleOption {
+	return func(o *roleOptions) {
+		o.scopeIds = append(o.scopeIds, scopeId)
+	}
 }
