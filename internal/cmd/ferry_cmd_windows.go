39262ab260bafae4ca88e0283ca6faf29afa386b
Todd <github@quaddi.com>
2024-02-29 07:37:33 -0800

    Add boundary ferry command (#4462)
    * Add boundary ferry command

---
+++ internal/cmd/ferry_cmd_windows.go
@@ -0,0 +1,22 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+//go:build amd64 || arm64
+
+package cmd
+
+import (
+	"github.com/hashicorp/boundary/internal/cmd/base"
+	"github.com/hashicorp/boundary/internal/cmd/commands/ferry"
+	"github.com/mitchellh/cli"
+)
+
+func init() {
+	extraCommandsFuncs = append(extraCommandsFuncs, func(ui, serverCmdUi cli.Ui, runOpts *RunOptions) {
+		Commands["ferry"] = func() (cli.Command, error) {
+			return &ferry.FerryCommand{
+				Command: base.NewCommand(ui),
+			}, nil
+		}
+	})
+}
