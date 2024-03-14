b251e7a8b9ea672ec0f2f2e3267bcf7a456a39fc
Michael Li <michael.li@hashicorp.com>
2024-03-14 10:09:51 -0400

    chore(e2e): Add session recording resources for UI tests (#4498)

--- enos/modules/test_e2e_ui/main.tf
+++ enos/modules/test_e2e_ui/main.tf
@@ -108,6 +108,16 @@ variable "aws_host_set_filter" {
   type        = string
   default     = ""
 }
+variable "aws_region" {
+  description = "AWS region where the resources will be created"
+  type        = string
+  default     = ""
+}
+variable "aws_bucket_name" {
+  description = "AWS S3 bucket name"
+  type        = string
+  default     = ""
+}
 variable "aws_host_set_ips" {
   description = "List of IP addresses in aws_host_set_filter1"
   type        = list(string)
@@ -153,6 +163,11 @@ variable "worker_token" {
   type        = string
   default     = ""
 }
+variable "worker_tag_egress" {
+  description = "Worker tag for the egress worker"
+  type        = string
+  default     = ""
+}
 
 locals {
   aws_ssh_private_key_path = abspath(var.aws_ssh_private_key_path)
@@ -180,6 +195,8 @@ resource "enos_local_exec" "run_e2e_test" {
     E2E_AWS_SECRET_ACCESS_KEY     = var.aws_secret_access_key
     E2E_AWS_HOST_SET_FILTER       = var.aws_host_set_filter
     E2E_AWS_HOST_SET_IPS          = local.aws_host_set_ips
+    E2E_AWS_REGION                = var.aws_region
+    E2E_AWS_BUCKET_NAME           = var.aws_bucket_name
     E2E_LDAP_ADDR                 = var.ldap_address
     E2E_LDAP_DOMAIN_DN            = var.ldap_domain_dn
     E2E_LDAP_ADMIN_DN             = var.ldap_admin_dn
@@ -188,6 +205,7 @@ resource "enos_local_exec" "run_e2e_test" {
     E2E_LDAP_USER_PASSWORD        = var.ldap_user_password
     E2E_LDAP_GROUP_NAME           = var.ldap_group_name
     E2E_WORKER_TOKEN              = var.worker_token
+    E2E_WORKER_TAG_EGRESS         = var.worker_tag_egress
   }
 
   inline = var.debug_no_run ? [""] : ["set -o pipefail; PATH=\"${var.local_boundary_dir}:$PATH\" yarn --cwd ${var.local_boundary_ui_src_dir}/ui/admin run e2e 2>&1 | tee ${path.module}/../../test-e2e-ui.log"]
