d3befc702e5487c4a88d80b6c832b9728bc422aa
Todd <github@quaddi.com>
2024-03-25 10:39:54 -0700

    Don't retry background add-token ferry requests

--- internal/cmd/commands/ferry/addtoken.go
+++ internal/cmd/commands/ferry/addtoken.go
@@ -48,6 +48,10 @@ func addToken(ctx context.Context, apiClient *api.Client, port uint) (*api.Respo
 	client.RetryWaitMin = 100 * time.Millisecond
 	client.RetryWaitMax = 1500 * time.Millisecond
 
+	// TODO: Until we release ferry feature, do not retry attempts to connect to
+	// the ferry daemon since it adds a noticeably long delay to the command.
+	client.RetryMax = 0
+
 	req, err := retryablehttp.NewRequestWithContext(ctx, "POST", ferryUrl(port, "v1/tokens"),
 		retryablehttp.ReaderFunc(func() (io.Reader, error) {
 			b, err := json.Marshal(&pa)
