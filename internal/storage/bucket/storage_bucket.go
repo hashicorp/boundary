7a3c484147285b91b6b594349bcc2b2a0b692d36
Damian Debkowski <damian.debkowski@hashicorp.com>
2024-04-02 10:19:14 -0400

    feat(storage): add UpsertStorageBucket & GetStorageBucketStates methods

---
+++ internal/storage/bucket/storage_bucket.go
@@ -0,0 +1,23 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package bucket
+
+import (
+	"github.com/hashicorp/boundary/sdk/pbs/controller/api/resources/storagebuckets"
+	plgpb "github.com/hashicorp/boundary/sdk/pbs/plugin"
+)
+
+// StorageBucketSingleton ensures safe and easy access to a storage bucket and
+// its storage bucket credential state.
+type StorageBucketSingleton interface {
+	GetState() *plgpb.StorageBucketCredentialState
+	GetBucket() *storagebuckets.StorageBucket
+	SetState(*plgpb.StorageBucketCredentialState) error
+	SetBucket(*storagebuckets.StorageBucket) error
+	HasWriteAccess() bool
+	HasReadAccess() bool
+	AddDependant(name string)
+	RemoveDependant(name string)
+	HasDependants() bool
+}
