55ee9b79f5463265401bd4bdeb2a0a53445b9899
Uruemu Aganbi <uruemu@hashicorp.com>
2024-05-24 10:22:04 -0400

    AppToken service #WIP

--- internal/db/schema/migrations/oss/postgres/1001/01_app_tokens.up.sql
+++ internal/db/schema/migrations/oss/postgres/1001/01_app_tokens.up.sql
@@ -23,7 +23,14 @@ create table app_token (
     constraint scope_id_key_fkey
     references iam_scope(public_id)
     on delete cascade
-    on update cascade
+    on update cascade,
+  grant_scope_id_or_special text not null -- pk
+    constraint grant_scope_id_or_special_is_valid
+    check (
+      length(trim(grant_scope_id_or_special)) = 12
+        or
+      grant_scope_id_or_special in ('global', 'this', 'children', 'descendants')
+    )
 );
 comment on table app_token is
   'app_token defines an application auth token';
@@ -37,6 +44,9 @@ $$ language plpgsql;
 
 create trigger immutable_app_token before update on app_token
   for each row execute procedure app_token_immutable();
+
+create trigger ensure_grant_scope_id_or_special_valid before insert or update on app_token
+  for each row execute procedure role_grant_scope_id_or_special_valid();
 
 create table app_token_periodic_expiration_interval (
   app_token_id wt_public_id primary key
