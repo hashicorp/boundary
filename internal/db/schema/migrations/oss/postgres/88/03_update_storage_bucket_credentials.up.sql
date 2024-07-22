1be0e5d41370d84d8b0d38602bba73063213b9d4
Damian Debkowski <Damian.Debkowski@hashicorp.com>
2024-07-22 09:06:35 -0700

    fix: pass along storage bucket credential id for sb update job (#4947)

--- internal/db/schema/migrations/oss/postgres/88/03_update_storage_bucket_credentials.up.sql
+++ internal/db/schema/migrations/oss/postgres/88/03_update_storage_bucket_credentials.up.sql
@@ -5,27 +5,27 @@ begin;
 
   create view update_worker_storage_bucket_credential as
     select distinct
-      sb.scope_id as storage_bucket_scope_id,
-      sb.name as storage_bucket_name,
-      sb.description as storage_bucket_description,
-      sb.bucket_name as storage_bucket_bucket_name,
-      sb.bucket_prefix as storage_bucket_bucket_prefix,
-      sb.worker_filter as storage_bucket_worker_filter,
-      sb.attributes as storage_bucket_attributes,
-      sb.plugin_id as plugin_id,
+      sb.public_id,
+      sb.scope_id,
+      sb.name,
+      sb.description,
+      sb.bucket_name,
+      sb.bucket_prefix,
+      sb.worker_filter,
+      sb.attributes,
+      sb.version,
+      sb.plugin_id,
+      sb.storage_bucket_credential_id,
       pl.name as plugin_name,
       pl.description as plugin_description,
-      sbc.storage_bucket_id as storage_bucket_id,
       sbcms.secrets_encrypted as ct_secrets,
       sbcms.key_id as key_id
 
-    from storage_bucket_credential sbc
-    join storage_plugin_storage_bucket sb
-      on sbc.storage_bucket_id = sb.public_id
+    from storage_plugin_storage_bucket sb
     join plugin pl
       on sb.plugin_id = pl.public_id
     left join storage_bucket_credential_managed_secret sbcms
-      on  sbc.private_id = sbcms.private_id;
+      on  sb.storage_bucket_credential_id = sbcms.private_id;
   comment on view update_worker_storage_bucket_credential is
     'update_worker_storage_bucket_credential is used find workers using storage bucket credentials that need to be updated to the latest version.';
 
