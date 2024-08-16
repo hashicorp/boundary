f847911d7464b41f6a52f9245ba197350e78f88e
Hugo <10965479+hugoghx@users.noreply.github.com>
2024-08-16 15:28:20 +0100

    chore(cmd): Remove IntVar and move to Int64Var

--- internal/cmd/common/flags.go
+++ internal/cmd/common/flags.go
@@ -72,7 +72,7 @@ func PopulateCommonFlags(c *base.Command, f *base.FlagSet, resourceType string, 
 				Usage:  fmt.Sprintf("Description to set on the %s.", resourceType),
 			})
 		case "version":
-			f.IntVar(&base.IntVar{
+			f.Int64Var(&base.Int64Var{
 				Name:   "version",
 				Target: &c.FlagVersion,
 				Usage:  fmt.Sprintf("The version of the %s against which to perform an update operation. If not specified, the command will perform a check-and-set automatically.", resourceType),
