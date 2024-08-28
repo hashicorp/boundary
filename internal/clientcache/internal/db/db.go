5457acf2d0615c7adb51bd0cc650e4ec7b406c95
Jim <jlambert@hashicorp.com>
2024-08-28 13:26:50 -0400

    feat (clientcache): use a persistent cache (#5051)
    * feat (clientcache): use a persistent cache
    
    By default, we will use a persistent cache for the
    client cache. This will allow us to keep the cache
    across restarts of the server and it will also
    reduce the amount of time it takes to start the
    server while reducing the amount of memory used.
    
    This change also includes a validation of the cache
    schema version. If the schema version is different
    from the one expected, the cache will be
    reset/recreated.

--- internal/clientcache/internal/db/db.go
+++ internal/clientcache/internal/db/db.go
@@ -7,6 +7,8 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/hashicorp/boundary/internal/db"
 	"github.com/hashicorp/boundary/internal/errors"
@@ -17,11 +19,16 @@ import (
 //go:embed schema.sql
 var cacheSchema string
 
+//go:embed schema_reset.sql
+var cacheSchemaReset string
+
 // DefaultStoreUrl uses a temp in-memory sqlite database see: https://www.sqlite.org/inmemorydb.html
 const DefaultStoreUrl = "file::memory:?_pragma=foreign_keys(1)"
 
 // Open creates a database connection. WithUrl is supported, but by default it
 // uses an in memory sqlite table. Sqlite is the only supported dbtype.
+// Supported options: WithUrl, WithGormFormatter, WithDebug,
+// WithTestValidSchemaVersion (for testing purposes)
 func Open(ctx context.Context, opt ...Option) (*db.DB, error) {
 	const op = "db.Open"
 	opts, err := getOpts(opt...)
@@ -50,14 +57,36 @@ func Open(ctx context.Context, opt ...Option) (*db.DB, error) {
 	conn.Debug(opts.withDebug)
 
 	switch {
-	case opts.withDbType == dbw.Sqlite:
+	case opts.withDbType == dbw.Sqlite && url == DefaultStoreUrl:
 		if err := createTables(ctx, conn); err != nil {
 			return nil, errors.Wrap(ctx, err, op)
+		}
+	case opts.withDbType == dbw.Sqlite && url != DefaultStoreUrl:
+		ok, err := validSchema(ctx, conn, opt...)
+		if err != nil {
+			return nil, errors.Wrap(ctx, err, op)
+		}
+		if !ok {
+			if err := resetSchema(ctx, conn); err != nil {
+				return nil, errors.Wrap(ctx, err, op)
+			}
+			if err := createTables(ctx, conn); err != nil {
+				return nil, errors.Wrap(ctx, err, op)
+			}
 		}
 	default:
 		return nil, errors.New(ctx, errors.InvalidParameter, op, fmt.Sprintf("%q is not a supported cache store type", opts.withDbType))
 	}
 	return conn, nil
+}
+
+func resetSchema(ctx context.Context, conn *db.DB) error {
+	const op = "db.resetSchema"
+	rw := db.New(conn)
+	if _, err := rw.Exec(ctx, cacheSchemaReset, nil); err != nil {
+		return errors.Wrap(ctx, err, op)
+	}
+	return nil
 }
 
 func createTables(ctx context.Context, conn *db.DB) error {
@@ -67,4 +96,57 @@ func createTables(ctx context.Context, conn *db.DB) error {
 		return errors.Wrap(ctx, err, op)
 	}
 	return nil
+}
+
+// validSchema checks of the schema is valid based on its version. Options
+// supported: withTestValidSchemaVersion (for testing purposes)
+func validSchema(ctx context.Context, conn *db.DB, opt ...Option) (bool, error) {
+	const op = "validateSchema"
+	switch {
+	case conn == nil:
+		return false, errors.New(ctx, errors.InvalidParameter, op, "conn is missing")
+	}
+	opts, err := getOpts(opt...)
+	if err != nil {
+		return false, errors.Wrap(ctx, err, op)
+	}
+	if opts.withSchemaVersion == "" {
+		opts.withSchemaVersion = schemaCurrentVersion
+	}
+
+	rw := db.New(conn)
+	s := schema{}
+	err = rw.LookupWhere(ctx, &s, "1=1", nil)
+	switch {
+	case err != nil && strings.Contains(err.Error(), "no such table: schema_version"):
+		return false, nil
+	case err != nil:
+		// not sure if we should return the error or just return false so the
+		// schema is recreated... for now return the error.
+		return false, fmt.Errorf("%s: unable to get version: %w", op, err)
+	case s.Version != opts.withSchemaVersion:
+		return false, nil
+	default:
+		return true, nil
+	}
+}
+
+// schema represents the current schema in the database
+type schema struct {
+	// Version of the schema
+	Version string
+	// UpdateTime is the last update of the version
+	UpdateTime time.Time
+	// CreateTime is the create time of the initial version
+	CreateTime time.Time
+}
+
+const (
+	schemaTableName      = "schema_version"
+	schemaCurrentVersion = "v0.0.1"
+)
+
+// TableName returns the table name
+func (s *schema) TableName() string {
+	return schemaTableName
 }
