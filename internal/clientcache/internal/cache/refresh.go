51a2b2035f9c6bb51fe8f5f1defeeb514183fecd
Timothy Messier <tim.messier@gmail.com>
2024-09-23 15:29:16 +0000

    lint: Fix linter errors

--- internal/clientcache/internal/cache/refresh.go
+++ internal/clientcache/internal/cache/refresh.go
@@ -19,8 +19,9 @@ import (
 	"github.com/hashicorp/go-hclog"
 )
 
-// This is used as an internal error to indicate that a refresh is already in
-// progress, as a signal that the caller may want to handle it a different way.
+// ErrRefreshInProgress is used as an internal error to indicate that a refresh
+// is already in progress, as a signal that the caller may want to handle it a
+// different way.
 var ErrRefreshInProgress error = stderrors.New("cache refresh in progress")
 
 type RefreshService struct {
