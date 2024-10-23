9529491fa6c27123c83e9c673353e2baf1c263e3
Michael Gaffney <mike@gaffney.cc>
2024-10-23 16:16:39 -0400

    feat(session): Delete terminated sessions in batches
    This changes the periodic job that deletes terminated sessions to delete
    the those sessions in batches. Prior to this, a single SQL DELETE
    command was used to delete all qualifying terminated sessions. In
    systems under heavy load, the statement could timeout leading to a
    situation where no qualifying terminated sessions were ever being
    deleted.

---
+++ internal/db/schema/migrations/oss/postgres/92/05_delete_terminated_session_job.up.sql
@@ -0,0 +1,34 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+
+  create table session_delete_terminated_job (
+    batch_size int not null
+      constraint batch_size_must_be_greater_than_0
+        check(batch_size > 0),
+    create_time wt_timestamp,
+    update_time wt_timestamp
+  );
+  comment on table session_delete_terminated_job is
+    'session_delete_terminated_job is a single row table that contains settings for the delete terminated sessions job.';
+
+  -- this index ensures that there will only ever be one row in the
+  -- table. see:
+  -- https://www.postgresql.org/docs/current/indexes-expressional.html
+  create unique index session_delete_terminated_job_one_row
+    on session_delete_terminated_job((batch_size is not null));
+
+  create trigger immutable_columns before update on session_delete_terminated_job
+    for each row execute procedure immutable_columns('create_time');
+
+  create trigger default_create_time_column before insert on session_delete_terminated_job
+    for each row execute procedure default_create_time();
+
+  create trigger update_time_column before update on session_delete_terminated_job
+    for each row execute procedure update_time_column();
+
+  insert into session_delete_terminated_job(batch_size) values(5000);
+
+commit;
+
--- internal/session/query.go
+++ internal/session/query.go
@@ -342,17 +342,6 @@ update session_connection
    %s
 returning public_id;
 `
-	deleteTerminated = `
-delete from session
-using session_state
-where
-	session.public_id = session_state.session_id
-and
-	session_state.state = 'terminated'
-and
-	lower(session_state.active_time_range) < wt_sub_seconds_from_now(@threshold_seconds)
-;
-`
 	sessionCredentialRewrapQuery = `
 select distinct
   cred.session_id,
@@ -454,6 +443,42 @@ values
 
 	sessionCredentialDynamicBatchInsertReturning = `
   returning session_id, library_id, credential_id, credential_purpose;
+`
+)
+
+// queries for the delete terminated sessions job
+const (
+	getDeleteJobParams = `
+with total (to_delete) as (
+  select count(session_id)
+    from session_state
+   where session_state.state                    = 'terminated'
+     and lower(session_state.active_time_range) < wt_sub_seconds_from_now(@threshold_seconds)
+),
+params (batch_size) as (
+  select batch_size
+    from session_delete_terminated_job
+)
+select total.to_delete                             as total_to_delete,
+       params.batch_size                           as batch_size,
+       wt_sub_seconds_from_now(@threshold_seconds) as window_start_time
+  from total, params;
+`
+	setDeleteJobBatchSize = `
+update session_delete_terminated_job
+   set batch_size = @batch_size;
+`
+	deleteTerminatedInBatch = `
+with batch (session_id) as (
+  select session_id
+    from session_state
+   where state                                  = 'terminated'
+     and lower(session_state.active_time_range) < @terminated_before
+   limit @batch_size
+)
+delete
+  from session
+ where public_id in (select session_id from batch);
 `
 )
 
