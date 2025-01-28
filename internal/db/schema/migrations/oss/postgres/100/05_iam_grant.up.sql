9e5a1556682669d62349a1ecb0485d2cbd8212df
Elim Tsiagbey <elim.tsiagbey@hashicorp.com>
2025-01-28 10:59:35 -0500

    chore: remove unapplicable comment

--- internal/db/schema/migrations/oss/postgres/100/05_iam_grant.up.sql
+++ internal/db/schema/migrations/oss/postgres/100/05_iam_grant.up.sql
@@ -6,11 +6,6 @@ begin;
   -- iam_grant is the root table for a grant value object.
   -- A grant can only reference a single resource, including the special
   -- strings "*" to indicate "all" resources, and "unknown" when no resource is set.
-  -- The set of actions that are included in the grant
-  -- get associated with the grant in the iam_grant_action
-  -- table. We could potentially have a trigger function on insert
-  -- that parses the grant and inserts the appropriate rows into the other tables.
-  -- This should be immutable, and there isn't really a need to delete them.
   create table iam_grant (
     canonical_grant text not null primary key,
     resource text not null
