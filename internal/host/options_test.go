acc40e0d08641fdd557f671e89493bf72136f285
Damian Debkowski <Damian.Debkowski@hashicorp.com>
2025-02-06 12:20:39 -0800

    fix: resolve small issues in db transactions (#5522)
    * fix: use the db transaction reader and writer
    
    * fix: internal/server: use ctx when scanning rows
    
    * fix(host): pass db tx reader/writer into helper funcs
    
    * fix: more precise nil check against db.Reader & db.Writer
    
    ---------
    
    Co-authored-by: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>

--- internal/host/options_test.go
+++ internal/host/options_test.go
@@ -77,4 +77,23 @@ func Test_GetOpts(t *testing.T) {
 		assert.Equal(opts.WithStartPageAfterItem.GetPublicId(), "s_1")
 		assert.Equal(opts.WithStartPageAfterItem.GetUpdateTime(), timestamp.New(updateTime))
 	})
+	t.Run("WithReaderWriter", func(t *testing.T) {
+		t.Parallel()
+		t.Run("nil writer", func(t *testing.T) {
+			t.Parallel()
+			_, err := GetOpts(WithReaderWriter(&db.Db{}, nil))
+			require.Error(t, err)
+		})
+		t.Run("nil reader", func(t *testing.T) {
+			t.Parallel()
+			_, err := GetOpts(WithReaderWriter(nil, &db.Db{}))
+			require.Error(t, err)
+		})
+		reader := &db.Db{}
+		writer := &db.Db{}
+		opts, err := GetOpts(WithReaderWriter(reader, writer))
+		require.NoError(t, err)
+		assert.Equal(t, reader, opts.WithReader)
+		assert.Equal(t, writer, opts.WithWriter)
+	})
 }
