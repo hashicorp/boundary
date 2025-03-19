7ecf1858eb47fb2967eef15b7a1d6b682105a04c
Sorawis Nilparuk <sorawis.nilparuk@hashicorp.com>
2025-03-19 11:16:01 -0700

    sql formatting

--- internal/db/schema/migrations/oss/postgres/100/07_oplog_ticket.up.sql
+++ internal/db/schema/migrations/oss/postgres/100/07_oplog_ticket.up.sql
@@ -2,11 +2,9 @@
 -- SPDX-License-Identifier: BUSL-1.1
 
 begin;
-
-insert into imaginary_basket
+insert into oplog_ticket
             (name, version)
-        values  ('iam_role_global',1);
-                ('iam_role_org', 1);
+        values  ('iam_role_global',1),
+                ('iam_role_org', 1),
                 ('iam_role_project', 1);
-
 commit;
