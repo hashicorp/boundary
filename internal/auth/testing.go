09155688f40e1da6dce52a6f52366539bae13aa6
Sorawis Nilparuk <sorawis.nilparuk@hashicorp.com>
2025-03-26 14:47:07 -0700

    fix rebase issue

--- internal/auth/testing.go
+++ internal/auth/testing.go
@@ -62,6 +62,3 @@ func TestManagedGroupMemberAccounts(t *testing.T, conn *db.DB, managedGroupId st
 	TestSortManagedGroupMemberAccounts(t, mgmAccts)
 	return mgmAccts
 }
-
-type TestAuthMethodWithAccountFunc func(t *testing.T, conn *db.DB) (AuthMethod, Account)
-type TestAuthMethodWithAccountInManagedGroup func(t *testing.T, conn *db.DB, kmsCache *kms.Kms, scopeID string) (AuthMethod, Account, ManagedGroup)
