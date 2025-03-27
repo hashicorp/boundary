f7b0a950061818aad4bf3d8180db3d2265a0b07c
Sorawis Nilparuk <sorawis.nilparuk@hashicorp.com>
2025-03-27 12:24:47 -0700

    improve notfound err message

--- internal/iam/role.go
+++ internal/iam/role.go
@@ -5,6 +5,7 @@ package iam
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/boundary/internal/db"
 	"github.com/hashicorp/boundary/internal/db/timestamp"
@@ -337,33 +338,30 @@ func (p *projectRole) Actions() map[string]action.Type {
 // getRoleScopeId returns scopeID for the Role from the base type iam_role table
 // use this to get scope ID to determine which of the role subtype tables to operate on
 func getRoleScopeId(ctx context.Context, r db.Reader, roleId string) (string, error) {
-	const (
-		op    = "iam.getRoleScopeId"
-		query = `select scope_id from iam_role where public_id = ?`
-	)
+	const op = "iam.getRoleScopeId"
 	if roleId == "" {
-		return "", errors.New(ctx, errors.InvalidParameter, op, "missing role ID")
+		return "", errors.New(ctx, errors.InvalidParameter, op, "missing role id")
 	}
 	if r == nil {
 		return "", errors.New(ctx, errors.InvalidParameter, op, "missing db.Reader")
 	}
-	rows, err := r.Query(ctx, query, []any{roleId})
+	rows, err := r.Query(ctx, scopeIdFromRoleIdQuery, []any{roleId})
 	if err != nil {
-		return "", errors.Wrap(ctx, err, op, errors.WithMsg("failed to lookup role scope ID"))
+		return "", errors.Wrap(ctx, err, op, errors.WithMsg("failed to lookup role scope"))
 	}
 	var scopeId string
 	cnt := 0
 	for rows.Next() {
 		cnt++
 		if err := r.ScanRows(ctx, rows, &scopeId); err != nil {
-			return "", errors.Wrap(ctx, err, op, errors.WithMsg("failed scan results from querying role scope ID"))
+			return "", errors.Wrap(ctx, err, op, errors.WithMsg("failed scan results from querying role scope"))
 		}
 	}
 	if err := rows.Err(); err != nil {
-		return "", errors.Wrap(ctx, err, op, errors.WithMsg("unexpected error scanning results from querying role scope ID"))
+		return "", errors.Wrap(ctx, err, op, errors.WithMsg("unexpected error scanning results from querying role scope"))
 	}
 	if cnt == 0 {
-		return "", errors.New(ctx, errors.NotFound, op, "role not found")
+		return "", errors.New(ctx, errors.NotFound, op, fmt.Sprintf("role %s not found", roleId))
 	}
 	return scopeId, nil
 }
