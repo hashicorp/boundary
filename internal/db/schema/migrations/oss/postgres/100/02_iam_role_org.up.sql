e7ba289a2e737749e5b2e40d88729e99dbca0050
Sorawis Nilparuk <sorawis.nilparuk@hashicorp.com>
2025-04-15 14:35:41 -0700

    fix rebase

--- internal/db/schema/migrations/oss/postgres/100/02_iam_role_org.up.sql
+++ internal/db/schema/migrations/oss/postgres/100/02_iam_role_org.up.sql
@@ -23,6 +23,7 @@ begin;
     ('children'),
     ('individual');       
 
+
   create table iam_role_org (
     public_id wt_role_id primary key
       constraint iam_role_fkey
@@ -70,11 +71,11 @@ begin;
   create trigger update_iam_role_org_grant_this_role_scope_update_time before update on iam_role_org
     for each row execute procedure insert_grant_this_role_scope_update_time();
 
-  create trigger update_iam_role_table_update_time after update on iam_role_org
+  create trigger update_iam_role_org_base_table_update_time after update on iam_role_org
     for each row execute procedure update_iam_role_table_update_time();
 
-  create trigger delete_iam_role_subtype after delete on iam_role_org
-    for each row execute procedure delete_iam_role_subtype();
+  create trigger delete_base_iam_role after delete on iam_role_org
+    for each row execute procedure delete_base_iam_role();
 
   create trigger default_create_time_column before insert on iam_role_org
     for each row execute procedure default_create_time();
@@ -86,7 +87,7 @@ begin;
     for each row execute procedure update_version_column();
 
   create trigger immutable_columns before update on iam_role_org
-    for each row execute procedure immutable_columns('scope_id', 'create_time');
+    for each row execute procedure immutable_columns('scope_id', 'create_time', 'grant_scope');
 
   create table iam_role_org_individual_grant_scope (
     role_id wt_role_id
