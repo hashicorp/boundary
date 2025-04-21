fca3967756390a6d4a7397add16183c778fbfedb
Sorawis Nilparuk <sorawis.nilparuk@hashicorp.com>
2025-04-21 11:33:54 -0700

    add more test cases and remove add-grants test

--- internal/iam/repository_role_grant_scope_test.go
+++ internal/iam/repository_role_grant_scope_test.go
@@ -5,7 +5,6 @@ package iam
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/hashicorp/boundary/globals"
@@ -13,45 +12,6 @@ import (
 	"github.com/hashicorp/boundary/internal/iam/store"
 	"github.com/stretchr/testify/require"
 )
-
-func Test_AddRoleGrantScopes(t *testing.T) {
-	ctx := context.Background()
-	conn, _ := db.TestSetup(t, "postgres")
-	wrap := db.TestWrapper(t)
-	iamRepo := TestRepo(t, conn, wrap)
-	testcases := []struct {
-		name             string
-		roleScopeId      string
-		grantScopesToAdd []string
-		want             []RoleGrantScope
-		wantErr          error
-	}{
-		{
-			name:             "",
-			grantScopesToAdd: []string{"this", "descendants"},
-			want: []RoleGrantScope{
-				{
-					CreateTime:       nil,
-					RoleId:           "",
-					ScopeIdOrSpecial: "",
-				},
-			},
-			wantErr: nil,
-		},
-	}
-
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			role := TestRole(t, conn, tc.roleScopeId, WithGrantScopeIds([]string{"testing-none"}))
-			got, err := iamRepo.AddRoleGrantScopes(ctx, role.PublicId, role.Version, tc.grantScopesToAdd)
-			require.NoError(t, err)
-			require.NotNil(t, got)
-			roleAfter, _, _, _, err := iamRepo.LookupRole(ctx, role.PublicId)
-			require.NoError(t, err)
-			require.Equal(t, role.Version+1, roleAfter.Version)
-		})
-	}
-}
 
 func Test_LookupRoleScope(t *testing.T) {
 	ctx := context.Background()
@@ -67,7 +27,8 @@ func Test_LookupRoleScope(t *testing.T) {
 		name string
 		// setup function that return []*Role that will be used as test input and list of expected RoleGrantScope
 		setupExpect func(t *testing.T) ([]*Role, []*RoleGrantScope)
-		wantErr     error
+		wantErr     bool
+		wantErrMsg  string
 	}{
 		{
 			name: "global role no individual scope grants happy path",
@@ -90,7 +51,41 @@ func Test_LookupRoleScope(t *testing.T) {
 					},
 				}
 			},
-			wantErr: nil,
+			wantErr: false,
+		},
+		{
+			name: "global role without this happy path",
+			setupExpect: func(t *testing.T) ([]*Role, []*RoleGrantScope) {
+				r := TestRole(t, conn, globals.GlobalPrefix)
+				gRole := allocGlobalRole()
+				gRole.PublicId = r.PublicId
+				require.NoError(t, rw.LookupByPublicId(ctx, &gRole))
+				gRole.GrantThisRoleScope = false
+				gRole.GrantScope = globals.GrantScopeDescendants
+				_, err := rw.Update(ctx, &gRole, []string{"GrantScope", "GrantThisRoleScope"}, []string{})
+				require.NoError(t, err)
+				return []*Role{r}, []*RoleGrantScope{
+					{
+						RoleId:           r.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeDescendants,
+					},
+				}
+			},
+			wantErr: false,
+		},
+		{
+			name: "global role without any grants return empty",
+			setupExpect: func(t *testing.T) ([]*Role, []*RoleGrantScope) {
+				r := TestRole(t, conn, globals.GlobalPrefix)
+				gRole := allocGlobalRole()
+				gRole.PublicId = r.PublicId
+				require.NoError(t, rw.LookupByPublicId(ctx, &gRole))
+				gRole.GrantThisRoleScope = false
+				_, err := rw.Update(ctx, &gRole, []string{"GrantThisRoleScope"}, []string{})
+				require.NoError(t, err)
+				return []*Role{r}, []*RoleGrantScope{}
+			},
+			wantErr: false,
 		},
 		{
 			name: "multiple roles with different grants union grants happy path",
@@ -98,8 +93,8 @@ func Test_LookupRoleScope(t *testing.T) {
 				firstRole := TestRole(t, conn, globals.GlobalPrefix)
 				secondRole := TestRole(t, conn, org2.PublicId)
 
-				firstRoleOrg1 := &GlobalRoleIndividualProjectGrantScope{
-					GlobalRoleIndividualProjectGrantScope: &store.GlobalRoleIndividualProjectGrantScope{
+				firstRoleOrg1 := &GlobalRoleIndividualOrgGrantScope{
+					GlobalRoleIndividualOrgGrantScope: &store.GlobalRoleIndividualOrgGrantScope{
 						RoleId:     firstRole.PublicId,
 						ScopeId:    org1.PublicId,
 						GrantScope: globals.GrantScopeIndividual,
@@ -115,8 +110,8 @@ func Test_LookupRoleScope(t *testing.T) {
 				}
 				require.NoError(t, rw.Create(ctx, firstRoleProj1))
 
-				secondRoleProj2 := &GlobalRoleIndividualOrgGrantScope{
-					GlobalRoleIndividualOrgGrantScope: &store.GlobalRoleIndividualOrgGrantScope{
+				secondRoleProj2 := &OrgRoleIndividualGrantScope{
+					OrgRoleIndividualGrantScope: &store.OrgRoleIndividualGrantScope{
 						RoleId:     secondRole.PublicId,
 						ScopeId:    proj2.PublicId,
 						GrantScope: globals.GrantScopeIndividual,
@@ -131,15 +126,23 @@ func Test_LookupRoleScope(t *testing.T) {
 					},
 					{
 						RoleId:           firstRole.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeThis,
+					},
+					{
+						RoleId:           firstRole.PublicId,
 						ScopeIdOrSpecial: proj1.PublicId,
 					},
 					{
 						RoleId:           secondRole.PublicId,
 						ScopeIdOrSpecial: proj2.PublicId,
 					},
+					{
+						RoleId:           secondRole.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeThis,
+					},
 				}
 			},
-			wantErr: nil,
+			wantErr: false,
 		},
 		{
 			name: "global role with individual scope grants happy path",
@@ -203,7 +206,89 @@ func Test_LookupRoleScope(t *testing.T) {
 					},
 				}
 			},
-			wantErr: nil,
+			wantErr: false,
+		},
+		{
+			name: "multiple role with children and descendants grants happy path",
+			setupExpect: func(t *testing.T) ([]*Role, []*RoleGrantScope) {
+				role1 := TestRole(t, conn, globals.GlobalPrefix)
+				role2 := TestRole(t, conn, org2.PublicId)
+
+				gRole := allocGlobalRole()
+				gRole.PublicId = role1.PublicId
+				require.NoError(t, rw.LookupByPublicId(ctx, &gRole))
+				gRole.GrantScope = globals.GrantScopeDescendants
+				_, err := rw.Update(ctx, &gRole, []string{"GrantScope"}, []string{})
+				require.NoError(t, err)
+
+				oRole := allocOrgRole()
+				oRole.PublicId = role2.PublicId
+				require.NoError(t, rw.LookupByPublicId(ctx, &oRole))
+				oRole.GrantScope = globals.GrantScopeChildren
+				_, err = rw.Update(ctx, &oRole, []string{"GrantScope"}, []string{})
+				require.NoError(t, err)
+
+				return []*Role{role1, role2}, []*RoleGrantScope{
+					{
+						RoleId:           role1.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeThis,
+					},
+					{
+						RoleId:           role1.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeDescendants,
+					},
+					{
+						RoleId:           role2.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeThis,
+					},
+					{
+						RoleId:           role2.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeChildren,
+					},
+				}
+			},
+			wantErr: false,
+		},
+		{
+			name: "multiple project role happy path",
+			setupExpect: func(t *testing.T) ([]*Role, []*RoleGrantScope) {
+				role1 := TestRole(t, conn, proj1.PublicId)
+				role2 := TestRole(t, conn, proj2.PublicId)
+				return []*Role{role1, role2}, []*RoleGrantScope{
+					{
+						RoleId:           role1.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeThis,
+					},
+					{
+						RoleId:           role2.PublicId,
+						ScopeIdOrSpecial: globals.GrantScopeThis,
+					},
+				}
+			},
+			wantErr: false,
+		},
+		{
+			name: "role does not exist returns empty grant scope",
+			setupExpect: func(t *testing.T) ([]*Role, []*RoleGrantScope) {
+				invalidRole := &Role{
+					PublicId: "r_123456",
+					ScopeId:  globals.GlobalPrefix,
+				}
+				return []*Role{invalidRole}, []*RoleGrantScope{}
+			},
+			wantErr: false,
+		},
+		{
+			name: "bad role id returns error",
+			setupExpect: func(t *testing.T) ([]*Role, []*RoleGrantScope) {
+				invalidRole := &Role{
+					PublicId: "",
+					ScopeId:  globals.GlobalPrefix,
+				}
+				return []*Role{invalidRole}, []*RoleGrantScope{}
+			},
+			wantErr:    true,
+			wantErrMsg: `iam.(Repository).ListRoleGrantScopes: missing role ids: parameter violation: error #100`,
 		},
 	}
 	for _, tc := range testcases {
@@ -215,14 +300,17 @@ func Test_LookupRoleScope(t *testing.T) {
 			}
 			expectRoleScopeMap := roleScopesToMap(t, expect)
 			got, err := iamRepo.ListRoleGrantScopes(ctx, roleIds)
+			if tc.wantErr {
+				require.Error(t, err)
+				require.ErrorContains(t, err, tc.wantErrMsg)
+				return
+			}
 			require.NoError(t, err)
 			gotRoleScopeMap := roleScopesToMap(t, got)
 			require.Equal(t, len(expectRoleScopeMap), len(gotRoleScopeMap))
 			for roleId, expectRoleScopes := range expectRoleScopeMap {
 				gotRoleScopes, found := gotRoleScopeMap[roleId]
 				require.True(t, found)
-				fmt.Println(gotRoleScopes)
-				fmt.Println(expectRoleScopes)
 				require.ElementsMatch(t, expectRoleScopes, gotRoleScopes)
 			}
 		})
@@ -230,6 +318,7 @@ func Test_LookupRoleScope(t *testing.T) {
 }
 
 func roleScopesToMap(t *testing.T, roleGrantScopes []*RoleGrantScope) map[string][]string {
+	t.Helper()
 	m := map[string][]string{}
 	for _, e := range roleGrantScopes {
 		scopes, ok := m[e.RoleId]
@@ -240,11 +329,4 @@ func roleScopesToMap(t *testing.T, roleGrantScopes []*RoleGrantScope) map[string
 		m[e.RoleId] = append(scopes, e.ScopeIdOrSpecial)
 	}
 	return m
-}
-
-func assertRoleGrantScope(t *testing.T, expect, got RoleGrantScope) {
-	require.Equal(t, expect.RoleId, got.RoleId)
-	require.Equal(t, expect.ScopeIdOrSpecial, got.ScopeIdOrSpecial)
-	require.NotNil(t, got.CreateTime)
-
 }
