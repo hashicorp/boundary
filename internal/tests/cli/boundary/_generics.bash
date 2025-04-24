e0c130a9388d71f78181ef3759048c3c004ec16c
dillanb-hashi <dillan.butler@hashicorp.com>
2025-04-24 08:29:49 -0700

    test(bats): Added bats tests for generic commands (#5677)
    * test(bats): Added bats tests for generic commands

---
+++ internal/tests/cli/boundary/_generics.bash
@@ -0,0 +1,18 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+
+export UPDATE_NAME='update'
+
+function generic_read() {
+    boundary read $1 -format json
+}
+
+function generic_update_name() {
+    local id=$1
+    local name=$2
+    boundary update $id -name $name
+}
+
+function generic_delete() {
+    boundary delete $1 -format json
+}
