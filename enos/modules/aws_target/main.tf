90b7d2c0d6ba285ec6a9a8d05700e8a8112fe857
Michael Li <michael.li@hashicorp.com>
2025-04-28 08:29:22 -0400

    chore(e2e): Add encryption to target ec2 instance (#5672)

--- enos/modules/aws_target/main.tf
+++ enos/modules/aws_target/main.tf
@@ -129,6 +129,10 @@ resource "aws_instance" "target" {
     "Environment" : var.environment
     "Enos User" : var.enos_user,
   })
+
+  root_block_device {
+    encrypted = true
+  }
 }
 
 resource "enos_remote_exec" "wait" {
