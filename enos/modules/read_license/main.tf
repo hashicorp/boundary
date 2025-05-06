aee3d005433e6f220894514823349071ccac1940
dillanb-hashi <dillan.butler@hashicorp.com>
2025-05-06 10:20:16 -0700

    cr

--- enos/modules/read_license/main.tf
+++ enos/modules/read_license/main.tf
@@ -1,8 +1,16 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: BUSL-1.1
 
-variable "license_path" {}
-variable "license" {}
+
+# Loads boundary license from file or environment variable
+# If license is null or unsuplied then the license is returned from the file at license_path
+variable "license_path" {
+  description = "Path to the boundary license file"
+}
+variable "license" {
+  description = "Boundary license" 
+  default = null
+}
 
 output "license" {
   value = var.license != null ? var.license : file(var.license_path)
