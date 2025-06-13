7040374b6f5ef0c32d46fd81e4ccc786fe440e39
Ryan Derr <54389874+RyanDerr@users.noreply.github.com>
2025-06-13 08:27:38 -0500

    Postgres 15 -> 16 Migration Update (#5825)
    * ICU-17235 fix(sql): add explicit casting to resolve Postgres 16 domain constraint changes
    
    This migration updates the `wt_alias` and `wt_target_alias` domains to include explicit `::text` casting in their constraints. Postgres 16 enforces stricter domain checks, requiring explicit casts to ensure compatibility. These changes address upgrade issues by aligning the constraints with the stricter evaluation rules.
    
    * Add Comment To Old Constraints

---
+++ internal/db/schema/migrations/oss/postgres/96/01_update_wt_alias_constraints.up.sql
@@ -0,0 +1,21 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+
+-- Modify the wt_alias domain to include explicit casts in its constraints
+alter domain wt_alias drop constraint if exists wt_alias_too_short;
+alter domain wt_alias drop constraint if exists wt_alias_no_suround_spaces;
+
+alter domain wt_alias add constraint wt_alias_too_short
+  check (length(trim(both from value::text)) > 0);
+alter domain wt_alias add constraint wt_alias_no_suround_spaces
+  check (trim(both from value::text) = value::text);
+
+-- Modify the wt_target_alias domain to include explicit casts in its constraints
+alter domain wt_target_alias drop constraint if exists wt_target_alias_too_long;
+
+alter domain wt_target_alias add constraint wt_target_alias_too_long
+  check (length(trim(both from value::text)) < 254);
+
+commit;
