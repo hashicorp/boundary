a3c2393a964d5645e411229029c633a5e9b34860
Sorawis Nilparuk (Bo) <sorawis.nilparuk@hashicorp.com>
2025-07-09 07:11:30 -0700

    fix(bug): database migration does not rollback schema version on hook failure (#5908)
    * add tests
    
    * correct expect schema version
    
    * use conventional named return
    
    * add rollback test and more description in godoc
    
    * refactor to use commit at the end and defer rollback
    
    * add comment
    
    * lint

--- internal/db/schema/internal/postgres/postgres_test.go
+++ internal/db/schema/internal/postgres/postgres_test.go
@@ -46,6 +46,38 @@ create table foo (
 	assert.Equal(t, v, 1001)
 }
 
+func TestRun_Rollback(t *testing.T) {
+	ctx := context.Background()
+	p, _, _ := setup(ctx, t)
+
+	statements := bytes.NewReader([]byte(`
+create table foo (
+  id bigint primary key,
+  bar text
+);
+`))
+
+	err := p.StartRun(ctx)
+	require.NoError(t, err)
+
+	err = p.EnsureVersionTable(ctx)
+	require.NoError(t, err)
+
+	err = p.EnsureMigrationLogTable(ctx)
+	require.NoError(t, err)
+
+	err = p.Run(ctx, statements, 1001, "oss")
+	require.NoError(t, err)
+
+	err = p.RollbackRun(ctx)
+	require.NoError(t, err)
+
+	v, i, err := p.CurrentState(ctx, "oss")
+	require.NoError(t, err)
+	assert.False(t, i)
+	assert.Equal(t, -1, v)
+}
+
 func TestRun_NoTxn(t *testing.T) {
 	ctx := context.Background()
 	p, _, _ := setup(ctx, t)
