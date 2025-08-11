ac4341e5b74f6393f680c6b182204a8bedbde3c7
dillanb-hashi <dillan.butler@hashicorp.com>
2025-08-11 13:41:45 -0700

    reverted scheduled job change

--- enos/modules/aws_windows_worker/scripts/setup.ps1
+++ enos/modules/aws_windows_worker/scripts/setup.ps1
@@ -18,9 +18,5 @@ $newPath = $existingPath + ";" + $destination
     [EnvironmentVariableTarget]::Machine
 )
 
-
-New-Item -Path ${test_dir}boundary_script.ps1 -ItemType File -Value 'boundary server -config ${test_dir}worker.hcl';
-$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NonInteractive -WindowStyle Hidden -File ${test_dir}boundary_script.ps1';
-$trigger = New-ScheduledTaskTrigger -AtStartup;
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName boundary_task -User $env:COMPUTERNAME\$env:USERNAME -RunLevel Highest -Force;
-schtasks /run /tn 'boundary_task';
+$trigger = New-JobTrigger -Once -At (Get-Date).AddSeconds(15)
+Register-ScheduledJob boundary { boundary server -config ${test_dir}worker.hcl } -trigger $trigger
