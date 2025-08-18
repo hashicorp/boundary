a7918bb3b05802498726879e136a5fd3528f7dbd
Michael Li <michael.li@hashicorp.com>
2025-08-18 16:22:47 -0400

    chore(e2e): Ensure domain controller reboots

--- enos/modules/aws_rdp_domain_controller/main.tf
+++ enos/modules/aws_rdp_domain_controller/main.tf
@@ -273,7 +273,7 @@ resource "aws_instance" "domain_controller" {
                   Add-WindowsFeature -name ad-domain-services -IncludeManagementTools
 
                   # causes the instance to reboot
-                  Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName ${var.active_directory_domain} -DomainNetbiosName ${var.active_directory_netbios_name} -ForestMode 7 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true
+                  Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName ${var.active_directory_domain} -DomainNetbiosName ${var.active_directory_netbios_name} -ForestMode 7 -InstallDns:$true -NoRebootOnCompletion:$false -SafeModeAdministratorPassword $password -Force:$true
                 </powershell>
               EOF
 
