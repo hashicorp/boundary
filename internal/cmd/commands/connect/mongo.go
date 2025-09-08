e23eecd369f799da5f66a144bc3b3b09bf43011e
Enbiya <100806254+enbiyagoral@users.noreply.github.com>
2025-09-09 00:41:19 +0300

    feat: add mongosh support to boundary connect mongo

--- internal/cmd/commands/connect/mongo.go
+++ internal/cmd/commands/connect/mongo.go
@@ -2,7 +2,6 @@ package connect
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/hashicorp/boundary/api/proxy"
@@ -21,9 +20,9 @@ func mongoOptions(c *Command, set *base.FlagSets) {
 		Name:       "style",
 		Target:     &c.flagMongoStyle,
 		EnvVar:     "BOUNDARY_CONNECT_MONGO_STYLE",
-		Completion: complete.PredictSet("mongo", "mongosh"),
+		Completion: complete.PredictSet("mongosh"),
 		Default:    "mongosh",
-		Usage:      `Specifies how the CLI will attempt to invoke a MongoDB client. This will also set a suitable default for -exec if a value was not specified. Currently-understood values are "mongo" and "mongosh".`,
+		Usage:      `Specifies how the CLI will attempt to invoke a MongoDB client. Currently only "mongosh" is supported.`,
 	})
 
 	f.StringVar(&base.StringVar{
@@ -65,65 +64,6 @@ func (m *mongoFlags) buildArgs(c *Command, port, ip, _ string, creds proxy.Crede
 	}
 
 	switch m.flagMongoStyle {
-	case "mongo":
-		// Handle password first - create a temporary file for MongoDB connection string
-		if password != "" {
-			passfile, err := os.CreateTemp("", "*")
-			if err != nil {
-				return nil, nil, proxy.Credentials{}, fmt.Errorf("error saving MongoDB password to tmp file: %w", err)
-			}
-			c.cleanupFuncs = append(c.cleanupFuncs, func() error {
-				if err := os.Remove(passfile.Name()); err != nil {
-					return fmt.Errorf("error removing temporary password file; consider removing %s manually: %w", passfile.Name(), err)
-				}
-				return nil
-			})
-			_, err = passfile.Write([]byte(password))
-			if err != nil {
-				_ = passfile.Close()
-				return nil, nil, proxy.Credentials{}, fmt.Errorf("error writing password file to %s: %w", passfile.Name(), err)
-			}
-			if err := passfile.Close(); err != nil {
-				return nil, nil, proxy.Credentials{}, fmt.Errorf("error closing password file after writing to %s: %w", passfile.Name(), err)
-			}
-			// Set password file as environment variable for MongoDB client
-			envs = append(envs, "MONGODB_PASSWORD_FILE="+passfile.Name())
-
-			if c.flagDbname == "" {
-				c.UI.Warn("Credentials are being brokered but no -dbname parameter provided. mongo may misinterpret another parameter as the database name.")
-			}
-		}
-
-		// Build MongoDB connection string
-		connectionString := "mongodb://"
-
-		// Add username and password to connection string
-		if username != "" {
-			connectionString += username
-			if password != "" {
-				connectionString += ":" + password
-			}
-			connectionString += "@"
-		} else if c.flagUsername != "" {
-			connectionString += c.flagUsername
-			if password != "" {
-				connectionString += ":" + password
-			}
-			connectionString += "@"
-		}
-
-		// Add host and port
-		connectionString += ip
-		if port != "" {
-			connectionString += ":" + port
-		}
-
-		// Add database name
-		if c.flagDbname != "" {
-			connectionString += "/" + c.flagDbname
-		}
-
-		args = append(args, connectionString)
 	case "mongosh":
 		// Build MongoDB connection string for mongosh
 		connectionString := "mongodb://"
