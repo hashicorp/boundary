da3b8934dba26a018ec3b22f41681f04e9f6859f
Tony <wongtonyb@gmail.com>
2025-09-26 09:33:22 -0400

    remove name var necessity in docker scenario

--- enos/modules/build_boundary_docker_crt/main.tf
+++ enos/modules/build_boundary_docker_crt/main.tf
@@ -23,10 +23,25 @@ variable "edition" {
   default = "oss"
 }
 
+locals {
+  docker_image_filename      = basename(var.path)
+  filename_parts             = split("_", replace(local.docker_image_filename, ".docker.tar", ""))
+  arch                       = local.filename_parts[3]
+  version_and_commit         = join("_", slice(local.filename_parts, 4, length(local.filename_parts)))
+  boundary_docker_image_name = "boundary/default/linux/${local.arch}:${local.version_and_commit}"
+}
+
 resource "enos_local_exec" "load_docker_image" {
-  inline = ["docker load -i ${var.path}"]
+  inline = [
+    "docker load -i ${var.path}",
+    "echo `Generated image name: ${local.boundary_docker_image_name}`"
+  ]
 }
 
 output "cli_zip_path" {
   value = var.cli_build_path
+}
+
+output "image_name" {
+  value = local.boundary_docker_image_name
 }
