02f9d3bc6da17e77b4e7a321ab9f6e09f0fec197
Tony <wongtonyb@gmail.com>
2025-09-26 11:34:16 -0400

    parse from docker load

--- enos/modules/build_boundary_docker_crt/main.tf
+++ enos/modules/build_boundary_docker_crt/main.tf
@@ -23,16 +23,19 @@ variable "edition" {
   default = "oss"
 }
 
-locals {
-  docker_image_filename      = basename(var.path)
-  filename_parts             = split("_", replace(local.docker_image_filename, ".docker.tar", ""))
-  arch                       = local.filename_parts[3]
-  version_and_commit         = join("_", slice(local.filename_parts, 4, length(local.filename_parts)))
-  boundary_docker_image_name = "boundary/default/linux/${local.arch}:${local.version_and_commit}"
-}
-
 resource "enos_local_exec" "load_docker_image" {
   inline = ["docker load -i ${var.path}"]
+}
+
+locals {
+  boundary_docker_image_name = replace(
+    element(
+      split("\n", trimspace(enos_local_exec.load_docker_image.stdout)),
+      -1
+    ),
+    "Loaded image: ",
+    ""
+  )
 }
 
 output "cli_zip_path" {
