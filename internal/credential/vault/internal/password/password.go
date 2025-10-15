c0b99ddf18b05b6c1920d7aee3f0eb0edf0e15da
Justin Nguyen <justin.nguyen@hashicorp.com>
2025-10-15 14:31:25 -0500

    feat(credential/vault): Implement repository for vault password credential (#6140)

---
+++ internal/credential/vault/internal/password/password.go
@@ -0,0 +1,118 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package password
+
+import (
+	"strings"
+
+	"github.com/mitchellh/pointerstructure"
+)
+
+type (
+	data map[string]any
+
+	// extractFunc attempts to extract the password
+	// from sd using the provided attribute names, using a known
+	// Vault data response format.
+	extractFunc func(sd data, passwordAttr string) string
+)
+
+// Extract attempts to extract the values of the password
+// stored within the provided data using the given attribute names.
+//
+// Extract does not return partial results, i.e. if one of the attributes
+// were extracted but not the other ("") will be returned.
+func Extract(d data, passwordAttr string) string {
+	for _, f := range []extractFunc{
+		defaultExtract,
+		kv2Extract,
+	} {
+		password := f(d, passwordAttr)
+		if password != "" {
+			// got valid password from secret
+			return password
+		}
+	}
+
+	return ""
+}
+
+// defaultExtract looks for the passwordAttr in the data map
+func defaultExtract(sd data, passwordAttr string) (password string) {
+	if sd == nil {
+		// nothing to do return early
+		return ""
+	}
+
+	var p any
+	switch {
+	case strings.HasPrefix(passwordAttr, "/"):
+		var err error
+		p, err = pointerstructure.Get(sd, passwordAttr)
+		if err != nil {
+			return ""
+		}
+
+	default:
+		p = sd[passwordAttr]
+	}
+
+	if p, ok := p.(string); ok {
+		password = p
+	}
+
+	return password
+}
+
+// kv2Extract looks for the the passwordAttr in the embedded
+// 'data' field within the data map.
+//
+// Additionally it validates the data is in the expected KV-v2 format:
+//
+//	{
+//		"data": {},
+//		"metadata: {}
+//	}
+//
+// If the format does not match, it returns (""). See:
+// https://www.vaultproject.io/api/secret/kv/kv-v2#sample-response-1
+func kv2Extract(sd data, passwordAttr string) (password string) {
+	if sd == nil {
+		// nothing to do return early
+		return ""
+	}
+
+	var data, metadata map[string]any
+	for k, v := range sd {
+		switch k {
+		case "data":
+			var ok bool
+			if data, ok = v.(map[string]any); !ok {
+				// data field should be of type map[string]interface{} in KV-v2
+				return ""
+			}
+		case "metadata":
+			var ok bool
+			if metadata, ok = v.(map[string]any); !ok {
+				// metadata field should be of type map[string]interface{} in KV-v2
+				return ""
+			}
+		default:
+			// secretData contains a non valid KV-v2 top level field
+			return ""
+		}
+	}
+	if data == nil || metadata == nil {
+		// missing required KV-v2 field
+		return ""
+	}
+
+	if p, ok := data[passwordAttr]; ok {
+		if p, ok := p.(string); ok {
+			password = p
+		}
+	}
+
+	return password
+}
