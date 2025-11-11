e6f1d9ec15a63e46d5ce9ec8a7dcf2ead8dfed02
Tony <wongtonyb@gmail.com>
2025-11-11 12:38:24 -0500

    chore(e2e): update vault create credential funcs

--- testing/internal/e2e/vault/vault.go
+++ testing/internal/e2e/vault/vault.go
@@ -270,7 +270,7 @@ func CreateKvPrivateKeyCredential(t testing.TB, secretPath string, user string, 
 // this function does not put any clean-up in place to run after a test is
 // complete. When applicable, callers should destroy the policy and secret this
 // function creates.
-func CreateKvPasswordCredential(t testing.TB, secretPath string, user string) (secretName string, policyName string, password string) {
+func CreateKvPasswordCredential(t testing.TB, secretPath string, user string, providedPassword ...string) (secretName string, policyName string, password string) {
 	secretName, err := base62.Random(16)
 	require.NoError(t, err)
 
@@ -285,6 +285,14 @@ func CreateKvPasswordCredential(t testing.TB, secretPath string, user string) (s
 	require.NoError(t, err)
 
 	policyName = WritePolicy(t, t.Context(), policyFilePath)
+
+	// Use provided password or generate random one
+	if len(providedPassword) > 0 && providedPassword[0] != "" {
+		password = providedPassword[0]
+	} else {
+		password, err = base62.Random(16)
+		require.NoError(t, err)
+	}
 
 	// Create secret
 	password, err = base62.Random(16)
@@ -308,7 +316,7 @@ func CreateKvPasswordCredential(t testing.TB, secretPath string, user string) (s
 // credential. Returns the name of the policy. Note that this function does not
 // put any clean-up in place to run after a test is complete. When applicable,
 // callers should destroy the policy and secret this function creates.
-func CreateKvPasswordDomainCredential(t testing.TB, secretPath string, user string, domain string) (secretName string, policyName string, password string) {
+func CreateKvPasswordDomainCredential(t testing.TB, secretPath string, user string, domain string, providedPassword ...string) (secretName string, policyName string, password string) {
 	secretName, err := base62.Random(16)
 	require.NoError(t, err)
 
@@ -323,6 +331,14 @@ func CreateKvPasswordDomainCredential(t testing.TB, secretPath string, user stri
 	require.NoError(t, err)
 
 	policyName = WritePolicy(t, t.Context(), policyFilePath)
+
+	// Use provided password or generate random one
+	if len(providedPassword) > 0 && providedPassword[0] != "" {
+		password = providedPassword[0]
+	} else {
+		password, err = base62.Random(16)
+		require.NoError(t, err)
+	}
 
 	// Create secret
 	password, err = base62.Random(16)
