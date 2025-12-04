687fbf4423b0bbad955ec1f6cba8e0dabfdb468a
Hugo <10965479+hugoghx@users.noreply.github.com>
2025-12-04 15:51:36 +0000

    backport of commit ab352b5a09ebaf872e24eacc3703526ddb21fd13

---
+++ internal/db/schema/migrations/oss/postgres/102/01_credential_vault_library_deleted_purge_job.sql
@@ -0,0 +1,12 @@
+-- Copyright (c) HashiCorp, Inc.
+-- SPDX-License-Identifier: BUSL-1.1
+
+begin;
+  -- The credential_vault_library_deleted table was renamed to
+  -- credential_vault_generic_library_deleted in
+  -- 99/01_credential_vault_library_refactor.up.sql, but the corresponding
+  -- legacy purge job was not deleted. This migration deletes that legacy job.
+  -- The new job that takes over the legacy one is
+  -- credential_vault_generic_library_deleted_items_table_purge.
+  delete from job where name = 'credential_vault_library_deleted_items_table_purge';
+commit;
