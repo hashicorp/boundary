b84a7b2f2cdcac3246cf833a6d03592cae1a7d25
April-May <18632637+AprilMay0@users.noreply.github.com>
2025-12-05 11:38:57 -0800

    clean up

--- internal/apptoken/repository_token_grant.go
+++ internal/apptoken/repository_token_grant.go
@@ -6,7 +6,6 @@ package apptoken
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"slices"
 	"strings"
 
@@ -28,15 +27,15 @@ type tempGrantTuples []tempGrantTuple
 
 // grantsForTokenResults represents the raw results from the grants for token queries
 type grantsForTokenResult struct {
-	permissionId          string
-	description           string
-	createTime            string
-	grantThisScope        bool
-	grantScope            string
-	appTokenId            string
-	appTokenParentScopeId string
-	canonicalGrants       []string
-	activeGrantScopes     []string
+	PermissionId          string
+	Description           string
+	CreateTime            string
+	GrantThisScope        bool
+	GrantScope            string
+	AppTokenId            string
+	AppTokenParentScopeId string
+	CanonicalGrants       []string
+	ActiveGrantScopes     []string
 }
 
 // GrantsForToken retrieves all grants for the given app token id and resource types within the given request scope id.
@@ -78,9 +77,6 @@ func (r *Repository) GrantsForToken(ctx context.Context, tokenId string, res []r
 	if err != nil {
 		return nil, errors.Wrap(ctx, err, op)
 	}
-	if appToken == nil {
-		return nil, errors.New(ctx, errors.NotFound, op, "app token not found")
-	}
 
 	// find the correct query to use
 	query, err := r.resolveAppTokenQuery(ctx, appToken.ScopeId, res, reqScopeId, opts.withRecursive)
@@ -92,8 +88,8 @@ func (r *Repository) GrantsForToken(ctx context.Context, tokenId string, res []r
 	var resources []string
 
 	resources = []string{resource.Unknown.String(), resource.All.String()}
-	for _, res := range res {
-		resources = append(resources, res.String())
+	for _, r := range res {
+		resources = append(resources, r.String())
 	}
 
 	args = append(args,
@@ -111,15 +107,15 @@ func (r *Repository) GrantsForToken(ctx context.Context, tokenId string, res []r
 	for rows.Next() {
 		var g grantsForTokenResult
 		if err := rows.Scan(
-			&g.permissionId,
-			&g.description,
-			&g.createTime,
-			&g.grantThisScope,
-			&g.grantScope,
-			&g.appTokenId,
-			&g.appTokenParentScopeId,
-			pq.Array(&g.canonicalGrants),
-			pq.Array(&g.activeGrantScopes),
+			&g.PermissionId,
+			&g.Description,
+			&g.CreateTime,
+			&g.GrantThisScope,
+			&g.GrantScope,
+			&g.AppTokenId,
+			&g.AppTokenParentScopeId,
+			pq.Array(&g.CanonicalGrants),
+			pq.Array(&g.ActiveGrantScopes),
 		); err != nil {
 			return nil, errors.Wrap(ctx, err, op)
 		}
@@ -131,23 +127,13 @@ func (r *Repository) GrantsForToken(ctx context.Context, tokenId string, res []r
 
 	resp := make(tempGrantTuples, 0, len(grants))
 	for _, grant := range grants {
-		if grant.grantThisScope {
-			resp = append(resp, tempGrantTuple{
-				AppTokenId:            grant.appTokenId,
-				AppTokenScopeId:       appToken.ScopeId,
-				AppTokenParentScopeId: "", // Not needed when GrantThisScope is true
-				GrantScopeId:          grant.grantScope,
-				Grant:                 strings.Join(grant.canonicalGrants, ","),
-			})
-		} else {
-			resp = append(resp, tempGrantTuple{
-				AppTokenId:            grant.appTokenId,
-				AppTokenScopeId:       appToken.ScopeId,
-				AppTokenParentScopeId: grant.appTokenParentScopeId,
-				GrantScopeId:          grant.grantScope,
-				Grant:                 strings.Join(grant.canonicalGrants, ","),
-			})
-		}
+		resp = append(resp, tempGrantTuple{
+			AppTokenId:            grant.AppTokenId,
+			AppTokenScopeId:       appToken.ScopeId,
+			AppTokenParentScopeId: grant.AppTokenParentScopeId,
+			GrantScopeId:          grant.GrantScope,
+			Grant:                 strings.Join(grant.CanonicalGrants, ","),
+		})
 	}
 
 	return resp, nil
@@ -157,32 +143,7 @@ func (r *Repository) GrantsForToken(ctx context.Context, tokenId string, res []r
 func (r *Repository) resolveAppTokenQuery(ctx context.Context, tokenScope string, res []resource.Type, reqScopeId string, isRecursive bool) (string, error) {
 	const op = "apptoken.(Repository).resolveAppTokenQuery"
 
-	// validations
-	if res == nil {
-		return "", errors.New(ctx, errors.InvalidParameter, op, "missing resource type")
-	}
-	if slices.Contains(res, resource.Unknown) {
-		return "", errors.New(ctx, errors.InvalidParameter, op, "resource type cannot be unknown")
-	}
-	if slices.Contains(res, resource.All) {
-		return "", errors.New(ctx, errors.InvalidParameter, op, "resource type cannot be all")
-	}
-	if tokenScope == "" {
-		return "", errors.New(ctx, errors.InvalidParameter, op, "missing token scope")
-	}
-	if reqScopeId == "" {
-		return "", errors.New(ctx, errors.InvalidParameter, op, "missing request scope id")
-	}
-
-	switch {
-	case strings.HasPrefix(reqScopeId, globals.GlobalPrefix):
-	case strings.HasPrefix(reqScopeId, globals.OrgPrefix):
-	case strings.HasPrefix(reqScopeId, globals.ProjectPrefix):
-	default:
-		return "", errors.New(ctx, errors.InvalidParameter, op, "request scope must be global scope, an org scope, or a project scope")
-	}
-
-	// Use the largest set of allowed scopes for the given resources
+	// Find the allowed-in scopes for the resource types
 	var resourceAllowedIn []scope.Type
 	for _, re := range res {
 		a, err := scope.AllowedIn(ctx, re)
@@ -195,62 +156,80 @@ func (r *Repository) resolveAppTokenQuery(ctx context.Context, tokenScope string
 	}
 
 	// Determine app token scope from token scope prefix
-	var isAppTokenGlobal, isAppTokenOrg, isAppTokenProject bool
-	isAppTokenGlobal = strings.HasPrefix(tokenScope, globals.GlobalPrefix)
-	isAppTokenOrg = strings.HasPrefix(tokenScope, globals.OrgPrefix)
-	isAppTokenProject = strings.HasPrefix(tokenScope, globals.ProjectPrefix)
+	isAppTokenGlobal := strings.HasPrefix(tokenScope, globals.GlobalPrefix)
+	isAppTokenOrg := strings.HasPrefix(tokenScope, globals.OrgPrefix)
+	isAppTokenProject := strings.HasPrefix(tokenScope, globals.ProjectPrefix)
 
 	// Determine request scope from request scope prefix
-	var isRequestScopeGlobal, isRequestScopeOrg, isRequestScopeProject bool
-	isRequestScopeGlobal = strings.HasPrefix(reqScopeId, globals.GlobalPrefix)
-	isRequestScopeOrg = strings.HasPrefix(reqScopeId, globals.OrgPrefix)
-	isRequestScopeProject = strings.HasPrefix(reqScopeId, globals.ProjectPrefix)
+	isRequestScopeGlobal := strings.HasPrefix(reqScopeId, globals.GlobalPrefix)
+	isRequestScopeOrg := strings.HasPrefix(reqScopeId, globals.OrgPrefix)
+	isRequestScopeProject := strings.HasPrefix(reqScopeId, globals.ProjectPrefix)
 
+	var query string
+	var err error
 	switch isRecursive {
-	// Recursive queries - based on token scope and resource allowed scopes
 	case true:
-		switch {
-		case isAppTokenGlobal:
-			if slices.Equal(resourceAllowedIn, []scope.Type{scope.Global, scope.Org, scope.Project}) {
-				return grantsForGlobalTokenGlobalOrgProjectResourcesRecursiveQuery, nil
-			} else if slices.Equal(resourceAllowedIn, []scope.Type{scope.Global, scope.Org}) {
-				return grantsForGlobalTokenGlobalOrgResourcesRecursiveQuery, nil
-			} else if slices.Equal(resourceAllowedIn, []scope.Type{scope.Project}) {
-				return grantsForGlobalTokenProjectResourcesRecursiveQuery, nil
-			}
-		case isAppTokenOrg:
-			if slices.Equal(resourceAllowedIn, []scope.Type{scope.Global, scope.Org, scope.Project}) {
-				return grantsForOrgTokenGlobalOrgProjectResourcesRecursiveQuery, nil
-			} else if slices.Equal(resourceAllowedIn, []scope.Type{scope.Global, scope.Org}) {
-				return grantsForOrgTokenGlobalOrgResourcesRecursiveQuery, nil
-			} else if slices.Equal(resourceAllowedIn, []scope.Type{scope.Project}) {
-				return grantsForOrgTokenProjectResourcesRecursiveQuery, nil
-			}
-		case isAppTokenProject:
-			return grantsForProjectTokenResourcesRecursiveQuery, nil
-		}
+		query, err = r.selectRecursiveQuery(ctx, isAppTokenGlobal, isAppTokenOrg, isAppTokenProject, resourceAllowedIn)
+	default:
+		query, err = r.selectNonRecursiveQuery(ctx, isAppTokenGlobal, isAppTokenOrg, isAppTokenProject, isRequestScopeGlobal, isRequestScopeOrg, isRequestScopeProject, resourceAllowedIn)
+	}
+	if err != nil {
+		return "", errors.Wrap(ctx, err, op, errors.WithMsg("no matching query found for token scope, request scope"))
+	}
 
-		// Non-recursive queries - based on token scope, request scope, and resource allowed scopes
-	case false:
-		switch {
-		case isAppTokenGlobal:
-			if isRequestScopeGlobal && slices.Contains(resourceAllowedIn, scope.Global) {
-				return grantsForGlobalTokenGlobalResourcesQuery, nil
-			} else if isRequestScopeOrg && slices.Contains(resourceAllowedIn, scope.Org) {
-				return grantsForGlobalTokenOrgResourcesQuery, nil
-			} else if isRequestScopeProject && slices.Contains(resourceAllowedIn, scope.Project) {
-				return grantsForGlobalTokenProjectResourcesQuery, nil
-			}
-		case isAppTokenOrg:
-			if isRequestScopeOrg && slices.Contains(resourceAllowedIn, scope.Org) {
-				return grantsForOrgTokenOrgResourcesQuery, nil
-			} else if isRequestScopeProject && slices.Contains(resourceAllowedIn, scope.Project) {
-				return grantsForOrgTokenProjectResourcesQuery, nil
-			}
-		case isAppTokenProject:
+	return query, nil
+}
+
+// selectRecursiveQuery selects the appropriate recursive query based on the app token scope and resource allowed-in scopes
+func (r *Repository) selectRecursiveQuery(ctx context.Context, isGlobal, isOrg, isProject bool, resourceAllowedIn []scope.Type) (string, error) {
+	const op = "apptoken.(Repository).selectRecursiveQuery"
+
+	switch {
+	case isGlobal:
+		if slices.Equal(resourceAllowedIn, []scope.Type{scope.Global, scope.Org, scope.Project}) {
+			return grantsForGlobalTokenGlobalOrgProjectResourcesRecursiveQuery, nil
+		} else if slices.Equal(resourceAllowedIn, []scope.Type{scope.Global, scope.Org}) {
+			return grantsForGlobalTokenGlobalOrgResourcesRecursiveQuery, nil
+		} else if slices.Equal(resourceAllowedIn, []scope.Type{scope.Project}) {
+			return grantsForGlobalTokenProjectResourcesRecursiveQuery, nil
+		}
+	case isOrg:
+		if slices.Equal(resourceAllowedIn, []scope.Type{scope.Global, scope.Org, scope.Project}) {
+			return grantsForOrgTokenGlobalOrgProjectResourcesRecursiveQuery, nil
+		} else if slices.Equal(resourceAllowedIn, []scope.Type{scope.Global, scope.Org}) {
+			return grantsForOrgTokenGlobalOrgResourcesRecursiveQuery, nil
+		} else if slices.Equal(resourceAllowedIn, []scope.Type{scope.Project}) {
+			return grantsForOrgTokenProjectResourcesRecursiveQuery, nil
+		}
+	case isProject:
+		return grantsForProjectTokenResourcesRecursiveQuery, nil
+	}
+	return "", errors.New(ctx, errors.InvalidParameter, op, "no matching recursive query found")
+}
+
+// selectNonRecursiveQuery selects the appropriate non-recursive query based on the app token scope, request scope, and resource allowed-in scopes
+func (r *Repository) selectNonRecursiveQuery(ctx context.Context, isGlobal, isOrg, isProject bool, isReqGlobal, isReqOrg, isReqProject bool, resourceAllowedIn []scope.Type) (string, error) {
+	const op = "apptoken.(Repository).selectNonRecursiveQuery"
+
+	switch {
+	case isGlobal:
+		if isReqGlobal && slices.Contains(resourceAllowedIn, scope.Global) {
+			return grantsForGlobalTokenGlobalResourcesQuery, nil
+		} else if isReqOrg && slices.Contains(resourceAllowedIn, scope.Org) {
+			return grantsForGlobalTokenOrgResourcesQuery, nil
+		} else if isReqProject && slices.Contains(resourceAllowedIn, scope.Project) {
+			return grantsForGlobalTokenProjectResourcesQuery, nil
+		}
+	case isOrg:
+		if isReqOrg && slices.Contains(resourceAllowedIn, scope.Org) {
+			return grantsForOrgTokenOrgResourcesQuery, nil
+		} else if isReqProject && slices.Contains(resourceAllowedIn, scope.Project) {
+			return grantsForOrgTokenProjectResourcesQuery, nil
+		}
+	case isProject:
+		if isReqProject && slices.Contains(resourceAllowedIn, scope.Project) {
 			return grantsForProjectTokenResourcesQuery, nil
 		}
 	}
-
-	return "", errors.New(ctx, errors.InvalidParameter, op, fmt.Sprintf("no matching query found for token scope, request scope %s, and resource types %v", reqScopeId, res))
+	return "", errors.New(ctx, errors.InvalidParameter, op, "no matching non-recursive query found")
 }
