614abbe6e8cd382e6485d4194d1f8aca7817907f
Emilia Grant <emilia.grant@hashicorp.com>
2026-01-14 11:27:17 -0500

    feat(clientcache): Add sort parameter parsing and validation in daemon handler (#6338)

--- internal/clientcache/internal/cache/search.go
+++ internal/clientcache/internal/cache/search.go
@@ -77,6 +77,10 @@ type SearchParams struct {
 	Filter string
 	// Max result set size is an override to the default max result set size
 	MaxResultSetSize int
+	// Which column to sort results by, default is resource specific
+	SortBy SortBy
+	// Which direction to sort results by (asc, desc), default is resource specific
+	SortDirection SortDirection
 }
 
 // SearchResult returns the results from searching the cache.
