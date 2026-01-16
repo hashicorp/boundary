d87ce01a59c77c0a2c1927c3ab1776afbd0cfd4f
April-May <18632637+AprilMay0@users.noreply.github.com>
2026-01-16 13:41:26 -0800

    maybe fix handler

---
+++ internal/daemon/controller/handlers/apptokens/apptoken_service.go
@@ -0,0 +1,25 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package apptokens
+
+import (
+	"github.com/hashicorp/boundary/internal/types/action"
+	"github.com/hashicorp/boundary/internal/types/resource"
+)
+
+var (
+	// IdActions contains the set of actions that can be performed on
+	// individual resources
+	IdActions = action.NewActionSet(
+		action.NoOp,
+	)
+
+	// CollectionActions contains the set of actions that can be performed on
+	// this collection
+	CollectionActions = action.NewActionSet()
+)
+
+func init() {
+	action.RegisterResource(resource.AppToken, IdActions, CollectionActions)
+}
