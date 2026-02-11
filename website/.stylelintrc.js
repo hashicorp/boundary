e06211ae38e216cb8bef5ab4ab2d368a9e99814c
Dan Heath <76443935+Dan-Heath@users.noreply.github.com>
2026-02-11 11:40:08 -0500

    docs: Deletes website content from repo

--- website/.stylelintrc.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright IBM Corp. 2020, 2025
- * SPDX-License-Identifier: BUSL-1.1
- */
-
-module.exports = {
-  ...require('@hashicorp/platform-cli/config/stylelint.config'),
-  /* Specify overrides here */
-}
