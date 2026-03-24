65a844af8ecddcf98e25646da2e5b605e1c6e86c
Michael Li <michael.li@hashicorp.com>
2026-03-24 00:10:31 -0400

    test(bats): Fix flaky connect helper methods

--- internal/tests/cli/boundary/_connect.bash
+++ internal/tests/cli/boundary/_connect.bash
@@ -4,18 +4,18 @@
 function connect_nc() {
   local id=$1
   # Note: When this command returns, the session immediately goes into a "canceling" state
-  echo "foo" | boundary connect -exec nc -target-id $id -- {{boundary.ip}} {{boundary.port}}
+  timeout 10 boundary connect -exec nc -target-id $id -- -z -w 3 -v {{boundary.ip}} {{boundary.port}}
 }
 
 function connect_alias() {
   local alias=$1
   # Note: When this command returns, the session immediately goes into a "canceling" state
-  echo "foo" | boundary connect $alias -exec nc -- {{boundary.ip}} {{boundary.port}}
+  timeout 10 boundary connect $alias -exec nc -- -z -w 3 -v {{boundary.ip}} {{boundary.port}}
 }
 
 function connect_alias_with_host_id() {
   local alias=$1
   local hostid=$2
   # Note: When this command returns, the session immediately goes into a "canceling" state
-  echo "foo" | boundary connect $alias -host-id $hostid -exec nc -- {{boundary.ip}} {{boundary.port}}
+  timeout 10 boundary connect $alias -host-id $hostid -exec nc -- -z -w 3 -v {{boundary.ip}} {{boundary.port}}
 }
